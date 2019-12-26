c1193bac0210e39c2505662ca681c89186cd6f03
Alex Dovenmuehle <alex.dovenmuehle@mattermost.com>
2019-12-26 11:39:46 -0500

    Added finance account util dates.

---
+++ transform/snowflake-dbt/models/finance/account_util_dates.sql
@@ -0,0 +1,23 @@
+{{config({
+    "materialized": "table"
+  })
+}}
+
+WITH account_dates AS (
+  SELECT account.sfid AS account_sfid, coalesce(parent_account.sfid, account.sfid) AS mASter_account_sfid, min(start_date__c) AS min_start_date, max(end_date__c) AS max_end_date
+    FROM orgm.account
+        LEFT JOIN {{ source('orgm', 'account') }} AS parent_account ON parent_account.sfid = account.parentid
+        JOIN {{ source('orgm', 'opportunity') }} ON opportunity.accountid = account.sfid
+        JOIN {{ source('orgm', 'opportunitylineitem') }} ON opportunity.sfid = opportunitylineitem.opportunityid
+    WHERE opportunity.iswon
+    GROUP BY 1, 2
+), with_util_dates AS (
+    SELECT
+        dates.date AS day,
+        account_sfid,
+        mASter_account_sfid
+    FROM {{ source('util', 'dates') }}
+    JOIN account_dates AS account ON 1 = 1
+    WHERE dates.date >= min_start_date - interval '1 day' AND dates.date <= max_end_date + interval '2 day'
+)
+SELECT * FROM with_util_dates
