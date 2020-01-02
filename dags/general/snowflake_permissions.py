e38f9948dedeb5fe127e02868169aea3ed27d0d8
Alex Dovenmuehle <alex.dovenmuehle@mattermost.com>
2020-01-02 15:55:21 -0500

    Fix path for real, also update pythonpath so it's right.

--- dags/general/snowflake_permissions.py
+++ dags/general/snowflake_permissions.py
@@ -31,10 +31,11 @@ default_args = {
 # Set the command for the container
 container_cmd = f"""
     {clone_repo_cmd} &&
+    export PYTHONPATH="/opt/bitnami/airflow/dags/git/:$PYTHONPATH" &&
     meltano init airflow_job &&
     cp dbt/load/snowflake/roles.yaml airflow_job/load/roles.yml &&
     cd airflow_job/ &&
-    meltano permissions grant load/roles.yml --db snowflake $DRY | python ../utils/run_snowflake_queries.py PERMISSIONS analytics
+    meltano permissions grant load/roles.yml --db snowflake $DRY | python ../dbt/utils/run_snowflake_queries.py PERMISSIONS analytics
 """
 
 # Create the DAG
