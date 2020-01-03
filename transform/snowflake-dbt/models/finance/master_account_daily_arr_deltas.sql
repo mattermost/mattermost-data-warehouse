675db4df8bd42f7fb440ad8ec5408f5a7ecde1d7
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-03 10:48:04 -0800

    updats

--- transform/snowflake-dbt/models/finance/master_account_daily_arr_deltas.sql
+++ transform/snowflake-dbt/models/finance/master_account_daily_arr_deltas.sql
@@ -11,35 +11,35 @@ WITH master_account_daily_arr_deltas AS (
         master_account_util_dates.master_account_sfid AS master_account_sfid,
         coalesce(new_day.total_arr_norm,0) AS new_day_total_arr_norm,
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.account_sfid = original.account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.accounmaster_account_sfidt_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr_norm,0)
         END  AS previous_day_total_arr_norm,
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.account_sfid = original.account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN true 
             ELSE  false 
         END  AS master_account_new_arr,
         coalesce(new_day.total_arr_norm,0) -
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.account_sfid = original.account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr_norm,0)
         END  AS total_arr_norm_delta,
         coalesce(new_day.total_arr,0) AS new_day_total_arr,
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.account_sfid = original.account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr,0) 
         END  AS previous_day_total_arr,
         coalesce(new_day.total_arr,0) -
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.account_sfid = original.account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr,0)
         END  AS total_arr_delta
     FROM {{ ref('master_account_util_dates') }}
-    LEFT JOIN {{ ref('master_account_daily_arr') }} AS new_day  ON  master_account_util_dates.master_account_sfid = new_day.account_sfid AND master_account_util_dates.day = new_day.day
-    LEFT JOIN {{ ref('master_account_daily_arr') }} AS previous_day  ON  master_account_util_dates.master_account_sfid = previous_day.account_sfid AND master_account_util_dates.day - interval '1 day' = previous_day.day
+    LEFT JOIN {{ ref('master_account_daily_arr') }} AS new_day  ON  master_account_util_dates.master_account_sfid = new_day.master_account_sfid AND master_account_util_dates.day = new_day.day
+    LEFT JOIN {{ ref('master_account_daily_arr') }} AS previous_day  ON  master_account_util_dates.master_account_sfid = previous_day.master_account_sfid AND master_account_util_dates.day - interval '1 day' = previous_day.day
 )
 SELECT * FROM master_account_daily_arr_deltas
--- transform/snowflake-dbt/models/finance/master_account_monthly_arr_deltas_by_type.sql
+++ transform/snowflake-dbt/models/finance/master_account_monthly_arr_deltas_by_type.sql
@@ -8,7 +8,6 @@ WITH master_account_monthly_arr_deltas_by_type AS (
     SELECT
         master_account_monthly_arr_deltas.month_start,
         master_account_monthly_arr_deltas.month_end,
-        master_account_monthly_arr_deltas.account_sfid,
         master_account_monthly_arr_deltas.master_account_sfid,
         sum(CASE WHEN account_new_arr THEN total_arr_norm_delta ELSE 0 END) AS total_arr_norm_new,
         sum(CASE WHEN not account_new_arr AND total_arr_norm_delta > 0 THEN total_arr_norm_delta ELSE 0 END) AS total_arr_norm_expansion,
@@ -22,8 +21,8 @@ WITH master_account_monthly_arr_deltas_by_type AS (
         sum(total_arr_delta) AS total_arr_delta
     FROM {{ ref('master_account_monthly_arr_deltas') }}
         LEFT JOIN {{ ref('master_account_daily_arr') }}
-            ON master_account_monthly_arr_deltas.account_sfid = master_account_daily_arr.account_sfid AND master_account_monthly_arr_deltas.month_end = master_account_daily_arr.day
+            ON master_account_monthly_arr_deltas.master_account_sfid = master_account_daily_arr.master_account_sfid AND master_account_monthly_arr_deltas.month_end = master_account_daily_arr.day
     WHERE abs(total_arr_norm_delta) > 0
-    GROUP BY 1, 2, 3, 4
+    GROUP BY 1, 2, 3
 )
 select * from master_account_monthly_arr_deltas_by_type
