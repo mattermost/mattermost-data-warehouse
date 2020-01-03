09c45ab1123ab5a4fdacc450afed4111ffd0ee0f
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-03 10:57:33 -0800

    updates

--- transform/snowflake-dbt/models/finance/master_account_daily_arr_deltas.sql
+++ transform/snowflake-dbt/models/finance/master_account_daily_arr_deltas.sql
@@ -11,30 +11,30 @@ WITH master_account_daily_arr_deltas AS (
         master_account_util_dates.master_account_sfid AS master_account_sfid,
         coalesce(new_day.total_arr_norm,0) AS new_day_total_arr_norm,
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.accounmaster_account_sfidt_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM {{ ref('master_account_daily_arr') }} AS original WHERE new_day.master_account_sfid = original.accounmaster_account_sfidt_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr_norm,0)
         END  AS previous_day_total_arr_norm,
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM {{ ref('master_account_daily_arr') }} AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN true 
             ELSE  false 
         END  AS master_account_new_arr,
         coalesce(new_day.total_arr_norm,0) -
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM {{ ref('master_account_daily_arr') }} AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr_norm,0)
         END  AS total_arr_norm_delta,
         coalesce(new_day.total_arr,0) AS new_day_total_arr,
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM {{ ref('master_account_daily_arr') }} AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr,0) 
         END  AS previous_day_total_arr,
         coalesce(new_day.total_arr,0) -
         CASE
-            WHEN master_account_util_dates.day = (SELECT min(day) FROM finance.master_account_daily_arr AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
+            WHEN master_account_util_dates.day = (SELECT min(day) FROM {{ ref('master_account_daily_arr') }} AS original WHERE new_day.master_account_sfid = original.master_account_sfid ) 
                 THEN 0 
             ELSE  coalesce(previous_day.total_arr,0)
         END  AS total_arr_delta
