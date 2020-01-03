dce4a1cbad18d40d737556e3d87e3916612b3876
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-03 10:53:58 -0800

    fixes

--- transform/snowflake-dbt/models/finance/master_account_util_dates.sql
+++ transform/snowflake-dbt/models/finance/master_account_util_dates.sql
@@ -7,9 +7,8 @@
 WITH master_account_dates AS (
     SELECT 
         master_account_sfid,
-        min(day) AS min_start_date,
-        max(day) AS max_end_date
+        day
     FROM {{ ref('account_util_dates') }}
-    GROUP BY 1
+    GROUP BY 1,2
 )
 SELECT * FROM master_account_dates
