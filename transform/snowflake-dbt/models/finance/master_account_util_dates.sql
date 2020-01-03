6b58eff32f77817d05194c004b12b3c8c3938538
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-03 10:32:52 -0800

    ALEXXXXXX is cool

--- transform/snowflake-dbt/models/finance/master_account_util_dates.sql
+++ transform/snowflake-dbt/models/finance/master_account_util_dates.sql
@@ -6,7 +6,7 @@
 
 WITH master_account_dates AS (
     SELECT 
-        master_account_sfid master_account_sfid,
+        master_account_sfid,
         min(min_start_date) AS min_start_date,
         max(max_end_date) AS max_end_date
     FROM {{ ref('account_util_dates') }}
