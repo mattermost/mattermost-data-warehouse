e433eb99b5ac7dadc1551e86434d804ea897e1d5
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MBP.localdomain>
2020-01-13 09:46:31 -0800

    adding map stuff

---
+++ transform/snowflake-dbt/models/mattermost/contributor_employee_map_data.sql
@@ -0,0 +1,20 @@
+{{config({
+    "materialized": "table",
+    "schema": "mattermost"
+  })
+}}
+
+WITH contributor_map_data AS (
+    SELECT 
+        'employee' AS reason, 
+        major_city,
+        email as unique_identifier
+    FROM {{ source('employee', 'staff_list') }}
+    UNION ALL
+    SELECT 
+        reason, 
+        city,
+        name
+    FROM {{ source('staging', 'contributor_map_data') }}
+)
+SELECT * FROM contributor_employee_map_data
