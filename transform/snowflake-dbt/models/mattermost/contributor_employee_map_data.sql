50bd4fd7693b99a45351c4d3e6cdab0cb2c1d0c7
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MBP.localdomain>
2020-01-13 10:21:38 -0800

    add region

--- transform/snowflake-dbt/models/mattermost/contributor_employee_map_data.sql
+++ transform/snowflake-dbt/models/mattermost/contributor_employee_map_data.sql
@@ -8,12 +8,14 @@ WITH contributor_employee_map_data AS (
     SELECT 
         'employee' AS reason, 
         major_city,
+        region_country
         email as unique_identifier
     FROM {{ source('employee', 'staff_list') }}
     UNION ALL
     SELECT 
         reason, 
         city,
+        region_country,
         name
     FROM {{ source('staging', 'contributor_map_data') }}
 )
