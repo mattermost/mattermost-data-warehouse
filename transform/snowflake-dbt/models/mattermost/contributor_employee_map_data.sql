ed2e6eae9d203a3ff8546a1f211a8db90097f630
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-21 10:48:52 -0800

    update logic

--- transform/snowflake-dbt/models/mattermost/contributor_employee_map_data.sql
+++ transform/snowflake-dbt/models/mattermost/contributor_employee_map_data.sql
@@ -4,19 +4,45 @@
   })
 }}
 
-WITH contributor_employee_map_data AS (
+WITH staff_list AS (
+        SELECT
+            CASE
+                WHEN major_city IN ('New York City','New Jersey') THEN 'New York'
+                WHEN major_city = 'Bangalore' THEN 'Bengalūru'
+                WHEN major_city = 'Lake Tahoe' THEN 'Reno'
+                WHEN major_city = 'Waterloo' OR major_city = 'Toronto/Waterloo' THEN 'Toronto'
+                WHEN major_city = 'Luzon' THEN 'Baguio City'
+                WHEN major_city = 'North District' THEN 'Nazareth'
+                ELSE major_city
+            END as major_city,
+            CASE
+                WHEN region_country = 'USA' THEN 'United States'
+                WHEN region_country = 'UK' THEN 'United Kingdom'
+                WHEN region_country = 'South Korea' THEN 'Korea, South'
+                ELSE region_country
+            END AS region_country,
+            email
+        FROM {{ source('employee', 'staff_list') }}
+), contributor_employee_map_data AS (
     SELECT 
         'Employee' AS reason, 
         major_city,
         region_country,
-        email as unique_identifier
-    FROM {{ source('employee', 'staff_list') }}
+        email as unique_identifier,
+        1 as count,
+        latitude,
+        longitude
+    FROM staff_list
+    LEFT JOIN {{ source('util', 'world_cities') }} ON staff_list.major_city = world_cities.city AND staff_list.region_country = world_cities.country
     UNION ALL
-    SELECT 
-        reason, 
+    SELECT
+        reason,
         city,
         region_country,
-        name
+        null,
+        count,
+        latitude,
+        longitude
     FROM {{ source('staging', 'contributor_map_data') }}
 )
 SELECT * FROM contributor_employee_map_data
