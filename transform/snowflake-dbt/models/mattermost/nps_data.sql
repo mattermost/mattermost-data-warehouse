1ac08315f144725d1c29816c44680a3a007f1696
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-22 14:33:32 -0800

    add nps data

---
+++ transform/snowflake-dbt/models/mattermost/nps_data.sql
@@ -0,0 +1,25 @@
+{{config({
+    "materialized": 'table',
+    "schema": "mattermost"
+  })
+}}
+
+WITH nps_data AS (
+    SELECT
+       nps_score.license_id,
+       nps_score.timestamp::DATE AS date,
+       nps_score.server_version,
+       nps_score.user_role,
+       nps_score.server_install_date,
+       nps_score.license_sku,
+       nps_score.user_create_at,
+       nps_score.score,
+       nps_score.user_actual_id AS user_id,
+       nps_score.user_id AS server_id,
+       nps_feedback.feedback
+	FROM {{ source('mattermost_nps', 'nps_score') }}
+    LEFT JOIN {{ source('mattermost_nps', 'nps_feedback') }}
+        ON nps_score.user_actual_id = nps_feedback.user_actual_id AND nps_score.timestamp::DATE = nps_feedback.timestamp::DATE
+)
+
+SELECT * FROM nps_data
