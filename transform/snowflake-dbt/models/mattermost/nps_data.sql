4cb05ef2b8767fa693a6a29810654c5ac3150ea3
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-01-22 15:49:42 -0800

    update logic

--- transform/snowflake-dbt/models/mattermost/nps_data.sql
+++ transform/snowflake-dbt/models/mattermost/nps_data.sql
@@ -4,23 +4,39 @@
   })
 }}
 
-WITH nps_data AS (
+WITH daily_nps_scores AS (
+    SELECT timestamp::date as date,
+  		*
+  	FROM (
+          SELECT ROW_NUMBER() over (PARTITION BY timestamp::DATE, user_id ORDER BY timestamp DESC) AS rownum, *
+          FROM {{ source('mattermost_nps', 'nps_score') }}
+      )
+  	where rownum = 1
+), daily_feedback_scores AS (
+    SELECT timestamp::date as date, *
+  	FROM (
+          SELECT ROW_NUMBER() over (PARTITION BY timestamp::DATE, user_id ORDER BY timestamp DESC) AS rownum, *
+          FROM {{ source('mattermost_nps', 'nps_feedback') }}
+      )
+  	where rownum = 1
+)
+nps_data AS (
     SELECT
-       nps_score.license_id,
-       nps_score.timestamp::DATE AS date,
-       nps_score.server_version,
-       nps_score.user_role,
-       to_timestamp(nps_score.server_install_date/1000)::DATE AS server_install_date,
-       nps_score.license_sku,
-       to_timestamp(nps_score.user_create_at/1000)::DATE AS user_create_at,
-       nps_score.score,
-       CASE WHEN nps_score.score < 7 THEN 'Detractor' WHEN nps_score.score < 9 THEN 'Passive' ELSE 'Promoters' END AS promotor_type,
-       nps_score.user_actual_id AS user_id,
-       nps_score.user_id AS server_id,
-       nps_feedback.feedback
-	FROM {{ source('mattermost_nps', 'nps_score') }}
-    LEFT JOIN {{ source('mattermost_nps', 'nps_feedback') }}
-        ON nps_score.user_actual_id = nps_feedback.user_actual_id AND nps_score.timestamp::DATE = nps_feedback.timestamp::DATE
+       daily_nps_scores.license_id,
+       daily_nps_scores.date,
+       daily_nps_scores.server_version,
+       daily_nps_scores.user_role,
+       to_timestamp(daily_nps_scores.server_install_date/1000)::DATE AS server_install_date,
+       daily_nps_scores.license_sku,
+       to_timestamp(daily_nps_scores.user_create_at/1000)::DATE AS user_create_at,
+       daily_nps_scores.score,
+       CASE WHEN daily_nps_scores.score < 7 THEN 'Detractor' WHEN daily_nps_scores.score < 9 THEN 'Passive' ELSE 'Promoters' END AS promotor_type,
+       daily_nps_scores.user_actual_id AS user_id,
+       daily_nps_scores.user_id AS server_id,
+       daily_feedback_scores.feedback
+	FROM daily_nps_scores
+    LEFT JOIN daily_feedback_scores
+        ON daily_nps_scores.user_actual_id = daily_feedback_scores.user_actual_id AND daily_nps_scores.date = daily_feedback_scores.date
 )
 
 SELECT * FROM nps_data
