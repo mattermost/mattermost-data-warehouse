ec0f8f22cf5f3c5e79771ec2e99e2b3fea912eed
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-01-30 08:24:43 -0800

    Fixed server_fact cte structure.

--- transform/snowflake-dbt/models/mattermost/server_fact.sql
+++ transform/snowflake-dbt/models/mattermost/server_fact.sql
@@ -5,17 +5,17 @@
 }}
 
 WITH server_fact AS (
-    SELECT *
+    SELECT
+        id AS server_id,
+        account_sfid,
+        license_id,
+        MIN(date) AS first_active_date,
+        MAX(date) AS last_active_date,
+        MAX(active_user_count) AS max_active_user_count,
+        MAX(CASE WHEN active_user_count > 0 THEN date ELSE null END) AS last_active_user_date
     FROM {{ ref('server_daily_details') }}
+    GROUP BY 1, 2, 3
 )
 
-SELECT
-    id AS server_id,
-    account_sfid,
-    license_id,
-    MIN(date) AS first_active_date,
-    MAX(date) AS last_active_date,
-    MAX(active_user_count) AS max_active_user_count,
-    MAX(CASE WHEN active_user_count > 0 THEN date ELSE null END) AS last_active_user_date
-FROM server_fact
-GROUP BY 1, 2, 3;
+SELECT *
+FROM server_fact;
