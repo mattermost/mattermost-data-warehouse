56681c971cd08a66db57f5aaf437ac9723bd3e60
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-02-07 13:23:05 -0800

    Migrated server_downloads.sql to downloads.sql file in mattermost schema.

--- transform/snowflake-dbt/models/mattermost/downloads.sql
+++ transform/snowflake-dbt/models/mattermost/downloads.sql
@@ -4,39 +4,121 @@
   })
 }}
 
-WITH downloads AS (
+WITH outliers         AS (
     SELECT
-        logdate::date as logdate,
-        cip,
-        CASE
-            WHEN uri LIKE '%desktop%' THEN 'Desktop'
-            WHEN uri LIKE '%team%' and uri NOT LIKE '%appcenter%' THEN 'TE'
-            WHEN uri NOT LIKE '%team%' and uri NOT LIKE '%appcenter%' THEN 'EE'
-            else '?'
-        END AS type,
-    	COUNT(*) AS count,
-    	MAX(CASE
-    	    WHEN uri LIKE '%desktop%' THEN split_part(uri, '/', 3)
-    	    ELSE split_part(uri, '/', 2)
-    	END) AS version,
-    	MAX(useragent) AS useragent
-    FROM {{ source('releases', 'log_entries') }} 
-    WHERE ((uri LIKE '%desktop%')
-            OR (
-                uri LIKE '/6.%'
-                OR uri LIKE '/5.%'
-                OR uri LIKE '/4.%'
-                OR uri LIKE '/3.%'
-                OR uri LIKE '/2.%'
-                OR uri LIKE '/1.%')
-            )
-    {% if is_incremental() %}
+        logdate::DATE                                                                              AS logdate
+      , logtime
+      , cip
+      , uri
+      , bytessent
+      , abs(bytessent - avg(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri)) AS diff
+      , avg(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri)                  AS avg
+      , max(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri)                  AS max
+      , stddev(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri) * 1           AS std
+      , count(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri)                AS count
+      , CASE
+            WHEN abs(bytessent - avg(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri)) >
+                 stddev(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri) * 1 THEN TRUE
+            ELSE FALSE END                                                                         AS one_std_from_mean
+      , CASE
+            WHEN abs(bytessent - avg(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri)) >
+                 stddev(bytessent) OVER (PARTITION BY date_trunc('year', logdate::DATE), uri) * 2 THEN TRUE
+            ELSE FALSE END                                                                         AS two_std_from_mean
+    FROM {{ source('releases', 'log_entries') }} log_entries
+    WHERE (regexp_like(uri, '^\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*') OR
+           regexp_like(log_entries.uri, '^\/desktop\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*'))
+      AND bytessent > 0
+      AND status LIKE '2%'
+),
 
-        -- this filter will only be applied on an incremental run
-        AND logdate > (SELECT MAX(logdate) FROM {{ this }})
+     server_downloads AS (
+         SELECT
+             log_entries.logdate::DATE                                                             AS logdate
+           , log_entries.logtime
+           , log_entries.cip                                                                       AS ip_address
+           , log_entries.uri
+           , CASE
+                 WHEN regexp_like(log_entries.uri, '^\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*') THEN 'server'
+                 WHEN regexp_like(log_entries.uri, '^\/desktop\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*') THEN 'app'
+                 ELSE NULL END                                                                     AS download_type
+           , CASE
+                 WHEN regexp_like(log_entries.uri, '^\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*')
+                     THEN CASE
+                              WHEN log_entries.uri LIKE '%team%' THEN 'team'
+                              ELSE 'enterprise' END
+                 WHEN regexp_like(log_entries.uri, '^\/desktop\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*')
+                     THEN 'desktop'
+                 ELSE NULL END                                                                     AS type
+           , CASE
+                 WHEN regexp_like(log_entries.uri, '^\/desktop\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*')
+                     THEN CASE
+                              WHEN log_entries.uri LIKE '%linux%' THEN 'linux'
+                              WHEN regexp_like(log_entries.uri,
+                                               '.*msi$|.*win[0-9]{2}.*$|.*\-win\-.*$|.*\-win\.[a-z]{3}$') THEN 'windows'
+                              WHEN regexp_like(log_entries.uri, '.*mac\.[a-z]{3}$|.*\-osx\..*$') THEN 'mac'
+                              ELSE NULL END
+                 WHEN regexp_like(log_entries.uri, '^\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*')
+                     THEN CASE
+                              WHEN log_entries.uri LIKE '%linux%' THEN 'linux'
+                              WHEN regexp_like(log_entries.uri, '.*\-windows\-.*$') THEN 'windows'
+                              ELSE NULL END
+                 ELSE NULL END                                                                     AS os
+           , CASE
+                 WHEN regexp_like(log_entries.uri, '^\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*')
+                     THEN split_part(log_entries.uri, '/', 2)
+                 WHEN regexp_like(log_entries.uri, '^\/desktop\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*')
+                     THEN split_part(log_entries.uri, '/', 3)
+                 ELSE NULL END                                                                     AS version
+           , nullif(regexp_replace(
+                            regexp_replace(regexp_replace(split_part(log_entries.creferrer, '/', 3), '^[w]{3}[.]', '')
+                                , '.com[0-9_a-z_A-Z:.]{1,}$', '.com'), ':{1}[0-9]{1,4}$', ''), '') AS server_source
+           , log_entries.edge
+           , log_entries.bytessent
+           , log_entries.method
+           , log_entries.host
+           , log_entries.status
+           , log_entries.creferrer
+           , log_entries.useragent
+           , log_entries.cs_uri_query
+           , log_entries.cookie
+           , log_entries.x_edge_result_type
+           , log_entries.x_edge_request_id
+           , log_entries.x_host_header
+           , log_entries.protocol
+           , log_entries.cs_bytes
+           , log_entries.time_taken
+           , log_entries.x_forwarded_for
+           , log_entries.ssl_protocol
+           , log_entries.ssl_cipher
+           , log_entries.x_edge_response_result_type
+           , log_entries.cs_protocol_version
+           , log_entries.file_status
+           , log_entries.file_encrypted_fields
+           , o.diff                                                                                AS bytessent_diff_from_uri_mean
+           , o.std                                                                                 AS bytessent_std_from_uri_mean
+           , o.avg                                                                                 AS bytessent_uri_avg                                                                              AS bytessent_uri_avg
+         FROM {{ source('releases', 'log_entries') }} log_entries
+              JOIN outliers o
+                   ON log_entries.logdate::DATE = o.logdate::DATE
+                       AND log_entries.cip = o.cip
+                       AND log_entries.uri = o.uri
+                       AND log_entries.bytessent = o.bytessent
+                       AND log_entries.logtime = o.logtime
+         WHERE (regexp_like(log_entries.uri, '^\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*') OR
+                regexp_like(log_entries.uri, '^\/desktop\/[1-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}\/.*'))
+           AND log_entries.bytessent > 1000000
+           AND log_entries.status LIKE '2%'
+           AND NOT o.one_std_from_mean
+           {% if is_incremental() %}
 
-    {% endif %}
-    GROUP BY logdate, cip, type
-)
+            -- this filter will only be applied on an incremental run
+            AND log_entries.logdate::date > (SELECT MAX(logdate) FROM {{ this }})
 
-SELECT * FROM downloads
+          {% endif %}
+         GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
+                , 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34
+     )
+
+SELECT *
+FROM server_downloads
+ORDER BY 1 DESC, 2 DESC
