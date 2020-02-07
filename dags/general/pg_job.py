34fbcd6bb24f4ff5210e1d36a98f8f0a2a7fec9c
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-02-07 15:30:23 -0800

    fix loop

--- dags/general/pg_job.py
+++ dags/general/pg_job.py
@@ -53,7 +53,7 @@ account_arr = get_container_operator('account-arr', 'account_arr')
 opportunitylineitem_snapshot = get_container_operator('opportunitylineitem_snapshot', 'opportunitylineitem_snapshot')
 opportunity_snapshot = get_container_operator('opportunity_snapshot', 'opportunity_snapshot')
 
-account_arr >> opportunitylineitem_snapshot >> opportunitylineitem_snapshot
+account_arr >> opportunity_snapshot >> opportunitylineitem_snapshot
 
 # Test comment
 test >> account_arr
