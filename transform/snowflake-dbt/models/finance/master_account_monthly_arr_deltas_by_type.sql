bb22535b8037b889dadf3dd869a306e0d9a39e38
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-02-12 07:05:57 -0800

    updating ARR stuff

--- transform/snowflake-dbt/models/finance/master_account_monthly_arr_deltas_by_type.sql
+++ transform/snowflake-dbt/models/finance/master_account_monthly_arr_deltas_by_type.sql
@@ -9,9 +9,16 @@ WITH master_account_monthly_arr_deltas_by_type AS (
         master_account_monthly_arr_deltas.month_start,
         master_account_monthly_arr_deltas.month_end,
         master_account_monthly_arr_deltas.master_account_sfid,
-        sum(CASE WHEN master_account_new_arr THEN total_arr_delta ELSE 0 END) AS total_arr_new,
-        sum(CASE WHEN not master_account_new_arr AND total_arr_delta > 0 THEN total_arr_delta ELSE 0 END) AS total_arr_expansion,
-        sum(CASE WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) != 0then total_arr_delta ELSE 0 END) AS total_arr_contraction,
+        CASE 
+            WHEN account_new_arr THEN 'New ARR'
+            WHEN not account_new_arr AND total_arr_delta > 0 THEN 'Expansion ARR'
+            WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) != 0 THEN 'Contraction ARR'
+            WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) = 0 THEN 'Churn ARR'
+            ELSE NULL
+        END as arr_type,
+        sum(CASE WHEN account_new_arr THEN total_arr_delta ELSE 0 END) AS total_arr_new,
+        sum(CASE WHEN not account_new_arr AND total_arr_delta > 0 THEN total_arr_delta ELSE 0 END) AS total_arr_expansion,
+        sum(CASE WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) != 0 THEN total_arr_delta ELSE 0 END) AS total_arr_contraction,
         sum(CASE WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) = 0 THEN total_arr_delta ELSE 0 END) AS total_arr_churn,
         sum(total_arr_delta) AS total_arr_delta
     FROM {{ ref('master_account_monthly_arr_deltas') }}
