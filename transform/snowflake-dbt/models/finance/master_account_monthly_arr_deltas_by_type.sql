323e7ffa4d50c420954e46804aaabeb6be8d6918
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-02-12 07:07:50 -0800

    update one partor arr

--- transform/snowflake-dbt/models/finance/master_account_monthly_arr_deltas_by_type.sql
+++ transform/snowflake-dbt/models/finance/master_account_monthly_arr_deltas_by_type.sql
@@ -10,14 +10,14 @@ WITH master_account_monthly_arr_deltas_by_type AS (
         master_account_monthly_arr_deltas.month_end,
         master_account_monthly_arr_deltas.master_account_sfid,
         CASE 
-            WHEN account_new_arr THEN 'New ARR'
-            WHEN not account_new_arr AND total_arr_delta > 0 THEN 'Expansion ARR'
+            WHEN master_account_new_arr THEN 'New ARR'
+            WHEN not master_account_new_arr AND total_arr_delta > 0 THEN 'Expansion ARR'
             WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) != 0 THEN 'Contraction ARR'
             WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) = 0 THEN 'Churn ARR'
             ELSE NULL
         END as arr_type,
-        sum(CASE WHEN account_new_arr THEN total_arr_delta ELSE 0 END) AS total_arr_new,
-        sum(CASE WHEN not account_new_arr AND total_arr_delta > 0 THEN total_arr_delta ELSE 0 END) AS total_arr_expansion,
+        sum(CASE WHEN master_account_new_arr THEN total_arr_delta ELSE 0 END) AS total_arr_new,
+        sum(CASE WHEN not master_account_new_arr AND total_arr_delta > 0 THEN total_arr_delta ELSE 0 END) AS total_arr_expansion,
         sum(CASE WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) != 0 THEN total_arr_delta ELSE 0 END) AS total_arr_contraction,
         sum(CASE WHEN total_arr_delta < 0 AND coalesce(master_account_daily_arr.total_arr,0) = 0 THEN total_arr_delta ELSE 0 END) AS total_arr_churn,
         sum(total_arr_delta) AS total_arr_delta
