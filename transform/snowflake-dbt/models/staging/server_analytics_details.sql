f8ea13073502b3ac0080a182f1441a38dc2f6ba5
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-02-13 19:17:04 -0800

    Created server_analytics_details staging table from mattermost2.config_analytics

---
+++ transform/snowflake-dbt/models/staging/server_analytics_details.sql
@@ -0,0 +1,27 @@
+{{config({
+    "materialized": "incremental",
+    "schema": "staging"
+  })
+}}
+
+WITH max_timestamp            AS (
+    SELECT
+        timestamp::DATE AS date
+      , user_id
+      , max(timestamp)  AS max_timestamp
+    FROM {{ source('mattermost2', 'config_analytics') }}
+    GROUP BY 1, 2
+),
+     server_analytics_details AS (
+         SELECT
+             timestamp::DATE                         AS date
+           , ca.user_id                              AS server_id
+           , max(isdefault_max_users_for_statistics) AS isdefault_max_users_for_statistics
+         FROM {{ source('mattermost2', 'config_analytics') }} ca
+              JOIN max_timestamp           mt
+                   ON ca.user_id = mt.user_id
+                       AND mt.max_timestamp = ca.timestamp
+         GROUP BY 1, 2
+     )
+SELECT *
+FROM server_analytics_details
