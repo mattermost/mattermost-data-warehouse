4f91b87a7af40753a50a653b69c25df7e7c49de1
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-02-18 10:43:40 -0800

    commenting out since there is apex issues

--- dags/general/pg_job.py
+++ dags/general/pg_job.py
@@ -50,8 +50,8 @@ dag = DAG(
 test = get_container_operator('test', 'test')
 
 account_arr = get_container_operator('account-arr', 'account_arr')
-opportunity_arr = get_container_operator('opportunity-arr', 'opportunity_arr')
-opportunitylineitem_arr = get_container_operator('opportunitylineitem-arr', 'opportunitylineitem_arr')
+# opportunity_arr = get_container_operator('opportunity-arr', 'opportunity_arr')
+# opportunitylineitem_arr = get_container_operator('opportunitylineitem-arr', 'opportunitylineitem_arr')
 opportunitylineitem_snapshot = get_container_operator('opportunitylineitem-snapshot', 'opportunitylineitem_snapshot')
 opportunity_snapshot = get_container_operator('opportunity-snapshot', 'opportunity_snapshot')
 
