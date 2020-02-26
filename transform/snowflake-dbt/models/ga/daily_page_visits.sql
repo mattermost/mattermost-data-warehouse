98ffc56158671a07b6381cc0093aba987a6070e9
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-02-26 12:40:05 -0800

    reorder

--- transform/snowflake-dbt/models/ga/daily_page_visits.sql
+++ transform/snowflake-dbt/models/ga/daily_page_visits.sql
@@ -9,22 +9,22 @@ WITH daily_page_visits AS (
         'mattermost.com' AS site, 
         start_date,
         end_date,
-        avgtimeonpage,
         pagepath,
         pagetitle,
         pageviews,
-        uniquepageviews
+        uniquepageviews,
+        avgtimeonpage
     FROM {{ source('ga_mattermost_com_pages_visits', 'report') }}
     UNION ALL
     SELECT 
         'developers.mattermost.com' AS site, 
         start_date,
         end_date,
-        avgtimeonpage,
         pagepath,
         pagetitle,
         pageviews,
-        uniquepageviews
+        uniquepageviews,
+        avgtimeonpage
     FROM {{ source('ga_developers_pages_visits', 'report') }}
 )
 SELECT * FROM daily_page_visits
