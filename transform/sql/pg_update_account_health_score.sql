30c6934a69fb59d1c0e987e78b7b012ec6e4851b
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-02-28 16:29:16 -0800

    remove begin and commit

--- transform/sql/pg_update_account_health_score.sql
+++ transform/sql/pg_update_account_health_score.sql
@@ -1,9 +1,5 @@
-BEGIN;
-
 UPDATE orgm.account
     SET health_score__c = account_health_score.health_score_w_override
 FROM staging.account_health_score
 WHERE account.sfid = account_health_score.account_sfid
     AND health_score__c <> account_health_score.health_score_w_override;
-
-COMMIT;
