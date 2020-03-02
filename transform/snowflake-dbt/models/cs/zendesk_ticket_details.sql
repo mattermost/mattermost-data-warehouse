1d78b1d38e7f7ff37ed9caca61aed811b73d4053
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-02 13:15:58 -0800

    adding a few more contextual columns

--- transform/snowflake-dbt/models/cs/zendesk_ticket_details.sql
+++ transform/snowflake-dbt/models/cs/zendesk_ticket_details.sql
@@ -10,6 +10,9 @@ WITH zendesk_ticket_details as (
         organizations.name as organization_name,
         account.sfid as account_sfid,
         users.name as assignee_name,
+        case when custom_ticket_fields.ticket_field_id = 24889383 then custom_ticket_fields.field_value else null end as enterprise_edition_version,
+        case when custom_ticket_fields.ticket_field_id = 24889383 then custom_ticket_fields.field_value else null end as customer_type,
+        organizations.organization_fields:premium_support as premium_support,
         tickets.created_at,
         ticket_metrics.agent_wait_time_in_minutes:business::int agent_wait_time_in_minutes_bus,
         ticket_metrics.agent_wait_time_in_minutes:calendar::int agent_wait_time_in_minutes_cal,
@@ -30,6 +33,7 @@ WITH zendesk_ticket_details as (
     LEFT JOIN {{ source('zendesk_raw', 'organizations') }} ON tickets.organization_id = organizations.id
     LEFT JOIN {{ source('orgm', 'account') }} ON organizations.id = account.zendesk__zendesk_organization_id__C
     LEFT JOIN {{ source('zendesk_raw', 'users') }} ON users.id = tickets.assignee_id
+    LEFT JOIN {{ ref('custom_ticket_fields') }} ON tickets.id = custom_ticket_fields.ticket_id
 )
 
 select * from zendesk_ticket_details
