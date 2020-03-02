a3b5b23a18fda134db78b47c7092200914e825c1
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-02 15:26:34 -0800

    adding post hook to send over sfid, sysmod, created, and now for data checking

---
+++ transform/snowflake-dbt/models/staging/orgm_opportunitylineitem_data_check.sql
@@ -0,0 +1,16 @@
+{{config({
+    "materialized": 'table',
+    "schema": "staging",
+    "post-hook": "{{ pg_import('staging.orgm_opportunitylineitem_data_check', 'pg_dummy_update') }}"
+  })
+}}
+
+WITH orgm_opportunitylineitem_data_check AS (
+    SELECT 
+        sfid,
+        systemmodstamp,
+        createddate,
+        now() AS processed_at
+    FROM {{ source('orgm','opportunitylineitem') }}
+)
+SELECT * FROM orgm_opportunitylineitem_data_check
