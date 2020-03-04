9b7cd0e158da8c78301cf05200e128d38d2fae38
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-04 12:57:37 -0800

    updates

--- transform/snowflake-dbt/models/tva/tva_curr_fy_arr_by_mo.sql
+++ transform/snowflake-dbt/models/tva/tva_curr_fy_arr_by_mo.sql
@@ -8,16 +8,16 @@ WITH actual_arr_by_mo AS (
     SELECT 
         date_trunc('month', day) AS month,
         sum(total_arr) AS total_arr
-    FROM {{ ref('finance', 'account_daily_arr') }}
+    FROM  {{ ref('account_daily_arr') }}
     WHERE date_part('day', day + interval '1 day') = 1
     GROUP BY 1
-), curr_fy_arr_by_mo AS (
+), tva_curr_fy_arr_by_mo AS (
     SELECT
-        month::date,
-        target,
-        total_arr,
-        round((total_arr/target),2) as tva
-    FROM {{ source('target', 'curr_fy_arr_by_mo') }}
+        curr_fy_arr_by_mo.month,
+        curr_fy_arr_by_mo.target,
+        actual_arr_by_mo.total_arr,
+        round((actual_arr_by_mo.total_arr/curr_fy_arr_by_mo.target),2) as tva
+    FROM {{ source('targets', 'curr_fy_arr_by_mo') }}
     LEFT JOIN actual_arr_by_mo ON curr_fy_arr_by_mo.month = actual_arr_by_mo.month
 )
 
