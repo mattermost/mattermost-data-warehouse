a6c841c11d8711abf6f0875f221e8bbeb2137e7b
rbradleyhaas <rachel.bradley-haas@mattermost.com>
2020-03-05 16:56:15 -0800

    Add more ARR Target Models (#108)
    * adding curr_fy_arr_by_mo
    
    * dd model
    
    * updates
    
    * updates to make one "monthly" tva table based on multiple monthly tvas
    
    * updates
    
    * finall updates
    
    * make it last day of month or current date
    
    * add new targets

--- transform/snowflake-dbt/models/tva/tva_all_by_mo.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_mo.sql
@@ -10,6 +10,21 @@ WITH tva_all_by_mo AS (
 
     UNION ALL
 
+    SELECT 'ARR Churn by Month' as target_name, 'Finance' as target_category, *
+    FROM {{ ref('tva_curr_fy_arr_churn_by_mo') }}
+
+    UNION ALL
+
+    SELECT 'ARR New by Month' as target_name, 'Finance' as target_category, *
+    FROM {{ ref('tva_curr_fy_arr_new_by_mo') }}
+
+    UNION ALL
+
+    SELECT 'ARR Expansion by Month' as target_name, 'Finance' as target_category, *
+    FROM {{ ref('tva_curr_fy_arr_exp_by_mo') }}
+
+    UNION ALL
+
     SELECT 'TEDAS (7 Day Active) by Month', 'Product', *
     FROM {{ ref('tva_curr_fy_tedas_7day_by_mo') }}
 )
