6d622f9372bb1f905640d193e9e7717ea65bad65
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-03-06 07:32:22 -0800

    Added event_mobile source references to mattermost2/sources.yml file. Updated events_registry model to include mobile events.

--- transform/snowflake-dbt/models/events/events_registry.sql
+++ transform/snowflake-dbt/models/events/events_registry.sql
@@ -19,6 +19,30 @@ WITH events          AS (
     {% endif %}
     GROUP BY 1, 2
 ),
+
+    mobile_events    AS (
+      SELECT
+        LOWER(type)                                                                   AS event_name
+      , CASE WHEN lower(category) = 'actions' THEN 'action' ELSE lower(category) END  AS event_category
+      , MIN(timestamp::date)                                                          AS date_added
+    FROM {{ source('mattermost2', 'event_mobile')}}
+    {% if is_incremental() %}
+
+        -- this filter will only be applied on an incremental run
+        WHERE LOWER(type) NOT IN (SELECT event_name FROM {{ this }} GROUP BY 1)
+
+    {% endif %}
+    GROUP BY 1, 2
+    ),
+
+    all_events       AS (
+      SELECT *
+      FROM events
+      UNION ALL
+      SELECT * 
+      FROM mobile_events
+    ),
+
      events_registry AS (
          SELECT
              {{ dbt_utils.surrogate_key('event_name', 'event_category') }}                AS event_id
@@ -27,9 +51,9 @@ WITH events          AS (
            , 'THIS IS A PLACEHOLDER DESCRIPTION. IT IS ONLY MEANT TO ESTABLISH THE MINIMUM LENGTH OF THIS FIELD. 
               THIS FIELD IS MEANT TO BE USED TO PROVIDE A BRIEF DESCRIPTION OF THE EVENT IN QUESTION 
               I.E. HOW THE EVENT IS TRIGGERED AND WHY IT FALLS INTO A SPECIFIC CATEGORY.' AS description
-           , date_added
-         FROM events
-         GROUP BY 1, 2, 3, 4, 5
+           , MIN(date_added)                                                              AS date_added
+         FROM all_events
+         GROUP BY 1, 2, 3, 4
      )
 SELECT *
 FROM events_registry
