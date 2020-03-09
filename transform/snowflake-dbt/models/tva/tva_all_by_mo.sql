a7f1b53287559a7e2700dea1d03881ab268cac5d
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-09 11:09:36 -0700

    fix last file

--- transform/snowflake-dbt/models/tva/tva_all_by_mo.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_mo.sql
@@ -6,27 +6,27 @@
 
 WITH tva_all_by_mo AS (
     SELECT 'ARR by Month' as target_name, 'Finance' as target_category, *
-    FROM {{ ref('tva_curr_fy_arr_by_mo') }}
+    FROM {{ ref('tva_arr_by_mo') }}
 
     UNION ALL
 
     SELECT 'ARR Churn by Month' as target_name, 'Finance' as target_category, *
-    FROM {{ ref('tva_curr_fy_arr_churn_by_mo') }}
+    FROM {{ ref('tva_arr_churn_by_mo') }}
 
     UNION ALL
 
     SELECT 'ARR New by Month' as target_name, 'Finance' as target_category, *
-    FROM {{ ref('tva_curr_fy_arr_new_by_mo') }}
+    FROM {{ ref('tva_arr_new_by_mo') }}
 
     UNION ALL
 
     SELECT 'ARR Expansion by Month' as target_name, 'Finance' as target_category, *
-    FROM {{ ref('tva_curr_fy_arr_exp_by_mo') }}
+    FROM {{ ref('tva_arr_exp_by_mo') }}
 
     UNION ALL
 
     SELECT 'TEDAS (7 Day Active) by Month', 'Product', *
-    FROM {{ ref('tva_curr_fy_tedas_7day_by_mo') }}
+    FROM {{ ref('tva_tedas_7day_by_mo') }}
 )
 
 SELECT * FROM tva_all_by_mo
