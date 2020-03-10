eada15b07d35f066e021dc9abfd56329e1a4d44d
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-09 18:52:49 -0700

    oops again

--- transform/snowflake-dbt/models/tva/tva_by_period/tva_all_by_qtr.sql
+++ transform/snowflake-dbt/models/tva/tva_by_period/tva_all_by_qtr.sql
@@ -10,7 +10,7 @@ WITH tva_all_by_qtr_raw AS (
 ), tva_all_by_qtr AS (
   SELECT target_fact.name, target_fact.category, tva_all_by_qtr_raw.*
   FROM {{ source('targets', 'target_fact') }}
-  JOIN tva_all_by_qtr_raw ON tva_all_by_wtr_raw.target_slug = target_fact.slug
+  JOIN tva_all_by_qtr_raw ON tva_all_by_qtr_raw.target_slug = target_fact.slug
 )
 
 SELECT * FROM tva_all_by_qtr
