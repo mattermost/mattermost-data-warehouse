e6bc6a09ee416575aa96d84401336f06eb9d8c2b
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-10 16:21:11 -0700

    add period_last_day

--- transform/snowflake-dbt/models/staging/tva_granular/tva_bookings_ren_by_qtr.sql
+++ transform/snowflake-dbt/models/staging/tva_granular/tva_bookings_ren_by_qtr.sql
@@ -8,6 +8,7 @@ WITH tva_bookings_ren_by_qtr AS (
     SELECT
         'bookings_ren_by_qtr' AS target_slug,
         dev_util.fiscal_year(tva_bookings_ren_by_mo.month)|| '-' || dev_util.fiscal_quarter(tva_bookings_ren_by_mo.month) AS qtr,
+        max(period_last_day) AS period_last_day,
         sum(tva_bookings_ren_by_mo.target) AS target,
         sum(tva_bookings_ren_by_mo.actual) as actual,
         round(sum(tva_bookings_ren_by_mo.actual)/sum(tva_bookings_ren_by_mo.target),2) AS tva
