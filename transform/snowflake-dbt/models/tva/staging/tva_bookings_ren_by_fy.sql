55c190df6245fa613d0174726758998265b3a0dc
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-10 15:47:44 -0700

    update lawjic for Cumulative Over Period Example

--- transform/snowflake-dbt/models/tva/staging/tva_bookings_ren_by_fy.sql
+++ transform/snowflake-dbt/models/tva/staging/tva_bookings_ren_by_fy.sql
@@ -4,38 +4,15 @@
   })
 }}
 
-WITH bookings_ren AS (
+WITH tva_bookings_ren_by_fy AS (
     SELECT
-      opportunity.closedate,
-      opportunity.sfid AS opportunity_sfid,
-      opportunitylineitem.sfid AS opportunitylineitem_sfid,
-      opportunity.type AS opportunity_type,
-      opportunitylineitem.product_line_type__c AS product_line_type,
-      CASE WHEN end_date__c::date - start_date__c::date + 1 > 365 THEN opportunitylineitem.arr_contributed__c ELSE opportunitylineitem.totalprice END AS bookings
-    FROM {{ source('orgm', 'opportunity') }} AS opportunity
-    LEFT JOIN {{ source('orgm', 'opportunitylineitem') }} AS opportunitylineitem ON opportunity.sfid = opportunitylineitem.opportunityid
-    WHERE iswon AND opportunitylineitem.product_line_type__c = 'Ren'
-), actual_bookings_ren_by_fy AS (
-    SELECT 
-        util.fiscal_year(closedate) AS fy,
-        sum(bookings) AS total_bookings
-    FROM bookings_ren
-    GROUP BY 1
-), bookings_ren_by_fy AS (
-    SELECT 
-        util.fiscal_year(month) AS fy,
-        SUM(target) as target
-    FROM {{ source('targets', 'bookings_ren_by_mo') }}
-    GROUP BY 1
-), tva_bookings_ren_by_fy AS (
-    SELECT
-        'bookings_ren_by_fy' as target_slug,
-        bookings_ren_by_fy.fy,
-        bookings_ren_by_fy.target,
-        actual_bookings_ren_by_fy.total_bookings as actual,
-        round((actual_bookings_ren_by_fy.total_bookings/bookings_ren_by_fy.target),2) as tva
-    FROM bookings_ren_by_fy
-    LEFT JOIN actual_bookings_ren_by_fy ON bookings_ren_by_fy.fy = actual_bookings_ren_by_fy.fy
+        'bookings_ren_by_fy' AS target_slug,
+        util.fiscal_year(tva_bookings_ren_by_mo.month) AS fy,
+        sum(bookings_ren_by_qtr.target) AS target,
+        sum(actual_bookings_ren_by_qtr.actual) as actual,
+        round(sum(actual_bookings_ren_by_qtr.actual/sum(bookings_ren_by_qtr.target)),2) AS tva
+    FROM {{ ref('tva_bookings_ren_by_mo') }}
+    GROUP BY 1,2
 )
 
 SELECT * FROM tva_bookings_ren_by_fy
--- transform/snowflake-dbt/models/tva/staging/tva_bookings_ren_by_qtr.sql
+++ transform/snowflake-dbt/models/tva/staging/tva_bookings_ren_by_qtr.sql
@@ -4,38 +4,15 @@
   })
 }}
 
-WITH bookings_ren AS (
+WITH tva_bookings_ren_by_qtr AS (
     SELECT
-      opportunity.closedate,
-      opportunity.sfid AS opportunity_sfid,
-      opportunitylineitem.sfid AS opportunitylineitem_sfid,
-      opportunity.type AS opportunity_type,
-      opportunitylineitem.product_line_type__c AS product_line_type,
-      CASE WHEN end_date__c::date - start_date__c::date + 1 > 365 THEN opportunitylineitem.arr_contributed__c ELSE opportunitylineitem.totalprice END AS bookings
-    FROM {{ source('orgm', 'opportunity') }} AS opportunity
-    LEFT JOIN {{ source('orgm', 'opportunitylineitem') }} AS opportunitylineitem ON opportunity.sfid = opportunitylineitem.opportunityid
-    WHERE iswon AND opportunitylineitem.product_line_type__c = 'Ren'
-), actual_bookings_ren_by_qtr AS (
-    SELECT 
-        util.fiscal_year(closedate)|| '-' || util.fiscal_quarter(closedate) AS qtr,
-        sum(bookings) AS total_bookings
-    FROM bookings_ren
-    GROUP BY 1
-), bookings_ren_by_qtr AS (
-    SELECT 
-        util.fiscal_year(month)|| '-' || util.fiscal_quarter(month) AS qtr,
-        SUM(target) as target
-    FROM {{ source('targets', 'bookings_ren_by_mo') }}
-    GROUP BY 1
-), tva_bookings_ren_by_qtr AS (
-    SELECT
-        'bookings_ren_by_qtr' as target_slug,
-        bookings_ren_by_qtr.qtr,
-        bookings_ren_by_qtr.target,
-        actual_bookings_ren_by_qtr.total_bookings as actual,
-        round((actual_bookings_ren_by_qtr.total_bookings/bookings_ren_by_qtr.target),2) as tva
-    FROM bookings_ren_by_qtr
-    LEFT JOIN actual_bookings_ren_by_qtr ON bookings_ren_by_qtr.qtr = actual_bookings_ren_by_qtr.qtr
+        'bookings_ren_by_qtr' AS target_slug,
+        util.fiscal_year(tva_bookings_ren_by_mo.month)|| '-' || util.fiscal_quarter(tva_bookings_ren_by_mo.month) AS qtr,
+        sum(bookings_ren_by_qtr.target) AS target,
+        sum(actual_bookings_ren_by_qtr.actual) as actual,
+        round(sum(actual_bookings_ren_by_qtr.actual/sum(bookings_ren_by_qtr.target)),2) AS tva
+    FROM {{ ref('tva_bookings_ren_by_mo') }}
+    GROUP BY 1,2
 )
 
 SELECT * FROM tva_bookings_ren_by_qtr
