06c96d7e1d14afc63d00f6ef7feb0fd402b1f032
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-10 17:20:11 -0700

    remove to join in looker

--- transform/snowflake-dbt/models/tva/tva_all_by_fy.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_fy.sql
@@ -4,7 +4,7 @@
   })
 }}
 
-WITH tva_all_by_fy_raw AS (
+WITH tva_all_by_fy AS (
 
   SELECT *
   FROM {{ ref('tva_arr_by_fy') }}
@@ -14,12 +14,6 @@ WITH tva_all_by_fy_raw AS (
   SELECT *
   FROM {{ ref('tva_bookings_ren_by_fy') }}
 
-), tva_all_by_fy AS (
-
-  SELECT target_fact.name, target_fact.category, tva_all_by_fy_raw.*
-  FROM {{ source('tva', 'target_fact') }}
-  JOIN tva_all_by_fy_raw ON tva_all_by_fy_raw.target_slug = target_fact.slug
-  
 )
 
 SELECT * FROM tva_all_by_fy
--- transform/snowflake-dbt/models/tva/tva_all_by_qtr.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_qtr.sql
@@ -4,7 +4,7 @@
   })
 }}
 
-WITH tva_all_by_qtr_raw AS (
+WITH tva_all_by_qtr AS (
 
   SELECT *
   FROM {{ ref('tva_arr_by_qtr') }}
@@ -14,12 +14,6 @@ WITH tva_all_by_qtr_raw AS (
   SELECT *
   FROM {{ ref('tva_bookings_ren_by_qtr') }}
 
-), tva_all_by_qtr AS (
-
-  SELECT target_fact.name, target_fact.category, tva_all_by_qtr_raw.*
-  FROM {{ source('tva', 'target_fact') }}
-  JOIN tva_all_by_qtr_raw ON tva_all_by_qtr_raw.target_slug = target_fact.slug
-  
 )
 
 SELECT * FROM tva_all_by_qtr
