1494f8dae2b9b2dc2eec72788b29980312ae36f2
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-03-21 09:27:50 -0700

    more

--- transform/snowflake-dbt/models/staging/server/server_server_details.sql
+++ transform/snowflake-dbt/models/staging/server/server_server_details.sql
@@ -62,7 +62,7 @@ WITH max_timestamp              AS (
            , l.feature_office365
            , l.feature_password
            , l.feature_saml
-         FROM {{ ref('licenses') }} l
+         FROM {{ source('mattermost','licenses') }} l
          GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
          , 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
      ),
