504975d84aee4197843489fd84825e9e0e072d59
Eric <43073688+enelson720@users.noreply.github.com>
2020-03-23 07:13:29 -0700

    Server details fix (#145)
    * Fixing date range filters for nightly server jobs.
    
    * Fixing events.security stupidity.
    
    * Fixed syntax issue.
    
    * Fixed syntax issue.
    
    * Fixed syntax issue.
    
    * Trouble shooting nightly job lag.
    
    * Fixing nightly job scheduling (airflow).
    
    * Added license data model to mattermost schema. Added team count to security table.
    
    * fixed source reference.
    
    * fixed source reference.
    
    * fixed source reference.
    
    * fixed source reference.
    
    * Fixed source reference.
    
    * Removed date column. Using start and end dates, as well as timestamp as date tracking mechanism.
    
    * Grouped license_overview data model to reduce fanning out.
    
    * Added license overview account and contact info.
    
    * Fixed contact_email column reference from contact_name.
    
    * Fixing license details for server details data model.
    
    * Fixing license details for server details data model.
    
    * Fixed server_server_details fanning out.
    
    * Fixed server_server_details fanning out.
    
    * Fixed server_licensee_details date range.
    
    * Added expire_date logic to prevent fanning out.
    
    * Fixed alias.
    
    * Fixed alias.
    
    * Added license_daily_details data model to analytics.mattermost schema. Updated incremental build logic in other license models.
    
    * Added license_daily_details to schema.yml file.
    
    * Fixed column reference for license_id in unique key.
    
    * Reversed table joins.
    
    * Fixing expire date reference for customers.
    
    * Fixing expire date reference for customers.
    
    * Fixing join keys.
    
    * Fixing trial identification logic.
    
    * Testing MAU on license_daily_details model.
    
    * Testing MAU on license_daily_details model.
    
    * Testing MAU on license_daily_details model.
    
    * Updated events/schema.yml file withserver_events_by_date model reference.
    
    * Updated license_daily_details w/ server_events_by_date reference.
    
    * Updated license_daily_details w/ server_events_by_date reference.
    
    * Updated user_events_by_date logic.
    
    * Updated user_events_by_date logic.
    
    * Fixing server_expire_date for join.
    
    * Fixing license_daily_details server-level aggregation.
    
    * Fixing license_daily_details server-level aggregation.
    
    * Fixing license_daily_details server-level aggregation.
    
    * Experimenting with dates.
    
    * Experimenting with dates.
    
    * Added customerid.
    
    * Fixing mattermost.licenses.
    
    * Fixing mattermost.licenses.
    
    * Fixing mattermost.licenses.
    
    * Fixing mattermost.licenses.
    
    * Time constraint on license_daily_details.
    
    * Time constraint on license_daily_details.
    
    * Time constraint on license_daily_details.
    
    * Time constraint on license_daily_details.
    
    * timestamp reference fix.
    
    * timestamp reference fix.
    
    * Fixing partitioning window.
    
    * Fixing partitioning window.
    
    * Fixing partitioning window.
    
    * Added mattermost2.activity active user data.
    
    * Added mattermost2.activity active user data.
    
    * Added new active user and incremental build logic.
    
    * Fixing server_license_details model logic.
    
    * Fixed server_id column reference.
    
    * Fixed server_id column reference.
    
    * Fixed server_expire_date column reference.
    
    * Fixed ambiguous column reference.
    
    * Fixing duplicate row issue.
    
    * Fixing duplicate row issue.
    
    * Fixing syntax and group by.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/mattermost/nightly/license_daily_details.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/license_daily_details.sql
@@ -31,7 +31,7 @@ WITH license_daily_details as (
            , MAX(l.feature_custom_brand)                                             AS feature_custom_brand
            , MAX(l.feature_custom_permissions_schemes)                               AS feature_custom_permissions_schemes
            , MAX(l.feature_data_retention)                                           AS feature_data_retention
-           , MAX(.feature_elastic_search)                                            AS feature_elastic_search
+           , MAX(l.feature_elastic_search)                                           AS feature_elastic_search
            , MAX(l.feature_email_notification_contents)                              AS feature_email_notification_contents                        
            , MAX(l.feature_future)                                                   AS feature_future
            , MAX(l.feature_google)                                                   AS feature_google
@@ -84,7 +84,7 @@ WITH license_daily_details as (
          AND l.date >= (SELECT MAX(date) FROM {{this}})
 
          {% endif %}
-         {{ dbt_utils.group_by(n=20) }}
+         {{ dbt_utils.group_by(n=19) }}
      )
      SELECT *
      FROM license_daily_details
