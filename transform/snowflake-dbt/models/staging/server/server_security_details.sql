034a4c703a336441af7cb47eda5bb1605d4aa443
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-03-26 15:28:44 -0700

    Updating license join logic to server staging tables.

--- transform/snowflake-dbt/models/staging/server/server_security_details.sql
+++ transform/snowflake-dbt/models/staging/server/server_security_details.sql
@@ -137,9 +137,10 @@ WITH security                AS (
            , l.feature_password
            , l.feature_saml
            , l.has_trial_and_non_trial
+           , l.trial
          FROM {{ ref('licenses') }} l
          GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
-         , 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41
+         , 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42
      ),
      server_security_details    AS (
          SELECT
