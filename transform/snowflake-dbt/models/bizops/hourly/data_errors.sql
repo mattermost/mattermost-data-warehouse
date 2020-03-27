5b24c7877c227a62c453d45824a4bad67cfb6a25
Michael Schiff <57653160+michaelschiffmm@users.noreply.github.com>
2020-03-27 16:40:03 -0500

    Added some account checks

--- transform/snowflake-dbt/models/bizops/hourly/data_errors.sql
+++ transform/snowflake-dbt/models/bizops/hourly/data_errors.sql
@@ -6,6 +6,40 @@
 
 WITH data_errors AS (
     SELECT
+        'account' AS object,
+        account.sfid AS object_id,
+        'No Company Type' AS error_short,
+        'No Company Type when owned by Brown' AS error_long
+    FROM {{ source('orgm','account')}}
+    JOIN {{ source('orgm','user')}} AS account_owner ON account.ownerid = account_owner.sfid
+    WHERE account_owner.lastname = 'Brown'
+    
+    UNION ALL
+                   
+    SELECT
+        'account' AS object,
+        account.sfid AS object_id,
+        'Customer, no ARR' AS error_short,
+        'Type = Customer, ARR = 0' AS error_long
+    FROM account
+    WHERE arr_current__c =0
+    AND type = 'Customer'
+                   
+    UNION ALL
+                   
+    SELECT
+        'account' AS object,
+        a.sfid AS object_id,
+        'Owner not Rep' AS error_short,
+        'Account owner System Type <> Rep' AS error_long
+    FROM account a, "user" u
+    WHERE
+        a.ownerid = u.sfid AND
+        u.system_type__c <> 'Rep'
+                   
+    UNION ALL
+                        
+    SELECT
         'opportunity' AS object,
         opportunity.sfid AS object_id,
         'oppt CSM <> acct csm' AS error_short,
@@ -14,17 +48,6 @@ WITH data_errors AS (
     JOIN {{ source('orgm','opportunity')}} ON opportunity.accountid = account.sfid
     WHERE opportunity.status_wlo__c = 'Open'
         AND opportunity.csm_owner__c <> account.csm_lookup__c
-    
-    UNION ALL
-
-    SELECT
-        'account' AS object,
-        account.sfid AS object_id,
-        'No Company Type' AS error_short,
-        'No Company Type when owned by Brown' AS error_long
-    FROM {{ source('orgm','account')}}
-    JOIN {{ source('orgm','user')}} AS account_owner ON account.ownerid = account_owner.sfid
-    WHERE account_owner.lastname = 'Brown'
     
     UNION ALL
 
