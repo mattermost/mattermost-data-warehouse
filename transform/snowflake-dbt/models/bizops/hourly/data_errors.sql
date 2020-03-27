b9b396854966126d99a211a3a9dc8e8172c01040
rbradleyhaas <rachel.bradley-haas@mattermost.com>
2020-03-27 14:02:45 -0700

    Data Errors dbt Job (#161)
    * add data_errors
    
    * move things around
    
    Co-authored-by: Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>

---
+++ transform/snowflake-dbt/models/bizops/hourly/data_errors.sql
@@ -0,0 +1,42 @@
+{{config({
+    "materialized": "table",
+    "schema": "bizops"
+  })
+}}
+
+WITH data_errors AS (
+    SELECT
+        'opportunity' AS object,
+        opportunity.sfid AS object_id,
+        'oppt CSM <> acct csm' AS error_short,
+        'oppt CSM <> acct csm on open oppts, override not set' AS error_long
+    FROM {{ source('orgm','account')}}
+    JOIN {{ source('orgm','opportunity')}} ON opportunity.accountid = account.sfid
+    WHERE opportunity.status_wlo__c = 'Open'
+        AND opportunity.csm_owner__c <> account.csm_lookup__c;
+    
+    UNION ALL
+
+    SELECT
+        'account' AS object,
+        account.sfid AS object_id,
+        'No Company Type' AS error_short,
+        'No Company Type when owned by Brown' AS error_long
+    FROM {{ source('orgm','account')}}
+    JOIN {{ source('orgm','user')}} AS account_owner ON account.ownerid = account_owner.sfid
+    WHERE account_owner.lastname = 'Brown'
+    
+    UNION ALL
+
+    SELECT
+        'opportunity' as object,
+        opportunity.sfid as object_id,
+        'oppt open in prior month' as error_short,
+        'oppt open in prior month/' || o.name  || '/' || u.name as error_long
+    FROM {{ source('orgm','opportunity')}}
+    JOIN {{ source('orgm','user')}} AS opportunity_owner ON opportunity.ownerid = opportunity_owner.sfid
+    WHERE opportunity.status_wlo__c = 'Open' 
+        AND to_char(opportunity.closedate,'YYYY-MM') < to_char(NOW(),'YYYY-MM')
+)
+
+SELECT * FROM data_errors
