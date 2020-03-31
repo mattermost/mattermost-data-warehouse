cae9ec5127b5ba1c4a08c600da13648231ba5780
Eric <43073688+enelson720@users.noreply.github.com>
2020-03-31 06:33:08 -0700

    User events (#172)
    * Adding browser, browser version, and os field to user_events_by_date data model.
    
    * Adding browser, browser version, and os field to user_events_by_date data model.
    
    * Adding browser, browser version, and os field to user_events_by_date data model.
    
    * Fixing syntax and case statement for version and browser fields in user_events_by_date model.
    
    * Fixing browser version syntax.
    
    * Added os version field.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/events/user_events_by_date.sql
+++ transform/snowflake-dbt/models/events/user_events_by_date.sql
@@ -6,28 +6,31 @@
 
 WITH mobile_events       AS (
     SELECT
-        m.timestamp::DATE       AS date
-      , m.user_id               AS server_id
-      , m.user_actual_id        AS user_id
-      , min(m.user_actual_role) AS user_role
+        m.timestamp::DATE                AS date
+      , m.user_id                        AS server_id
+      , m.user_actual_id                 AS user_id
+      , min(m.user_actual_role)          AS user_role
       , CASE
           WHEN context_device_type = 'ios' THEN 'iPhone'            
           WHEN context_device_type = 'android' THEN 'Android'
           ELSE 'Other'
-          END                   AS browser
+          END                            AS browser
       , CASE
           WHEN context_device_type = 'ios'    THEN 'iPhone'
           WHEN context_device_type = 'android'     THEN 'Android'
           ELSE 'Other'
-          END                   AS os
+          END                            AS os
       , CASE
           WHEN context_device_type = 'ios' THEN context_app_version::VARCHAR
           WHEN context_device_type = 'android' THEN context_app_version::VARCHAR
           ELSE 'Other'
-          END                   AS version
-      , LOWER(m.type)           AS event_name
-      , 'mobile'                AS event_type
-      , COUNT(*)                AS num_events
+          END                            AS version
+      , CASE
+          WHEN context_device_os IS NOT NULL THEN context_device_os::varchar
+          ELSE 'Unknown' END             AS os_version
+      , LOWER(m.type)                    AS event_name
+      , 'mobile'                         AS event_type
+      , COUNT(*)                         AS num_events
     FROM {{ source('mattermost_rn_mobile_release_builds_v2', 'event')}} m
     WHERE user_actual_id IS NOT NULL
     AND m.timestamp::DATE <= CURRENT_DATE - INTERVAL '1 DAY'
@@ -36,7 +39,7 @@ WITH mobile_events       AS (
       AND timestamp::DATE > (SELECT MAX(date) from {{this}})
 
     {% endif %}
-    GROUP BY 1, 2, 3, 5, 6, 7, 8, 9
+    GROUP BY 1, 2, 3, 5, 6, 7, 8, 9, 10
 ),
      events              AS (
          SELECT
@@ -50,9 +53,11 @@ WITH mobile_events       AS (
               WHEN context_user_agent LIKE '%Android%'   THEN 'Android'
               WHEN context_user_agent LIKE '%Electron/%' THEN 'Electron'
               WHEN context_user_agent LIKE '%Edge/%'     THEN 'Edge'
+              WHEN context_user_agent LIKE '%Edg/%'      THEN 'Edge'
               WHEN context_user_agent LIKE '%MSIE%'      THEN 'IE'
-              WHEN context_user_agent LIKE '%Chrome/%'   THEN 'Chrome'
+              WHEN context_user_agent LIKE '%Trident/%'  THEN 'IE'
               WHEN context_user_agent LIKE '%Firefox/%'  THEN 'Firefox'
+              WHEN context_user_agent LIKE '%Chrome/%'   THEN 'Chrome'
               WHEN context_user_agent LIKE '%Safari/%'   THEN 'Safari'
               ELSE 'Other'
               END                                                                                        AS browser
@@ -77,6 +82,20 @@ WITH mobile_events       AS (
               WHEN context_user_agent LIKE '%Safari/%'  THEN split_part(split_part(context_user_agent, 'Version/', 2), ' ', 1)
               ELSE 'Other'
               END                                                                                      AS version
+           , CASE
+              WHEN context_user_agent LIKE '%iPhone%'    THEN split_part(split_part(context_user_agent, 'iPhone OS ', 2), ' ', 1)
+              WHEN context_user_agent LIKE '%iPad%'      THEN split_part(split_part(context_user_agent, 'CPU OS ', 2), ' ', 1)
+              WHEN context_user_agent LIKE '%CrOS%'      THEN split_part(
+                split_part(split_part(context_user_agent, 'CrOS ', 2), ')', 1), 'CS', 1)
+              WHEN context_user_agent LIKE '%Android%'
+                                                   THEN split_part(split_part(context_user_agent, 'Android ', 2), ';', 1)
+              WHEN context_user_agent LIKE '%Macintosh%'
+                                                   THEN split_part(split_part(context_user_agent, 'Mac OS X ', 2), ')', 1)
+              WHEN context_user_agent LIKE '%Windows%'   THEN split_part(
+                split_part(split_part(context_user_agent, 'Windows ', 2), ')', 1), ';', 1)
+              WHEN context_user_agent LIKE '%Linux%'     THEN split_part(split_part(context_user_agent, 'Linux ', 2), ')', 1)
+              ELSE 'Unknown'
+              END                                                                                      AS os_version
            , LOWER(e.type)                                                                             AS event_name
            , CASE WHEN LOWER(e.context_user_agent) LIKE '%electron%' THEN 'desktop' ELSE 'web_app' END AS event_type
            , COUNT(*)                                                                                  AS num_events
@@ -88,7 +107,7 @@ WITH mobile_events       AS (
           AND timestamp::DATE > (SELECT MAX(date) from {{this}})
 
          {% endif %}
-         GROUP BY 1, 2, 3, 5, 6, 7, 8, 9
+         GROUP BY 1, 2, 3, 5, 6, 7, 8, 9, 10
      ),
 
      all_events          AS (
@@ -110,6 +129,7 @@ WITH mobile_events       AS (
            , e.os
            , e.browser
            , e.version                                                                                AS browser_version
+           , e.os_version
            , r.event_id                                                                               AS event_id
            , e.event_name
            , sum(e.num_events)                                                                        AS total_events
@@ -119,7 +139,7 @@ WITH mobile_events       AS (
          FROM all_events                  e
               LEFT JOIN {{ ref('events_registry') }} r
                    ON e.event_name = r.event_name
-         GROUP BY 1, 2, 3, 7, 8, 9, 10, 11
+         GROUP BY 1, 2, 3, 7, 8, 9, 10, 11, 12
      )
 SELECT *
 FROM user_events_by_date
