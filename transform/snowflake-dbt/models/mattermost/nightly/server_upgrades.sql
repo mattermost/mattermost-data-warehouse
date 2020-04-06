5026e1381e479b17511639b76c4d451e5665277d
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-04-06 11:03:10 -0700

    Adding TE to EE upgrade dimensions.

--- transform/snowflake-dbt/models/mattermost/nightly/server_upgrades.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/server_upgrades.sql
@@ -10,16 +10,27 @@ WITH upgrade         AS (
       , server_id
       , lag(version) OVER (PARTITION BY server_id ORDER BY date) AS prev_version
       , version                                                  AS current_version
+      , edition                                                  AS current_edition
+      , lag(edition) OVER (PARTITION BY server_id ORDER BY date) AS prev_edition
     FROM {{ ref('server_daily_details') }}
     WHERE NOT tracking_disabled
                         ),
      server_upgrades AS (
-         SELECT *
+         SELECT
+             date
+           , server_id
+           , substr(prev_version, 0, length(current_version)) AS prev_version
+           , substr(current_version, 0, length(prev_version)) AS current_version
+           , prev_edition
+           , current_edition
          FROM upgrade
-         WHERE substr(current_version, 0, length(prev_version)) > substr(prev_version, 0, length(current_version))
+         WHERE (substr(current_version, 0, length(prev_version)) >
+                coalesce(substr(prev_version, 0, length(current_version)),
+                         substr(current_version, 0, length(prev_version)))
+             OR (current_edition = 'true' AND coalesce(prev_edition, 'true') <> current_edition))
          {% if is_incremental() %}
-
-         AND date > (SELECT MAX(date) FROM {{this}})
+         
+         AND date > (SELECT MAX(date) FROM {{this}} )
 
          {% endif %}
      )
