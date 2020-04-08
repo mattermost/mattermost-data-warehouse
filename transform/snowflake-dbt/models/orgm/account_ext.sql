5bb4347b0ebc9f155d572e232c2098d8500764ae
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-04-08 15:07:20 -0700

    updates to account_ext

--- transform/snowflake-dbt/models/orgm/account_ext.sql
+++ transform/snowflake-dbt/models/orgm/account_ext.sql
@@ -11,10 +11,18 @@ WITH account_ext AS (
         COUNT(DISTINCT CASE WHEN opportunity.iswon THEN opportunity.sfid ELSE NULL END) AS count_won_oppt,
         COUNT(DISTINCT CASE WHEN NOT opportunity.isclosed THEN opportunity.sfid ELSE NULL END) AS count_open_oppt,
         COUNT(DISTINCT CASE WHEN opportunity.isclosed AND NOT opportunity.iswon THEN opportunity.sfid ELSE NULL END) AS count_lost_oppt,
-        SUM(opportunity_ext.sum_new_amount) AS sum_new_amount,
-        SUM(opportunity_ext.sum_expansion_amount) AS sum_expansion_amount,
-        SUM(opportunity_ext.sum_renewal_amount) AS sum_renewal_amount,
-        SUM(opportunity_ext.sum_multi_amount) AS sum_multi_amount
+        SUM(CASE WHEN opportunity.iswon THEN opportunity_ext.sum_new_amount ELSE 0 END) AS sum_new_amount_won,
+        SUM(CASE WHEN opportunity.iswon THEN opportunity_ext.sum_expansion_amount ELSE 0 END) AS sum_expansion_amount_won,
+        SUM(CASE WHEN opportunity.iswon THEN opportunity_ext.sum_renewal_amount ELSE 0 END) AS sum_renewal_amount_won,
+        SUM(CASE WHEN opportunity.iswon THEN opportunity_ext.sum_multi_amount ELSE 0 END) AS sum_multi_amount_won,
+        SUM(CASE WHEN NOT opportunity.isclosed THEN opportunity_ext.sum_new_amount ELSE 0 END) AS sum_new_amount_open,
+        SUM(CASE WHEN NOT opportunity.isclosed THEN opportunity_ext.sum_expansion_amount ELSE 0 END) AS sum_expansion_amount_open,
+        SUM(CASE WHEN NOT opportunity.isclosed THEN opportunity_ext.sum_renewal_amount ELSE 0 END) AS sum_renewal_amount_open,
+        SUM(CASE WHEN NOT opportunity.isclosed THEN opportunity_ext.sum_multi_amount ELSE 0 END) AS sum_multi_amount_open,
+        SUM(CASE WHEN opportunity.isclosed AND NOT opportunity.iswon THEN opportunity_ext.sum_new_amount ELSE 0 END) AS sum_new_amount_lost,
+        SUM(CASE WHEN opportunity.isclosed AND NOT opportunity.iswon THEN opportunity_ext.sum_expansion_amount ELSE 0 END) AS sum_expansion_amount_lost,
+        SUM(CASE WHEN opportunity.isclosed AND NOT opportunity.iswon THEN opportunity_ext.sum_renewal_amount ELSE 0 END) AS sum_renewal_amount_lost,
+        SUM(CASE WHEN opportunity.isclosed AND NOT opportunity.iswon THEN opportunity_ext.sum_multi_amount ELSE 0 END) AS sum_multi_amount_lost
     FROM {{ source('orgm', 'account') }}
     LEFT JOIN {{ source('orgm', 'opportunity') }} ON account.sfid = opportunity.accountid
     LEFT JOIN {{ ref('opportunity_ext') }} ON account.sfid = opportunity_ext.accountid
