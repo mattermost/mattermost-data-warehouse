0e4da1caa398948cea3e058defbba6c3d2201472
Eric <43073688+enelson720@users.noreply.github.com>
2020-04-15 15:55:27 -0700

    Nps server (#203)
    * Adding nps_server_monthly_score model and schema.yml reference.
    
    * Adding nps scores to server fact.
    
    * Fixed table alias reference.
    
    * Fixed parentheses.
    
    * Added additional fields including response counts and feedback.
    
    * Added updates to server_fact table.
    
    * Fixed syntax issue.
    
    * Fixed incorrect column reference.
    
    * Adding nps daily score.
    
    * Updating nps user monthly score model to daily for joinning purposes at user and server level.
    
    * Updating nps server monthly score to daily level.
    
    * fixing server fact nps server monthly score referece.
    
    * fixing user_daily_details user nps score model reference.
    
    * Adding server edition and first server edition.
    
    * Adding first paid and trial license dates.
    
    * Fixing syntax issue.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/mattermost/nightly/server_fact.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/server_fact.sql
@@ -16,10 +16,20 @@ WITH server_details AS (
       , MAX(CASE
                 WHEN license_id1 IS NOT NULL OR license_id2 IS NOT NULL THEN date
                                                                         ELSE NULL END) AS last_active_license_date
+      , MIN(CASE WHEN license_id1 IS NOT NULL OR license_id2 IS NOT NULL THEN date
+                                                                        ELSE NULL END) AS first_active_license_date
       , MIN(version) AS                                                                   first_server_version
     FROM {{ ref('server_daily_details') }}
     GROUP BY 1
     ), 
+    licenses AS (
+      SELECT 
+          server_id
+        , MIN(CASE WHEN NOT TRIAL THEN date ELSE NULL END) AS first_paid_license_date
+        , MIN(CASE WHEN TRIAL THEN date ELSE NULL END)     AS first_trial_license_date
+      FROM {{ ref('licenses') }}
+      GROUP BY 1
+    ),
   server_upgrades AS (
     SELECT
         server_id
@@ -30,7 +40,17 @@ WITH server_details AS (
         CASE WHEN PREV_EDITION = 'false' AND CURRENT_EDITION = 'true' THEN server_id ELSE NULL END) AS edition_upgrade_count
     FROM {{ ref('server_upgrades') }}
     GROUP BY 1
-    ), 
+    ),
+    first_server_edition AS (
+      SELECT
+          s.server_id
+        , s.edition      AS first_server_edition
+      FROM server_details sd
+      JOIN {{ ref('server_daily_details') }} s
+           ON sd.server_id = s.server_id
+           AND sd.first_telemetry_active_date = s.date
+      GROUP BY 1, 2
+    ),
   server_active_users AS (
     SELECT
         server_id
@@ -56,11 +76,15 @@ WITH server_details AS (
         server_details.server_id
       , server_daily_details.version
       , server_details.first_server_version
+      , server_daily_details.edition        AS server_edition
+      , fse.first_server_edition
       , upgrades.version_upgrade_count
       , upgrades.edition_upgrade_count
-      , server_daily_details.account_sfid AS last_account_sfid
-      , server_daily_details.license_id1 AS last_license_id1
-      , server_daily_details.license_id2 AS last_license_id2
+      , server_daily_details.account_sfid   AS last_account_sfid
+      , server_daily_details.license_id1    AS last_license_id1
+      , server_daily_details.license_id2    AS last_license_id2
+      , licenses.first_paid_license_date
+      , licenses.first_trial_license_date
       , server_details.first_active_date
       , server_details.last_active_date
       , server_details.first_telemetry_active_date
@@ -86,7 +110,7 @@ WITH server_details AS (
       , nps.promoters
       , nps.detractors
       , nps.passives
-      , nps.avg_score AS avg_nps_user_score
+      , nps.avg_score                   AS avg_nps_user_score
     FROM server_details
         JOIN {{ ref('server_daily_details') }}
             ON server_details.server_id = server_daily_details.server_id
@@ -98,7 +122,11 @@ WITH server_details AS (
             ON server_details.server_id = upgrades.server_id
         LEFT JOIN server_active_users sau
             ON server_details.server_id = sau.server_id
-        {{ dbt_utils.group_by(n=34) }}
+        LEFT JOIN first_server_edition fse
+            ON server_details.server_id = fse.server_id
+        LEFT JOIN licenses
+            ON server_details.server_id = licenses.server_id
+        {{ dbt_utils.group_by(n=38) }}
     )
 SELECT *
 FROM server_fact
