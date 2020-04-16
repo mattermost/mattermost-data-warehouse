269730fb9823309fbcba029003c51f1d6d2db52f
Eric <43073688+enelson720@users.noreply.github.com>
2020-04-16 07:34:50 -0700

    Updating Server Fact Data Model with First & Last Edition Logic (#204)
    * Adding nps_server_monthly_score model and schema.yml reference.
    
    * Adding nps scores to server fact.
    
    * Fixed table alias reference.
    
    * Fixed parentheses.
    
    * Added additional fields including response counts and feedback.
    
    * Added updates to server_fact table.
    
    * Fixed syntax issue.
    
    * Fixed incorrect column reference.
    
    * Adding nps daily score.
    
    * Updating nps user monthly score model to daily for joinning purposes at user and server level.
    
    * Updating nps server monthly score to daily level.
    
    * fixing server fact nps server monthly score referece.
    
    * fixing user_daily_details user nps score model reference.
    
    * Adding server edition and first server edition.
    
    * Adding first paid and trial license dates.
    
    * Fixing syntax issue.
    
    * Adding additional logic to server fact.
    
    * Adding first edition date.
    
    * Adding first edition date.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/mattermost/nightly/server_fact.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/server_fact.sql
@@ -19,6 +19,8 @@ WITH server_details AS (
       , MIN(CASE WHEN license_id1 IS NOT NULL OR license_id2 IS NOT NULL THEN date
                                                                         ELSE NULL END) AS first_active_license_date
       , MIN(version) AS                                                                   first_server_version
+      , MIN(CASE WHEN edition IS NOT NULL THEN date ELSE NULL END)                     AS first_edition_date
+      , MAX(CASE WHEN edition IS NOT NULL THEN date ELSE NULL END)                     AS last_edition_date
     FROM {{ ref('server_daily_details') }}
     GROUP BY 1
     ), 
@@ -44,77 +46,91 @@ WITH server_details AS (
     first_server_edition AS (
       SELECT
           s.server_id
-        , s.edition      AS first_server_edition
+        , MAX(CASE WHEN sd.first_edition_date = s.date THEN s.edition ELSE NULL END)      AS first_server_edition
+        , MAX(CASE WHEN sd.last_edition_date = s.date THEN s.edition ELSE NULL END)       AS edition
       FROM server_details sd
       JOIN {{ ref('server_daily_details') }} s
            ON sd.server_id = s.server_id
-           AND sd.first_telemetry_active_date = s.date
-      GROUP BY 1, 2
+           AND (sd.first_edition_date = s.date
+           OR sd.last_edition_date = s.date)
+      GROUP BY 1
     ),
-  server_active_users AS (
+  last_server_date AS (
     SELECT
         server_id
-      , dau_total
-      , mobile_dau
-      , mau_total
-      , first_time_mau
-      , reengaged_mau
-      , current_mau
-      , total_events
-      , desktop_events
-      , web_app_events
-      , mobile_events
-      , events_alltime
-      , mobile_events_alltime
-      , USERS
+      , MAX(CASE WHEN total_events > 0 THEN date ELSE NULL END) AS last_event_date
     FROM {{ ref('server_events_by_date') }}
-    WHERE DATE = CURRENT_DATE - INTERVAL '1 DAY'
-        {{ dbt_utils.group_by(n=14) }}
+    GROUP BY 1
+  ),
+  server_active_users AS (
+    SELECT
+        s1.server_id
+      , s1.dau_total
+      , s1.mobile_dau
+      , s1.mau_total
+      , s1.first_time_mau
+      , s1.reengaged_mau
+      , s1.current_mau
+      , s1.total_events
+      , s1.desktop_events
+      , s1.web_app_events
+      , s1.mobile_events
+      , s1.events_alltime
+      , s1.mobile_events_alltime
+      , s1.USERS
+      , s2.last_event_date
+    FROM {{ ref('server_events_by_date') }} s1
+    JOIN last_server_date s2
+         ON s1.server_id = s2.server_id
+         AND s1.date = s2.last_event_date
+        {{ dbt_utils.group_by(n=15) }}
     ), 
   server_fact AS (
     SELECT
         server_details.server_id
-      , server_daily_details.version
-      , server_details.first_server_version
-      , server_daily_details.edition        AS server_edition
-      , fse.first_server_edition
-      , upgrades.version_upgrade_count
-      , upgrades.edition_upgrade_count
-      , server_daily_details.account_sfid   AS last_account_sfid
-      , server_daily_details.license_id1    AS last_license_id1
-      , server_daily_details.license_id2    AS last_license_id2
-      , licenses.first_paid_license_date
-      , licenses.first_trial_license_date
-      , server_details.first_active_date
-      , server_details.last_active_date
-      , server_details.first_telemetry_active_date
-      , server_details.last_telemetry_active_date
-      , server_details.max_active_user_count
-      , server_details.last_active_user_date
-      , sau.dau_total
-      , sau.mobile_dau
-      , sau.mau_total
-      , sau.first_time_mau
-      , sau.reengaged_mau
-      , sau.current_mau
-      , sau.total_events
-      , sau.desktop_events
-      , sau.web_app_events
-      , sau.mobile_events
-      , sau.events_alltime
-      , sau.mobile_events_alltime
-      , sau.users
-      , server_details.last_active_license_date
-      , nps.nps_users
-      , nps.nps_score
-      , nps.promoters
-      , nps.detractors
-      , nps.passives
-      , nps.avg_score                   AS avg_nps_user_score
+      , MAX(server_daily_details.version)                 AS version
+      , MAX(server_details.first_server_version)          AS first_server_version
+      , MAX(fse.edition)                                  AS server_edition
+      , MAX(fse.first_server_edition)                     AS first_server_edition
+      , MAX(upgrades.version_upgrade_count)               AS version_upgrade_count
+      , MAX(upgrades.edition_upgrade_count)               AS edition_upgrade_count
+      , MAX(server_daily_details.account_sfid)            AS last_account_sfid
+      , MAX(server_daily_details.license_id1)             AS last_license_id1
+      , MAX(server_daily_details.license_id2)             AS last_license_id2
+      , MAX(licenses.first_paid_license_date)             AS first_paid_license_date
+      , MAX(licenses.first_trial_license_date)            AS first_trial_license_date
+      , MAX(server_details.first_active_date)             AS first_active_date
+      , MAX(server_details.last_active_date)              AS last_active_date
+      , MAX(server_details.first_telemetry_active_date)   AS first_telemetry_active_date
+      , MAX(server_details.last_telemetry_active_date)    AS last_telemetry_active_date
+      , MAX(server_details.max_active_user_count)         AS max_active_user_count
+      , MAX(server_details.last_active_user_date)         AS last_telemetry_active_user_date
+      , MAX(sau.last_event_date)                          AS last_event_active_user_date
+      , MAX(sau.dau_total)                                AS dau_total
+      , MAX(sau.mobile_dau)                               AS mobile_dau
+      , MAX(sau.mau_total)                                AS mau_total
+      , MAX(sau.first_time_mau)                           AS first_time_mau
+      , MAX(sau.reengaged_mau)                            AS reengaged_mau
+      , MAX(sau.current_mau)                              AS current_mau
+      , MAX(sau.total_events)                             AS total_events
+      , MAX(sau.desktop_events)                           AS desktop_events
+      , MAX(sau.web_app_events)                           AS web_app_events
+      , MAX(sau.mobile_events)                            AS mobile_events
+      , MAX(sau.events_alltime)                           AS events_alltime
+      , MAX(sau.mobile_events_alltime)                    AS mobile_events_alltime
+      , MAX(sau.users)                                    AS users
+      , MAX(server_details.last_active_license_date)      AS last_active_license_date
+      , MAX(nps.nps_users)                                AS nps_users
+      , MAX(nps.nps_score)                                AS nps_score
+      , MAX(nps.promoters)                                AS promoters
+      , MAX(nps.detractors)                               AS detractors
+      , MAX(nps.passives)                                 AS passives
+      , MAX(nps.avg_score)                                AS avg_nps_user_score
     FROM server_details
         JOIN {{ ref('server_daily_details') }}
             ON server_details.server_id = server_daily_details.server_id
-            AND server_details.last_active_date = server_daily_details.date
+            AND (server_details.last_active_date = server_daily_details.date
+            OR server_details.last_active_date - INTERVAL '1 DAY' = server_daily_details.date)
         LEFT JOIN {{ ref('nps_server_daily_score') }} nps
             ON server_details.server_id = nps.server_id
             AND nps.date = DATE_TRUNC('day', CURRENT_DATE - INTERVAL '1 DAY')
@@ -126,7 +142,7 @@ WITH server_details AS (
             ON server_details.server_id = fse.server_id
         LEFT JOIN licenses
             ON server_details.server_id = licenses.server_id
-        {{ dbt_utils.group_by(n=38) }}
+        {{ dbt_utils.group_by(n=1) }}
     )
 SELECT *
 FROM server_fact
