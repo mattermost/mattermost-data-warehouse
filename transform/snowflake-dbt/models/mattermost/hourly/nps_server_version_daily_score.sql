cec9bdf52c50d6fca0d9066138cdb86533a8dea7
Eric <43073688+enelson720@users.noreply.github.com>
2020-04-22 07:40:10 -0700

    Nps server (#213)
    * Adding nps_server_monthly_score model and schema.yml reference.
    
    * Adding nps scores to server fact.
    
    * Fixed table alias reference.
    
    * Fixed parentheses.
    
    * Added additional fields including response counts and feedback.
    
    * Added updates to server_fact table.
    
    * Fixed syntax issue.
    
    * Fixed incorrect column reference.
    
    * Adding nps daily score.
    
    * Updating nps user monthly score model to daily for joinning purposes at user and server level.
    
    * Updating nps server monthly score to daily level.
    
    * fixing server fact nps server monthly score referece.
    
    * fixing user_daily_details user nps score model reference.
    
    * Adding server edition and first server edition.
    
    * Adding first paid and trial license dates.
    
    * Fixing syntax issue.
    
    * Adding additional logic to server fact.
    
    * Adding first edition date.
    
    * Adding first edition date.
    
    * Adding daily server version nps data model.
    
    * Adding posts to server_events model.
    
    * Adding posts to server_events model.
    
    * Adding posts to server_events model.
    
    * Removing excludable servers filter from security table.
    
    * Adding first_mm2_telemetry_date.
    
    * Adding nps_server_version_daily_score.
    
    * Adding nps_server_version_daily_score to schema file.
    
    * Adding nps_server_version_daily_score to schema file.
    
    * Adding nps_server_version_daily_score.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

---
+++ transform/snowflake-dbt/models/mattermost/hourly/nps_server_version_daily_score.sql
@@ -0,0 +1,81 @@
+{{config({
+    "materialized": 'incremental',
+    "unique_key": 'id',
+    "schema": "mattermost"
+  })
+}}
+
+WITH nps_data                       AS (
+    SELECT
+        last_score_date
+      , server_id
+      , user_id
+      , server_version
+      , score
+      , license_id
+      , license_sku
+      , promoter_type
+      , user_created_at
+      , server_install_date
+      , responses_alltime
+      , user_role
+      , MAX(feedback)               AS feedback
+      , MAX(last_feedback_date)     AS last_feedback_date
+    FROM {{ ref('nps_user_daily_score') }}
+    {{ dbt_utils.group_by(n=12) }}
+                                       ),
+
+     min_nps_by_version             AS (
+         SELECT
+             server_id
+           , user_id
+           , server_version
+           , MIN(last_score_date) AS min_version_nps_date
+         FROM nps_data
+         {{ dbt_utils.group_by(n=3) }}
+     ),
+
+     nps_server_vesion              AS (
+         SELECT
+             d.date
+           , nps.server_id
+           , nps.user_id
+           , nps.server_version
+         FROM {{ source('util', 'dates') }}              d
+              JOIN min_nps_by_version nps
+                   ON d.date >= nps.min_version_nps_date
+                       AND d.date <= CURRENT_DATE
+     ),
+     nps_server_version_daily_score AS (
+         SELECT
+             n1.date
+           , n2.server_id
+           , n2.user_id
+           , n2.user_role
+           , n2.last_score_date
+           , n2.server_version
+           , n2.score
+           , n2.license_id
+           , n2.license_sku
+           , n2.promoter_type
+           , n2.user_created_at
+           , n2.server_install_date
+           , n2.responses_alltime
+           , n2.feedback
+           , n2.last_feedback_date
+           , {{ dbt_utils.surrogate_key('n1.date', 'n2.user_id', 'n2.server_id', 'n2.server_version') }} AS id
+         FROM nps_server_vesion n1
+              JOIN nps_data     n2
+                   ON n1.server_id = n2.server_id
+                       AND n1.user_id = n2.user_id
+                       AND n1.server_version = n2.server_version
+                       AND n1.date >= n2.last_score_date
+        {% if is_incremental() %}
+
+        WHERE n1.date >= (SELECT MAX(date) FROM {{this}})
+
+        {% endif %}
+        {{ dbt_utils.group_by(n=16) }}
+     )
+     SELECT *
+     FROM nps_server_version_daily_score
