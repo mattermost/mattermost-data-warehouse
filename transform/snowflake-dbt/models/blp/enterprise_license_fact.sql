a1e58ca2a0d7f1a0024fcf83334246c57f552611
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-04-28 21:54:58 -0700

    updates

--- transform/snowflake-dbt/models/blp/enterprise_license_fact.sql
+++ transform/snowflake-dbt/models/blp/enterprise_license_fact.sql
@@ -6,22 +6,25 @@
 
 WITH enterprise_license_fact AS (
     SELECT 
-        account_sfid,
-        licenseid,
-        expiresat,
-        last_license_telemetry_date,
-        SUM(license_daily_details.license_server_dau)/COUNT(license_daily_details.date) AS current_rolling_7day_avg_dau,
-        SUM(license_daily_details.license_server_mau)/COUNT(license_daily_details.date) AS current_rolling_7day_avg_mau,
-        MAX(license_daily_details.license_server_dau) AS current_max_license_server_dau,
-        MAX(license_daily_details.license_server_mau) AS current_max_license_server_mau,
-        SUM(license_daily_details.servers)/COUNT(license_daily_details.date) as current_ago_avg_servers,
-        MAX(license_daily_details.servers) as current_ago_max_servers,
-        SUM(month_ago.license_server_dau)/COUNT(month_ago.date) AS month_ago_rolling_7day_avg_dau,
-        SUM(month_ago.license_server_mau)/COUNT(month_ago.date) AS month_ago_rolling_7day_avg_mau,
-        MAX(month_ago.license_server_dau) AS month_ago_max_license_server_dau,
-        MAX(month_ago.license_server_mau) AS month_ago_max_license_server_mau,
-        SUM(month_ago.servers)/COUNT(month_ago.date) as month_ago_avg_servers,
-        MAX(month_ago.servers) as month_ago_max_servers
+        enterprise_license_mapping.account_sfid,
+        enterprise_license_mapping.licenseid,
+        license_daily_details.start_date,
+        enterprise_license_mapping.expiresat::date as expiresat,
+        license_daily_details.last_license_telemetry_date,
+        MAX(COALESCE(license_daily_details.license_users,license_daily_details.customer_users))::int AS current_max_licensed_users,
+        AVG(license_daily_details.license_server_dau)::int AS current_rolling_7day_avg_dau,
+        AVG(license_daily_details.license_server_mau)::int AS current_rolling_7day_avg_mau,
+        MAX(license_daily_details.license_server_dau)::int AS current_max_license_server_dau,
+        MAX(license_daily_details.license_server_mau)::int AS current_max_license_server_mau,
+        ROUND(AVG(license_daily_details.servers),1) as current_avg_servers,
+        MAX(license_daily_details.servers)::int as current_max_servers,
+        MAX(COALESCE(month_ago.license_users,month_ago.customer_users))::int AS month_ago_max_licensed_users,
+        AVG(month_ago.license_server_dau)::int AS month_ago_rolling_7day_avg_dau,
+        AVG(month_ago.license_server_mau)::int AS month_ago_rolling_7day_avg_mau,
+        MAX(month_ago.license_server_dau)::int AS month_ago_max_license_server_dau,
+        MAX(month_ago.license_server_mau)::int AS month_ago_max_license_server_mau,
+        ROUND(AVG(month_ago.servers),1) as month_ago_avg_servers,
+        MAX(month_ago.servers)::int as month_ago_max_servers
     FROM {{ ref('enterprise_license_mapping') }}
     LEFT JOIN {{ ref('license_daily_details') }} ON enterprise_license_mapping.licenseid = license_daily_details.license_id
         AND license_daily_details.date >= CURRENT_DATE - INTERVAL '8 days'
@@ -30,7 +33,7 @@ WITH enterprise_license_fact AS (
         AND month_ago.date >= CURRENT_DATE - INTERVAL '38 days'
         AND month_ago.date < CURRENT_DATE - INTERVAL '30 days'
     WHERE enterprise_license_mapping.expiresat >= CURRENT_DATE
-    GROUP BY 1, 2, 3, 4
+    GROUP BY 1, 2, 3, 4, 5
 )
 
 SELECT * FROM enterprise_license_fact
--- transform/snowflake-dbt/models/blp/enterprise_license_mapping.sql
+++ transform/snowflake-dbt/models/blp/enterprise_license_mapping.sql
@@ -32,6 +32,6 @@ WITH enterprise_license_mapping AS (
     LEFT JOIN {{ source('licenses', 'licenses') }} ON opportunity.license_key__c = licenses.licenseid
     WHERE NOT EXISTS (SELECT 1 FROM {{ source('staging','hist_license_mapping')}} WHERE hist_license_mapping.licenseid = opportunity.license_key__c)
         AND opportunity.license_key__c IS NOT NULL
-    GROUP BY 1, 2, 3, 4, 5, 6
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8
 )
 select * from enterprise_license_mapping
