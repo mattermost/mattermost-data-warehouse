76c2eb9531c4d9a00b493065b1f9d24f71a18c03
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-04-30 09:51:04 -0700

    updates after testing

--- transform/snowflake-dbt/models/orgm/opportunity_ext.sql
+++ transform/snowflake-dbt/models/orgm/opportunity_ext.sql
@@ -4,8 +4,7 @@
   })
 }}
 
-WITH w_end_date AS
- (
+WITH w_end_date AS (
   SELECT
         opportunityid as opportunity_sfid,
         min(end_date__c) as min_end_date,
@@ -14,8 +13,8 @@ WITH w_end_date AS
   FROM {{ source('orgm', 'opportunitylineitem') }}
   WHERE product_type__c='Recurring'
   GROUP BY 1
-), w_start_date AS
- ( SELECT
+), w_start_date AS ( 
+  SELECT
         opportunityid as opportunity_sfid,
         min(start_date__c) as min_start_date,
         max(start_date__c) as max_start_date,
@@ -23,42 +22,42 @@ WITH w_end_date AS
   FROM {{ source('orgm', 'opportunitylineitem') }}
   WHERE product_type__c='Recurring'
   GROUP BY 1
-), w_oppt_commit AS
- ( SELECT
-        o.sfid as opportunity_sfid,
-        min(ofh.createddate) as first_commit_date,
-        max(ofh.createddate) as last_commit_date,
-        count(distinct(to_char(ofh.createddate,'YYYY-MM'))) as num_times_slipped
-   FROM {{ source('orgm', 'opportunityfieldhistory') }} ofh
-   LEFT JOIN {{ source('orgm', 'opportunityfieldhistory') }} o ON (ofh.opportunityid = o.sfid)
-   WHERE field = 'ForecastCategoryName'
-   AND newvalue = 'Commit'
-   AND o.e_purchase_date__c IS NULL
-   GROUP BY 1
+), w_oppt_commit AS ( 
+  SELECT
+       o.sfid as opportunity_sfid,
+       min(ofh.createddate) as first_commit_date,
+       max(ofh.createddate) as last_commit_date,
+       count(distinct(to_char(ofh.createddate,'YYYY-MM'))) as num_times_slipped
+  FROM {{ source('orgm', 'opportunityfieldhistory') }} ofh
+  LEFT JOIN {{ source('orgm', 'opportunity') }} o ON (ofh.opportunityid = o.sfid)
+  WHERE ofh.field = 'ForecastCategoryName'
+  AND ofh.newvalue = 'Commit'
+  AND o.e_purchase_date__c IS NULL
+  GROUP BY 1
 ), opportunity_ext AS (
-   SELECT
-        opportunity.sfid as opportunity_sfid,
-        opportunity.accountid as accountid,
-        min_end_date,
-        max_end_date,
-        num_diff_end_dates,
-        min_start_date,
-        max_start_date,
-        num_diff_start_dates,
-        first_commit_date,
-        last_commit_date,
-        num_times_slipped,
-        SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'New' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_new_amount,
-        SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_expansion_amount,
-        SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' AND is_prorated_expansion__c IS NULL THEN totalprice WHEN is_prorated_expansion__c IS NOT NULL THEN 12 * totalprice/term_months__c ELSE 0 END) AS sum_expansion_w_proration_amount,
-        SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Ren' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_renewal_amount,
-        SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Multi' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_multi_amount
-    FROM {{ source('orgm', 'opportunity') }}
-    LEFT JOIN {{ source('orgm', 'opportunitylineitem') }} ON opportunity.sfid = opportunitylineitem.opportunityid
-    LEFT JOIN w_end_date ON opportunity.sfid = w_end_date.opportunity_sfid
-    LEFT JOIN w_start_date ON opportunity.sfid = w_start_date.opportunity_sfid
-    LEFT JOIN w_oppt_commit ON opportunity.sfid = w_oppt_commit.opportunity_sfid
-    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+  SELECT
+      opportunity.sfid as opportunity_sfid,
+      opportunity.accountid as accountid,
+      min_end_date,
+      max_end_date,
+      num_diff_end_dates,
+      min_start_date,
+      max_start_date,
+      num_diff_start_dates,
+      first_commit_date,
+      last_commit_date,
+      num_times_slipped,
+      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'New' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_new_amount,
+      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_expansion_amount,
+      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' AND is_prorated_expansion__c IS NULL THEN totalprice WHEN is_prorated_expansion__c IS NOT NULL THEN 12 * totalprice/term_months__c ELSE 0 END) AS sum_expansion_w_proration_amount,
+      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Ren' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_renewal_amount,
+      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Multi' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_multi_amount
+  FROM {{ source('orgm', 'opportunity') }}
+  LEFT JOIN {{ source('orgm', 'opportunitylineitem') }} ON opportunity.sfid = opportunitylineitem.opportunityid
+  LEFT JOIN w_end_date ON opportunity.sfid = w_end_date.opportunity_sfid
+  LEFT JOIN w_start_date ON opportunity.sfid = w_start_date.opportunity_sfid
+  LEFT JOIN w_oppt_commit ON opportunity.sfid = w_oppt_commit.opportunity_sfid
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
 )
 
  SELECT * FROM opportunity_ext
