f213078eb7025f2e92c0211015bd3abba329c804
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-05-04 19:46:09 -0700

    add new logic for > 5k proration

--- transform/snowflake-dbt/models/orgm/opportunity_ext.sql
+++ transform/snowflake-dbt/models/orgm/opportunity_ext.sql
@@ -49,7 +49,7 @@ WITH w_end_date AS (
       num_times_slipped,
       SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'New' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_new_amount,
       SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_expansion_amount,
-      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' AND is_prorated_expansion__c IS NULL THEN totalprice WHEN is_prorated_expansion__c IS NOT NULL THEN 12 * totalprice/term_months__c ELSE 0 END) AS sum_expansion_w_proration_amount,
+      SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Expansion' AND is_prorated_expansion__c IS NULL THEN totalprice WHEN is_prorated_expansion__c IS NOT NULL THEN 365 * totalprice/(end_date__c::date - start_date__c::date + 1) ELSE 0 END) AS sum_expansion_w_proration_amount,
       SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Ren' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_renewal_amount,
       SUM(CASE WHEN opportunitylineitem.product_line_type__c = 'Multi' THEN opportunitylineitem.totalprice ELSE 0 END) AS sum_multi_amount
   FROM {{ source('orgm', 'opportunity') }}
