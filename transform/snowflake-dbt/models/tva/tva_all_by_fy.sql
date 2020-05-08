c60e38eeb77a4462b27c3936a1d339e8fa5b3027
Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
2020-05-07 21:27:12 -0700

    update tva

--- transform/snowflake-dbt/models/tva/tva_all_by_fy.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_fy.sql
@@ -34,6 +34,11 @@ WITH tva_all_by_fy AS (
   SELECT target_slug,fy,period_first_day,period_last_day,target,actual,tva
   FROM {{ ref('tva_downloads_by_fy') }}
 
+  UNION ALL
+
+  SELECT target_slug,month,period_first_day,period_last_day,target,actual,tva
+  FROM {{ ref('tva_bookings_new_and_exp_by_rep_by_fy') }}
+
 )
 
 SELECT * FROM tva_all_by_fy
--- transform/snowflake-dbt/models/tva/tva_all_by_mo.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_mo.sql
@@ -52,6 +52,12 @@ WITH tva_all_by_mo AS (
 
   SELECT target_slug,month,period_first_day,period_last_day,target,actual,tva
   FROM {{ ref('tva_downloads_by_mo') }}
+
+  UNION ALL
+
+  SELECT target_slug,month,period_first_day,period_last_day,target,actual,tva
+  FROM {{ ref('tva_bookings_new_and_exp_by_rep_by_mo') }}
+
 )
 
 SELECT * FROM tva_all_by_mo
