f8a434568e96a6f60745e72218a6e96b9fb24216
Rachel Bradley-Haas <rachel@mattermost.com>
2020-05-11 16:36:09 -0700

    add to tva_all tables

--- transform/snowflake-dbt/models/tva/tva_all_by_qtr.sql
+++ transform/snowflake-dbt/models/tva/tva_all_by_qtr.sql
@@ -44,6 +44,11 @@ WITH tva_all_by_qtr AS (
   SELECT target_slug,qtr,period_first_day,period_last_day,target,actual,tva
   FROM {{ ref('tva_attain_new_and_exp_by_segment_by_qtr') }}
 
+  UNION ALL
+
+  SELECT target_slug,qtr,period_first_day,period_last_day,target,actual,tva
+  FROM {{ ref('tva_bookings_ren_by_segment_by_qtr') }}
+
 )
 
 SELECT * FROM tva_all_by_qtr
