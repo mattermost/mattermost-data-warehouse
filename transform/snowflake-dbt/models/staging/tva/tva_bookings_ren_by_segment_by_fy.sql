a38ce6e098409b5a2778bc7e3fd16b393bb2b73f
rbradleyhaas <rachel.bradley-haas@mattermost.com>
2020-05-11 16:34:36 -0700

    TvA Renewal by Segment (#236)
    * update tva
    
    * readd?
    
    * remove
    
    * add tva_by_segment
    
    * fixes
    
    * updates
    
    * update tva stuff
    
    * rename to attainment
    
    * updates for tva
    
    Co-authored-by: Rachel Bradley-Haas <rachelbradleyhaas@Rachels-MacBook-Pro.local>
    Co-authored-by: Rachel Bradley-Haas <rachel@mattermost.com>

---
+++ transform/snowflake-dbt/models/staging/tva/tva_bookings_ren_by_segment_by_fy.sql
@@ -0,0 +1,20 @@
+{{config({
+    "materialized": 'table',
+    "schema": "staging"
+  })
+}}
+
+WITH tva_bookings_ren_by_segment_by_fy AS (
+    SELECT
+        REPLACE(target_slug,'_qtr_','_fy_') AS target_slug,
+        util.fiscal_year(tva_bookings_ren_by_segment_by_qtr.period_first_day) AS fy,
+        min(period_first_day) AS period_first_day,
+        max(period_last_day) AS period_last_day,
+        sum(tva_bookings_ren_by_segment_by_qtr.target) AS target,
+        sum(tva_bookings_ren_by_segment_by_qtr.actual) AS actual,
+        round(sum(tva_bookings_ren_by_segment_by_qtr.actual)/sum(tva_bookings_ren_by_segment_by_qtr.target),2) AS tva
+    FROM {{ ref('tva_bookings_ren_by_segment_by_qtr') }}
+    GROUP BY 1,2
+)
+
+SELECT * FROM tva_bookings_ren_by_segment_by_fy
