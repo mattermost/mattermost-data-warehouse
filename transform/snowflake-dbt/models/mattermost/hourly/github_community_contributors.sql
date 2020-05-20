644ea06945add230b9087ee2d83d46015e3fac11
Rachel Bradley-Haas <rachel@mattermost.com>
2020-05-19 17:56:50 -0700

    updates

---
+++ transform/snowflake-dbt/models/mattermost/hourly/github_community_contributors.sql
@@ -0,0 +1,21 @@
+{{config({
+    "materialized": 'table',
+    "schema": "mattermost"
+  })
+}}
+
+WITH github_community_contributors AS (
+    SELECT 
+        author, 
+        MIN(merged_at) AS min_contribution, 
+        MAX(merged_at) AS max_contribution,
+        COUNT(*) AS total_contributions
+    FROM {{ source('mattermost', 'github_contributions_all') }}
+    LEFT JOIN {{ source('mattermost', 'github_repo_categorization')}} ON github_contributions_all.repo = github_repo_categorization.repo
+    LEFT JOIN {{ source('employee', 'staff_github_usernames')}} ON github_contributions_all.author = staff_github_usernames.username
+    WHERE github_repo_categorization.repo IN ('community') AND staff_github_usernames.username IS NULL
+    GROUP BY 1
+)
+
+SELECT * FROM github_community_contributors
+
