98caed49447ee80a8e5e8b1df8e39fb91e347445
Rachel Bradley-Haas <rachel@mattermost.com>
2020-05-22 15:23:29 -0700

    removed from job

--- dags/general/pg_job.py
+++ dags/general/pg_job.py
@@ -60,4 +60,4 @@ opportunity_snapshot = get_container_operator(
 )
 heroku_connect_retry = get_container_operator("heroku-connect-retry", "connect_retry")
 
-tasks_filtered >> account_arr >> account_type >> owner_segment_updates >> lead_account >> update_opportunitylineitem_amounts >> opportunity_snapshot >> opportunitylineitem_snapshot >> heroku_connect_retry
+tasks_filtered >> account_arr >> account_type >> owner_segment_updates >> lead_account >> opportunity_snapshot >> opportunitylineitem_snapshot >> heroku_connect_retry
