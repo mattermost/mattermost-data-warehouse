6d25b81348411ee1277f44785ce3190208c0818a
Rachel Bradley-Haas <rachel@mattermost.com>
2020-05-27 15:53:45 -0700

    remove PG jobs

---
+++ transform/snowflake-dbt/models/orgm/opportunitylineitem_snapshot.sql
@@ -0,0 +1,37 @@
+
+{{config({
+    "materialized": "incremental",
+    "unique_key": 'id',
+    "schema": "orgm"
+  })
+}}
+
+WITH opportunitylineitem_snapshot AS (
+    SELECT
+        {{ dbt_utils.surrogate_key('current_date', 'sfid') }} AS id,
+        current_date AS date,
+        sfid,
+        name,
+        opportunityid,
+        product2id,
+        product_type__c,
+        description,
+        listprice,
+        unitprice,
+        discount,
+        quantity,
+        totalprice,
+        start_date__c,
+        end_date__c
+        arr_contributed__c,
+        revenue_type__c,
+        product_line_type__c,
+        new_amount__c,
+        renewal_amount__c,
+        expansion_amount__c,
+        coterm_expansion_amount__c,
+        leftover_expansion_amount__c,
+        multi_amount__c
+        FROM {{ source('orgm', 'opportunitylineitem') }}
+)
+SELECT * FROM opportunitylineitem_snapshot
