c3ab16f33d15ad028795f0e09b8934a647ebe205
Rachel Bradley-Haas <rachel@mattermost.com>
2020-05-28 16:12:09 -0700

    No, I don't want no scrubs A scrub is a guy that can't get no love from me

--- transform/snowflake-dbt/models/staging/tva/tva_bookings_ren_by_qtr.sql
+++ transform/snowflake-dbt/models/staging/tva/tva_bookings_ren_by_qtr.sql
@@ -4,23 +4,13 @@
   })
 }}
 
-WITH bookings_ren AS (
+WITH actual_bookings_ren_by_qtr AS (
     SELECT
-      opportunity.closedate,
-      opportunity.sfid AS opportunity_sfid,
-      opportunitylineitem.sfid AS opportunitylineitem_sfid,
-      opportunity.type AS opportunity_type,
-      opportunitylineitem.product_line_type__c AS product_line_type,
-      CASE WHEN end_date__c::date - start_date__c::date + 1 > 365 THEN opportunitylineitem.arr_contributed__c ELSE opportunitylineitem.totalprice END AS bookings
+      util.fiscal_year(bookings_ren.closedate)|| '-' || util.fiscal_quarter(bookings_ren.closedate) AS qtr,
+      ROUND(SUM(renewal_amount__c)) AS actual
     FROM {{ source('orgm', 'opportunity') }} AS opportunity
     LEFT JOIN {{ source('orgm', 'opportunitylineitem') }} AS opportunitylineitem ON opportunity.sfid = opportunitylineitem.opportunityid
     WHERE iswon AND opportunitylineitem.product_line_type__c = 'Ren'
-), actual_bookings_ren_by_qtr AS (
-    SELECT 
-        util.fiscal_year(bookings_ren.closedate)|| '-' || util.fiscal_quarter(bookings_ren.closedate) AS qtr,
-        sum(bookings) AS total_bookings
-    FROM bookings_ren
-    GROUP BY 1
 ), tva_bookings_ren_by_qtr AS (
     SELECT
         'bookings_ren_by_qtr' AS target_slug,
