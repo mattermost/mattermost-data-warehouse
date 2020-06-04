be84f3892ce3b59bbe84e16c26941781f516a44f
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-06-04 07:40:42 -0700

    Updated to chronological sequence logic.

--- utils/update_chronological_sequence.py
+++ utils/update_chronological_sequence.py
@@ -12,12 +12,13 @@ def update_chronological_sequence():
         seconds_after_prev_event = a.seconds_after_prev_event
     FROM (
         SELECT id,
+            updated_at,
             ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY min_timestamp) as chronological_sequence,
             datediff(second, lag(min_timestamp) over (partition by user_id order by min_timestamp), min_timestamp) as seconds_after_prev_event
         FROM ANALYTICS.EVENTS.USER_EVENTS_BY_DATE
-        WHERE user_id in (SELECT user_id from analytics.events.user_events_by_date where updated_at = (SELECT MAX(UPDATED_AT) FROM analytics.events.user_events_by_date) GROUP BY 1)
+        WHERE user_id in (SELECT user_id from analytics.events.user_events_by_date where updated_at >= (SELECT MAX(UPDATED_AT) - interval '30 minutes' FROM analytics.events.user_events_by_date) GROUP BY 1)
     ) a
-    WHERE user_events_by_date.updated_at::timestamp = (SELECT MAX(UPDATED_AT)::timestamp FROM analytics.events.user_events_by_date)
+    WHERE user_events_by_date.updated_at >= (SELECT MAX(UPDATED_AT) - interval '30 minutes' FROM analytics.events.user_events_by_date)
     AND a.id = user_events_by_date.id;
     '''
 
