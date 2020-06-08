4b440bc3eb9bbaef0c5580b72cbb7a39d6b037a3
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-06-08 09:10:11 -0700

    Updating excludable server_logic.

---
+++ transform/snowflake-dbt/models/mattermost/nightly/excludable_servers.sql
@@ -0,0 +1,46 @@
+{{config({
+    "materialized": 'incremental',
+    "schema": "events",
+    "unique_key":'server_id'
+  })
+}}
+
+WITH seed_file AS (
+    SELECT *
+    FROM {{ ref('excludable_servers') }}
+),
+
+license_exclusions AS (
+    SELECT
+        s.server_id
+      , 'internal_license' AS reason
+    FROM {{ ref('server_fact') }} s
+    JOIN {{ ref('licenses') }} l
+        ON s.server_id = l.server_id
+        AND trim(l.company) in (
+                            'Mattermost Developers',
+                            'Mattermost SA - INTERNAL',
+                            'Mattermost  Inc.',
+                            'Mattermost Cloud',
+                            'Mattermost'
+                            )
+    LEFT JOIN seed_file sf
+        ON s.server_id = sf.server_id
+    WHERE sf.server_id is null
+    GROUP BY 1, 2
+),
+
+excludable_servers AS (
+    SELECT *
+    FROM seed_file
+    UNION ALL
+    SELECT *
+    FROM license_exclusions
+)
+SELECT *
+FROM excludable_servers
+{% if is_incremental() %}
+
+WHERE server_id NOT IN (SELECT server_id from {{THIS}})
+
+{% endif %}
