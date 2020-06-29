e935d31ec206f51bd85ab9e690b6db23d9989ce2
Alex Dovenmuehle <alex.dovenmuehle@mattermost.com>
2020-06-29 15:11:45 -0400

    Add ability to run one off SQL files against snowflake.

---
+++ dags/general/snowflake_job.py
@@ -0,0 +1,67 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from dags.airflow_utils import (
+    DATA_IMAGE,
+    clone_repo_cmd,
+    mm_failed_task,
+    pod_defaults,
+    pod_env_vars,
+    xs_warehouse,
+)
+from dags.kube_secrets import (
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+)
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+env_vars = {**pod_env_vars, **{}}
+
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "owner": "airflow",
+    "on_failure_callback": mm_failed_task,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "sla": timedelta(hours=8),
+    "start_date": datetime(2019, 1, 1, 0, 0, 0),
+}
+
+# Create the DAG
+dag = DAG("snowflake_job", default_args=default_args, schedule_interval="None")
+
+
+def get_container_operator(task_name, job_name, schema):
+    cmd = f"""
+        {clone_repo_cmd} &&
+        cd mattermost-data-warehouse &&
+        export PYTHONPATH="/opt/bitnami/airflow/dags/git/mattermost-data-warehouse/:$PYTHONPATH" &&
+        python utils/run_snowflake_queries.py {job_name} TRANSFORMER {schema}
+    """
+    return KubernetesPodOperator(
+        **pod_defaults,
+        image=DATA_IMAGE,
+        task_id=f"snowflake-{task_name}",
+        name=task_name,
+        secrets=[
+            SNOWFLAKE_ACCOUNT,
+            SNOWFLAKE_USER,
+            SNOWFLAKE_PASSWORD,
+            SNOWFLAKE_TRANSFORM_ROLE,
+            SNOWFLAKE_TRANSFORM_WAREHOUSE,
+            SNOWFLAKE_TRANSFORM_SCHEMA,
+        ],
+        arguments=[cmd],
+        dag=dag,
+    )
+
+
+test = get_container_operator("tester", "test", "staging")
