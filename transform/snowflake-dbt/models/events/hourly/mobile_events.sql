2446054f9eb0e644187057519ed021f8c48cf0d5
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-07-13 08:48:17 -0700

    Fixing mobile_events_telemetry logic.

--- transform/snowflake-dbt/models/events/hourly/mobile_events.sql
+++ transform/snowflake-dbt/models/events/hourly/mobile_events.sql
@@ -5,5 +5,5 @@
   })
 }}
 
-{% set rudder_relations = get_rudder_relations('mm_mobile_prod', 'RAW') %}
+{% set rudder_relations = get_rudder_relations(schema=["mm_mobile_prod"], database='RAW') %}
 {{ union_relations(relations = rudder_relations) }}
