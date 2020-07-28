a4382e04fe73a6e7e6fbd55d50db71a7f3ae0878
rbradleyhaas <rachel.bradley-haas@mattermost.com>
2020-07-28 16:12:20 -0700

    Update update_account_arr_and_seats.sql

--- transform/sql/update_account_arr_and_seats.sql
+++ transform/sql/update_account_arr_and_seats.sql
@@ -1,6 +1,6 @@
 UPDATE orgm.account
     SET arr_current__c = orgm_account_arr_and_seats.total_arr,
-        seats_licenses__c = orgm_account_arr_and_seats.seats
+        seats_licensed__c = orgm_account_arr_and_seats.seats
 FROM staging.orgm_account_arr_and_seats
 WHERE orgm_account_arr_and_seats.account_sfid = account.sfid
-    AND (orgm_account_arr_and_seats.total_arr,orgm_account_arr_and_seats.seats) IS DISTINCT FROM (account.arr_current__c,account.seats_licenses__c);
+    AND (orgm_account_arr_and_seats.total_arr,orgm_account_arr_and_seats.seats) IS DISTINCT FROM (account.arr_current__c,account.seats_licensed__c);
