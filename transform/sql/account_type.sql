128028093b430c350c4ac9015f3a4b48359a9a99
Rachel Bradley-Haas <rachel@mattermost.com>
2020-07-28 16:02:26 -0700

    updates

--- transform/sql/account_type.sql
+++ transform/sql/account_type.sql
@@ -1,5 +1,3 @@
-BEGIN;
-
 UPDATE orgm.account
 SET type = 'Customer (Attrited)',
     seats_active_mau__c = NULL,
@@ -8,7 +6,12 @@ SET type = 'Customer (Attrited)',
     seats_active_latest__c = NULL,
     latest_telemetry_date__c = NULL
 FROM (
-    SELECT account.sfid, COALESCE(account.arr_current__c,0) AS account_arr, SUM(COALESCE(child.arr_current__c,0)) AS child_arr
+    SELECT 
+        account.sfid, 
+        COALESCE(account.arr_current__c,0) AS account_arr, 
+        SUM(COALESCE(child.arr_current__c,0)) AS child_arr,
+        COALESCE(account.seats_licensed__c,0) AS account_seats, 
+        SUM(COALESCE(child.seats_licensed__c,0)) AS child_seats 
     FROM orgm.account
     LEFT JOIN orgm.account AS child ON child.parentid = account.sfid
     GROUP BY 1, 2
@@ -16,18 +19,24 @@ FROM (
 WHERE account.sfid = account_arr_details.sfid
     AND account_arr_details.account_arr = 0
     AND account_arr_details.child_arr = 0
+    AND account_arr_details.account_seats = 0
+    AND account_arr_details.child_seats = 0
     AND type = 'Customer';
 
 UPDATE orgm.account
 SET type = 'Customer'
 FROM (
-    SELECT account.sfid, COALESCE(account.arr_current__c,0) AS account_arr, SUM(COALESCE(child.arr_current__c,0)) AS child_arr
+    SELECT 
+        account.sfid, 
+        COALESCE(account.arr_current__c,0) AS account_arr, 
+        SUM(COALESCE(child.arr_current__c,0)) AS child_arr,
+        COALESCE(account.seats_licensed__c,0) AS account_seats, 
+        SUM(COALESCE(child.seats_licensed__c,0)) AS child_seats
     FROM orgm.account
     LEFT JOIN orgm.account AS child ON child.parentid = account.sfid
     GROUP BY 1, 2
 ) AS account_arr_details
 WHERE account.sfid = account_arr_details.sfid
-    AND (account_arr_details.account_arr != 0 OR account_arr_details.child_arr != 0)
+    AND (account_arr_details.account_arr != 0 OR account_arr_details.child_arr != 0 
+        OR account_arr_details.account_seats != 0 OR account_arr_details.child_seats != 0)
     AND type IS DISTINCT FROM 'Customer';
-
-COMMIT;
