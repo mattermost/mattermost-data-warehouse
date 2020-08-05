697e0cfb4cc14f8572751cd55da783e696f52dca
Eric <43073688+enelson720@users.noreply.github.com>
2020-08-05 12:27:55 -0700

    Looker NPS Data Action Updates (#366)
    * Adding rudder web traffic.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Adding web traffic and user agent registry rudder data.
    
    * Updating user_agent_parser.py.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating max active user count logic.
    
    * Updating where conditions.
    
    * Coalescing user_id with uuid_string.
    
    * Coalescing user_id with uuid_string.
    
    * First reg users date thresholds.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Adding chronological sequence script to update.
    
    * Adding chronological sequence script to update.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Updates to user_events_by_date_agg.
    
    * Updates to user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Fixing user_events_by_date_agg data model logic.
    
    * updating user_daily_details logic to omit uuid user_ids
    
    * Addihg bak event registry.
    
    * Removing uuid users.
    
    * Removing uuid users.
    
    * Updating user_fact table.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Fixing merge conflict.
    
    * Fixing merge conflict.
    
    * Adding nps categorization update script.
    
    * Adding nps categorization update script.
    
    * Adding nps categorization update script.
    
    * Fixing NPS Plugin Test Data Model.
    
    * Fixing NPS Plugin Test Data Model.
    
    * Fixing NPS Data Action.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/sql/snowflake/data_action_nps_feedback_category_update.sql
+++ transform/sql/snowflake/data_action_nps_feedback_category_update.sql
@@ -1,20 +1,20 @@
 MERGE INTO analytics.mattermost.nps_feedback_classification
     USING (
         SELECT 
-            date
-          , id
+            last_feedback_date
           , user_id
           , server_id
+          , feedback
           , field
           , new_value as category
         FROM (
                 SELECT 
                     ROW_NUMBER() OVER 
-                        (PARTITION BY PARSE_JSON(other_params):date, PARSE_JSON(other_params):id, field ORDER BY triggered_at DESC) AS row_num
+                        (PARTITION BY PARSE_JSON(other_params):last_feedback_date, PARSE_JSON(other_params):user_id, PARSE_JSON(other_params):server_id, PARSE_JSON(other_params):feedback, field ORDER BY triggered_at DESC) AS row_num
                   , PARSE_JSON(other_params):user_id::varchar AS user_id
                   , PARSE_JSON(other_params):server_id::varchar AS server_id
-                  , PARSE_JSON(other_params):date::date AS date
-                  , PARSE_JSON(other_params):id::varchar AS id
+                  , PARSE_JSON(other_params):last_feedback_date::date AS last_feedback_date
+                  , PARSE_JSON(other_params):feedback::varchar AS feedback
                   , field
                   , new_value
                 FROM zapier_data_actions.data
@@ -23,11 +23,14 @@ MERGE INTO analytics.mattermost.nps_feedback_classification
         WHERE row_num = 1
         GROUP BY 1, 2, 3, 4, 5, 6
     ) AS recent_updates
-    ON nps_feedback_classification.id = recent_updates.id
+    ON nps_feedback_classification.last_feedback_date = recent_updates.last_feedback_date
+        AND nps_feedback_classification.user_id = recent_updates.user_id
+        AND nps_feedback_classification.server_id = recent_updates.server_id
+        AND nps_feedback_classification.feedback = recent_updates.feedback
 WHEN MATCHED THEN UPDATE
     SET nps_feedback_classification.category = recent_updates.category
 WHEN NOT MATCHED THEN
-    INSERT(date, server_id, user_id, id, category) values (recent_updates.date, recent_updates.server_id, recent_updates.user_id, recent_updates.id, recent_updates.category);
+    INSERT(date, server_id, user_id, feedback, category) values (recent_updates.date, recent_updates.server_id, recent_updates.user_id, recent_updates.feedback, recent_updates.category);
 
 UPDATE zapier_data_actions.data
 SET dwh_processed_at = current_timestamp()
--- transform/sql/snowflake/data_action_nps_feedback_subcategory_update.sql
+++ transform/sql/snowflake/data_action_nps_feedback_subcategory_update.sql
@@ -2,20 +2,20 @@ UPDATE analytics.mattermost.nps_feedback_classification
     SET nps_feedback_classification.subcategory = recent_updates.subcategory
     FROM (
         SELECT 
-            date
-          , id
+            last_feedback_date
           , user_id
           , server_id
+          , feedback
           , field
           , new_value as subcategory
         FROM (
                 SELECT 
                     ROW_NUMBER() OVER 
-                        (PARTITION BY PARSE_JSON(other_params):date, PARSE_JSON(other_params):id, field ORDER BY triggered_at DESC) AS row_num
+                        (PARTITION BY PARSE_JSON(other_params):last_feedback_date, PARSE_JSON(other_params):user_id, PARSE_JSON(other_params):server_id, PARSE_JSON(other_params):feedback, field ORDER BY triggered_at DESC) AS row_num
                   , PARSE_JSON(other_params):user_id::varchar AS user_id
                   , PARSE_JSON(other_params):server_id::varchar AS server_id
-                  , PARSE_JSON(other_params):date::date AS date
-                  , PARSE_JSON(other_params):id::varchar AS id
+                  , PARSE_JSON(other_params):last_feedback_date::date AS last_feedback_date
+                  , PARSE_JSON(other_params):feedback::varchar AS feedback
                   , field
                   , new_value
                 FROM zapier_data_actions.data
@@ -24,7 +24,10 @@ UPDATE analytics.mattermost.nps_feedback_classification
         WHERE row_num = 1
         GROUP BY 1, 2, 3, 4, 5, 6
     ) AS recent_updates
-    WHERE nps_feedback_classification.id = recent_updates.id;
+    where nps_feedback_classification.last_feedback_date = recent_updates.last_feedback_date
+        AND nps_feedback_classification.user_id = recent_updates.user_id
+        AND nps_feedback_classification.server_id = recent_updates.server_id
+        AND nps_feedback_classification.feedback = recent_updates.feedback;
 
 UPDATE zapier_data_actions.data
 SET dwh_processed_at = current_timestamp()
