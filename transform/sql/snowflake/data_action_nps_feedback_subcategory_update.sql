4a61903d478be91e494fb7476bd55de6864d9c09
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-08-05 07:50:26 -0700

    Adding nps categorization update script.

--- transform/sql/snowflake/data_action_nps_feedback_subcategory_update.sql
+++ transform/sql/snowflake/data_action_nps_feedback_subcategory_update.sql
@@ -1,25 +1,28 @@
-UPDATE mattermost.nps_feedback_classification
+UPDATE analytics.mattermost.nps_feedback_classification
     SET nps_feedback_classification.subcategory = recent_updates.subcategory
     FROM (
-        SELECT date,
-            id,
-            user_id,
-            server_id,
-            field,
-            new_value as subcategory
+        SELECT 
+            date
+          , id
+          , user_id
+          , server_id
+          , field
+          , new_value as subcategory
         FROM (
-                SELECT ROW_NUMBER()OVER (PARTITION BY PARSE_JSON(other_params):date, PARSE_JSON(other_params):id, field ORDER BY triggered_at DESC) AS row_num,
-                        PARSE_JSON(other_params):user_id::varchar AS user_id,
-                        PARSE_JSON(other_params):server_id::varchar AS server_id,
-                        PARSE_JSON(other_params):date::date AS date,
-                        PARSE_JSON(other_params):id::varchar AS id,
-                        field,
-                        new_value
+                SELECT 
+                    ROW_NUMBER() OVER 
+                        (PARTITION BY PARSE_JSON(other_params):date, PARSE_JSON(other_params):id, field ORDER BY triggered_at DESC) AS row_num
+                  , PARSE_JSON(other_params):user_id::varchar AS user_id
+                  , PARSE_JSON(other_params):server_id::varchar AS server_id
+                  , PARSE_JSON(other_params):date::date AS date
+                  , PARSE_JSON(other_params):id::varchar AS id
+                  , field
+                  , new_value
                 FROM zapier_data_actions.data
                 WHERE table_name = 'nps_feedback_classification' AND field IN ('subcategory') AND dwh_processed_at IS NULL
             )
         WHERE row_num = 1
-        GROUP BY 1, 2
+        GROUP BY 1, 2, 3, 4, 5, 6
     ) AS recent_updates
     WHERE nps_feedback_classification.id = recent_updates.id;
 
