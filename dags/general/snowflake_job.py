aca8021cf5cf61f997089bc2086c8f1e379c93be
Eric <43073688+enelson720@users.noreply.github.com>
2020-08-18 09:29:35 -0700

    Update snowflake_job.py

--- dags/general/snowflake_job.py
+++ dags/general/snowflake_job.py
@@ -71,8 +71,8 @@ scrub_updates = get_container_operator(
     "scrub-updates", "data_action_scrub_update", "sales"
 )
 
-automated_nps_category_updates = get_container_operator(
-    "automated-nps-category-updates", "automated_nps_feedback_category_update", "mattermost"
+automated-nps-feedback-category-update = get_container_operator(
+    "automated-nps-feedback-category-update", "automated_nps_feedback_category_update", "mattermost"
 )
 
 nps_category_updates = get_container_operator(
@@ -83,4 +83,4 @@ nps_subcategory_updates = get_container_operator(
     "nps-subcategory-updates", "data_action_nps_feedback_subcategory_update", "mattermost"
 )
 
-scrub_updates >> automated-nps-category-updates >> nps_subcategory_updates >> nps_category_updates
+scrub_updates >> automated_nps_feedback_category_update >> nps_subcategory_updates >> nps_category_updates
