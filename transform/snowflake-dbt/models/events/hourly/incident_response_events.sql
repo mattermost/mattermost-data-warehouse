8cfa34ccc026db6464d3698ade77efd13616f1a9
Eric <43073688+enelson720@users.noreply.github.com>
2020-08-26 11:45:56 -0700

    Incident response & License Server Fact Updates (#396)
    * Adding rudder web traffic.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Adding web traffic and user agent registry rudder data.
    
    * Updating user_agent_parser.py.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating max active user count logic.
    
    * Updating where conditions.
    
    * Coalescing user_id with uuid_string.
    
    * Coalescing user_id with uuid_string.
    
    * First reg users date thresholds.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Adding chronological sequence script to update.
    
    * Adding chronological sequence script to update.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Updates to user_events_by_date_agg.
    
    * Updates to user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Fixing user_events_by_date_agg data model logic.
    
    * updating user_daily_details logic to omit uuid user_ids
    
    * Addihg bak event registry.
    
    * Removing uuid users.
    
    * Removing uuid users.
    
    * Updating user_fact table.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Fixing merge conflict.
    
    * Fixing merge conflict.
    
    * Adding incident response data model.
    
    * Adding license_server_fact fix.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

---
+++ transform/snowflake-dbt/models/events/hourly/incident_response_events.sql
@@ -0,0 +1,10 @@
+{{config({
+    "materialized": "incremental",
+    "schema": "events",
+    "tags":"hourly",
+    "unique_key":"id"
+  })
+}}
+
+{% set rudder_relations = get_rudder_relations(schema=["incident_response_prod"], database='RAW') %}
+{{ union_relations(relations = rudder_relations) }}
