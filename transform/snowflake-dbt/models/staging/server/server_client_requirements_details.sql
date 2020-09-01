a2ce39193a6f0cf32cb667f039aa559131480762
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-09-01 06:55:47 -0700

    Adding rudder telemetry to first half of server telemerty from diagnostics.go

--- transform/snowflake-dbt/models/staging/server/server_client_requirements_details.sql
+++ transform/snowflake-dbt/models/staging/server/server_client_requirements_details.sql
@@ -5,7 +5,7 @@
   })
 }}
 
-WITH max_timestamp               AS (
+WITH max_segment_timestamp               AS (
     SELECT
         timestamp::DATE AS date
       , user_id
@@ -20,34 +20,64 @@ WITH max_timestamp               AS (
     {% endif %}
     GROUP BY 1, 2
 ),
+
+max_rudder_timestamp               AS (
+    SELECT
+        timestamp::DATE AS date
+      , user_id
+      , MAX(timestamp)  AS max_timestamp
+    FROM {{ source('mm_telemetry_prod', 'config_client_requirements') }}
+    WHERE timestamp::DATE <= CURRENT_DATE
+    {% if is_incremental() %}
+
+        -- this filter will only be applied on an incremental run
+        AND timestamp::date >= (SELECT MAX(date) FROM {{ this }})
+
+    {% endif %}
+    GROUP BY 1, 2
+),
      server_client_requirements_details AS (
          SELECT
-             timestamp::DATE                           AS date
-           , ccr.user_id                               AS server_id
-           , MAX(allow_edit_post)                      AS allow_edit_post
-           , MAX(android_latest_version)               AS android_latest_version
-           , MAX(android_min_version)                  AS android_min_version
-           , MAX(cluster_log_timeout_milliseconds)     AS cluster_log_timeout_milliseconds
-           , MAX(desktop_latest_version)               AS desktop_latest_version
-           , MAX(desktop_min_version)                  AS desktop_min_version
-           , MAX(enable_apiv3)                         AS enable_apiv3
-           , MAX(enable_channel_viewed_messages)       AS enable_channel_viewed_messages
-           , MAX(enable_commands)                      AS enable_commands
-           , MAX(enable_custom_emoji)                  AS enable_custom_emoji
-           , MAX(enable_developer)                     AS enable_developer
-           , MAX(enable_emoji_picker)                  AS enable_emoji_picker
-           , MAX(enable_incoming_webhooks)             AS enable_incoming_webhooks
-           , MAX(enable_insecure_outgoing_connections) AS enable_insecure_outgoing_connections
-           , MAX(enable_multifactor_authentication)    AS enable_multifactor_authentication
-           , MAX(enable_oauth_service_provider)        AS enable_oauth_service_provider
-           , MAX(enable_only_admin_integrations)       AS enable_only_admin_integrations
-           , MAX(ios_latest_version)                   AS ios_latest_version
-           , MAX(ios_min_version)                      AS ios_min_version
-           , {{ dbt_utils.surrogate_key('timestamp::date', 'ccr.user_id') }} AS id
-         FROM {{ source('mattermost2', 'config_client_requirements') }} ccr
-              JOIN max_timestamp              mt
-                   ON ccr.user_id = mt.user_id
-                       AND mt.max_timestamp = ccr.timestamp
+             COALESCE(s.timestamp::DATE, r.timestamp::date)              AS date
+           , COALESCE(s.user_id, r.user_id)                              AS server_id
+           , MAX(COALESCE(s.allow_edit_post, NULL))                      AS allow_edit_post
+           , MAX(COALESCE(s.android_latest_version, r.android_latest_version))               AS android_latest_version
+           , MAX(COALESCE(s.android_min_version, r.android_min_version))                  AS android_min_version
+           , MAX(COALESCE(s.cluster_log_timeout_milliseconds, NULL))     AS cluster_log_timeout_milliseconds
+           , MAX(COALESCE(s.desktop_latest_version, r.desktop_latest_version))               AS desktop_latest_version
+           , MAX(COALESCE(s.desktop_min_version, r.desktop_min_version))                  AS desktop_min_version
+           , MAX(COALESCE(s.enable_apiv3, NULL))                         AS enable_apiv3
+           , MAX(COALESCE(s.enable_channel_viewed_messages, NULL))       AS enable_channel_viewed_messages
+           , MAX(COALESCE(s.enable_commands, NULL))                      AS enable_commands
+           , MAX(COALESCE(s.enable_custom_emoji, NULL))                  AS enable_custom_emoji
+           , MAX(COALESCE(s.enable_developer, NULL))                     AS enable_developer
+           , MAX(COALESCE(s.enable_emoji_picker, NULL))                  AS enable_emoji_picker
+           , MAX(COALESCE(s.enable_incoming_webhooks, NULL))             AS enable_incoming_webhooks
+           , MAX(COALESCE(s.enable_insecure_outgoing_connections, NULL)) AS enable_insecure_outgoing_connections
+           , MAX(COALESCE(s.enable_multifactor_authentication, NULL))    AS enable_multifactor_authentication
+           , MAX(COALESCE(s.enable_oauth_service_provider, NULL))        AS enable_oauth_service_provider
+           , MAX(COALESCE(s.enable_only_admin_integrations, NULL))       AS enable_only_admin_integrations
+           , MAX(COALESCE(s.ios_latest_version, r.ios_latest_version))                   AS ios_latest_version
+           , MAX(COALESCE(s.ios_min_version, r.ios_min_version))                      AS ios_min_version
+           , {{ dbt_utils.surrogate_key('COALESCE(s.timestamp::DATE, r.timestamp::date)', 'COALESCE(s.user_id, r.user_id)') }} AS id
+           FROM 
+            (
+              SELECT s.*
+              FROM {{ source('mattermost2', 'config_client_requirements') }} s
+              JOIN max_segment_timestamp        mt
+                   ON s.user_id = mt.user_id
+                       AND mt.max_timestamp = s.timestamp
+            ) s
+          FULL OUTER JOIN
+            (
+              SELECT r.*
+              FROM {{ source('mm_telemetry_prod', 'config_client_requirements') }} r
+              JOIN max_rudder_timestamp mt
+                  ON r.user_id = mt.user_id
+                    AND mt.max_timestamp = r.timestamp
+            ) r
+            ON s.timestamp::date = r.timestamp::date
+            AND s.user_id = r.user_id
          GROUP BY 1, 2
      )
 SELECT *
--- transform/snowflake-dbt/models/staging/server/server_file_details.sql
+++ transform/snowflake-dbt/models/staging/server/server_file_details.sql
@@ -5,7 +5,7 @@
   })
 }}
 
-WITH max_timestamp       AS (
+WITH max_segment_timestamp       AS (
     SELECT
         timestamp::DATE AS date
       , user_id
@@ -20,33 +20,64 @@ WITH max_timestamp       AS (
     {% endif %}
     GROUP BY 1, 2
 ),
+
+max_rudder_timestamp       AS (
+    SELECT
+        timestamp::DATE AS date
+      , user_id
+      , MAX(timestamp)  AS max_timestamp
+    FROM {{ source('mm_telemetry_prod', 'config_file') }}
+    WHERE timestamp::DATE <= CURRENT_DATE
+    {% if is_incremental() %}
+
+        -- this filter will only be applied on an incremental run
+        AND timestamp::date >= (SELECT MAX(date) FROM {{ this }})
+
+    {% endif %}
+    GROUP BY 1, 2
+),
+
      server_file_details AS (
          SELECT
-             timestamp::DATE              AS date
-           , f.user_id                    AS server_id
-           , MAX(amazon_s3_signv2)        AS amazon_s3_signv2
-           , MAX(amazon_s3_sse)           AS amazon_s3_sse
-           , MAX(amazon_s3_ssl)           AS amazon_s3_ssl
-           , MAX(amazon_s3_trace)         AS amazon_s3_trace
-           , MAX(driver_name)             AS driver_name
-           , MAX(enable_file_attachments) AS enable_file_attachments
-           , MAX(enable_mobile_download)  AS enable_mobile_download
-           , MAX(enable_mobile_upload)    AS enable_mobile_upload
-           , MAX(enable_public_links)     AS enable_public_links
-           , MAX(isabsolute_directory)    AS isabsolute_directory
-           , MAX(isdefault_directory)     AS isdefault_directory
-           , MAX(max_file_size)           AS max_file_size
-           , MAX(preview_height)          AS preview_height
-           , MAX(preview_width)           AS preview_width
-           , MAX(profile_height)          AS profile_height
-           , MAX(profile_width)           AS profile_width
-           , MAX(thumbnail_height)        AS thumbnail_height
-           , MAX(thumbnail_width)         AS thumbnail_width
-           , {{ dbt_utils.surrogate_key('timestamp::date', 'f.user_id') }} AS id
-         FROM {{ source('mattermost2', 'config_file') }} f
-              JOIN max_timestamp      mt
-                   ON f.user_id = mt.user_id
-                       AND mt.max_timestamp = f.timestamp
+             COALESCE(s.timestamp::DATE, r.timestamp::date)              AS date
+           , COALESCE(s.user_id, r.user_id)                              AS server_id
+           , MAX(COALESCE(s.amazon_s3_signv2, r.amazon_s3_signv2))        AS amazon_s3_signv2
+           , MAX(COALESCE(s.amazon_s3_sse, r.amazon_s3_sse))           AS amazon_s3_sse
+           , MAX(COALESCE(s.amazon_s3_ssl, r.amazon_s3_ssl))           AS amazon_s3_ssl
+           , MAX(COALESCE(s.amazon_s3_trace, r.amazon_s3_trace))         AS amazon_s3_trace
+           , MAX(COALESCE(s.driver_name, r.driver_name))             AS driver_name
+           , MAX(COALESCE(s.enable_file_attachments, r.enable_file_attachments)) AS enable_file_attachments
+           , MAX(COALESCE(s.enable_mobile_download, r.enable_mobile_download))  AS enable_mobile_download
+           , MAX(COALESCE(s.enable_mobile_upload, r.enable_mobile_upload))    AS enable_mobile_upload
+           , MAX(COALESCE(s.enable_public_links, r.enable_public_links))     AS enable_public_links
+           , MAX(COALESCE(s.isabsolute_directory, r.isabsolute_directory))    AS isabsolute_directory
+           , MAX(COALESCE(s.isdefault_directory, r.isdefault_directory))     AS isdefault_directory
+           , MAX(COALESCE(s.max_file_size, r.max_file_size))           AS max_file_size
+           , MAX(COALESCE(s.preview_height, NULL))          AS preview_height
+           , MAX(COALESCE(s.preview_width, NULL))           AS preview_width
+           , MAX(COALESCE(s.profile_height, NULL))          AS profile_height
+           , MAX(COALESCE(s.profile_width, NULL))           AS profile_width
+           , MAX(COALESCE(s.thumbnail_height, NULL))        AS thumbnail_height
+           , MAX(COALESCE(s.thumbnail_width, NULL))         AS thumbnail_width
+           , {{ dbt_utils.surrogate_key('COALESCE(s.timestamp::DATE, r.timestamp::date)', 'COALESCE(s.user_id, r.user_id)') }} AS id
+           FROM 
+            (
+              SELECT s.*
+              FROM {{ source('mattermost2', 'config_file') }} s
+              JOIN max_segment_timestamp        mt
+                   ON s.user_id = mt.user_id
+                       AND mt.max_timestamp = s.timestamp
+            ) s
+          FULL OUTER JOIN
+            (
+              SELECT r.*
+              FROM {{ source('mm_telemetry_prod', 'config_file') }} r
+              JOIN max_rudder_timestamp mt
+                  ON r.user_id = mt.user_id
+                    AND mt.max_timestamp = r.timestamp
+            ) r
+            ON s.timestamp::date = r.timestamp::date
+            AND s.user_id = r.user_id
          GROUP BY 1, 2
      )
 SELECT *
