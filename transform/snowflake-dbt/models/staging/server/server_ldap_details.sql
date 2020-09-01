ca661855d68d83be4f7ea69881ec31fb8e286bac
Eric <43073688+enelson720@users.noreply.github.com>
2020-09-01 07:04:13 -0700

    Qa test (#404)
    * Adding rudder web traffic.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Adding web traffic and user agent registry rudder data.
    
    * Updating user_agent_parser.py.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating max active user count logic.
    
    * Updating where conditions.
    
    * Coalescing user_id with uuid_string.
    
    * Coalescing user_id with uuid_string.
    
    * First reg users date thresholds.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Adding chronological sequence script to update.
    
    * Adding chronological sequence script to update.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Updates to user_events_by_date_agg.
    
    * Updates to user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Fixing user_events_by_date_agg data model logic.
    
    * updating user_daily_details logic to omit uuid user_ids
    
    * Addihg bak event registry.
    
    * Removing uuid users.
    
    * Removing uuid users.
    
    * Updating user_fact table.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Fixing merge conflict.
    
    * Fixing merge conflict.
    
    * Adding portal events to qa web & desktop events data model.
    
    * Adding rudder telemetry to first half of server telemerty from diagnostics.go
    
    * Adding additional column to server_daily_details_ext.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/staging/server/server_ldap_details.sql
+++ transform/snowflake-dbt/models/staging/server/server_ldap_details.sql
@@ -5,7 +5,23 @@
   })
 }}
 
-WITH max_timestamp       AS (
+WITH max_segment_timestamp                AS (
+    SELECT
+        timestamp::DATE AS date
+      , user_id
+      , MAX(timestamp)  AS max_timestamp
+    FROM {{ source('mattermost2', 'config_ldap') }}
+    WHERE timestamp::DATE <= CURRENT_DATE
+    {% if is_incremental() %}
+
+        -- this filter will only be applied on an incremental run
+        AND timestamp::date >= (SELECT MAX(date) FROM {{ this }})
+
+    {% endif %}
+    GROUP BY 1, 2
+),
+
+max_rudder_timestamp                AS (
     SELECT
         timestamp::DATE AS date
       , user_id
@@ -22,39 +38,53 @@ WITH max_timestamp       AS (
 ),
      server_ldap_details AS (
          SELECT
-             timestamp::DATE                             AS date
-           , l.user_id                                   AS server_id
-           , MAX(connection_security)                    AS connection_security
-           , MAX(enable)                                 AS enable_ldap
-           , MAX(enable_admin_filter)                    AS enable_admin_filter
-           , MAX(enable_sync)                            AS enable_sync
-           , MAX(isdefault_email_attribute)              AS isdefault_email_attribute
-           , MAX(isdefault_first_name_attribute)         AS isdefault_first_name_attribute
-           , MAX(isdefault_group_display_name_attribute) AS isdefault_group_display_name_attribute
-           , MAX(isdefault_group_id_attribute)           AS isdefault_group_id_attribute
-           , MAX(isdefault_id_attribute)                 AS isdefault_id_attribute
-           , MAX(isdefault_last_name_attribute)          AS isdefault_last_name_attribute
-           , MAX(isdefault_login_button_border_color)    AS isdefault_login_button_border_color
-           , MAX(isdefault_login_button_color)           AS isdefault_login_button_color
-           , MAX(isdefault_login_button_text_color)      AS isdefault_login_button_text_color
-           , MAX(isdefault_login_field_name)             AS isdefault_login_field_name
-           , MAX(isdefault_login_id_attribute)           AS isdefault_login_id_attribute
-           , MAX(isdefault_nickname_attribute)           AS isdefault_nickname_attribute
-           , MAX(isdefault_position_attribute)           AS isdefault_position_attribute
-           , MAX(isdefault_username_attribute)           AS isdefault_username_attribute
-           , MAX(isempty_admin_filter)                   AS isempty_admin_filter
-           , MAX(isempty_group_filter)                   AS isempty_group_filter
-           , MAX(isempty_guest_filter)                   AS isempty_guest_filter
-           , MAX(max_page_size)                          AS max_page_size
-           , MAX(query_timeout)                          AS query_timeout
-           , MAX(segment_dedupe_id)                      AS segment_dedupe_id
-           , MAX(skip_certificate_verification)          AS skip_certificate_verification
-           , MAX(sync_interval_minutes)                  AS sync_interval_minutes
-           , {{ dbt_utils.surrogate_key('timestamp::date', 'l.user_id') }} AS id
-         FROM {{ source('mattermost2', 'config_ldap') }} l
-              JOIN max_timestamp      mt
-                   ON l.user_id = mt.user_id
-                       AND mt.max_timestamp = l.timestamp
+             COALESCE(s.timestamp::DATE, r.timestamp::date)              AS date
+           , COALESCE(s.user_id, r.user_id)                              AS server_id
+           , MAX(COALESCE(s.connection_security, r.connection_security))                    AS connection_security
+           , MAX(COALESCE(s.enable, r.enable))                                 AS enable_ldap
+           , MAX(COALESCE(s.enable_admin_filter, r.enable_admin_filter))                    AS enable_admin_filter
+           , MAX(COALESCE(s.enable_sync, r.enable_sync))                            AS enable_sync
+           , MAX(COALESCE(s.isdefault_email_attribute, r.isdefault_email_attribute))              AS isdefault_email_attribute
+           , MAX(COALESCE(s.isdefault_first_name_attribute, r.isdefault_first_name_attribute))         AS isdefault_first_name_attribute
+           , MAX(COALESCE(s.isdefault_group_display_name_attribute, r.isdefault_group_display_name_attribute)) AS isdefault_group_display_name_attribute
+           , MAX(COALESCE(s.isdefault_group_id_attribute, r.isdefault_group_id_attribute))           AS isdefault_group_id_attribute
+           , MAX(COALESCE(s.isdefault_id_attribute, r.isdefault_id_attribute))                 AS isdefault_id_attribute
+           , MAX(COALESCE(s.isdefault_last_name_attribute, r.isdefault_last_name_attribute))          AS isdefault_last_name_attribute
+           , MAX(COALESCE(s.isdefault_login_button_border_color, r.isdefault_login_button_border_color))    AS isdefault_login_button_border_color
+           , MAX(COALESCE(s.isdefault_login_button_color, r.isdefault_login_button_color))           AS isdefault_login_button_color
+           , MAX(COALESCE(s.isdefault_login_button_text_color, r.isdefault_login_button_text_color))      AS isdefault_login_button_text_color
+           , MAX(COALESCE(s.isdefault_login_field_name, r.isdefault_login_field_name))             AS isdefault_login_field_name
+           , MAX(COALESCE(s.isdefault_login_id_attribute, r.isdefault_login_id_attribute))           AS isdefault_login_id_attribute
+           , MAX(COALESCE(s.isdefault_nickname_attribute, r.isdefault_nickname_attribute))           AS isdefault_nickname_attribute
+           , MAX(COALESCE(s.isdefault_position_attribute, r.isdefault_position_attribute))           AS isdefault_position_attribute
+           , MAX(COALESCE(s.isdefault_username_attribute, r.isdefault_username_attribute))           AS isdefault_username_attribute
+           , MAX(COALESCE(s.isempty_admin_filter, r.isempty_admin_filter))                   AS isempty_admin_filter
+           , MAX(COALESCE(s.isempty_group_filter, r.isempty_group_filter))                   AS isempty_group_filter
+           , MAX(COALESCE(s.isempty_guest_filter, r.isempty_guest_filter))                   AS isempty_guest_filter
+           , MAX(COALESCE(s.max_page_size, r.max_page_size))                          AS max_page_size
+           , MAX(COALESCE(s.query_timeout, r.query_timeout))                          AS query_timeout
+           , MAX(COALESCE(s.segment_dedupe_id, NULL))                      AS segment_dedupe_id
+           , MAX(COALESCE(s.skip_certificate_verification, r.skip_certificate_verification))          AS skip_certificate_verification
+           , MAX(COALESCE(s.sync_interval_minutes, r.sync_interval_minutes))                  AS sync_interval_minutes
+           , {{ dbt_utils.surrogate_key('COALESCE(s.timestamp::DATE, r.timestamp::date)', 'COALESCE(s.user_id, r.user_id)') }} AS id
+           FROM 
+            (
+              SELECT s.*
+              FROM {{ source('mattermost2', 'config_ldap') }} s
+              JOIN max_segment_timestamp        mt
+                   ON s.user_id = mt.user_id
+                       AND mt.max_timestamp = s.timestamp
+            ) s
+          FULL OUTER JOIN
+            (
+              SELECT r.*
+              FROM {{ source('mm_telemetry_prod', 'config_ldap') }} r
+              JOIN max_rudder_timestamp mt
+                  ON r.user_id = mt.user_id
+                    AND mt.max_timestamp = r.timestamp
+            ) r
+            ON s.timestamp::date = r.timestamp::date
+            AND s.user_id = r.user_id
          GROUP BY 1, 2
      )
 SELECT *
