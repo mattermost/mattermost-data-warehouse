427a187990a826c4c7fc9173fbec54794fd89372
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2020-09-02 14:11:44 -0700

    updates

--- transform/sql/update_account_telemetry.sql
+++ transform/sql/update_account_telemetry.sql
@@ -4,7 +4,7 @@ UPDATE orgm.account
         seats_active_mau__c = orgm_account_telemetry.mau,
         seats_active_max__c = CASE WHEN orgm_account_telemetry.dau > COALESCE(account.seats_active_max__c,0) THEN orgm_account_telemetry.dau ELSE account.seats_active_max__c END,
         latest_telemetry_date__c = orgm_account_telemetry.last_telemetry_date,
-        server_version__c = server_version
+        server_version__c = orgm_account_telemetry.server_version
 FROM staging.orgm_account_telemetry
 WHERE orgm_account_telemetry.account_sfid = account.sfid
     AND seats_active_override__c = FALSE
