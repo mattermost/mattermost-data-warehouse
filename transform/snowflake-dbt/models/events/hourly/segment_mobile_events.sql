aa5cee6bd9981b0326f856ce4cba38dd84e793a4
Eric <43073688+enelson720@users.noreply.github.com>
2020-09-11 14:58:35 -0700

    Cloud fix (#426)
    * Adding rudder web traffic.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Updating sources for website data models.
    
    * Adding web traffic and user agent registry rudder data.
    
    * Updating user_agent_parser.py.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating user events with rudder event telemetry.
    
    * Updating max active user count logic.
    
    * Updating where conditions.
    
    * Coalescing user_id with uuid_string.
    
    * Coalescing user_id with uuid_string.
    
    * First reg users date thresholds.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Event chronological sequencing.
    
    * Adding chronological sequence script to update.
    
    * Adding chronological sequence script to update.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Adding first event details to user_fact model.
    
    * Updates to user_events_by_date_agg.
    
    * Updates to user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Commenting out user_events_by_date_agg.
    
    * Fixing user_events_by_date_agg data model logic.
    
    * updating user_daily_details logic to omit uuid user_ids
    
    * Addihg bak event registry.
    
    * Removing uuid users.
    
    * Removing uuid users.
    
    * Updating user_fact table.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Fixing chronological sequence update.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_fact table.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Updating user_events_by_date and chronological sequence update.
    
    * Fixing merge conflict.
    
    * Fixing merge conflict.
    
    * Fixing unique key.
    
    * server fact installation id and user event telemetry data model.
    
    * server fact installation id and user event telemetry data model.
    
    * server fact installation id and user event telemetry data model.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/events/hourly/segment_mobile_events.sql
+++ transform/snowflake-dbt/models/events/hourly/segment_mobile_events.sql
@@ -8,10 +8,13 @@
 SELECT
     segment.*
 FROM {{ source('mattermost_rn_mobile_release_builds_v2', 'event') }} segment
-     LEFT JOIN {{ ref('mobile_events') }} rudder
-        ON segment.user_actual_id = rudder.user_actual_id AND rudder.timestamp::DATE = segment.timestamp::DATE AND
-             rudder.type = segment.type
-WHERE rudder.user_actual_id is NULL
+     LEFT JOIN (
+                SELECT user_id, MIN(TIMESTAMP::DATE) AS MIN_DATE
+                FROM {{ ref('mobile_events') }}
+                GROUP BY 1
+              ) rudder
+        ON segment.user_id = rudder.user_id AND segment.timestamp::DATE >= rudder.MIN_DATE
+WHERE rudder.user_id is NULL
 AND segment.timestamp::date <= CURRENT_DATE
 AND segment.timestamp::date >= '2019-02-01'
 {% if is_incremental() %}
