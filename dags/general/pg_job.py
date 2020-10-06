bbe249a182a19486e538d0f65ade24a471610ee3
rbradleyhaas <rachel.bradley-haas@mattermost.com>
2020-10-06 13:41:54 -0700

    Handle Non-Actively Being Sequenced SCL-Outreach Leads (#473)
    * add registered users
    
    * updates
    
    * contrib update
    
    * update lead routing stuff
    
    Co-authored-by: Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>

--- dags/general/pg_job.py
+++ dags/general/pg_job.py
@@ -60,7 +60,11 @@ update_tasks_and_lead_connected = get_container_operator(
     "update-tasks-and-lead-connected", "update_tasks_and_lead_connected"
 )
 
+update_unsequenced_outreach_leads = get_container_operator(
+    "update-unsequenced-outreach-leads", "update_unsequenced_outreach_leads"
+)
+
 
 heroku_connect_retry = get_container_operator("heroku-connect-retry", "connect_retry")
 
-tasks_filtered >> account_type >> owner_segment_updates >> lead_account >> update_tasks_and_lead_connected >> update_opportunitylineitem_amounts >> pg_update_opportunity_time_in_stage >> heroku_connect_retry
+tasks_filtered >> account_type >> owner_segment_updates >> lead_account >> update_tasks_and_lead_connected >> update_unsequenced_outreach_leads >> update_opportunitylineitem_amounts >> pg_update_opportunity_time_in_stage >> heroku_connect_retry
