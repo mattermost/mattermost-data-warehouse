b4204833cbb7233af9f6b0475be5ff4f10666a09
Eric <43073688+enelson720@users.noreply.github.com>
2020-10-13 06:56:59 -0700

    License Server Fact (#476)
    * Updating union macro to omit high volume, low value events.
    
    * Updating union macro to omit high volume, low value events.
    
    * Updating server daily details logic back to nightly job.
    
    * Updating rudder and segment event jobs to omit high-volume low-value events.
    
    * Server fact activity fix.
    
    * License Daily Details update.
    
    * Adding new experiemental telemetry.
    
    * Adding new experiemental telemetry.
    
    * Adding additional customer-level aggregate server measures to license_server_fact.
    
    * Adding window for aggregate customer license and server measures.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/blp/license_server_fact.sql
+++ transform/snowflake-dbt/models/blp/license_server_fact.sql
@@ -27,7 +27,7 @@ with account_mapping as (
   LEFT JOIN {{ source('orgm', 'account') }} a
       ON elm.account_sfid = a.sfid
   GROUP BY 1, 2, 3, 4, 5
-), 
+),
 
 licensed_servers as (
 SELECT
@@ -93,7 +93,7 @@ nonactivated_licenses as (
   GROUP BY 1, 2, 3, 7, 8, 9, 10, 16, 17, 18
 ),
 
-license_server_fact as (
+license_union as (
   SELECT *
   FROM licensed_servers
   
@@ -101,8 +101,26 @@ license_server_fact as (
 
   SELECT *
   FROM nonactivated_licenses
-)
+),
 
+last_server_telemetry as (
+  SELECT 
+        server_id
+      , MAX(date) as max_date
+  FROM {{ ref('server_activity_details') }}
+  WHERE DATE <= CURRENT_DATE
+  GROUP BY 1
+),
+
+server_activity AS (
+  SELECT a1.*
+  FROM {{ ref('server_activity_details') }} a1
+  JOIN last_server_telemetry a2
+       ON a1.server_id = a2.server_id
+       AND a1.date = a2.max_date
+),
+
+license_server_fact as (
 SELECT 
         id
       , server_id
@@ -212,7 +230,8 @@ SELECT
       , license_activation_date
       , last_active_date
       , server_activation_date
-       , ROW_NUMBER() OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY trial DESC, license_activation_date desc) AS license_priority_rank
+      , ROW_NUMBER() OVER (PARTITION BY license_id ORDER BY LAST_ACTIVE_DATE NULLS LAST) AS LICENSE_RANK
+       , ROW_NUMBER() OVER (PARTITION BY coalesce(server_id, license_id) ORDER BY last_active_date desc NULLS LAST) AS license_priority_rank
        , CASE WHEN COALESCE((lead(start_date, 4) OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY issued_date))::DATE, expire_date::DATE + INTERVAL '1 DAY') <= expire_date 
             THEN (lead(start_date, 4) OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY issued_date))::DATE - interval '1 day'
           WHEN COALESCE((lead(start_date, 3) OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY issued_date))::DATE, expire_date::DATE + INTERVAL '1 DAY') <= expire_date 
@@ -222,7 +241,7 @@ SELECT
           WHEN COALESCE((lead(start_date, 1) OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY issued_date))::DATE, expire_date::DATE + INTERVAL '1 DAY') <= expire_date
             THEN (lead(start_date, 1) OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY issued_date))::DATE - interval '1 day'
           ELSE expire_date END as license_retired_date
-FROM license_server_fact
+FROM license_union
 {% if is_incremental() %}
 
 WHERE (
@@ -232,3 +251,80 @@ WHERE (
       )
 
 {% endif %}
+)
+
+SELECT 
+     l.id                      
+   , l.server_id               
+   , l.license_id              
+   , l.customer_id             
+   , l.customer_name           
+   , l.company                 
+   , l.edition                 
+   , l.users                   
+   , l.trial                   
+   , l.issued_date             
+   , l.start_date              
+   , l.expire_date             
+   , l.license_email           
+   , l.contact_sfid            
+   , l.account_sfid            
+   , l.account_name            
+   , l.opportunity_sfid        
+   , l.stripeid                
+   , l.license_customer_id     
+   , l.number                  
+   , l.license_activation_date 
+   , l.last_active_date        
+   , l.server_activation_date  
+   , l.license_priority_rank   
+   , l.license_retired_date
+   , activity.date as last_server_telemetry
+   , MAX(CASE WHEN license_priority_rank = 1 then activity.date else null end) OVER (PARTITION BY customer_id, trial) AS last_telemetry_date   
+   , SUM(COALESCE(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1  
+                    THEN activity.active_users 
+                    ELSE NULL END, 
+                  CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1  
+                    THEN activity.active_users_daily
+                    ELSE NULL END)) OVER (PARTITION BY customer_id, trial) AS active_users
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.active_users_monthly ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS monthly_active_users
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.bot_accounts ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS bot_accounts
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.bot_posts_previous_day ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS bot_posts_previous_day
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS'AND LICENSE_PRIORITY_RANK = 1 THEN activity.direct_message_channels ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS direct_message_channels
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.incoming_webhooks ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS incoming_webhooks
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.outgoing_webhooks ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS outgoing_webhooks
+   , SUM(CASE WHEN LICENSE_PRIORITY_RANK = 1 THEN activity.posts ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS posts
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.posts_previous_day ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS posts_previous_day
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.private_channels ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS private_channels
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.private_channels_deleted ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS private_channels_deleted
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.public_channels ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS public_channels
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.public_channels_deleted ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS public_channels_deleted
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1  THEN activity.registered_deactivated_users ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS registered_deactivated_users
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1  THEN activity.registered_inactive_users ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS registered_inactive_users
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.registered_users ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS registered_users
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.slash_commands ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS slash_commands
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.teams ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS teams
+   , SUM(CASE WHEN activity.date >= CURRENT_DATE - INTERVAL '7 DAYS' AND LICENSE_PRIORITY_RANK = 1 THEN activity.guest_accounts ELSE NULL END) 
+        OVER (PARTITION BY customer_id, trial) AS guest_accounts
+   , SUM(CASE WHEN LICENSE_PRIORITY_RANK = 1 AND LICENSE_RETIRED_DATE >= CURRENT_DATE AND LICENSE_RANK = 1 THEN l.users ELSE NULL END)
+        OVER (PARTITION BY customer_id, trial) AS customer_license_users
+FROM license_server_fact l
+LEFT JOIN server_activity activity
+    ON l.server_id = activity.server_id
