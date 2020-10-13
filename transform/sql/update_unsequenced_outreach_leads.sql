42f768df6cc5f91de62752efcc8bfe7ae698db99
rbradleyhaas <rachel.bradley-haas@mattermost.com>
2020-10-13 07:55:49 -0700

    Update update_unsequenced_outreach_leads.sql

--- transform/sql/update_unsequenced_outreach_leads.sql
+++ transform/sql/update_unsequenced_outreach_leads.sql
@@ -1,7 +1,7 @@
 UPDATE orgm.lead
 SET never_connected__c = now(),
     status = 'Recycled',
-    lead_status_minor__c = 'Never Connected'
+    recycle_reason__c = 'Never Connected'
 WHERE NOT actively_being_sequenced__c
     AND lead.status = 'SCL'
     AND lead.lead_status_minor__c = 'Outreach';
