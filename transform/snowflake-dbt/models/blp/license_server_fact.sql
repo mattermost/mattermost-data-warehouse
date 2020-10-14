ebf41ae7c6b822696a95d42b36791149e4f15b02
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-10-14 10:33:28 -0700

    Added cloud subscriptions to license billing model.

--- transform/snowflake-dbt/models/blp/license_server_fact.sql
+++ transform/snowflake-dbt/models/blp/license_server_fact.sql
@@ -120,6 +120,71 @@ server_activity AS (
        AND a1.date = a2.max_date
 ),
 
+max_sku AS (
+  SELECT DISTINCT
+      s1.subscription
+    , MAX(s1.plan_name) AS plan_name
+  FROM {{ ref('subscription_items')}} s1
+  JOIN (
+    SELECT
+        subscription
+      , MAX(plan_amount) as max_amount
+    FROM {{ ref('subscription_items')}}
+    GROUP BY 1
+  ) s2
+      ON s1.subscription = s2.subscription
+      AND s1.plan_amount = s2.max_amount
+  GROUP BY 1
+),
+
+cloud_subscriptions AS (
+  SELECT 
+      {{ dbt_utils.surrogate_key('sf.server_id', 
+         'COALESCE(sf.server_id, server.user_id)')}}               AS id
+    , COALESCE(sf.server_id, server.user_id)                       AS server_id
+    , s.cws_installation                                           AS license_id
+    , c.cws_customer                                               AS customer_id
+    , INITCAP(SPLIT_PART(replace(s.cws_dns, '-', ' '), '.', 1))    AS customer_name
+    , INITCAP(SPLIT_PART(replace(s.cws_dns, '-', ' '), '.', 1))    AS company
+    , COALESCE(ms.plan_name, 'Mattermost Cloud')                   AS edition
+    , s.quantity                                                   AS users
+    , FALSE                                                        AS trial
+    , s.created::DATE                                              AS issued_date
+    , COALESCE(MIN(sf.first_active_date), 
+               MIN(server.timestamp::date), 
+               MIN(s.current_period_start::DATE))                  AS start_date
+    , s.current_period_end::DATE                                   AS expire_date
+    , c.email                                                      AS license_email
+    , NULL                                                         AS contact_sfid
+    , NULL                                                         AS account_sfid
+    , NULL                                                         AS account_name
+    , NULL                                                         AS opportunity_sfid
+    , c.id                                                         AS stripeid
+    , c.cws_customer                                               AS license_customer_id
+    , NULL                                                         AS number
+    , s.created::TIMESTAMP                                         AS license_activation_date
+    , COALESCE(MAX(sf.last_active_date::TIMESTAMP), 
+        MAX(server.timestamp::TIMESTAMP))                          AS last_active_date
+    , COALESCE(MIN(sf.first_active_date::DATE),
+        MIN(server.timestamp::DATE))                               AS server_activation_date
+    , 1                                                            AS license_rank
+    , 1                                                            AS license_priority_rank
+    , s.current_period_end::DATE                                   AS license_retired_date
+  FROM {{ ref('subscriptions') }}             s
+        LEFT JOIN {{ ref('customers') }}       c
+                  ON s.customer = c.id
+        LEFT JOIN {{ ref('server_fact') }} sf
+                  ON s.cws_installation = sf.installation_id
+        LEFT JOIN {{ source('mm_telemetry_prod', 'server') }} server
+                  ON s.cws_installation = server.context_traits_installationid
+                      AND server.context_traits_installationid IS NOT NULL
+        LEFT JOIN max_sku ms
+                  ON s.id = ms.subscription
+  WHERE s.cws_installation IS NOT NULL
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
+  , 24, 25, 26
+),
+
 license_server_fact as (
 SELECT 
         id
@@ -227,7 +292,7 @@ SELECT
       , stripeid
       , customer_id   AS license_customer_id
       , number
-      , license_activation_date
+      , license_activation_date::TIMESTAMP AS license_activation_date
       , last_active_date
       , server_activation_date
       , ROW_NUMBER() OVER (PARTITION BY license_id ORDER BY LAST_ACTIVE_DATE NULLS LAST) AS LICENSE_RANK
@@ -242,15 +307,11 @@ SELECT
             THEN (lead(start_date, 1) OVER (PARTITION BY COALESCE(server_id, license_id) ORDER BY start_date, issued_date))::DATE - interval '1 day'
           ELSE expire_date END as license_retired_date
 FROM license_union
-{% if is_incremental() %}
 
-WHERE (
-        (last_active_date >= (SELECT MAX(last_active_date) FROM {{this}}))
-         OR 
-        (issued_date >= (SELECT MAX(issued_date) FROM {{this}}))
-      )
+UNION ALL
 
-{% endif %}
+SELECT *
+FROM cloud_subscriptions
 )
 
 SELECT 
@@ -394,3 +455,11 @@ SELECT
 FROM license_server_fact l
 LEFT JOIN server_activity activity
     ON l.server_id = activity.server_id
+{% if is_incremental() %}
+
+WHERE 
+  issued_date >= (SELECT MAX(issued_date) FROM {{this}})
+  OR 
+  last_active_date >= (SELECT MAX(last_active_date) FROM {{this}})
+
+{% endif %}
