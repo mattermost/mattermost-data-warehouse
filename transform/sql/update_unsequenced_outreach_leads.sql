d8b19661a7bf705187d66d629103ee0da6b62cd4
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2020-10-14 17:06:36 -0700

    update

--- transform/sql/update_unsequenced_outreach_leads.sql
+++ transform/sql/update_unsequenced_outreach_leads.sql
@@ -1,6 +1,6 @@
 UPDATE orgm.lead
 SET never_connected__c = now(),
-    status = 'Recycled',
+    status = 'Recycle',
     recycle_reason__c = 'Never Connected'
 WHERE NOT actively_being_sequenced__c
     AND lead.status = 'SCL'
