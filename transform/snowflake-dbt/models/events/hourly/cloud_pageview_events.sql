ab1ff8980fb9b3b1c8288a4a14d25fba77317c86
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-10-19 06:57:06 -0700

    Adding cloud pageview events.

---
+++ transform/snowflake-dbt/models/events/hourly/cloud_pageview_events.sql
@@ -0,0 +1,30 @@
+{{config({
+    "materialized": "incremental",
+    "schema": "events",
+    "tags":"preunion",
+    "unique_key":"id"
+  })
+}}
+
+with cloud_pageview_events AS (
+SELECT p.*
+FROM {{ source('mm_telemetry_prod', 'pages') }} p
+LEFT JOIN 
+        (
+          SELECT DISTINCT ID AS JOIN_KEY
+          FROM {{ this }}
+          WHERE TIMESTAMP::DATE >= (SELECT MAX(timestamp::date) FROM {{ this }}) - INTERVAL '1 DAYS'
+          AND name != 'ApplicationLoaded'
+        ) a
+  ON p.id = a.JOIN_KEY
+WHERE name != 'ApplicationLoaded'
+{% if is_incremental() %}
+
+AND p.timestamp::date >= (SELECT MAX(timestamp::date) FROM {{ this }}) - INTERVAL '1 DAYS'
+AND a.join_key is NULL
+
+{% endif %}
+)
+
+SELECT *
+FROM cloud_pageview_events
