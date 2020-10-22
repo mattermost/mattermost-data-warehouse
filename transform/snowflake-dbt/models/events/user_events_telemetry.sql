f9501b67ebc63819c8ea2a5d226208e297558612
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-10-22 09:15:03 -0700

    Adding cloud pageview events from customer portal.

--- transform/snowflake-dbt/models/events/user_events_telemetry.sql
+++ transform/snowflake-dbt/models/events/user_events_telemetry.sql
@@ -5,5 +5,5 @@
   })
 }}
 
-{% set rudder_relations = get_rudder_relations(schema=["events"], database='ANALYTICS', table_inclusions="'portal_events','mobile_events','rudder_webapp_events','segment_webapp_events','segment_mobile_events', 'cloud_pageview_events'") %}
+{% set rudder_relations = get_rudder_relations(schema=["events"], database='ANALYTICS', table_inclusions="'portal_events','mobile_events','rudder_webapp_events','segment_webapp_events','segment_mobile_events', 'cloud_pageview_events', 'cloud_portal_pageview_events'") %}
 {{ union_relations(relations = rudder_relations) }}
