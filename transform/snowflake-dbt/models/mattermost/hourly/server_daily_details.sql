93ed6bbce409d6d2f10defef11ad36a1741def6c
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-11-16 10:42:07 -0800

    Updating logic.

--- transform/snowflake-dbt/models/mattermost/hourly/server_daily_details.sql
+++ transform/snowflake-dbt/models/mattermost/hourly/server_daily_details.sql
@@ -108,7 +108,7 @@ dates as (
     {% if is_incremental() %}
 
         -- this filter will only be applied on an incremental run
-        WHERE date >= (SELECT MAX(date) FROM {{ this }})
+        WHERE date >= (SELECT MAX(date) FROM {{ this }}) - interval '1 day'
 
     {% endif %}
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
