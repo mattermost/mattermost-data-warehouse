86edb6750c535b5cc4be35ef09c00041e285f671
Eric <43073688+enelson720@users.noreply.github.com>
2020-11-18 06:50:41 -0800

    Invoice & Enterprise License Fact (#522)
    * Updating server server model timeframes.
    
    * Updating blapi invoice incremental build logic.
    
    * Fixing subscription incremental build logic.
    
    * Fixing subscription incremental build logic.
    
    * Fixing subscription incremental build logic.
    
    * Incorporating discounts into forecasted total.
    
    * Updating registered users in Enterprise License Fact model
    
    * Updating registered users in Enterprise License Fact model & invoice forecast total.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/blapi/invoices_blapi.sql
+++ transform/snowflake-dbt/models/blapi/invoices_blapi.sql
@@ -46,8 +46,8 @@ invoices AS (
                 -- Only calculate forecasted remaining month invoice if last usage > 10 users else 0  
             + CASE WHEN fi.max_users_previous_day > 10 THEN ((fi.max_users_previous_day * 10/ DATEDIFF(DAY, DATE_TRUNC('MONTH', CURRENT_DATE), LAST_DAY(CURRENT_DATE, MONTH) + INTERVAL '1 DAY'))
                 * datediff(DAY, CURRENT_DATE, LAST_DAY(current_date, MONTH) + INTERVAL '1 DAY'))
-                ELSE 0 END, i.total
-                ) - i.discounts_total, 2) AS forecasted_total
+                ELSE 0 END, (i.total/100.0)
+                )::float - (i.discounts_total/100.0)::float, 2) AS forecasted_total
     FROM {{ source('blapi', 'invoices') }} i
     LEFT JOIN forecasted_invoice fi
         ON i.subscription_id = fi.subscription_id
