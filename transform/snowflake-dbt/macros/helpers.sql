2485603542908c5e17a1244f2b4576d509fd82b0
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-11-25 08:22:26 -0800

    Adding incremental build logic to helpers.

--- transform/snowflake-dbt/macros/helpers.sql
+++ transform/snowflake-dbt/macros/helpers.sql
@@ -229,23 +229,6 @@ select get_sys_var({{ var_name }})
                 AND timestamp <= CURRENT_TIMESTAMP
                 AND coalesce(type, event) NOT IN ('api_profiles_get_in_channel', 'api_profiles_get_by_usernames', 'api_profiles_get_by_ids', 'application_backgrounded', 'application_opened')
                 AND (a.join_id is null)
-            {% elif is_incremental() and this.schema == 'qa' %}
-            LEFT JOIN 
-                (
-                 SELECT 
-                    id as join_id
-                 FROM {{ this }}
-                 WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }}
-                 AND timestamp <= CURRENT_TIMESTAMP
-                 AND timestamp::date >= 
-                     (SELECT MAX(TIMESTAMP::date) FROM {{ this }} WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }} AND timestamp <= CURRENT_TIMESTAMP) - INTERVAL '1 DAYS'
-                 GROUP BY 1
-                ) a
-                ON {{ relation }}.id = a.join_id
-                WHERE timestamp::date >= 
-                     (SELECT MAX(TIMESTAMP::date) FROM {{ this }} WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }} AND timestamp <= CURRENT_TIMESTAMP) - INTERVAL '1 DAYS'
-                AND timestamp <= CURRENT_TIMESTAMP
-                AND (a.join_id is null)
             {% elif is_incremental() and adapter.quote(relation)[7:28] != 'MM_PLUGIN_DEV.NPS_NPS' %}
             LEFT JOIN 
                 (
@@ -278,6 +261,23 @@ select get_sys_var({{ var_name }})
                 ON {{ relation }}.id = a.join_id
                 WHERE original_timestamp <= CURRENT_TIMESTAMP
                 AND original_timestamp::date >= (SELECT MAX(ORIGINAL_TIMESTAMP::date) FROM {{ this }} WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }}) - INTERVAL '1 DAYS'
+                AND (a.join_id is null)
+            {% elif is_incremental() and this.schema == 'qa' %}
+            LEFT JOIN 
+                (
+                 SELECT 
+                    id as join_id
+                 FROM {{ this }}
+                 WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }}
+                 AND timestamp <= CURRENT_TIMESTAMP
+                 AND timestamp::date >= 
+                     (SELECT MAX(TIMESTAMP::date) FROM {{ this }} WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }} AND timestamp <= CURRENT_TIMESTAMP) - INTERVAL '1 DAYS'
+                 GROUP BY 1
+                ) a
+                ON {{ relation }}.id = a.join_id
+                WHERE timestamp::date >= 
+                     (SELECT MAX(TIMESTAMP::date) FROM {{ this }} WHERE _dbt_source_relation = {{ ["'", relation, "'"]|join }} AND timestamp <= CURRENT_TIMESTAMP) - INTERVAL '1 DAYS'
+                AND timestamp <= CURRENT_TIMESTAMP
                 AND (a.join_id is null)
             {% endif %}
         )
