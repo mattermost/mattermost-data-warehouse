0d5d6fe520f52720cad271c3d2b469521bf2ccfd
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-12-10 08:16:12 -0800

    Disabling unused user tables.

--- transform/snowflake-dbt/models/mattermost/nightly/user_daily_details.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/user_daily_details.sql
@@ -1,7 +1,7 @@
 {{config({
     "materialized": 'incremental',
     "schema": "mattermost",
-    "enabled":"false"
+    "enabled":false
   })
 }}
 
--- transform/snowflake-dbt/models/mattermost/nightly/user_fact.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/user_fact.sql
@@ -1,7 +1,7 @@
 {{config({
     "materialized": 'table',
     "schema": "mattermost",
-    "enabled":"false"
+    "enabled":false
   })
 }}
 
