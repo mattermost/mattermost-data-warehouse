63e5f66bd89df0c3a307ac2b77f111c2af7f13b9
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-12-11 13:36:10 -0800

    Fixing some paritioning.

--- transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
+++ transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
@@ -5,28 +5,36 @@
   })
 }}
 
+{% if is_incremental() %}
+ WITH max_timestamp AS (
+   SELECT MAX(timestamp) - INTERVAL '1 DAY' AS max_time FROM {{ this }} WHERE timestamp <= CURRENT_TIMESTAMP
+ ),
+  
+join_key AS (
+  SELECT join_key.ID AS JOIN_KEY
+  FROM {{ this }} join_key
+  JOIN max_timestamp mt
+    ON join_key.timestamp >= mt.max_time
+  AND TIMESTAMP <= CURRENT_TIMESTAMP
+  AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
+)
+
+
 SELECT e.*
 FROM {{ source('mm_telemetry_prod', 'event') }} e
-{% if is_incremental() %}
-
-LEFT JOIN 
-        (
-          SELECT DISTINCT ID AS JOIN_KEY
-          FROM {{ this }}
-          WHERE TIMESTAMP::DATE >= (SELECT MAX(timestamp::date) FROM {{ this }} WHERE timestamp <= CURRENT_TIMESTAMP) - INTERVAL '1 DAYS'
-          AND timestamp <= CURRENT_TIMESTAMP
-          AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
-        ) a
+JOIN max_timestamp mt
+    ON e.timestamp >= mt.max_time
+LEFT JOIN join_key a
   ON e.id = a.JOIN_KEY
-WHERE e.timestamp::date >= (SELECT MAX(timestamp::date) FROM {{ this }}) - INTERVAL '1 DAYS'
 AND e.timestamp <= CURRENT_TIMESTAMP
 AND a.JOIN_KEY IS NULL
--- Date rudder started sending telemetry
-AND timestamp::date >= '2020-05-14'
 AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
 
 {% else %}
+SELECT e.*
+FROM {{ source('mm_telemetry_prod', 'event') }} e
 WHERE e.timestamp <= CURRENT_TIMESTAMP
+AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
 {% endif %}
 
 
--- transform/snowflake-dbt/models/events/hourly/segment_webapp_events.sql
+++ transform/snowflake-dbt/models/events/hourly/segment_webapp_events.sql
@@ -5,48 +5,58 @@
   })
 }}
 
-SELECT s.*
-FROM (
-SELECT
-    segment.*
-FROM {{ source('mattermost2', 'event') }} segment
-     LEFT JOIN (
-                SELECT user_id, MIN(TIMESTAMP::DATE) AS MIN_DATE
-                FROM {{ source('mm_telemetry_prod', 'event') }}
+{% if is_incremental() %}
+
+with rudder as (
+ SELECT user_id, TO_DATE(MIN(TIMESTAMP)) AS MIN_DATE
+                FROM {{ source('mm_telemetry_prod', 'server') }}
                 GROUP BY 1
-              ) rudder
-        ON segment.user_id = rudder.user_id AND segment.timestamp::DATE >= rudder.MIN_DATE
-{% if is_incremental() %}
+),
 
+max_time as (
+ SELECT MAX(timestamp) - interval '1 days' as max_time FROM {{ this }}
+),
+
+join_key as (
+SELECT join_key.ID AS JOIN_KEY
+          FROM {{ this }} join_key
+          JOIN max_time mt
+            ON join_key.timestamp >= mt.max_time
+),
+
+segment as (
+SELECT segment.*
+FROM {{ source('mattermost2', 'event') }} segment
+JOIN max_time
+    ON segment.timestamp >= max_time.max_time
+LEFT JOIN rudder
+    ON segment.user_id = rudder.user_id AND TO_DATE(segment.timestamp) >= rudder.MIN_DATE
 WHERE rudder.user_id is NULL
-AND segment.timestamp::date >= (SELECT MAX(timestamp::date) FROM {{ this }}) - INTERVAL '1 DAYS'
-AND segment.timestamp::date <= CURRENT_TIMESTAMP
-AND segment.timestamp::date >= '2019-02-01'
-) s
+AND segment.timestamp <= CURRENT_TIMESTAMP
+AND segment.timestamp >= '2019-02-01'
+)
 
-{% else %}
-
-WHERE rudder.user_id is NULL
-AND segment.timestamp::date <= CURRENT_TIMESTAMP
-AND segment.timestamp::date >= '2019-02-01'
-) s
-
-{% endif %}
-{% if is_incremental() %}
-
-LEFT JOIN 
-        (
-          SELECT DISTINCT ID AS JOIN_KEY
-          FROM {{ this }}
-          WHERE timestamp::date >= (SELECT MAX(timestamp::date) FROM {{ this }}) - INTERVAL '1 DAYS'
-        ) a
+SELECT s.*
+FROM segment s
+LEFT JOIN JOIN_KEY a
   ON s.id = a.JOIN_KEY
-WHERE s.timestamp::date >= (SELECT MAX(timestamp::date) FROM {{ this }}) - INTERVAL '1 DAYS'
-AND s.timestamp <= CURRENT_TIMESTAMP
-AND a.JOIN_KEY IS NULL
+WHERE s.timestamp <= CURRENT_TIMESTAMP
+AND a.join_key is null
 
 {% else %}
 
-s.timestamp <= CURRENT_TIMESTAMP
+with rudder as (
+ SELECT user_id, TO_DATE(MIN(TIMESTAMP)) AS MIN_DATE
+                FROM {{ source('mm_telemetry_prod', 'server') }}
+                GROUP BY 1
+)
+
+SELECT segment.*
+FROM {{ source('mattermost2', 'event') }} segment
+LEFT JOIN rudder
+    ON segment.user_id = rudder.user_id AND TO_DATE(segment.timestamp) >= rudder.MIN_DATE
+WHERE rudder.user_id is NULL
+AND segment.timestamp <= CURRENT_TIMESTAMP
+AND segment.timestamp >= '2019-02-01'
 
 {% endif %}
