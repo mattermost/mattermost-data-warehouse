0f89549eb509bfa6fee3220bb23264a797526ece
Eric <43073688+enelson720@users.noreply.github.com>
2020-12-14 09:43:58 -0800

    Fix spend (#566)
    * fixing unioning macro.
    
    * fixing unioning macro.
    
    * Fixing missing where condition.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
+++ transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
@@ -7,7 +7,9 @@
 
 {% if is_incremental() %}
  WITH max_timestamp AS (
-   SELECT MAX(timestamp) - INTERVAL '1 DAY' AS max_time FROM {{ this }} WHERE timestamp <= CURRENT_TIMESTAMP
+   SELECT MAX(timestamp) - INTERVAL '1 DAY' AS max_time 
+   FROM {{ this }} 
+   WHERE timestamp <= CURRENT_TIMESTAMP
  ),
   
 join_key AS (
@@ -15,7 +17,7 @@ join_key AS (
   FROM {{ this }} join_key
   JOIN max_timestamp mt
     ON join_key.timestamp >= mt.max_time
-  AND TIMESTAMP <= CURRENT_TIMESTAMP
+  WHERE TIMESTAMP <= CURRENT_TIMESTAMP
   AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
 )
 
@@ -26,7 +28,7 @@ JOIN max_timestamp mt
     ON e.timestamp >= mt.max_time
 LEFT JOIN join_key a
   ON e.id = a.JOIN_KEY
-AND e.timestamp <= CURRENT_TIMESTAMP
+WHERE e.timestamp <= CURRENT_TIMESTAMP
 AND a.JOIN_KEY IS NULL
 AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
 
