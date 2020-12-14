f80b45978ba7e55e2a510444e283b869ee550fc4
Alex Dovenmuehle <alex.dovenmuehle@mattermost.com>
2020-12-14 17:04:20 -0500

    Try to reduce number of threads and go back to dbt 0.18.1

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -8,7 +8,7 @@ from typing import List
 from airflow.contrib.kubernetes.pod import Resources
 
 DATA_IMAGE = "docker.io/adovenmm/data-image:latest"
-DBT_IMAGE = "docker.io/adovenmm/dbt-image:v0.17.2"
+DBT_IMAGE = "docker.io/adovenmm/dbt-image:v0.18.1"
 PERMIFROST_IMAGE = "docker.io/adovenmm/permifrost-image:latest"
 PSQL_IMAGE = "docker.io/adovenmm/data-warehouse-psql:latest"
 PIPELINEWISE_IMAGE = "docker.io/adovenmm/pipelinewise:latest"
