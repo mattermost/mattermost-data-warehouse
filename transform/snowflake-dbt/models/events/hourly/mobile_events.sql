7a95b144fa08d7b644ffce5cc86a4a3ee3d4ddbe
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2020-12-17 09:46:14 -0800

    Updating mobile_events to omit irrelevant tables.

--- transform/snowflake-dbt/models/events/hourly/mobile_events.sql
+++ transform/snowflake-dbt/models/events/hourly/mobile_events.sql
@@ -5,5 +5,5 @@
   })
 }}
 
-{% set rudder_relations = get_rudder_relations(schema=["mm_mobile_prod"], database='RAW') %}
+{% set rudder_relations = get_rudder_relations(schema=["mm_mobile_prod"], database='RAW',table_exclusions="'api_profiles_get_in_channel','api_profiles_get_by_usernames','api_profiles_get_by_ids'") %}
 {{ union_relations(relations = rudder_relations) }}
