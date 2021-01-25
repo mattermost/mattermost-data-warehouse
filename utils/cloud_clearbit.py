373b7da47a8290b1326e6119b16168f97b86d547
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2021-01-25 07:23:54 -0800

    Adding additional clearbit logic to prevent errors when data for columns not available.

--- utils/cloud_clearbit.py
+++ utils/cloud_clearbit.py
@@ -15,6 +15,26 @@ connection = engine.connect()
 # SET CLEARBIT API KEY
 clearbit.key = os.getenv('CLEARBIT_KEY')
 
+# RETRIEVE COLUMN NAMES IF TABLE ALREADY EXISTS
+try:
+    col_q = f'''
+    SELECT COLUMN_NAME 
+    FROM ANALYTICS.INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'CLOUD_CLEARBIT'
+    AND TABLE_SCHEMA = 'MATTERMOST'
+    ORDER BY ORDINAL_POSITION
+    '''
+    col_df = execute_dataframe(engine, query=col_q)
+    clearbit_cols = []
+    if len(col_df) > 0:
+        for index, row in col_df.iterrows():
+            clearbit_cols.append(row['COLUMN_NAME'])
+    else:
+       clearbit_cols = None
+
+except:
+    clearbit_cols = None
+
 # DETERMINE CLOUD_CLEARBIT TABLE IS BUILT AND HAS NOT BEEN DROPPED
 try:
     query = f'''SELECT * FROM ANALYTICS.MATTERMOST.CLOUD_CLEARBIT'''
@@ -54,40 +74,47 @@ for index, row in df.iterrows():
     if response is not None:
         cloud_clearbit.append([row['SERVER_ID'], response])
 
-# CREATE EMPTY LIST TO STORE COLUMN NAMES FOR NESTED CLEARBIT DATA
-cols = []
-
-# CREATE DICTIONARY TO STORE RESULTS AS SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIRS
-d = {}
-
-# RETRIEVE ALL COLUMN NAMES REQUIRED TO GENERATE A DATAFRAME TO STORE EACH CLEARBIT PROPERTY AND STORE IN LIST
+# CHECK IF NEW DATA TO LOAD
 if len(cloud_clearbit) >= 1:
-    for i in cloud_clearbit:
-        for key, value in i[1].items():
-            if isinstance(value,dict):
-                for k, v in value.items():
-                    if isinstance(v, dict):
-                        for k1, v1 in v.items():
-                            if isinstance(v1, dict):
-                                for k2, v2 in v1.items():
-                                    cols.append(key + '_' + k + '_' + k1 + '_' + k2)
-                            else:
-                                cols.append(key + '_' + k + '_' + k1)
-                    else:
-                        cols.append(key + '_' + k)
-            else:
-                cols.append(key)
-    # GENERATE SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIR RECORDS AND STORE IN DICTIONARY
-        d[i[0]] = i[1]
-    # ONLY RETAIN UNIQUE COLUMN VALUES AS SET THEN RECAST AS LIST FOR ITERATING PURPOSES.
-    cols = set(cols)
-    cols = list(cols)
+    # USE EXISTING COLUMN NAMES IF TABLE ALREADY EXISTS
+    if clearbit_cols is not None:
+        d = {}
+        clearbit_df = pd.DataFrame(columns=clearbit_cols)
+        clearbit_df['SERVER_ID'] = df['SERVER_ID'].unique()
+        for i in cloud_clearbit:
+            d[i[0]] = i[1] 
+    else:
+        # CREATE EMPTY LIST TO STORE COLUMN NAMES FOR NESTED CLEARBIT DATA
+        cols = []
+        # CREATE DICTIONARY TO STORE RESULTS AS SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIRS
+        d = {}
+        # RETRIEVE ALL COLUMN NAMES REQUIRED TO GENERATE A DATAFRAME TO STORE EACH CLEARBIT PROPERTY AND STORE IN LIST
+        for i in cloud_clearbit:
+            for key, value in i[1].items():
+                if isinstance(value,dict):
+                    for k, v in value.items():
+                        if isinstance(v, dict):
+                            for k1, v1 in v.items():
+                                if isinstance(v1, dict):
+                                    for k2, v2 in v1.items():
+                                        cols.append(key.lower() + '_' + k.lower() + '_' + k1.lower() + '_' + k2.lower())
+                                else:
+                                    cols.append(key.lower() + '_' + k.lower() + '_' + k1.lower())
+                        else:
+                            cols.append(key.lower() + '_' + k.lower())
+                else:
+                    cols.append(key.lower())
+        # GENERATE SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIR RECORDS AND STORE IN DICTIONARY
+            d[i[0]] = i[1]
+        # ONLY RETAIN UNIQUE COLUMN VALUES AS SET THEN RECAST AS LIST FOR ITERATING PURPOSES.
+        cols = set(cols)
+        cols = list(cols)
 
-    # CREATE CLEARBIT DATAFRAME STRUCTURE USING UNIQUE COLUMN VALUES
-    clearbit_df = pd.DataFrame(columns=cols)
+        # CREATE CLEARBIT DATAFRAME STRUCTURE USING UNIQUE COLUMN VALUES
+        clearbit_df = pd.DataFrame(columns=cols)
 
-    # ADD A ROW FOR EACH UNIQUE WORKSPACE (SERVER_ID)
-    clearbit_df['server_id'] = df['SERVER_ID'].unique()
+        # ADD A ROW FOR EACH UNIQUE WORKSPACE (SERVER_ID)
+        clearbit_df['server_id'] = df['SERVER_ID'].unique()
 
     # ITERATE THROUGH CLOUD WORKSPACE CLEARBIT KEY-VALUE PAIRS
     # UPDATE EACH RESPECTIVE COLUMN PROPERTY USING INDEX AND COLUMN NAME
@@ -100,20 +127,20 @@ if len(cloud_clearbit) >= 1:
                             for k2, v2 in v1.items():
                                 if isinstance(v2, dict):
                                     for k3, v3 in v2.items():
-                                        clearbit_df.loc[index, k + '_' + k1 + '_' + k2 + '_' + k3] = v3
+                                        clearbit_df.loc[index, k.lower() + '_' + k1.lower() + '_' + k2.lower() + '_' + k3.lower()] = v3
                                 else:
-                                    clearbit_df.loc[index, k + '_' + k1 + '_' + k2] = v2
+                                    clearbit_df.loc[index, k.lower() + '_' + k1.lower() + '_' + k2.lower()] = v2
                         else:
-                            clearbit_df.loc[index, k + '_' + k1] = v1
+                            clearbit_df.loc[index, k.lower() + '_' + k1.lower()] = v1
                 else:
-                    clearbit_df.loc[index, k] = v
+                    clearbit_df.loc[index, k.lower()] = v
 
     # Convert clearbit object data types to next best fit.
     clearbit_df2 = clearbit_df.convert_dtypes()
 
     # CAST REMAINING CLEARBIT OBJECT COLUMNS TO STRINGS
-    columns = ['company_site_phoneNumbers','company_techCategories','company_domainAliases','company_tech','person_gravatar_urls','person_gravatar_avatars',\
-            'company_tags','company_site_emailAddresses']
+    columns = ['company_site_phonenumbers','company_techcategories','company_domainaliases','company_tech','person_gravatar_urls','person_gravatar_avatars',\
+            'company_tags','company_site_emailaddresses']
     clearbit_df2[columns] = clearbit_df2[columns].astype(str)
 
     # CONVERT COLUMN NAMES TO LOWERCASE FOR LOADING PURPOSES
