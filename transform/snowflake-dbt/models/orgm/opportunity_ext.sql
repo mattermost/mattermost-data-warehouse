492ac1c6327c033f6a107ff66183fc2ff888fc01
rbradleyhaas <bradleyhaasr@gmail.com>
2021-01-28 11:22:53 -0800

    Fixy Fixy (#603)
    * update to add payment method
    
    * oops
    
    Co-authored-by: Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>

--- transform/snowflake-dbt/models/orgm/opportunity_ext.sql
+++ transform/snowflake-dbt/models/orgm/opportunity_ext.sql
@@ -99,7 +99,7 @@ WITH w_end_date AS (
   LEFT JOIN opportunity_paid_netsuite ON opportunity.sfid = opportunity_paid_netsuite.opportunity_sfid
   LEFT JOIN opportunity_marketing ON opportunity.sfid = opportunity_marketing.opportunity_sfid
   LEFT JOIN opportunity_fc_amounts ON opportunity.sfid = opportunity_fc_amounts.opportunity_sfid
-  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
+  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
 )
 
  SELECT * FROM opportunity_ext
