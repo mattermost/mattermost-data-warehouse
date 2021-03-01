f9963637f44102f8c5ee5a4eed3093db46279284
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-03-01 11:08:36 -0800

    updates

--- transform/sql/snowflake/data_action_nps_feedback_subcategory_update.sql
+++ transform/sql/snowflake/data_action_nps_feedback_subcategory_update.sql
@@ -1,30 +1,28 @@
-INSERT INTO analytics.mattermost.nps_feedback_classification(last_feedback_date, server_id, user_id, subcategory, categorized_at, categorized_by, manually_categorized)
-        SELECT 
-            last_feedback_date
-          , server_id
-          , user_id
-          , new_value as subcategory
-          , triggered_at as categorized_at
-          , triggered_by as categorized_by
-          , TRUE as manually_categorized
-        FROM (
-                SELECT 
-                    ROW_NUMBER() OVER 
-                        (PARTITION BY PARSE_JSON(other_params):last_feedback_date, PARSE_JSON(other_params):user_id, PARSE_JSON(other_params):server_id, field ORDER BY triggered_at DESC) AS row_num
-                  , PARSE_JSON(other_params):user_id::varchar AS user_id
-                  , PARSE_JSON(other_params):server_id::varchar AS server_id
-                  , PARSE_JSON(other_params):last_feedback_date::date AS last_feedback_date
-                  , field
-                  , new_value
-                  , triggered_at
-                  , triggered_by
-                FROM zapier_data_actions.data
-                WHERE table_name = 'nps_feedback_classification' AND field IN ('subcategory') AND dwh_processed_at IS NULL
-            )
-        WHERE row_num = 1
-        GROUP BY 1, 2, 3, 4, 5, 6, 7
-    ;
-
-UPDATE zapier_data_actions.data
-SET dwh_processed_at = current_timestamp()
-WHERE dwh_processed_at IS NULL AND table_name IN ('nps_feedback_classification') and field IN ('subcategory');
+INSERT INTO analytics.mattermost.nps_feedback_classification(last_feedback_date, server_id, user_id, subcategory, categorized_at, categorized_by, manually_categorized, manually_categorized_id)
+    SELECT 
+        last_feedback_date
+    , server_id
+    , user_id
+    , new_value as subcategory
+    , triggered_at as categorized_at
+    , triggered_by as categorized_by
+    , TRUE as manually_categorized
+    , data_action_id as manually_categorized_id
+    FROM (
+            SELECT
+                ROW_NUMBER() OVER
+                    (PARTITION BY PARSE_JSON(data):last_feedback_date, PARSE_JSON(data):user_id, PARSE_JSON(data):server_id, PARSE_JSON(data):field ORDER BY PARSE_JSON(data):triggered_at DESC) AS row_num
+            , PARSE_JSON(data):user_id::varchar AS user_id
+            , PARSE_JSON(data):server_id::varchar AS server_id
+            , PARSE_JSON(data):last_feedback_date::date AS last_feedback_date
+            , PARSE_JSON(data):field_name::varchar AS field_name
+            , PARSE_JSON(FORM_PARAMS):new_value::varchar as new_value
+            , _SDC_RECEIVED_AT as triggered_at
+            , PARSE_JSON(data):action_performed_by::varchar as triggered_by
+            , __SDC_PRIMARY_KEY as data_action_id
+            FROM LOOKER_DATA_ACTIONS.data
+            WHERE PARSE_JSON(data):table = 'nps_feedback_classification' AND PARSE_JSON(data):field_name IN ('subcategory')
+        ) as data_action_data
+    WHERE row_num = 1
+        AND NOT EXISTS (SELECT 1 FROM analytics.mattermost.nps_feedback_classification WHERE manually_categorized_id = data_action_data.data_action_id)
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8;
