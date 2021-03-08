667ec404a3d919e93cba9fc3efee52ac47a3ab4e
rbradleyhaas <bradleyhaasr@gmail.com>
2021-03-08 10:49:09 -0800

    Final Fix (#626)
    * updates
    
    * update to ref
    
    * updates
    
    * fixy fixy
    
    * fix helper
    
    * updates for history objects
    
    * final fix
    
    Co-authored-by: Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>

--- transform/snowflake-dbt/models/orgm_raw/opportunityfieldhistory.sql
+++ transform/snowflake-dbt/models/orgm_raw/opportunityfieldhistory.sql
@@ -7,5 +7,16 @@
     )
 }}
 
-{{ latest_record (source('orgm_raw','opportunityfieldhistory'))}}
+
+SELECT DISTINCT o.id as sfid, coalesce(NEWVALUE__BO::VARCHAR,NEWVALUE__DE::VARCHAR,NEWVALUE__FL::VARCHAR,NEWVALUE__ST::VARCHAR) as newvalue, coalesce(OLDVALUE__BO::VARCHAR,OLDVALUE__DE::VARCHAR,OLDVALUE__FL::VARCHAR,OLDVALUE__ST::VARCHAR) as oldvalue
+FROM {{ source('orgm_raw','opportunityfieldhistory') }} o
+INNER JOIN (
+    SELECT id,
+            MAX(_sdc_sequence) AS seq,
+            MAX(_sdc_batched_at) AS batch
+    FROM {{ source('orgm_raw','opportunityfieldhistory') }}
+    GROUP BY id) oo
+ON o.id = oo.id
+    AND o._sdc_sequence = oo.seq
+    AND o._sdc_batched_at = oo.batch
 WHERE NOT isdeleted
