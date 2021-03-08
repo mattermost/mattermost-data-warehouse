4a236308eff259176153121567b76306ee883eab
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-03-08 10:44:43 -0800

    updates for history objects

--- transform/snowflake-dbt/models/orgm_raw/customer_risk__history.sql
+++ transform/snowflake-dbt/models/orgm_raw/customer_risk__history.sql
@@ -7,4 +7,16 @@
     )
 }}
 
-{{ latest_record (source('orgm_raw','customer_risk__history'))}}
+
+SELECT DISTINCT o.id as sfid, coalesce(VARCHAR,NEWVALUE__FL::VARCHAR,NEWVALUE__ST::VARCHAR) as newvalue, coalesce(OLDVALUE__FL::VARCHAR,OLDVALUE__ST::VARCHAR) as oldvalue
+FROM {{ source('orgm_raw','customer_risk__history') }} o
+INNER JOIN (
+    SELECT id,
+            MAX(_sdc_sequence) AS seq,
+            MAX(_sdc_batched_at) AS batch
+    FROM {{ source('orgm_raw','customer_risk__history') }}
+    GROUP BY id) oo
+ON o.id = oo.id
+    AND o._sdc_sequence = oo.seq
+    AND o._sdc_batched_at = oo.batch
+WHERE NOT isdeleted
