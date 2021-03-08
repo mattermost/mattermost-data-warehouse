39beb5b769208165837a54b8825acd51de735226
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-03-08 09:42:57 -0800

    fixy fixy

--- transform/snowflake-dbt/models/cs/account_health_score.sql
+++ transform/snowflake-dbt/models/cs/account_health_score.sql
@@ -13,13 +13,13 @@ WITH account_health_facts AS (
         max(end_date__c) > current_date AS existing_customer,
         (least(max(end_date__c)::date, current_date) - min(start_date__c)::date) / 365 AS tenure_in_yrs,
         count(distinct tickets.ID) AS count_tickets_prev_90,
-        current_date - max(tasks_filtered.createddate)::date AS days_since_last_task,
+        current_date - max(task.createddate)::date AS days_since_last_task,
         min(CASE WHEN risk.status__c = 'At Risk' THEN 20 WHEN risk.status__c IN ('Early Warning','Delayed') THEN 50 ELSE NULL END) AS risk_override_score,
         CASE WHEN max(customer_reference.status__c) IS NOT NULL THEN TRUE ELSE FALSE END AS reference
     FROM {{ ref( 'account') }}
         LEFT JOIN {{ ref( 'opportunity') }}  ON opportunity.accountid = account.sfid AND iswon
         LEFT JOIN {{ ref( 'opportunitylineitem') }} ON opportunitylineitem.opportunityid = opportunity.sfid
-        LEFT JOIN {{ ref( 'tasks_filtered') }} ON account.sfid = tasks_filtered.accountid
+        LEFT JOIN {{ ref( 'task') }} ON account.sfid = task.accountid
         LEFT JOIN {{ source('zendesk_raw', 'organizations') }} ON left(organizations.external_id,15) = left(account.sfid,15)
         LEFT JOIN {{ source('zendesk_raw', 'tickets') }} ON tickets.organization_id = organizations.id AND tickets.created_at > current_date - INTERVAL '90 days'
         LEFT JOIN {{ ref( 'customer_risk__c') }} AS risk ON risk.account__c = account.sfid AND risk.status__c IN ('At Risk','Early Warning','Delayed')
