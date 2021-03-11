da5a976964d23fd3166f75262b5bd69dcdbba25b
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2021-03-11 11:45:37 -0800

    Adding daily_server_user_agent_events

--- transform/snowflake-dbt/models/events/hourly/daily_server_user_agent_events.sql
+++ transform/snowflake-dbt/models/events/hourly/daily_server_user_agent_events.sql
@@ -50,7 +50,7 @@ WITH daily_server_user_agent_events AS (
       , COUNT(DISTINCT COALESCE(events.user_actual_id, events.anonymous_id))          AS user_count
       , COUNT(COALESCE(events.user_actual_id, events.anonymous_id))                   AS events
     FROM {{ ref('user_events_telemetry') }}        events
-         JOIN ANALYTICS.mattermost.user_agent_registry user_agent
+         JOIN ANALYTICS.web.user_agent_registry user_agent
               ON COALESCE(events.context_user_agent, events.context_useragent) = user_agent.context_useragent
     WHERE COALESCE(events.user_id, events.context_traits_server, events.context_server) IS NOT NULL
     {% if is_incremental() %}
