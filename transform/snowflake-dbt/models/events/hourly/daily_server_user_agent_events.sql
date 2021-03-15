fc0ef4cf8b5b73a3bbb8a72562512b89f46315f8
Eric <43073688+enelson720@users.noreply.github.com>
2021-03-15 08:51:17 -0700

    User agent (#644)
    * Adding daily_server_user_agent_events
    
    * Adding daily_server_user_agent_events
    
    * Updating user_agent_parser.py script for mobile fix.
    
    * Updating user_agent_parser.py script for mobile fix.
    
    * Updating user_agent_parser.py script for mobile fix.
    
    * Updating daily_server_user_agent_events model file.
    
    * Updating daily_server_user_agent_events model file.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/events/hourly/daily_server_user_agent_events.sql
+++ transform/snowflake-dbt/models/events/hourly/daily_server_user_agent_events.sql
@@ -23,7 +23,14 @@ WITH daily_server_user_agent_events AS (
             WHEN SPLIT_PART(events._dbt_source_relation2, '.', 3) IN ('PORTAL_EVENTS')
                                                                                                                 THEN 'Customer Portal'
                                                                                                                 ELSE 'WebApp' END AS event_source
-      , user_agent.*
+      , COALESCE(user_agent.context_useragent, COALESCE(events.context_user_agent, events.context_useragent)) AS context_useragent
+      , COALESCE(user_agent.browser, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN 'Mobile App' ELSE NULL END) AS browser
+      , COALESCE(user_agent.browser_version, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN events.context_app_version ELSE NULL END) AS browser_version
+      , COALESCE(user_agent.operating_system, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN events.context_os_name ELSE NULL END) AS operating_system
+      , COALESCE(user_agent.os_version, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN events.context_os_version ELSE NULL END) AS os_version
+      , COALESCE(user_agent.device_type, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN events.context_device_model ELSE NULL END) AS device_type
+      , COALESCE(user_agent.device_brand, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN events.context_device_manufacturer ELSE NULL END) AS device_brand
+      , COALESCE(user_agent.device_model, CASE WHEN events._dbt_source_relation2 IN ('"ANALYTICS".EVENTS.MOBILE_EVENTS') THEN events.context_device_model ELSE NULL END) AS device_model
       , {{ dbt_utils.surrogate_key(['events.timestamp::DATE'
                                     , 'COALESCE(events.user_id, events.context_traits_server, events.context_server)'
                                     , 'CASE
@@ -39,21 +46,21 @@ WITH daily_server_user_agent_events AS (
                                         WHEN SPLIT_PART(events._dbt_source_relation2, \'.\', 3) IN (\'PORTAL_EVENTS\')
                                                                                                                                             THEN \'Customer Portal\'
                                                                                                                                             ELSE \'WebApp\' END'
-                                    , 'user_agent.context_useragent'
-                                    , 'user_agent.browser'
-                                    , 'user_agent.browser_version'
-                                    , 'user_agent.operating_system'
-                                    , 'user_agent.os_version'
-                                    , 'user_agent.device_type'
-                                    , 'user_agent.device_brand'
-                                    , 'user_agent.device_model'])}}                   AS id
+      , 'COALESCE(user_agent.context_useragent, COALESCE(events.context_user_agent, events.context_useragent))'
+      , 'COALESCE(user_agent.browser, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN \'Mobile App\' ELSE NULL END)'
+      , 'COALESCE(user_agent.browser_version, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN events.context_app_version ELSE NULL END)'
+      , 'COALESCE(user_agent.operating_system, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN events.context_os_name ELSE NULL END)'
+      , 'COALESCE(user_agent.os_version, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN events.context_os_version ELSE NULL END)'
+      , 'COALESCE(user_agent.device_type, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN events.context_device_model ELSE NULL END)'
+      , 'COALESCE(user_agent.device_brand, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN events.context_device_manufacturer ELSE NULL END)'
+      , 'COALESCE(user_agent.device_model, CASE WHEN events._dbt_source_relation2 IN (\'"ANALYTICS".EVENTS.MOBILE_EVENTS\') THEN events.context_device_model ELSE NULL END)'])}}                   AS id
       , COUNT(DISTINCT COALESCE(events.user_actual_id, events.anonymous_id))          AS user_count
       , COUNT(COALESCE(events.user_actual_id, events.anonymous_id))                   AS events
       , COUNT(CASE WHEN COALESCE(events.type, events.event) in ('api_posts_create') THEN events.id ELSE NULL END) AS posts
     FROM {{ ref('user_events_telemetry') }}        events
-         JOIN ANALYTICS.web.user_agent_registry user_agent
+         LEFT JOIN ANALYTICS.web.user_agent_registry user_agent
               ON COALESCE(events.context_user_agent, events.context_useragent) = user_agent.context_useragent
-    WHERE COALESCE(events.user_id, events.context_traits_server, events.context_server) IS NOT NULL
+    WHERE (COALESCE(events.user_id, events.context_traits_server, events.context_server) IS NOT NULL)
     {% if is_incremental() %}
     AND events.timestamp::DATE >= (SELECT MAX(date) FROM {{this}})
     {% endif %}
