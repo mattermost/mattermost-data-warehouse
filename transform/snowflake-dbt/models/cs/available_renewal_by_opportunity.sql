30008ff4ccfe82d45747ecf483f488231952581f
rbradleyhaas <bradleyhaasr@gmail.com>
2021-03-29 09:50:05 -0700

    Fix Available Renewa (#655)
    * arr changes for ARR
    
    * update to renewals
    
    Co-authored-by: Rachel Bradley-Haas <rachel.bradley-haas@mattermost.com>

--- transform/snowflake-dbt/models/cs/available_renewal_by_opportunity.sql
+++ transform/snowflake-dbt/models/cs/available_renewal_by_opportunity.sql
@@ -4,6 +4,6 @@
   })
 }}
 
-select * from {{ source('sales_and_cs_gsheets','available_renewals_current_fy') }}
+select account_id, available_renewal, renewal_license_start, renewal_opportunity, renewal_qtr from {{ source('sales_and_cs_gsheets','available_renewals_current_fy') }}
 union all 
-select * from {{ source('sales_and_cs_gsheets','available_renewals_historical') }}
+select account_id, available_renewal, renewal_license_start, renewal_opportunity, renewal_qtr from {{ source('sales_and_cs_gsheets','available_renewals_historical') }}
