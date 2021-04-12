d565584d6ca326f630fb2ece099263f0ec12d62c
Eric Nelson <enelson@Erics-MBP.home>
2021-04-12 09:52:36 -0700

    Adding docs to website data.

--- transform/snowflake-dbt/models/web/daily_website_traffic.sql
+++ transform/snowflake-dbt/models/web/daily_website_traffic.sql
@@ -5,5 +5,5 @@
   })
 }}
 
-{% set rudder_relations = get_rudder_relations(schema=["mattermostcom", "portal_prod", "support_portal"], database='RAW', table_inclusions="'pages'") %}
+{% set rudder_relations = get_rudder_relations(schema=["mattermostcom", "portal_prod", "support_portal", "mattermost_docs"], database='RAW', table_inclusions="'pages'") %}
 {{ union_relations(relations = rudder_relations) }}
