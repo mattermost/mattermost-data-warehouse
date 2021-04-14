91e37a2633b8c1eddd4cf3b9dd1f9c169f38d2a7
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-04-13 22:12:07 -0700

    group by

--- transform/snowflake-dbt/models/hightouch/campaignmember_insert.sql
+++ transform/snowflake-dbt/models/hightouch/campaignmember_insert.sql
@@ -7,13 +7,13 @@
 
 
 with campaignmember_insert as (
-    select campaignmember_dwh_external_id, license_issued_at, in_product_trial_request_campaignid as campaignid, campaign_status, contact_dwh_external_id, lead_dwh_external_id
+    select campaignmember_dwh_external_id, max(license_issued_at) as last_occurence, in_product_trial_request_campaignid as campaignid, campaign_status, contact_dwh_external_id, lead_dwh_external_id
     from {{ ref('contact_in_product_trial_request') }}
     where campaignmember_sfid is null
 
     union all
 
-    select campaignmember_dwh_external_id, license_issued_at, in_product_trial_request_campaignid as campaignid, campaign_status, null, lead_dwh_external_id
+    select campaignmember_dwh_external_id, max(license_issued_at) as last_occurence, in_product_trial_request_campaignid as campaignid, campaign_status, null, lead_dwh_external_id
     from {{ ref('lead_in_product_trial_request') }}
     where campaignmember_sfid is null
 )
--- transform/snowflake-dbt/models/hightouch/campaignmember_update.sql
+++ transform/snowflake-dbt/models/hightouch/campaignmember_update.sql
@@ -7,13 +7,13 @@
 
 
 with campaignmember_update as (
-    select campaignmember_sfid, license_issued_at, campaign_status
+    select campaignmember_sfid, max(license_issued_at) as last_occurence, campaign_status
     from {{ ref('contact_in_product_trial_request') }}
     where campaignmember_sfid is not null
 
     union all
 
-    select campaignmember_sfid, license_issued_at, campaign_status
+    select campaignmember_sfid, max(license_issued_at) as last_occurence, campaign_status
     from {{ ref('lead_in_product_trial_request') }}
     where campaignmember_sfid is not null
 )
