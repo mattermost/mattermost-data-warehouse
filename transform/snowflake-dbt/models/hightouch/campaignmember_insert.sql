7a1e8db04b72328156db829a8344d078cafdd278
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-04-13 22:14:30 -0700

    redo

--- transform/snowflake-dbt/models/hightouch/campaignmember_insert.sql
+++ transform/snowflake-dbt/models/hightouch/campaignmember_insert.sql
@@ -9,8 +9,9 @@
 with campaignmember_insert as (
     select campaignmember_dwh_external_id, in_product_trial_request_campaignid as campaignid, campaign_status, contact_dwh_external_id, lead_dwh_external_id, max(license_issued_at) as last_occurence
     from {{ ref('contact_in_product_trial_request') }}
-    where campaignmember_sfid5) }}
-
+    where campaignmember_sfid is null
+    {{ dbt_utils.group_by(n=5) }}
+    
     union all
 
     select campaignmember_dwh_external_id, in_product_trial_request_campaignid as campaignid, campaign_status, null, lead_dwh_external_id, max(license_issued_at) as last_occurence
