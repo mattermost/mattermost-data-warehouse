476efdce872f3bf4f8ed93238048a162308d4819
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-04-13 22:25:28 -0700

    final changes

--- transform/snowflake-dbt/models/hightouch/contact_in_product_trial_request.sql
+++ transform/snowflake-dbt/models/hightouch/contact_in_product_trial_request.sql
@@ -28,13 +28,11 @@ with recent_in_product_trial_requests as (
         recent_in_product_trial_requests.email,
         recent_in_product_trial_requests.first_name,
         recent_in_product_trial_requests.last_name,
-        recent_in_product_trial_requests.ownerid,
         recent_in_product_trial_requests.most_recent_action,
         recent_in_product_trial_requests.most_recent_action_detail,
         recent_in_product_trial_requests.most_recent_lead_source,
         recent_in_product_trial_requests.most_recent_lead_source_detail,
         not recent_in_product_trial_requests.receive_emails_accepted as optedoutofemail,
-        recent_in_product_trial_requests.license_issued_at,
         '7013p000001NkNoAAK' as in_product_trial_request_campaignid,
         'Responded' as campaign_status,
         contact.dwh_external_id__c as contact_dwh_external_id,
@@ -42,13 +40,14 @@ with recent_in_product_trial_requests as (
         lead.dwh_external_id__c as lead_dwh_external_id,
         lead.sfid as lead_sfid,
         campaignmember.sfid as campaignmember_sfid,
-        coalesce(campaignmember.dwh_external_id__c, uuid_string()) as campaignmember_dwh_external_id
+        coalesce(campaignmember.dwh_external_id__c, uuid_string()) as campaignmember_dwh_external_id,
+        max(recent_in_product_trial_requests.license_issued_at) as license_issued_at
     from recent_in_product_trial_requests
     join {{ ref('contact') }} on lower(contact.email) = recent_in_product_trial_requests.email or recent_in_product_trial_requests.trial_requests_sfid = contact.sfid
     left join  {{ ref('lead') }} on contact.sfid = lead.convertedcontactid
     left join {{ ref('campaignmember') }} on contact.sfid = campaignmember.contactid and campaignmember.campaignid = '7013p000001NkNoAAK'
     where (contact.trial_req_date__c is null or contact.trial_req_date__c::date < license_issued_at::date)
-    {{ dbt_utils.group_by(n=18) }}
+    {{ dbt_utils.group_by(n=16) }}
 )
 
 select * from contact_in_product_trial_request
--- transform/snowflake-dbt/models/hightouch/lead_in_product_trial_request.sql
+++ transform/snowflake-dbt/models/hightouch/lead_in_product_trial_request.sql
@@ -16,6 +16,7 @@ with recent_in_product_trial_requests as (
         'In-Product Trial Request' as most_recent_action_detail,
         'Referral' as most_recent_lead_source,
         'MM Product' as most_recent_lead_source_detail,
+        case when site_name = 'Mattermost' then 'Unknown' else site_name end as company,
         receive_emails_accepted,
         trial_requests.sfid as trial_requests_sfid,
         max(license_issued_at) as license_issued_at
@@ -23,30 +24,30 @@ with recent_in_product_trial_requests as (
     where not exists (select 1 from {{ ref('contact') }} where lower(contact.email) = lower(trial_requests.email) or trial_requests.sfid = contact.sfid)
         and trial_requests.site_url != 'https://mattermost.com'
         and REGEXP_LIKE(UPPER(email),'^[A-Z0-9._%+-/!#$%&''*=?^_`{|}~]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$')
-    {{ dbt_utils.group_by(n=10) }}
+    {{ dbt_utils.group_by(n=11) }}
 ), lead_in_product_trial_request as (
     select 
         recent_in_product_trial_requests.email,
         coalesce(lead.firstname,recent_in_product_trial_requests.first_name) as first_name,
         coalesce(lead.firstname,recent_in_product_trial_requests.last_name) as last_name,
-        recent_in_product_trial_requests.ownerid,
         recent_in_product_trial_requests.most_recent_action,
         recent_in_product_trial_requests.most_recent_action_detail,
         recent_in_product_trial_requests.most_recent_lead_source,
         recent_in_product_trial_requests.most_recent_lead_source_detail,
         not recent_in_product_trial_requests.receive_emails_accepted as optedoutofemail,
-        recent_in_product_trial_requests.license_issued_at,
         '7013p000001NkNoAAK' as in_product_trial_request_campaignid,
         'Responded' as campaign_status,
         coalesce(lead.dwh_external_id__c, uuid_string()) as lead_dwh_external_id,
         lead.sfid as lead_sfid,
         campaignmember.sfid as campaignmember_sfid,
-        coalesce(campaignmember.dwh_external_id__c, uuid_string()) as campaignmember_dwh_external_id
+        coalesce(campaignmember.dwh_external_id__c, uuid_string()) as campaignmember_dwh_external_id,
+        max(coalesce(lead.company,recent_in_product_trial_requests.company)) as company,
+        max(recent_in_product_trial_requests.license_issued_at) as license_issued_at
     from recent_in_product_trial_requests
     left join {{ ref('lead') }} on lower(lead.email) = recent_in_product_trial_requests.email or recent_in_product_trial_requests.trial_requests_sfid = lead.sfid 
     left join {{ ref('campaignmember') }} on lead.sfid = campaignmember.leadid and campaignmember.campaignid = '7013p000001NkNoAAK'
     where (lead.request_a_trial_date__c is null or lead.request_a_trial_date__c::date < license_issued_at::date)
-    {{ dbt_utils.group_by(n=16) }}
+    {{ dbt_utils.group_by(n=14) }}
 )
 
 select * from lead_in_product_trial_request
