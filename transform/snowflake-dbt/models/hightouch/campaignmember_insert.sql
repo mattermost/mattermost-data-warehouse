354fdf8d7b3b87515363ae844f9c213415b879e6
Rachel Bradley-Haas <rachelbradley-haas@Rachels-iMac.local>
2021-04-13 22:13:59 -0700

    add group by

--- transform/snowflake-dbt/models/hightouch/campaignmember_insert.sql
+++ transform/snowflake-dbt/models/hightouch/campaignmember_insert.sql
@@ -7,15 +7,16 @@
 
 
 with campaignmember_insert as (
-    select campaignmember_dwh_external_id, max(license_issued_at) as last_occurence, in_product_trial_request_campaignid as campaignid, campaign_status, contact_dwh_external_id, lead_dwh_external_id
+    select campaignmember_dwh_external_id, in_product_trial_request_campaignid as campaignid, campaign_status, contact_dwh_external_id, lead_dwh_external_id, max(license_issued_at) as last_occurence
     from {{ ref('contact_in_product_trial_request') }}
-    where campaignmember_sfid is null
+    where campaignmember_sfid5) }}
 
     union all
 
-    select campaignmember_dwh_external_id, max(license_issued_at) as last_occurence, in_product_trial_request_campaignid as campaignid, campaign_status, null, lead_dwh_external_id
+    select campaignmember_dwh_external_id, in_product_trial_request_campaignid as campaignid, campaign_status, null, lead_dwh_external_id, max(license_issued_at) as last_occurence
     from {{ ref('lead_in_product_trial_request') }}
     where campaignmember_sfid is null
+    {{ dbt_utils.group_by(n=5) }}
 )
 
 select * from campaignmember_insert
--- transform/snowflake-dbt/models/hightouch/campaignmember_update.sql
+++ transform/snowflake-dbt/models/hightouch/campaignmember_update.sql
@@ -7,15 +7,17 @@
 
 
 with campaignmember_update as (
-    select campaignmember_sfid, max(license_issued_at) as last_occurence, campaign_status
+    select campaignmember_sfid, campaign_status, max(license_issued_at) as last_occurence
     from {{ ref('contact_in_product_trial_request') }}
     where campaignmember_sfid is not null
+    {{ dbt_utils.group_by(n=2) }}
 
     union all
 
-    select campaignmember_sfid, max(license_issued_at) as last_occurence, campaign_status
+    select campaignmember_sfid, campaign_status, max(license_issued_at) as last_occurence
     from {{ ref('lead_in_product_trial_request') }}
     where campaignmember_sfid is not null
+    {{ dbt_utils.group_by(n=2) }}
 )
 
 select * from campaignmember_update
