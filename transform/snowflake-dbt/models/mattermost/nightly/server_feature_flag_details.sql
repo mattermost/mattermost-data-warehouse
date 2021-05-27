a6474764b8413ef58b6a5eea1a8a98657eddf77e
Eric <43073688+enelson720@users.noreply.github.com>
2021-05-27 07:16:19 -0700

    Feature Flags (#716)
    * Adding logic for IC running totals.
    
    * Adding licensing fix.
    
    * Adding licensing fix.
    
    * NPS Fix
    
    * NPS Fix
    
    * NPS Fix
    
    * NPS Fix
    
    * Incident Collaobration Measure additions.
    
    * Incident Collaobration Measure additions.
    
    * Incident Collaobration Measure additions.
    
    * Adding first product edition.
    
    * Adding feature flag telemetry data model.
    
    * Adding timestamp
    
    * Adding timestamp
    
    * Adding date fix to feature flag model.
    
    * Fixing feature flag dupes.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/mattermost/nightly/server_feature_flag_details.sql
+++ transform/snowflake-dbt/models/mattermost/nightly/server_feature_flag_details.sql
@@ -9,7 +9,7 @@
 WITH max_flag_timestamp       AS (
     SELECT
         timestamp::DATE AS date
-      , user_id
+      , user_id AS server_id
       , MAX(r.timestamp)  AS max_timestamp
     FROM {{ ref('feature_flag_telemetry') }} r
     WHERE timestamp::DATE <= CURRENT_DATE
@@ -23,11 +23,37 @@ WITH max_flag_timestamp       AS (
 ),
 
 server_feature_flag_details AS (
-    SELECT ff.*
+    SELECT 
+        TIMESTAMP
+      , TEST_BOOL_FEATURE
+      , ORIGINAL_TIMESTAMP
+      , CONTEXT_TRAITS_INSTALLATION_ID
+      , CLOUD_DELINQUENT_EMAIL_JOBS_ENABLED
+      , USER_ID
+      , UUID_TS
+      , APPS_ENABLED
+      , COLLAPSED_THREADS
+      , ENABLE_REMOTE_CLUSTER_SERVICE
+      , ID
+      , CONTEXT_IP
+      , EVENT_TEXT
+      , RECEIVED_AT
+      , TEST_FEATURE
+      , CUSTOM_USER_STATUSES
+      , EVENT
+      , SENT_AT
+      , PLUGIN_APPS
+      , ANONYMOUS_ID
+      , CONTEXT_REQUEST_IP
+      , PLUGIN_INCIDENT_MANAGEMENT
+      , CUSTOM_DATA_RETENTION_ENABLED
+      , CONTEXT_LIBRARY_NAME
+      , CONTEXT_LIBRARY_VERSION
     FROM {{ ref('feature_flag_telemetry')}} ff
     JOIN max_flag_timestamp        mt
-                   ON ff.user_id = mt.user_id
+                   ON ff.user_id = mt.server_id
                        AND mt.max_timestamp = ff.timestamp
+    {{dbt_utils.group_by(n=25)}}
 )
 
 SELECT *
