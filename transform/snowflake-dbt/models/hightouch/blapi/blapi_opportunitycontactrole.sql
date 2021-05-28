40b80be6edf40c70702d6154142007e55665e967
Alex Dovenmuehle <alex@bigtimedata.io>
2021-05-28 16:21:59 -0400

    Hightouch sync for blapi.

---
+++ transform/snowflake-dbt/models/hightouch/blapi/blapi_opportunitycontactrole.sql
@@ -0,0 +1,36 @@
+{{config({
+    "schema": "hightouch",
+    "materialized": "view",
+    "tags":["hourly","blapi"]
+  })
+}}
+
+WITH onprem_ocrs_to_sync as (
+    SELECT
+        customers_with_onprem_subs.*,
+        '0053p0000064nt8AAA' AS ownerid
+    FROM {{ ref('customers_with_onprem_subs') }}
+    LEFT JOIN {{ ref('opportunity') }}
+        ON customers_with_onprem_subs.opportunity_external_id = opportunity.dwh_external_id__c
+            OR customers_with_onprem_subs.stripe_charge_id = opportunity.stripe_id__c
+    LEFT JOIN {{ ref('contact') }}
+        ON customers_with_onprem_subs.email = contact.email
+            OR customers_with_onprem_subs.contact_external_id = contact.dwh_external_id__c
+    LEFT JOIN {{ ref('opportunitycontactrole') }}
+        ON contact.sfid = opportunitycontactrole.contactid
+            AND opportunity.sfid = opportunitycontactrole.opportunityid
+    WHERE opportunity.sfid IS NULL AND contact.sfid IS NULL AND opportunitycontactrole.sfid IS NULL
+)
+SELECT *,
+    UUID_STRING(
+        '78157189-82de-4f4d-9db3-88c601fbc22e',
+        onprem_ocrs_to_sync.contact_external_id || 'billingcontact')
+    AS ocr_external_id,
+'Billing Contact' as role, false as isprimary FROM onprem_ocrs_to_sync
+UNION
+SELECT *,
+    UUID_STRING(
+        '78157189-82de-4f4d-9db3-88c601fbc22e',
+        onprem_ocrs_to_sync.contact_external_id || 'technicalevaluator')
+    AS oocr_external_id,
+'Technical Evaluator' as role, true as isprimary FROM onprem_ocrs_to_sync
--- transform/snowflake-dbt/models/stripe/subscriptions.sql
+++ transform/snowflake-dbt/models/stripe/subscriptions.sql
@@ -7,7 +7,7 @@
 }}
 
 WITH subscriptions AS (
-    SELECT 
+    SELECT
         subscriptions.billing
         ,subscriptions.billing_cycle_anchor
         ,subscriptions.cancel_at_period_end
@@ -22,6 +22,12 @@ WITH subscriptions AS (
         ,subscriptions.metadata:"cws-blapi-subscription"::varchar as cws_blapi_subscription
         ,subscriptions.metadata:"cws-installation"::varchar as cws_installation
         ,subscriptions.metadata:"billing-type"::varchar as billing_type
+        ,subscriptions.metadata:"cws-renewed-from-sub"::varchar as renewed_from_sub_id
+        ,subscriptions.metadata:"cws-license-id"::varchar as license_id
+        ,subscriptions.metadata:"sfdc-migrated-opportunity-sfid"::varchar as sfdc_migrated_opportunity_sfid
+        ,subscriptions.metadata:"internal_purchase_order"::varchar as purchase_order_num
+        ,TO_TIMESTAMP_NTZ(subscriptions.metadata:"cws-license-end-date"::int) as license_end_date
+        ,TO_TIMESTAMP_NTZ(subscriptions.metadata:"cws-actual-renewal-date"::int / 1000) as actual_renewal_date
         ,subscriptions."START"
         ,subscriptions.status
         ,subscriptions.updated
