a79236762650c49e2b8c689ef3a583e85239f617
Eric <43073688+enelson720@users.noreply.github.com>
2021-06-21 08:43:18 -0700

    Adding focalboard telemetry data. (#734)
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

---
+++ transform/snowflake-dbt/models/focalboard/focalboard_workspaces.sql
@@ -0,0 +1,49 @@
+{{config({
+    "materialized": "incremental",
+    "schema": "focalboard",
+    "tags":"hourly",
+    "unique_key":"id"
+  })
+}}
+
+WITH max_time AS (
+  SELECT 
+    user_id
+  , timestamp::date as date
+  , MAX(TIMESTAMP) AS max_time
+  FROM {{ source('hacktoberboard_prod', 'workspaces') }}
+  WHERE TIMESTAMP::DATE <= CURRENT_DATE
+  GROUP BY 1, 2
+),
+
+focalboard_workspaces AS (
+    SELECT 
+        workspaces.timestamp::date as logging_date
+        , workspaces.original_timestamp
+        , workspaces.workspaces
+        , workspaces.uuid_ts
+        , workspaces.context_request_ip
+        , workspaces.context_library_version
+        , workspaces.received_at
+        , workspaces.event
+        , workspaces.context_ip
+        , workspaces.context_library_name
+        , workspaces.timestamp
+        , workspaces.event_text
+        , workspaces.anonymous_id
+        , workspaces.id
+        , workspaces.sent_at
+        , workspaces.user_id
+      , {{ dbt_utils.surrogate_key(['workspaces.timestamp::date', 'workspaces.user_id'])}} as id
+    FROM {{ source('hacktoberboard_prod', 'workspaces') }} workspaces
+    JOIN max_time mt
+      ON workspaces.user_id = mt.user_id
+      AND workspaces.timestamp = mt.max_time
+    WHERE workspaces.TIMESTAMP::DATE <= CURRENT_DATE
+    {% if is_incremental() %}
+      and workspaces.timestamp::date >= (select max(logging_date) from {{ this }})
+    {% endif %}
+)
+
+SELECT *
+FROM focalboard_workspaces 
