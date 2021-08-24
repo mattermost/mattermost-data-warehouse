25332876c6cd7ac027ae8ba621e2c26363cb7b84
Alex Dovenmuehle <alex@bigtimedata.io>
2021-08-24 13:23:00 -0400

    Add owner ID back to blapi onprem opportunity.

--- transform/snowflake-dbt/models/hightouch/blapi/blapi_onprem_opportunity.sql
+++ transform/snowflake-dbt/models/hightouch/blapi/blapi_onprem_opportunity.sql
@@ -10,6 +10,7 @@ WITH onprem_opportunities_to_sync AS (
         customers_with_onprem_subs.*,
         'New Subscription' AS opportunity_type,
         'Online' as order_type,
+        '0053p0000064nt8AAA' AS ownerid,
         '6. Closed Won' as stagename,
         CASE WHEN account.name = 'Hold Public'
             THEN
