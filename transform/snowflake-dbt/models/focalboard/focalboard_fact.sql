f617928be4ecb8cafa12c22730e0d3e5a901c424
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2021-09-13 08:38:42 -0700

    Adding table reference.

--- transform/snowflake-dbt/models/focalboard/focalboard_fact.sql
+++ transform/snowflake-dbt/models/focalboard/focalboard_fact.sql
@@ -16,7 +16,7 @@ WITH focalboard_fact AS (
         , MAX(focalboard_activity.weekly_active_users) AS weekly_active_users_max
         , MAX(focalboard_activity.monthly_active_users) AS monthly_active_users_max
         , COUNT(DISTINCT CASE WHEN focalboard_activity.daily_active_users > 0 THEN focalboard_activity.timestamp::date ELSE NULL END) as days_active
-        , MAX(focalboard_activity.received_at) as latest_received
+        , MAX(focalboard_activity.received_at) as latest_received_at
     FROM {{ref('focalboard_activity')}}
     LEFT JOIN {{ ref('focalboard_server') }} server
       ON focalboard_activity.user_id = server.user_id
