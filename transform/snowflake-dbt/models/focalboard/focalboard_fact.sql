bbf3cf223fab4ecdfa760de78b6ff8e52ca7947e
Eric <43073688+enelson720@users.noreply.github.com>
2021-09-13 08:54:49 -0700

    Focalboard server (#800)
    * Adding server_id to focalboard.
    
    * Adding additional focalboard logic.
    
    * Adding table reference.
    
    * Adding table reference.
    
    * Adding table reference.
    
    * Adding table reference.
    
    * Adding table reference.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/focalboard/focalboard_fact.sql
+++ transform/snowflake-dbt/models/focalboard/focalboard_fact.sql
@@ -16,12 +16,13 @@ WITH focalboard_fact AS (
         , MAX(focalboard_activity.weekly_active_users) AS weekly_active_users_max
         , MAX(focalboard_activity.monthly_active_users) AS monthly_active_users_max
         , COUNT(DISTINCT CASE WHEN focalboard_activity.daily_active_users > 0 THEN focalboard_activity.timestamp::date ELSE NULL END) as days_active
+        , MAX(focalboard_activity.received_at) as latest_received_at
     FROM {{ref('focalboard_activity')}}
     LEFT JOIN {{ ref('focalboard_server') }} server
       ON focalboard_activity.user_id = server.user_id
     GROUP BY 1, 2
     {% if is_incremental() %}
-    HAVING MAX(focalboard_activity.received_at) >= (SELECT MAX({{this}}.received_at) FROM {{this}})
+    HAVING MAX(focalboard_activity.received_at) >= (SELECT MAX(latest_received_at) FROM {{this}})
     {% endif %}
 )
 
