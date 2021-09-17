a914fbdd72e0ce0a4e01fd36d65438bdea2e4a72
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2021-09-17 09:22:34 -0700

    Simplifying union macro.

--- transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
+++ transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
@@ -6,19 +6,23 @@
   })
 }}
 
-{% if is_incremental() %}
+{% set rudder_relations = get_rudder_relations(schema=["mm_telemetry_prod"], database='RAW', 
+                          table_inclusions="'event'") %}
+{{ union_relations(relations = rudder_relations) }}
+
+{# {% if is_incremental() %}
  WITH max_timestamp AS (
-   SELECT MAX(timestamp) - INTERVAL '1 DAY' AS max_time 
+   SELECT MAX(received_at) - INTERVAL '2 hours' AS max_time 
    FROM {{ this }} 
-   WHERE timestamp <= CURRENT_TIMESTAMP
+   WHERE received_at <= CURRENT_TIMESTAMP
  ),
   
 join_key AS (
   SELECT join_key.ID AS JOIN_KEY
   FROM {{ this }} join_key
   JOIN max_timestamp mt
-    ON join_key.timestamp >= mt.max_time
-  WHERE TIMESTAMP <= CURRENT_TIMESTAMP
+    ON join_key.received_at >= mt.max_time
+  WHERE received_at <= CURRENT_TIMESTAMP
   AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
 )
 
@@ -26,18 +30,18 @@ join_key AS (
 SELECT e.*
 FROM {{ source('mm_telemetry_prod', 'event') }} e
 JOIN max_timestamp mt
-    ON e.timestamp >= mt.max_time
+    ON e.received_at >= mt.max_time
 LEFT JOIN join_key a
   ON e.id = a.JOIN_KEY
-WHERE e.timestamp <= CURRENT_TIMESTAMP
+WHERE e.received_at <= CURRENT_TIMESTAMP
 AND a.JOIN_KEY IS NULL
 AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
 
 {% else %}
 SELECT e.*
 FROM {{ source('mm_telemetry_prod', 'event') }} e
-WHERE e.timestamp <= CURRENT_TIMESTAMP
+WHERE e.received_at <= CURRENT_TIMESTAMP
 AND COALESCE(type, event) NOT IN ('api_profiles_get_by_ids', 'api_profiles_get_by_usernames','api_profiles_get_in_channel', 'application_backgrounded', 'application_opened')
-{% endif %}
+{% endif %} #}
 
 
