94caa6aa26f9c889b9825c710d67f368512e8b9d
Eric <43073688+enelson720@users.noreply.github.com>
2021-09-22 11:09:12 -0700

    Add Column Union Macro (#816)
    * Fixing mql_to_close ambiguous column reference.
    
    * Adding cloud payment method added field.
    
    * Adding column add logic to union_relation macro.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/events/hourly/mobile_events.sql
+++ transform/snowflake-dbt/models/events/hourly/mobile_events.sql
@@ -7,4 +7,4 @@
 }}
 
 {% set rudder_relations = get_rudder_relations(schema=["mm_mobile_prod"], database='RAW',table_exclusions="'api_profiles_get_in_channel','api_profiles_get_by_usernames','api_profiles_get_by_ids'") %}
-{{ union_relations(relations = rudder_relations) }}
+{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1]) }}
