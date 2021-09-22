988ce9811c78d8b6e6e3d4e9ef175fa3b0a09269
Eric Nelson <enelson@Erics-MacBook-Pro.local>
2021-09-22 11:47:41 -0700

    Adding correct variable references to overlooked models using union_macro.

--- transform/snowflake-dbt/models/qa/incident_response_telemetry.sql
+++ transform/snowflake-dbt/models/qa/incident_response_telemetry.sql
@@ -6,5 +6,4 @@
 }}
 
 {% set rudder_relations = get_rudder_relations(schema=["incident_response_dev"], database='RAW') %}
-
-{{ union_relations(relations = rudder_relations) }}
+{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1]) }}
--- transform/snowflake-dbt/models/qa/server_telemetry.sql
+++ transform/snowflake-dbt/models/qa/server_telemetry.sql
@@ -6,5 +6,4 @@
 }}
 
 {% set rudder_relations = get_rudder_relations(schema=["mm_telemetry_qa", "mm_telemetry_rc"], database='RAW', table_exclusions="'event'") %}
-
-{{ union_relations(relations = rudder_relations) }}
+{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1]) }}
