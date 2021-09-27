7bac8816283d865169962c608f0349f11c1d0c6e
Eric <43073688+enelson720@users.noreply.github.com>
2021-09-27 07:49:49 -0700

    Dev Testing Flag (#830)
    * Adding correct variable references to overlooked models using union_macro.
    
    * Adding active user logic.
    
    * Adding focalboard fix for dupe records.
    
    * Adding focalboard fix for dupe records.
    
    * Adding dev testing reference fix.
    
    Co-authored-by: Eric Nelson <enelson@Erics-MacBook-Pro.local>

--- transform/snowflake-dbt/models/mattermost/hourly/server_fact.sql
+++ transform/snowflake-dbt/models/mattermost/hourly/server_fact.sql
@@ -57,6 +57,7 @@ WITH sdd AS (
       , MAX(sdde.POSTS)                                                                     AS max_posts
       , MAX(sdde.enabled_plugins)                                                           AS max_enabled_plugins
       , MAX(sdde.disabled_plugins)                                                           AS max_disabled_plugins
+      , MAX(CASE WHEN COALESCE(enable_testing, FALSE) or COALESCE(enable_developer_service, FALSE) THEN TRUE ELSE FALSE END)  AS dev_testing_enabled
     FROM {{ ref('server_daily_details_ext') }} sdde
     WHERE DATE <= CURRENT_DATE - INTERVAL '1 DAY'
     GROUP BY 1
@@ -338,6 +339,7 @@ WITH sdd AS (
         , MAX(lsd.retention_28day_users) AS retention_28day_users
         , MAX(COALESCE(nullif(TRIM(server_activity.last_ip_address), ''), NULLIF(fse.last_ip_address, ''))) AS last_ip_address
         , MIN(cpm.first_payment_method_date) AS cloud_payment_method_added
+        , MAX(server_details.dev_testing_enabled) AS dev_testing_enabled
     FROM sdd
         LEFT JOIN server_details
           ON sdd.server_id = server_details.server_id
