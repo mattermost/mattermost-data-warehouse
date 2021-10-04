7dd878a5a90e5fe703058fa45d4f5e85b1532603
nat-gunner <nat-gunner@users.noreply.github.com>
2021-10-04 15:36:01 -0700

    playing with mql filter

--- transform/snowflake-dbt/models/sales/marketing_funnel.sql
+++ transform/snowflake-dbt/models/sales/marketing_funnel.sql
@@ -47,6 +47,7 @@ lead_creation as (
 mql as (
     select date_trunc(month,most_recent_mql_date__c) as month, count(*) as count_mql
     from {{ ref('lead') }}
+    where First_MQL_Date__c != '' or (most_recent_mql_date__c != '' and Most_Recent_Reengaged_Date__c != '')
     group by 1
 ),
 
