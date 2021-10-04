21c209481b6cd1e2a1fc2db5a0391aa097d7ea8d
Eric Nelson <enelson@erics-mbp.lan>
2021-10-04 13:07:24 -0700

    Adding focalboard user event telemetry data model and schema reference.

---
+++ transform/snowflake-dbt/models/events/hourly/focalboard_event_telemetry.sql
@@ -0,0 +1,10 @@
+{{config({
+    "materialized": "incremental",
+    "schema": "events",
+    "tags":"preunion"
+  })
+}}
+
+{% set rudder_relations = get_rudder_relations(schema=["hacktoberboard_prod"], database='RAW', 
+                          table_inclusions="'event'") %}
+{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1]) }}
