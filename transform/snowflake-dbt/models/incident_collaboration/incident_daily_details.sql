c9b24a713a4db493305a802b821e9e36d9f44418
Eric Nelson <enelson@erics-mbp.lan>
2021-10-20 11:11:53 -0700

    Fixing incident_daily_details.

--- transform/snowflake-dbt/models/incident_collaboration/incident_daily_details.sql
+++ transform/snowflake-dbt/models/incident_collaboration/incident_daily_details.sql
@@ -60,62 +60,62 @@ incident_daily_details AS (
       , count(distinct COALESCE(events.playbook_id, events.playbookid))                               AS playbooks
       , COUNT(DISTINCT CASE WHEN event = 'playbook' and action = 'create' 
                             THEN events.id
-                            ELSE NULL END)                                                            AS playbooks_created
+                            ELSE NULL END)                                                            AS playbooks_created_alltime
       , COUNT(DISTINCT CASE WHEN event = 'playbook' and action = 'update' 
                             THEN events.id
-                            ELSE NULL END)                                                            AS playbooks_edited
+                            ELSE NULL END)                                                            AS playbooks_edited_alltime
       , COUNT(DISTINCT CASE WHEN event = 'playbook' and action = 'deleted' 
                             THEN events.id
-                            ELSE NULL END)                                                            AS playbooks_deleted
+                            ELSE NULL END)                                                            AS playbooks_deleted_alltime
       , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Reported' 
-                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS reported_incidents
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS reported_incidents_alltime
       , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Active'  
-                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS acknowledged_incidents
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS acknowledged_incidents_alltime
       , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Archived'  
-                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS archived_incidents
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS archived_incidents_alltime
       , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Resolved'  
-                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS resolved_incidents
-      , COUNT(DISTINCT COALESCE(events.useractualid, events.user_actual_id))                          AS incident_contributors
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS resolved_incidents_alltime
+      , COUNT(DISTINCT COALESCE(events.useractualid, events.user_actual_id))                          AS incident_contributors_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'incident' AND action = 'update_status' THEN events.id
-                                                                       ELSE NULL END)                 AS status_updates
+                                                                       ELSE NULL END)                 AS status_updates_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'incident' AND action = 'change_stage' THEN events.id
-                                                                      ELSE NULL END)                  AS stages_changed
+                                                                      ELSE NULL END)                  AS stages_changed_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'incident' AND action = 'add_timeline_event_from_post' THEN events.id
-                                                                                      ELSE NULL END)  AS timeline_events_added
+                                                                                      ELSE NULL END)  AS timeline_events_added_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'incident' AND action = 'change_commander' THEN events.id
-                                                                          ELSE NULL END)              AS commanders_changed
+                                                                          ELSE NULL END)              AS commanders_changed_alltime
       , COUNT(DISTINCT CASE WHEN event = 'frontend' AND action = 'timeline_tab_clicked' 
-                  THEN events.id ELSE NULL END)                                                       AS timeline_tab_clicks
-      , COUNT(DISTINCT CASE WHEN event = 'incident' AND action = 'end' THEN events.id ELSE NULL END)  AS ended_incident
+                  THEN events.id ELSE NULL END)                                                       AS timeline_tab_clicks_alltime
+      , COUNT(DISTINCT CASE WHEN event = 'incident' AND action = 'end' THEN events.id ELSE NULL END)  AS ended_incident_alltime
       , COUNT(
                 DISTINCT CASE WHEN event = 'incident' AND action = 'restart' 
-                THEN events.id ELSE NULL END)                                                         AS restarted_incident
+                THEN events.id ELSE NULL END)                                                         AS restarted_incident_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'tasks' AND action = 'publish_retrospective' THEN events.id
-                                                                            ELSE NULL END)            AS retrospectives_published
+                                                                            ELSE NULL END)            AS retrospectives_published_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'tasks' AND action = 'update_retrospective' THEN events.id
-                                                                            ELSE NULL END)            AS retrospectives_updated
+                                                                            ELSE NULL END)            AS retrospectives_updated_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'tasks' AND action = 'set_assignee_for_task' THEN events.id
-                                                                            ELSE NULL END)            AS task_assignees_set
+                                                                            ELSE NULL END)            AS task_assignees_set_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'tasks' AND action = 'modify_task_state' THEN events.id
-                                                                        ELSE NULL END)                AS task_states_modified
+                                                                        ELSE NULL END)                AS task_states_modified_alltime
       , COUNT(DISTINCT CASE WHEN event = 'tasks' AND action = 'add_task' 
-                THEN events.id ELSE NULL END)                                                         AS tasks_added
+                THEN events.id ELSE NULL END)                                                         AS tasks_added_alltime
       , COUNT(DISTINCT CASE WHEN event = 'tasks' AND action = 'remove_task' 
-                THEN events.id ELSE NULL END)                                                         AS tasks_removed
+                THEN events.id ELSE NULL END)                                                         AS tasks_removed_alltime
       , COUNT(DISTINCT CASE WHEN event = 'tasks' AND action = 'rename_task' 
-              THEN events.id ELSE NULL END)                                                           AS tasks_renamed
+              THEN events.id ELSE NULL END)                                                           AS tasks_renamed_alltime
       , COUNT(DISTINCT CASE
                   WHEN event = 'tasks' AND action = 'run_task_slash_command' THEN events.id
-                                                                             ELSE NULL END)           AS task_slash_commands_run
-      , COUNT(CASE WHEN event = 'tasks' AND action = 'move_task' THEN events.id ELSE NULL END)          AS tasks_moved
+                                                                             ELSE NULL END)           AS task_slash_commands_run_alltime
+      , COUNT(CASE WHEN event = 'tasks' AND action = 'move_task' THEN events.id ELSE NULL END)          AS tasks_moved_alltime
       , COUNT(DISTINCT COALESCE(events.user_actual_id, useractualid)) AS version_users_to_date
       , COUNT(DISTINCT CASE WHEN events.timestamp::date = d.date 
                         THEN COALESCE(events.user_actual_id, useractualid) ELSE NULL END)             AS daily_active_users
@@ -123,6 +123,66 @@ incident_daily_details AS (
                   THEN COALESCE(events.user_actual_id, useractualid) ELSE NULL END)                   AS weekly_active_users
       , COUNT(DISTINCT CASE WHEN events.timestamp::date >= d.date - INTERVAL '30 DAYS'
                   THEN COALESCE(events.user_actual_id, useractualid) ELSE NULL END)                   AS monthly_active_users
+      , COUNT(DISTINCT CASE WHEN event = 'playbook' and action = 'create' and events.timestamp::date = d.date 
+                            THEN events.id
+                            ELSE NULL END)                                                            AS playbooks_created
+      , COUNT(DISTINCT CASE WHEN event = 'playbook' and action = 'update' and events.timestamp::date = d.date 
+                            THEN events.id
+                            ELSE NULL END)                                                            AS playbooks_edited
+      , COUNT(DISTINCT CASE WHEN event = 'playbook' and action = 'deleted' and events.timestamp::date = d.date
+                            THEN events.id
+                            ELSE NULL END)                                                            AS playbooks_deleted
+      , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Reported' and events.timestamp::date = d.date 
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS reported_incidents
+      , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Active'  and events.timestamp::date = d.date 
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS acknowledged_incidents
+      , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Archived'  and events.timestamp::date = d.date 
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS archived_incidents
+      , COUNT(DISTINCT CASE WHEN COALESCE(currentstatus, current_status) = 'Resolved'  and events.timestamp::date = d.date 
+                              THEN COALESCE(events.incident_id, events.incidentid) ELSE NULL END)     AS resolved_incidents
+      , COUNT(DISTINCT CASE WHEN events.timestamp::date = d.date THEN COALESCE(events.useractualid, events.user_actual_id)
+                        ELSE NULL END)                                                                AS incident_contributors
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'incident' AND action = 'update_status' and events.timestamp::date = d.date THEN events.id
+                                                                       ELSE NULL END)                 AS status_updates
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'incident' AND action = 'change_stage' and events.timestamp::date = d.date THEN events.id
+                                                                      ELSE NULL END)                  AS stages_changed
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'incident' AND action = 'add_timeline_event_from_post' and events.timestamp::date = d.date THEN events.id
+                                                                                      ELSE NULL END)  AS timeline_events_added
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'incident' AND action = 'change_commander' and events.timestamp::date = d.date THEN events.id
+                                                                          ELSE NULL END)              AS commanders_changed
+      , COUNT(DISTINCT CASE WHEN event = 'frontend' AND action = 'timeline_tab_clicked' and events.timestamp::date = d.date 
+                  THEN events.id ELSE NULL END)                                                       AS timeline_tab_clicks
+      , COUNT(DISTINCT CASE WHEN event = 'incident' AND action = 'end' and events.timestamp::date = d.date
+                              THEN events.id ELSE NULL END)                                           AS ended_incident
+      , COUNT(
+                DISTINCT CASE WHEN event = 'incident' AND action = 'restart' and events.timestamp::date = d.date 
+                THEN events.id ELSE NULL END)                                                         AS restarted_incident
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'tasks' AND action = 'publish_retrospective' and events.timestamp::date = d.date THEN events.id
+                                                                            ELSE NULL END)            AS retrospectives_published
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'tasks' AND action = 'update_retrospective' and events.timestamp::date = d.date THEN events.id
+                                                                            ELSE NULL END)            AS retrospectives_updated
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'tasks' AND action = 'set_assignee_for_task' and events.timestamp::date = d.date THEN events.id
+                                                                            ELSE NULL END)            AS task_assignees_set
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'tasks' AND action = 'modify_task_state' and events.timestamp::date = d.date THEN events.id
+                                                                        ELSE NULL END)                AS task_states_modified
+      , COUNT(DISTINCT CASE WHEN event = 'tasks' AND action = 'add_task' and events.timestamp::date = d.date 
+                THEN events.id ELSE NULL END)                                                         AS tasks_added
+      , COUNT(DISTINCT CASE WHEN event = 'tasks' AND action = 'remove_task' and events.timestamp::date = d.date 
+                THEN events.id ELSE NULL END)                                                         AS tasks_removed
+      , COUNT(DISTINCT CASE WHEN event = 'tasks' AND action = 'rename_task' and events.timestamp::date = d.date 
+              THEN events.id ELSE NULL END)                                                           AS tasks_renamed
+      , COUNT(DISTINCT CASE
+                  WHEN event = 'tasks' AND action = 'run_task_slash_command' and events.timestamp::date = d.date THEN events.id
+                                                                             ELSE NULL END)           AS task_slash_commands_run
+      , COUNT(CASE WHEN event = 'tasks' AND action = 'move_task' and events.timestamp::date = d.date THEN events.id ELSE NULL END)          AS tasks_moved
     FROM dates d
     JOIN {{ ref('incident_response_events') }} events
       ON d.server_id = COALESCE(events.user_id, events.anonymous_id)
