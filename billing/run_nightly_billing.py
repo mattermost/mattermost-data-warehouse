9922ad5532bbb7dd69afae36bbd05a88d90fd27b
Alex Dovenmuehle <alex@bigtimedata.io>
2021-10-20 13:31:28 -0400

    Small change to try to get invoice build to run correctly.

--- billing/run_nightly_billing.py
+++ billing/run_nightly_billing.py
@@ -67,13 +67,16 @@ def main():
                         url, json=payload, headers=header, timeout=(0.1, 10)
                     )
                     break
-                except requests.exceptions.ReadTimeout:
-                    pass
+                except requests.exceptions.RequestException:
+                    message = f"Retrying: Error building invoice for subscription {sub['id']} with dates {start_date}-{end_date}"
+                    logging.error(message)
 
             if resp and resp.status_code != requests.codes.ok:
                 message = f"Error building invoice for subscription {sub['id']} with dates {start_date}-{end_date}"
                 logging.error(message)
                 errors.append(message)
+            else:
+                logging.info(f"Successfully built invoice for subscription {sub.id}")
 
         if errors:
             raise Exception("\n".join(errors))
