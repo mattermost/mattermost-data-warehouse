7790ea5841eb32d3062b99e64d2846b7c78391c4
Eric <43073688+enelson720@users.noreply.github.com>
2021-11-04 08:54:50 -0700

    Page load (#866)
    * Adding performance events nightly data model.
    
    * Adding performance events nightly data model.
    
    Co-authored-by: Eric Nelson <enelson@erics-mbp.lan>

---
+++ transform/snowflake-dbt/models/events/nightly/performance_events.sql
@@ -0,0 +1,11 @@
+{{config({
+    "materialized": "incremental",
+    "schema": "events",
+    "tags":"nightly",
+    "snowflake_warehouse": "transform_xs"
+  })
+}}
+
+{% set rudder_relations = get_rudder_relations(schema=["mm_telemetry_prod"], database='RAW', 
+                          table_inclusions="'event'") %}
+{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1]) }}
