3645ce09e28d091d9023384b7c50ac43f3e344c9
Alex Dovenmuehle <alex@bigtimedata.io>
2021-12-15 12:48:38 -0800

    Added external ID models to backfill any salesforce objects that are missing an external ID. (#879)

---
+++ transform/snowflake-dbt/models/hightouch/operations/line_item_external_id.sql
@@ -0,0 +1,17 @@
+{{config({
+    "schema": "hightouch",
+    "materialized": "view",
+    "tags":["hourly","blapi"]
+  })
+}}
+
+select
+    sfid,
+    left(sfid, 8) || '-'
+    || substring(sfid, 9, 4) || '-'
+    || substring(sfid, 13, 4) || '-'
+    || substring(sfid, 15, 4) || '-'
+    || right(sfid, 12) as dwh_external_id__c
+from
+    {{ ref('opportunitylineitem') }}
+where dwh_external_id__c is null
