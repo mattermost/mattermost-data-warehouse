dc17e17e8e09483ca4bbaa313ce2610746868301
Alex Dovenmuehle <alex@bigtimedata.io>
2022-01-06 10:14:20 -0500

    Added cloud invoice sync to SFDC for monthly billing.

---
+++ transform/snowflake-dbt/models/blapi/cloud_subscriptions.sql
@@ -0,0 +1,30 @@
+{{config({
+    "schema": "blapi",
+    "unique_key":"id",
+    "tags":["hourly","blapi"]
+  })
+}}
+
+WITH latest_payment AS (
+    SELECT
+        payments.stripe_charge_id,
+        payments.subscription_id,
+        'ONL' || payments.invoice_number AS invoice_number,
+        ROW_NUMBER() OVER (PARTITION BY payments.subscription_id ORDER BY payments.created_at DESC) as row_num
+    FROM {{ ref('payments') }}
+), subscriptions AS (
+    SELECT
+        s.*,
+        p.sku,
+        latest_payment.stripe_charge_id,
+        latest_payment.invoice_number,
+        ROW_NUMBER() OVER (PARTITION BY s.id ORDER BY s.transaction_id DESC) as row_num
+    FROM {{ source('blapi', 'subscriptions_version') }} s
+    JOIN {{ source('blapi', 'products') }} p ON s.product_id = p.id
+    JOIN latest_payment ON s.id = latest_payment.subscription_id AND latest_payment.row_num = 1
+    WHERE s.subscription_version_id_mod
+        AND p.name in ('Mattermost Cloud', 'Cloud Enterprise', 'Cloud Starter', 'Cloud Professional')
+        AND s.cloud_dns is not null
+)
+SELECT * FROM subscriptions
+WHERE row_num = 1
---
+++ transform/snowflake-dbt/models/hightouch/blapi/blapi_cloud_account.sql
@@ -0,0 +1,21 @@
+{{config({
+    "schema": "hightouch",
+    "materialized": "view",
+    "tags":["hourly","blapi"]
+  })
+}}
+
+WITH cloud_accounts_to_sync as (
+    SELECT
+        customers_with_cloud_subs.*,
+        '0053p0000064nt8AAA' AS ownerid
+    FROM {{ ref('customers_with_cloud_subs') }}
+    LEFT JOIN {{ ref('account') }}
+        ON customers_with_cloud_subs.domain = account.cbit__clearbitdomain__c
+            OR customers_with_cloud_subs.account_external_id = account.dwh_external_id__c
+    LEFT JOIN {{ source('orgm', 'account_domain_mapping') }}
+        ON customers_with_cloud_subs.domain = account_domain_mapping.domain
+    WHERE account.id IS NULL AND account_domain_mapping.accountid IS NULL
+    AND customers_with_cloud_subs.hightouch_sync_eligible
+)
+SELECT * FROM cloud_accounts_to_sync
