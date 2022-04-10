0e6d5db8db29e4f2d1b8de08307a6ebb6920634f
Kevin Fayle <kevin.fayle@mattermost.com>
2022-04-10 16:41:00 +0000

    switching back to license id matching for table join

--- transform/snowflake-dbt/models/hightouch/operations/lead_update_trial_activation.sql
+++ transform/snowflake-dbt/models/hightouch/operations/lead_update_trial_activation.sql
@@ -7,10 +7,11 @@
 with lead_update_trial_activation as (
   select l.Id, l.Email, l.Trial_License_Id__c, lsf.LICENSE_ACTIVATION_DATE as Activation_Date
   from {{ ref('lead') }} l 
-  left join {{ ref('license_server_fact') }} lsf on lsf.LICENSE_EMAIL = l.Email -- on lsf.LICENSE_ID = l.Trial_License_Id__c
+  left join {{ ref('license_server_fact') }} lsf on lsf.LICENSE_ID = l.Trial_License_Id__c -- on lsf.LICENSE_EMAIL = l.Email
   where lsf.LICENSE_ACTIVATION_DATE is not null and 
   l.Trial_License_Id__c is not null and
-  l.Trial_Activation_Date__c is null
+  l.Trial_Activation_Date__c is null and
+  DATEDIFF(day, lsf.START_DATE, current_timestamp) < 14
 )
 
-select Id, Activation_Date from lead_update_trial_activation
+select Id, Trial_License_Id__c, Activation_Date from lead_update_trial_activation
