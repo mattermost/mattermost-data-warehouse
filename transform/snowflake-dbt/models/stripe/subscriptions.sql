d8f54dfaf50943f47a32dc1dcc5d616990bb8ade
Munish Sharma <munish7771@gmail.com>
2022-06-07 12:24:54 +0530

    Revert "MM-42451-Freemium-cloud-subscriptions-flow  (#925)" (#934)
    This reverts commit 569f3fd77d1fe536e44457bc1d5e0272996626d7.

--- transform/snowflake-dbt/models/stripe/subscriptions.sql
+++ transform/snowflake-dbt/models/stripe/subscriptions.sql
@@ -29,7 +29,6 @@ WITH subscriptions AS (
         ,subscriptions.plan:"name"::varchar as edition
         ,subscriptions.metadata:"sfdc-migrated-opportunity-sfid"::varchar as sfdc_migrated_opportunity_sfid
         ,subscriptions.metadata:"internal_purchase_order"::varchar as purchase_order_num
-        ,subscriptions.metadata:"cws-date-converted-to-paid"::varchar as date_converted_to_paid
         ,TO_TIMESTAMP_NTZ(subscriptions.metadata:"cws-license-end-date"::int) as license_end_date
         ,TO_TIMESTAMP_NTZ(subscriptions.metadata:"cws-actual-renewal-date"::int / 1000) as actual_renewal_date
         ,subscriptions."START"
