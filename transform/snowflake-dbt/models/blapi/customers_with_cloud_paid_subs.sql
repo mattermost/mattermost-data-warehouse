30f912d0cc5eebaac1fd92d593d73c743d07dcee
Munish Sharma <munish7771@gmail.com>
2022-07-06 21:14:40 +0530

    Updated models

---
+++ transform/snowflake-dbt/models/blapi/customers_with_cloud_paid_subs.sql
@@ -0,0 +1,46 @@
+{{config({
+    "schema": "hightouch",
+    "unique_key":"id",
+    "tags":["hourly","blapi"]
+  })
+}}
+-- customer info along with current paid subscriptions
+-- professional subscriptions with status cancelled and active will be considered as new subscriptions. Total = active + cancelled
+with current_subscriptions AS(
+    SELECT 
+        *,
+        cloud_paid_subscriptions."START" as start_date
+    FROM       
+        {{ ref('cloud_paid_subscriptions') }}
+        WHERE row_num = 1
+), customers_with_paid_subs AS (
+    SELECT
+        customers.id as customer_id,
+        UUID_STRING(
+                '78157189-82de-4f4d-9db3-88c601fbc22e',
+                customers.id)
+        AS account_external_id,
+        UUID_STRING(
+                '78157189-82de-4f4d-9db3-88c601fbc22e',
+                current_subscriptions.id)
+        AS opportunity_external_id,
+        customers.email,
+        customers_blapi.first_name,
+        coalesce(NULLIF(TRIM(customers_blapi.last_name), ''), customers.email) as last_name,
+        SPLIT_PART(customers.email, '@', 2) as domain,
+        -- invoices.id as invoice_id,
+        current_subscriptions.id as subscription_id,
+        current_subscriptions.cws_dns as cloud_dns,
+        current_subscriptions.name as sku,
+        current_subscriptions.company,
+        left(coalesce(current_subscriptions.company, customers.email), 40) as company_name,
+        current_subscriptions.status,
+        to_varchar(current_subscriptions.start_date, 'yyyy-MM-dd HH:mm:ss.SSS Z') as start_date,
+        to_varchar(DATEADD(month, 1, current_subscriptions.start_date), 'yyyy-MM-dd HH:mm:ss.SSS Z') as end_date, -- only for cloud professional subscriptions
+        current_subscriptions.created >= '2022-06-14' as hightouch_sync_eligible
+    FROM {{ source('stripe','customers') }} 
+        JOIN current_subscriptions ON customers.id = current_subscriptions.customer
+        -- LEFT JOIN {{ source('stripe', 'invoices') }} ON invoices.subscription = current_subscriptions.id
+        LEFT JOIN {{ source('blapi', 'customers') }} customers_blapi ON customers_blapi.stripe_id = customers.id
+)
+select * from customers_with_paid_subs
--- transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_opportunitylineitem.sql
+++ transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_opportunitylineitem.sql
@@ -5,13 +5,13 @@
   })
 }}
 
-WITH freemium_opportunitylineitems_to_sync AS (
+WITH opportunitylineitems_to_sync AS (
     SELECT DISTINCT
-        customers_with_freemium_subs.*,
+        customers_with_cloud_paid_subs.*,
         opportunitylineitem.sfid as opportunitylineitem_sfid,
         COALESCE(
             opportunitylineitem.dwh_external_id__c,
-            UUID_STRING('78157189-82de-4f4d-9db3-88c601fbc22e', customers_with_freemium_subs.invoice_number)
+            UUID_STRING('78157189-82de-4f4d-9db3-88c601fbc22e', customers_with_cloud_paid_subs.invoice_number)
         ) AS opportunitylineitem_external_id,
         '6. Closed Won' as stagename,
         '01t3p00000RcfjJAAR' as product_id,
@@ -21,13 +21,15 @@ WITH freemium_opportunitylineitems_to_sync AS (
         1 as quantity,
         'Discount' as pricing_method,
         0 as discount_calc,
-        false as sales_price_needs_to_be_updated
-    FROM {{ ref('customers_with_freemium_subs') }}
+        false as sales_price_needs_to_be_updated,
+        customers_with_cloud_paid_subs.subscription_id
+    FROM {{ ref('customers_with_cloud_paid_subs') }}
     LEFT JOIN {{ ref('opportunity') }} 
-        ON customers_with_freemium_subs.opportunity_external_id = opportunity.dwh_external_id__c
+        ON customers_with_cloud_paid_subs.opportunity_external_id = opportunity.dwh_external_id__c
     LEFT JOIN {{ ref('opportunitylineitem') }} 
         ON opportunitylineitem.opportunityid = opportunity.sfid
-    WHERE customers_with_freemium_subs.hightouch_sync_eligible
-    AND customers_with_freemium_subs.sku = 'Cloud Professional'
+    WHERE customers_with_cloud_paid_subs.hightouch_sync_eligible
+    AND customers_with_cloud_paid_subs.sku = 'Cloud Professional'
+    AND customers_with_cloud_paid_subs.status in ('active','canceled')
 )
-SELECT * FROM freemium_opportunitylineitems_to_sync
+SELECT * FROM opportunitylineitems_to_sync
