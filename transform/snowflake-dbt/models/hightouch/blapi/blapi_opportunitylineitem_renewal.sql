375276d0a819759e9ca9e4faf85808070f98a3cb
Munish Sharma <munish7771@gmail.com>
2022-08-12 19:53:55 +0530

    Update blapi_opportunitylineitem_renewal.sql

--- transform/snowflake-dbt/models/hightouch/blapi/blapi_opportunitylineitem_renewal.sql
+++ transform/snowflake-dbt/models/hightouch/blapi/blapi_opportunitylineitem_renewal.sql
@@ -16,8 +16,7 @@ WITH onprem_olis_to_sync AS (
         (customers_with_onprem_subs.listed_total - customers_with_onprem_subs.total)/customers_with_onprem_subs.listed_total as discount_percent,
         customers_with_onprem_subs.listed_total/customers_with_onprem_subs.num_seats * (1 - (customers_with_onprem_subs.listed_total - customers_with_onprem_subs.total)/customers_with_onprem_subs.listed_total) as unit_price,
         0 AS zero_amount,
-        customers_with_onprem_subs.total - customers_with_onprem_subs.renewed_from_total as expansion_amount,
-        customers_with_onprem_subs.total / customers_with_onprem_subs.num_seats AS list_price
+        customers_with_onprem_subs.total - customers_with_onprem_subs.renewed_from_total as expansion_amount
     FROM {{ ref('customers_with_onprem_subs') }}
     LEFT JOIN {{ ref('opportunitylineitem') }}
         ON customers_with_onprem_subs.subscription_version_id = opportunitylineitem.subs_version_id__c
