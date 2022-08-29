4aededb99b86bc6c162393ddbf972e59cc0a554f
Munish Sharma <munish7771@gmail.com>
2022-08-29 20:18:10 +0530

    Update trial_requests.sql

--- transform/snowflake-dbt/models/blapi/trial_requests.sql
+++ transform/snowflake-dbt/models/blapi/trial_requests.sql
@@ -38,6 +38,7 @@ WITH lead AS (
       WHERE license_issued_at >= (SELECT MAX(license_issued_at) FROM {{this}})
 
     {% endif %}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY trial_requests.id ORDER BY trial_requests.license_issued_at) = 1
 )
 
 SELECT distinct * FROM trial_requests
