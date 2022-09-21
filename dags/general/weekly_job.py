877c1f02d149e43366b4666842428ed24ae0fd88
Munish Sharma <munish7771@gmail.com>
2022-09-21 15:40:27 -0400

    Create weekly_job.py
    added as daily job

---
+++ dags/general/weekly_job.py
@@ -0,0 +1,75 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.models import Variable
+from dags.airflow_utils import (
+    DATA_IMAGE,
+    clone_repo_cmd,
+    mm_failed_task,
+    pod_defaults,
+    pod_env_vars,
+    xs_warehouse,
+)
+from dags.kube_secrets import (
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_TRANSFORM_ROLE,
+    SNOWFLAKE_TRANSFORM_SCHEMA,
+    SNOWFLAKE_TRANSFORM_WAREHOUSE,
+    SNOWFLAKE_TRANSFORM_DATABASE,
+    docs_WEBHOOK_URL
+)
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+env_vars = {**pod_env_vars, **{}}
+
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "owner": "airflow",
+    "on_failure_callback": mm_failed_task,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "sla": timedelta(hours=8),
+    "start_date": datetime(2019, 1, 1, 0, 0, 0),
+}
+
+# Create the DAG
+dag = DAG("daily_job", default_args=default_args, schedule_interval="0 12 * * 1")
+
+def get_container_operator(task_name):
+    cmd = f"""
+        {clone_repo_cmd} &&
+        cd mattermost-data-warehouse &&
+        export PYTHONPATH="/opt/bitnami/airflow/dags/git/mattermost-data-warehouse/:$PYTHONPATH" &&
+        python utils/post_docs_data.py
+    """
+    return KubernetesPodOperator(
+        **pod_defaults,
+        image=DATA_IMAGE,
+        task_id=task_name,
+        name=task_name,
+        secrets=[
+            SNOWFLAKE_ACCOUNT,
+            SNOWFLAKE_USER,
+            SNOWFLAKE_PASSWORD,
+            SNOWFLAKE_TRANSFORM_ROLE,
+            SNOWFLAKE_TRANSFORM_WAREHOUSE,
+            SNOWFLAKE_TRANSFORM_SCHEMA,
+            SNOWFLAKE_TRANSFORM_DATABASE,
+            docs_WEBHOOK_URL
+        ],
+        arguments=[cmd],
+        dag=dag,
+    )
+
+post_docs_feedback = get_container_operator(
+    "post-docs-feedback"
+)
+
+post_docs_feedback 
