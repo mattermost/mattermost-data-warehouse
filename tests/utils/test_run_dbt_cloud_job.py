dd4c08cfd50f89babe54cdfa486cc215eb352790
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2022-10-14 11:00:39 +0300

    MM-47325: use factory for requests expectations

--- tests/utils/test_run_dbt_cloud_job.py
+++ tests/utils/test_run_dbt_cloud_job.py
@@ -1,24 +1,22 @@
 import json
-from pathlib import Path
 
 import pytest
-from responses import Response
 
 from utils.run_dbt_cloud_job import trigger_dbt_run, poll_dbt_run
 
+EXPECTED_RUN_TRIGGER_URL = "https://cloud.getdbt.com/api/v2/accounts/1001/jobs/101/run/"
+EXPECTED_RUN_POLL_URL = "https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/"
 
-def test_should_trigger_run(responses):
+
+def test_should_trigger_run(responses, given_request_to):
     # GIVEN: DBT cloud ready to accept job
-    rsp = Response(method="POST", url="https://cloud.getdbt.com/api/v2/accounts/1001/jobs/101/run/",
-                   status=200, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body("job.200.json"))
-    responses.add(rsp)
+    given_request_to(EXPECTED_RUN_TRIGGER_URL, "job.200.json", method="POST")
 
     # WHEN: trigger job
     run_id = trigger_dbt_run(101, "Test Job")
 
     # THEN: expect request to have been sent
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/jobs/101/run/", 1)
+    responses.assert_call_count(EXPECTED_RUN_TRIGGER_URL, 1)
     assert json.loads(responses.calls[0].request.body) == {"cause": "Airflow run - Test Job"}
 
     # THEN: expect run id to have been properly loaded from response
@@ -26,93 +24,72 @@ def test_should_trigger_run(responses):
 
 
 @pytest.mark.parametrize("status,content", [(400, 'job.400.json'), (404, 'job.404.json')])
-def test_should_handle_failure(responses, status, content):
+def test_should_handle_failure(responses, given_request_to, status, content):
     # GIVEN: DBT cloud returns a non-200 response
-    rsp = Response(method="POST", url="https://cloud.getdbt.com/api/v2/accounts/1001/jobs/101/run/",
-                   status=status, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body(content))
-    responses.add(rsp)
+    given_request_to(EXPECTED_RUN_TRIGGER_URL, content, method="POST", status=status)
 
     # WHEN: trigger job
     run_id = trigger_dbt_run(101, "Test Job")
 
     # THEN: expect request to have been sent
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/jobs/101/run/", 1)
+    responses.assert_call_count(EXPECTED_RUN_TRIGGER_URL, 1)
     assert json.loads(responses.calls[0].request.body) == {"cause": "Airflow run - Test Job"}
 
     # THEN: expect run id to be None
     assert run_id is None
 
 
-def test_poll_dbt_run_job_completed(responses):
+def test_poll_dbt_run_job_completed(responses, given_request_to):
     # GIVEN: DBT cloud reports run as successful
-    rsp = Response(method="GET", url="https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/",
-                   status=200, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body("run.200.completed.json"))
-    responses.add(rsp)
+    given_request_to(EXPECTED_RUN_POLL_URL, "run.200.completed.json")
 
     # WHEN: poll for job completion
     poll_dbt_run(42)
 
     # THEN: expect run status to have been called once
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/", 1)
+    responses.assert_call_count(EXPECTED_RUN_POLL_URL, 1)
 
 
-def test_poll_dbt_run_job_should_wait_for_completion(responses):
-    # GIVEN: DBT cloud reports run as successful
-    rsp = Response(method="GET", url="https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/",
-                   status=200, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body("run.200.running.json"))
-    responses.add(rsp)
-    rsp = Response(method="GET", url="https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/",
-                   status=200, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body("run.200.completed.json"))
-    responses.add(rsp)
+def test_poll_dbt_run_job_should_wait_for_completion(responses, given_request_to):
+    # GIVEN: DBT cloud reports run as running then successful
+    given_request_to(EXPECTED_RUN_POLL_URL, "run.200.running.json")
+    given_request_to(EXPECTED_RUN_POLL_URL, "run.200.completed.json")
 
     # WHEN: poll for job completion
     poll_dbt_run(42)
 
     # THEN: expect run status to have been called twice
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/", 2)
+    responses.assert_call_count(EXPECTED_RUN_POLL_URL, 2)
 
 
-def test_poll_dbt_run_job_failed_job(responses):
+def test_poll_dbt_run_job_failed_job(responses, given_request_to):
     # GIVEN: DBT cloud reports run as failed
-    rsp = Response(method="GET", url="https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/",
-                   status=200, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body("run.200.failed.json"))
-    responses.add(rsp)
+    given_request_to(EXPECTED_RUN_POLL_URL, "run.200.failed.json")
 
     # WHEN: poll for job completion, an exception must be raised
     with pytest.raises(Exception, match='Error running dbt cloud job -- make this link to dbt cloud'):
         poll_dbt_run(42)
 
     # THEN: expect run status to have been called once
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/", 1)
+    responses.assert_call_count(EXPECTED_RUN_POLL_URL, 1)
 
 
-def test_poll_dbt_run_poll_timeout(responses):
-    # GIVEN: DBT cloud reports run as failed
-    rsp = Response(method="GET", url="https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/",
-                   status=200, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body("run.200.running.json"))
-    responses.add(rsp)
+def test_poll_dbt_run_poll_timeout(responses, given_request_to):
+    # GIVEN: DBT cloud reports run as running
+    given_request_to(EXPECTED_RUN_POLL_URL, "run.200.running.json")
 
     # WHEN: poll for job completion, an exception must be raised
     with pytest.raises(Exception, match='dbt Cloud job ran out of time'):
         poll_dbt_run(42)
 
     # THEN: expect run status to have been called once
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/", 3)
+    responses.assert_call_count(EXPECTED_RUN_POLL_URL, 3)
 
 
 @pytest.mark.parametrize("status,content", [(400, 'run.400.json'), (404, 'run.404.json')])
-def test_should_handle_failure(responses, status, content):
+def test_should_handle_failure(responses, given_request_to, status, content):
     # GIVEN: DBT cloud returns a non-200 response
-    rsp = Response(method="GET", url="https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/",
-                   status=status, headers={"Authorization": f"Token test-dbt-key", "Content-Type": "application/json"},
-                   body=with_response_body(content))
-    responses.add(rsp)
+    given_request_to(EXPECTED_RUN_POLL_URL, content, status=status)
 
     # WHEN: poll for job completion, retry on failures but fail due to timeout
     # TODO: reconsider how to handle failures from API
@@ -120,12 +97,5 @@ def test_should_handle_failure(responses, status, content):
         poll_dbt_run(42)
 
     # THEN: expect request to have been sent
-    responses.assert_call_count("https://cloud.getdbt.com/api/v2/accounts/1001/runs/42/", 3)
+    responses.assert_call_count(EXPECTED_RUN_POLL_URL, 3)
 
-
-def with_response_body(filename):
-    """
-    Loads a sample response body from a file.
-    """
-    with open(Path(__file__).parent / 'fixtures' / 'dbt' / filename) as fp:
-        return fp.read()
