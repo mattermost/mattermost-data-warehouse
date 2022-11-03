8177991fb3df7dca285b3adce7862950cd2cd0ae
Munish Sharma <munish7771@gmail.com>
2022-11-03 20:23:46 +0530

    initial commit

---
+++ dags/development/mattermost_operator.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+
+from operators.mattermost_operator import MattermostOperator
+from datetime import datetime
+MATTERMOST_CONN_ID = 'mattermost'
+
+
+dag = DAG('mattermost_operator_dag', description='Mattermost Send Message DAG',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+with dag:
+    mattermost = MattermostOperator(mattermost_conn_id=MATTERMOST_CONN_ID,
+                                    endpoint="",
+                                    text='Hello from mattermost operator\nCurrent run: {{ run_id }}',
+                                    icon_url='https://mattermost.com/wp-content/uploads/2022/02/icon.png',
+                                    task_id='notify')
+
+mattermost
---
+++ plugins/operators/mattermost_operator.py
@@ -0,0 +1,60 @@
+from airflow.plugins_manager import AirflowPlugin
+# airflow.providers.http.operators.http -> airflow 2
+from airflow.operators.http_operator import SimpleHttpOperator
+from hooks.mattermost_webhook_hook import *
+
+
+class MattermostOperator(SimpleHttpOperator):
+
+    """
+    Operator that allows sending messages to Mattermost. For more details on the parameters, please check
+    `Incoming webhooks <https://developers.mattermost.com/integrate/webhooks/incoming/#parameters>_` documentation.
+    :param http_conn_id: :ref:`http connection<howto/connection:http>` that has the base
+        url i.e https://mattermost.example.com/ and webhook's id under password.
+    :param text: The message to send.
+    :param channel:	Overrides the channel the message posts in. Use the channel’s name and not the display name, e.g.
+        use town-square, not Town Square.
+    :param username: Overrides the username the message posts as.
+    :param type: Sets the post type, mainly for use by plugins.
+    :param props: Sets the post props, a JSON property bag for storing extra or meta data on the post.
+    """
+
+    template_fields = (
+        'text',
+        'channel',
+        'username',
+        'type',
+        'props',
+    )
+
+    def __init__(
+        self,
+        *,
+        mattermost_conn_id,
+        text="",
+        channel=None,
+        username=None,
+        type=None,
+        props=None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.mattermost_conn_id = mattermost_conn_id
+        self.text = text
+        self.channel = channel
+        self.username = username
+        self.type = type
+        self.props = props
+
+    def hook(self):
+        return MattermostWebhookHook(
+            mattermost_conn_id=self.mattermost_conn_id,
+            text=self.text,
+            channel=self.channel,
+            username=self.username,
+            type=self.type,
+            props=self.props,
+        )
+
+    def execute(self, context):
+        self.hook().execute()
