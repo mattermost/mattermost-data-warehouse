2a08926db67ddd38b50409bf609852387b9cdaec
Munish Sharma <munish7771@gmail.com>
2022-11-17 17:01:19 +0530

    Update post_nps_data.py

--- utils/post_nps_data.py
+++ utils/post_nps_data.py
@@ -20,53 +20,53 @@ def post_nps():
         schema = os.getenv("SNOWFLAKE_TRANSFORM_SCHEMA")
         )
 
-try:
-    cur = conn.cursor()
-    nps_webhook_url = os.getenv("NPS_WEBHOOK_URL")
-    nps_webhook_url = nps_webhook_url.strip('\n')
-    sql = f'''
-    select distinct 
-        feedback, 
-        subcategory as category, 
-        score, 
-        server_version, 
-        case 
-            when sf.installation_id is null then 'Self-Hosted' 
-            when sf.installation_id is not null then 'Cloud' end as INSTALLATION_TYPE,
-        user_role, 
-        lsf.customer_name,
-        lsf.license_email,
-        sf.max_active_user_count as user_count
-    from ANALYTICS.MATTERMOST.NPS_USER_DAILY_SCORE nps
-    left join mattermost.server_fact sf on nps.server_id = sf.server_id
-    left join blp.license_server_fact lsf on nps.server_id = lsf.server_id 
-    left join analytics.mattermost.nps_feedback_classification fc on nps.user_id = fc.user_id and nps.server_id = fc.server_id and nps.last_feedback_date = fc.last_feedback_date
-    where
-        nps.last_feedback_DATE = CURRENT_DATE - 1 and subcategory <> 'Invalid' and 
-        feedback <> ''
-    '''
+    try:
+        cur = conn.cursor()
+        nps_webhook_url = os.getenv("NPS_WEBHOOK_URL")
+        nps_webhook_url = nps_webhook_url.strip('\n')
+        sql = f'''
+        select distinct 
+            feedback, 
+            subcategory as category, 
+            score, 
+            server_version, 
+            case 
+                when sf.installation_id is null then 'Self-Hosted' 
+                when sf.installation_id is not null then 'Cloud' end as INSTALLATION_TYPE,
+            user_role, 
+            lsf.customer_name,
+            lsf.license_email,
+            sf.max_active_user_count as user_count
+        from ANALYTICS.MATTERMOST.NPS_USER_DAILY_SCORE nps
+        left join mattermost.server_fact sf on nps.server_id = sf.server_id
+        left join blp.license_server_fact lsf on nps.server_id = lsf.server_id 
+        left join analytics.mattermost.nps_feedback_classification fc on nps.user_id = fc.user_id and nps.server_id = fc.server_id and nps.last_feedback_date = fc.last_feedback_date
+        where
+            nps.last_feedback_DATE = CURRENT_DATE - 1 and subcategory <> 'Invalid' and 
+            feedback <> ''
+        '''
 
-    cur.execute(sql)
-    out = cur.fetchall()
-    out = tuple(map(lambda x: format_row(x) , out))
-    out = tabulate(out, headers=['Feedback','Category','Score','Server Version','Installation Type','User Role','Customer Name', 'License Email', 'User Count'], tablefmt='github')
-    payload='{"text": "%s", "channel": "mattermost-nps-feedback"}' % out
-    nps_webhook_url = os.getenv("NPS_WEBHOOK_URL")
-    nps_webhook_url = nps_webhook_url.strip('\n')
-    response = requests.post(
-            nps_webhook_url, data=payload.encode('utf-8'),
-            headers={'Content-Type': 'application/json'}
+        cur.execute(sql)
+        out = cur.fetchall()
+        out = tuple(map(lambda x: format_row(x) , out))
+        out = tabulate(out, headers=['Feedback','Category','Score','Server Version','Installation Type','User Role','Customer Name', 'License Email', 'User Count'], tablefmt='github')
+        payload='{"text": "%s", "channel": "mattermost-nps-feedback"}' % out
+        nps_webhook_url = os.getenv("NPS_WEBHOOK_URL")
+        nps_webhook_url = nps_webhook_url.strip('\n')
+        response = requests.post(
+                nps_webhook_url, data=payload.encode('utf-8'),
+                headers={'Content-Type': 'application/json'}
+            )
+        if response.status_code != 200:
+            print('response is ',response.text)
+            raise ValueError(
+            'Request to Mattermost returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
         )
-    if response.status_code != 200:
-        print('response is ',response.text)
-        raise ValueError(
-        'Request to Mattermost returned an error %s, the response is:\n%s'
-        % (response.status_code, response.text)
-    )
 
-    finally:
-        cur.close()
-        conn.close()
+        finally:
+            cur.close()
+            conn.close()
 
 if __name__ == '__main__':
     post_nps()
