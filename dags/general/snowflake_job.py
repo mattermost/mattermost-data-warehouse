9520ab821d6ecc91aec9d5db73f5dcded787560e
Munish Sharma <munish7771@gmail.com>
2022-11-17 16:24:30 +0530

    updated dags with send_alert callback

--- dags/general/snowflake_job.py
+++ dags/general/snowflake_job.py
@@ -6,7 +6,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from dags.airflow_utils import (
     DATA_IMAGE,
     clone_repo_cmd,
-    mm_failed_task,
+    send_alert,
     pod_defaults,
     pod_env_vars,
     xs_warehouse,
@@ -30,7 +30,7 @@ default_args = {
     "catchup": False,
     "depends_on_past": False,
     "owner": "airflow",
-    "on_failure_callback": mm_failed_task,
+    "on_failure_callback": send_alert,
     "retries": 0,
     "retry_delay": timedelta(minutes=1),
     "sla": timedelta(hours=8),
