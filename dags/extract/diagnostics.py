db9b3e37a034557cc1229b8004fa07f4c018498c
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2022-11-29 14:02:50 +0200

    MM-48683: migrate to new docker image for diagnostics dag

--- dags/extract/diagnostics.py
+++ dags/extract/diagnostics.py
@@ -1,21 +1,18 @@
-import os
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from dags.airflow_utils import DATA_IMAGE, clone_and_setup_extraction_cmd, pod_defaults, send_alert
+
+from dags.airflow_utils import pod_defaults, send_alert
 from dags.kube_secrets import (
     DIAGNOSTIC_LOCATION_ONE,
     DIAGNOSTIC_LOCATION_TWO,
-    SNOWFLAKE_LOAD_USER,
-    SNOWFLAKE_LOAD_PASSWORD,
     SNOWFLAKE_ACCOUNT,
     SNOWFLAKE_LOAD_DATABASE,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_USER,
     SNOWFLAKE_LOAD_WAREHOUSE,
 )
-
-# Load the env vars into a dict and set Secrets
-env = os.environ.copy()
 
 # Default arguments for the DAG
 default_args = {
@@ -28,21 +25,12 @@ default_args = {
     "start_date": datetime(2019, 1, 1),
 }
 
-# Set the command for the container
-# Note the {{{{ }}}} is because we format this string but want the resulting string to just have {{ ds }}
-container_cmd = f"""
-    {clone_and_setup_extraction_cmd} &&
-    python extract/s3_extract/diagnostics_job.py {{{{ ds }}}}
-"""
-
 # Create the DAG
-dag = DAG(
-    "diagnostics", default_args=default_args, schedule_interval="0 3 * * *"
-)
+dag = DAG("diagnostics", default_args=default_args, schedule_interval="0 3 * * *")
 
 KubernetesPodOperator(
     **pod_defaults,
-    image=DATA_IMAGE,
+    image="mattermost/mattermost-data-warehouse:master",
     task_id="diagnostics",
     name="diagnostics",
     secrets=[
@@ -54,7 +42,6 @@ KubernetesPodOperator(
         SNOWFLAKE_LOAD_DATABASE,
         SNOWFLAKE_LOAD_WAREHOUSE,
     ],
-    env_vars={},
-    arguments=[container_cmd],
+    arguments=["python -m extract.s3_extract.diagnostics_job.py {{ ds }}"],
     dag=dag,
 )
