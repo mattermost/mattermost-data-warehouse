35bc5d800e55ad857d1511b0d157e63d3a806d79
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2022-12-01 15:56:44 +0200

    MM-48683: migrate to new docker image for dbt dag (#1082)

--- dags/transformation/dbt.py
+++ dags/transformation/dbt.py
@@ -1,38 +1,23 @@
-import os
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from dags.airflow_utils import (
-    DBT_IMAGE,
-    DATA_IMAGE,
-    dbt_install_deps_and_seed_cmd,
-    dbt_install_deps_cmd,
-    clone_and_setup_extraction_cmd,
-    send_alert,
-    pod_defaults,
-    pod_env_vars,
-    xs_warehouse,
-)
+
+from dags.airflow_utils import pod_defaults, pod_env_vars, send_alert
 from dags.kube_secrets import (
+    CLEARBIT_KEY,
+    DBT_CLOUD_API_ACCOUNT_ID,
+    DBT_CLOUD_API_KEY,
     SNOWFLAKE_ACCOUNT,
     SNOWFLAKE_PASSWORD,
     SNOWFLAKE_TRANSFORM_ROLE,
     SNOWFLAKE_TRANSFORM_SCHEMA,
     SNOWFLAKE_TRANSFORM_WAREHOUSE,
     SNOWFLAKE_USER,
-    AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY,
-    DBT_CLOUD_API_ACCOUNT_ID,
-    DBT_CLOUD_API_KEY,
-    PG_IMPORT_BUCKET,
-    HEROKU_POSTGRESQL_URL,
     SSH_KEY,
-    CLEARBIT_KEY,
 )
 
 # Load the env vars into a dict and set Secrets
-env = os.environ.copy()
 env_vars = {**pod_env_vars, **{}}
 
 # Default arguments for the DAG
@@ -50,15 +35,9 @@ default_args = {
 # Create the DAG
 dag = DAG("dbt", default_args=default_args, schedule_interval="5 * * * *")
 
-
-user_agent_cmd = f"""
-    {clone_and_setup_extraction_cmd} &&
-    python utils/user_agent_parser.py
-"""
-
 user_agent = KubernetesPodOperator(
     **pod_defaults,
-    image=DATA_IMAGE,
+    image="mattermost/mattermost-data-warehouse:master",  # Uses latest build from master
     task_id="user-agent",
     name="user-agent",
     secrets=[
@@ -68,18 +47,13 @@ user_agent = KubernetesPodOperator(
         SNOWFLAKE_TRANSFORM_WAREHOUSE,
     ],
     env_vars=env_vars,
-    arguments=[user_agent_cmd],
+    arguments=["python -m utils.user_agent_parser"],
     dag=dag,
 )
 
-dbt_run_cloud_cmd = f"""
-    {clone_and_setup_extraction_cmd} &&
-    python utils/run_dbt_cloud_job.py 19444 "Airflow dbt hourly"
-"""
-
 dbt_run_cloud = KubernetesPodOperator(
     **pod_defaults,
-    image=DATA_IMAGE,
+    image="mattermost/mattermost-data-warehouse:master",  # Uses latest build from master
     task_id="dbt-cloud-run",
     name="dbt-cloud-run",
     secrets=[
@@ -94,18 +68,14 @@ dbt_run_cloud = KubernetesPodOperator(
         SSH_KEY,
     ],
     env_vars=env_vars,
-    arguments=[dbt_run_cloud_cmd],
+    arguments=["python -m  utils.run_dbt_cloud_job 19444 \"Airflow dbt hourly\""],
     dag=dag,
 )
 
-update_clearbit_cmd = f"""
-    {clone_and_setup_extraction_cmd} &&
-    python utils/cloud_clearbit.py
-"""
 
 update_clearbit = KubernetesPodOperator(
     **pod_defaults,
-    image=DATA_IMAGE,
+    image="mattermost/mattermost-data-warehouse:master",  # Uses latest build from master
     task_id="update-clearbit",
     name="update-clearbit",
     secrets=[
@@ -116,18 +86,13 @@ update_clearbit = KubernetesPodOperator(
         CLEARBIT_KEY,
     ],
     env_vars=env_vars,
-    arguments=[update_clearbit_cmd],
+    arguments=["python -m utils.cloud_clearbit"],
     dag=dag,
 )
 
-update_onprem_clearbit_cmd = f"""
-    {clone_and_setup_extraction_cmd} &&
-    python utils/onprem_clearbit.py
-"""
-
 update_onprem_clearbit = KubernetesPodOperator(
     **pod_defaults,
-    image=DATA_IMAGE,
+    image="mattermost/mattermost-data-warehouse:master",  # Uses latest build from master
     task_id="update-onprem-clearbit",
     name="update-onprem-clearbit",
     secrets=[
@@ -138,7 +103,7 @@ update_onprem_clearbit = KubernetesPodOperator(
         CLEARBIT_KEY,
     ],
     env_vars=env_vars,
-    arguments=[update_onprem_clearbit_cmd],
+    arguments=["python -m utils.onprem_clearbit"],
     dag=dag,
 )
 
