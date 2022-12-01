5b8138e6d275c145263a957a831a7561afa7489b
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2022-12-01 15:27:23 +0200

    Revert "MM-48683: migrate to new docker image snowflake job (#1084)" (#1087)
    This reverts commit 032fdd0235760e61f5eaf9e6e0479c43c2c72816.

--- dags/general/snowflake_job.py
+++ dags/general/snowflake_job.py
@@ -1,18 +1,29 @@
+import os
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-
-from dags.airflow_utils import pod_defaults, send_alert
+from dags.airflow_utils import (
+    DATA_IMAGE,
+    clone_repo_cmd,
+    send_alert,
+    pod_defaults,
+    pod_env_vars,
+    xs_warehouse,
+)
 from dags.kube_secrets import (
     SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_USER,
     SNOWFLAKE_PASSWORD,
-    SNOWFLAKE_TRANSFORM_DATABASE,
     SNOWFLAKE_TRANSFORM_ROLE,
     SNOWFLAKE_TRANSFORM_SCHEMA,
     SNOWFLAKE_TRANSFORM_WAREHOUSE,
-    SNOWFLAKE_USER,
+    SNOWFLAKE_TRANSFORM_DATABASE,
 )
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+env_vars = {**pod_env_vars, **{}}
 
 # Default arguments for the DAG
 default_args = {
@@ -31,9 +42,15 @@ dag = DAG("snowflake_job", default_args=default_args, schedule_interval="*/5 * *
 
 
 def get_container_operator(task_name, job_name, schema):
+    cmd = f"""
+        {clone_repo_cmd} &&
+        cd mattermost-data-warehouse &&
+        export PYTHONPATH="/opt/bitnami/airflow/dags/git/mattermost-data-warehouse/:$PYTHONPATH" &&
+        python utils/run_snowflake_queries.py {job_name} TRANSFORMER {schema}
+    """
     return KubernetesPodOperator(
         **pod_defaults,
-        image="mattermost/mattermost-data-warehouse:master",  # Uses latest build from master
+        image=DATA_IMAGE,
         task_id=f"snowflake-{task_name}",
         name=task_name,
         secrets=[
@@ -45,10 +62,9 @@ def get_container_operator(task_name, job_name, schema):
             SNOWFLAKE_TRANSFORM_SCHEMA,
             SNOWFLAKE_TRANSFORM_DATABASE,
         ],
-        arguments=[f"python -m utils.run_snowflake_queries {job_name} TRANSFORMER {schema}"],
+        arguments=[cmd],
         dag=dag,
     )
-
 
 automated_nps_feedback_category_update = get_container_operator(
     "automated-nps-feedback-category-update", "automated_nps_feedback_category_update", "mattermost"
