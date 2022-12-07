cef517806147632c3646256001e2a6d7a51d4ece
Munish Sharma <munish7771@gmail.com>
2022-12-07 20:48:19 +0530

    Adding tests

---
+++ dags/tests/general/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import json
+from pathlib import Path
+
+@pytest.fixture(autouse=True)
+def config_utils(monkeypatch):
+    # patching environment variables used in airflow_utils
+    monkeypatch.setenv("NAMESPACE", "test_namespace")
+    monkeypatch.setenv("AIRFLOW_BASE_URL", "https://test.airflow.mattermost.com")
+
+@pytest.fixture
+def load_data():
+    # loads data from a file
+    def _load_data(filename):
+        file = open(Path(__file__).parent / 'fixtures' / filename, encoding='utf-8')
+        return file.read()
+    
+    return _load_data
---
+++ dags/tests/general/test_monitoring.py
@@ -0,0 +1,40 @@
+import pytest
+
+# First test loads imports into global variables used by other tests, to prevent reloading of imports
+def test_stitch_check_extractions_pass(load_data):
+    global stitch_check_extractions, stitch_check_loads
+    from general.monitoring import stitch_check_extractions, stitch_check_loads
+
+    response = load_data('monitoring/extractions_success.json')
+    failed_extrations = stitch_check_extractions(response)
+    # Expected to return empty dict as no extractions failed
+    assert failed_extrations == {}
+
+def test_stitch_check_extractions_fail(load_data):
+    response = load_data('monitoring/extractions_fail.json')
+    failed_extractions = stitch_check_extractions(response)
+    # Expected to return dict containing failed extractions
+    assert failed_extractions == {99999: 'Terminated'}
+
+def test_stitch_check_extractions_error():
+    # Expected to raise exception since response body is empty
+    with pytest.raises(TypeError):
+        stitch_check_extractions({})
+
+def test_stitch_check_loads_pass(load_data):
+    response = load_data('monitoring/loads_success.json')
+    failed_loads = stitch_check_loads(response)
+    # Expected to return empty dict as no loads failed
+    assert failed_loads == {}
+
+def test_stitch_check_loads_fail(load_data):
+    response = load_data('monitoring/loads_fail.json')
+    failed_loads = stitch_check_loads(response)
+    # Expected to return dict containing failed loads
+    assert failed_loads == {'raw_source_2': 'some_error_value'}
+
+def test_stitch_check_loads_error(load_data):
+    # Expected to raise exception since response body is empty
+    with pytest.raises(TypeError):
+        stitch_check_loads({})
+    
