4932906555f09342e6eda9d9b5840274890b74b6
Munish Sharma <munish7771@gmail.com>
2022-12-08 19:31:33 +0530

    Update test_monitoring.py

--- dags/general/monitoring.py
+++ dags/general/monitoring.py
@@ -92,7 +92,7 @@ default_args={
 dag = DAG('monitoring', 
         default_args=default_args,
         start_date=datetime(2017, 3, 20), 
-        schedule_interval='@hourly'
+        schedule_interval='@hourly',
         catchup=False)
 
 with dag:
