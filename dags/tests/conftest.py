674a1e88401c34afcf89b41510eff22fedab9867
Munish Sharma <munish7771@gmail.com>
2022-12-09 16:22:40 +0530

    Revert "Adding dependency for airflow"
    This reverts commit a8d8ff46be38b5509bc41903e4975bec662f04dc.

--- dags/tests/conftest.py
+++ dags/tests/conftest.py
@@ -6,38 +6,34 @@ import pytest
 import pendulum
 from datetime import datetime
 
-
 @pytest.fixture(autouse=True)
 def config_utils(monkeypatch):
 
-    # patching environment variables used in airflow_utils
-    monkeypatch.setenv("NAMESPACE", "test_namespace")
-    monkeypatch.setenv("AIRFLOW_BASE_URL", "https://test.airflow.mattermost.com")
-
+        # patching environment variables used in airflow_utils
+        monkeypatch.setenv("NAMESPACE", "test_namespace")
+        monkeypatch.setenv("AIRFLOW_BASE_URL", "https://test.airflow.mattermost.com")
 
 @pytest.fixture
 def config_alert_context(mocker):
-    """
-    Mocks airflow context passed as callback function parameter.
-    Dict contains only required keys used in the function.
+        """
+        Mocks airflow context passed as callback function parameter.
+        Dict contains only required keys used in the function. 
 
-    'dag': instance of airflow DAG.
-    'ts': execution timestamp
-    'execution_date': execution date
-    'task': instance of BaseOperator. A task defined in the above dag.
-    'task_instance': instance of TaskInstance
-    'exception': instance of AirflowException
-    """
-    execution_date = pendulum.datetime(2022, 11, 15, 0, 0, 0)
-    dag = DAG("test_utils_dag", start_date=datetime(2022, 11, 15))
-    task = BaseOperator(task_id='test_task', dag=dag)
-    task_instance = TaskInstance(task, execution_date, state=None)
-    context = {
-        'dag': dag,
-        'ts': execution_date,
-        'execution_date': execution_date,
-        'task': task,
-        'task_instance': task_instance,
-        'exception': AirflowException('Test Exception message'),
-    }
-    return context
+        'dag': instance of airflow DAG.
+        'ts': execution timestamp
+        'execution_date': execution date
+        'task': instance of BaseOperator. A task defined in the above dag.
+        'task_instance': instance of TaskInstance
+        'exception': instance of AirflowException
+        """
+        execution_date = pendulum.datetime(2022, 11, 15, 0, 0, 0)
+        dag = DAG("test_utils_dag", start_date=datetime(2022, 11, 15))
+        task = BaseOperator(task_id='test_task', dag = dag)
+        task_instance = TaskInstance(task, execution_date, state=None)
+        context = {'dag': dag,
+                'ts': execution_date,
+                'execution_date': execution_date, 
+                'task': task, 
+                'task_instance': task_instance, 
+                'exception': AirflowException('Test Exception message')}
+        return context
