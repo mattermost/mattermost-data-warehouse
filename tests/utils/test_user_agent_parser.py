77a708c8b259c12ff342948bc84c33f5cf538166
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2022-12-16 14:27:16 +0200

    Chore: update tests/utils code according to lint rules

--- tests/utils/test_user_agent_parser.py
+++ tests/utils/test_user_agent_parser.py
@@ -12,9 +12,7 @@ def test_parse_user_agent(mocker, user_agent_input, user_agent_df):
     mock_engine.connect.return_value = mock_connection
     mock_execute_query = mocker.patch("utils.user_agent_parser.execute_query")
     mock_execute_df = mocker.patch("utils.user_agent_parser.execute_dataframe")
-    mock_execute_df.return_value = pd.DataFrame({
-        "CONTEXT_USERAGENT": user_agent_input
-    })
+    mock_execute_df.return_value = pd.DataFrame({"CONTEXT_USERAGENT": user_agent_input})
     # Capture calls to pandas' to_sql. This is a trick to capture the dataframe that is created internally.
     # TODO: separate processing and storing logic in user_agent_parser.py
     mock_to_sql = mocker.patch("pandas.io.sql.to_sql")
@@ -29,10 +27,7 @@ def test_parse_user_agent(mocker, user_agent_input, user_agent_df):
     # THEN: expect to have saved a single batch to database
     mock_to_sql.assert_called_once()
     # THEN: expect user agents have been parsed correctly - this is the most important check regarding data
-    pd.testing.assert_frame_equal(
-        mock_to_sql.call_args_list[0][0][0],
-        user_agent_df
-    )
+    pd.testing.assert_frame_equal(mock_to_sql.call_args_list[0][0][0], user_agent_df)
 
 
 def test_parse_user_agent_batching(mocker):
@@ -45,12 +40,17 @@ def test_parse_user_agent_batching(mocker):
     mock_execute_query = mocker.patch("utils.user_agent_parser.execute_query")
     mock_execute_df = mocker.patch("utils.user_agent_parser.execute_dataframe")
     # GIVEN: 32k user agents (enough for 2 batches)
-    mock_execute_df.return_value = pd.DataFrame({
-        "CONTEXT_USERAGENT": [
-            f"Mozilla/5.0 (Linux; Android {i}.0; Nexus 6 Build/NBD90Z) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36"
-            for i in range(1, 32_000)
-        ]
-    })
+    mock_execute_df.return_value = pd.DataFrame(
+        {
+            "CONTEXT_USERAGENT": [
+                "Mozilla/5.0 "
+                f"(Linux; Android {i}.0; Nexus 6 Build/NBD90Z) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/53.0.2785.124 Mobile Safari/537.36"
+                for i in range(1, 32_000)
+            ]
+        }
+    )
     mock_to_sql = mocker.patch("pandas.io.sql.to_sql")
 
     # WHEN: trigger user agent parsing
