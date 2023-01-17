43f4688f17667388b5c41a5fc14f02ea58f8c3be
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-01-17 13:47:28 +0200

    Chore: remove unused code

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -1,9 +1,6 @@
 """ This file contains common operators/functions to be used across multiple DAGs """
-import json
 import os
 import urllib.parse
-from datetime import date, timedelta
-from typing import List
 
 from airflow.contrib.kubernetes.pod import Resources
 
@@ -17,75 +14,6 @@ MATTERMOST_DATAWAREHOUSE_IMAGE = "mattermost/mattermost-data-warehouse:master"
 
 mm_webhook_url = os.getenv("MATTERMOST_WEBHOOK_URL")
 DEFAULT_AIRFLOW_NAMESPACE = "airflow-dev"  # to prevent tests from failing
-
-
-def mm_failed_task(context):
-    """
-    Function to be used as a callable for on_failure_callback.
-    Send a Mattermost alert.
-    """
-    if mm_webhook_url is None or os.getenv("AIRFLOW_BASE_URL") != "https://airflow.internal.mattermost.com":
-        return
-
-    # Set all of the contextual vars
-    base_url = "https://airflow.internal.mattermost.com/"
-    execution_date = context["ts"]
-    dag_context = context["dag"]
-    dag_name = dag_context.dag_id
-    dag_id = context["dag"].dag_id
-    task_name = context["task"].task_id
-    task_id = context["task_instance"].task_id
-    execution_date_pretty = context["execution_date"].strftime("%a, %b %d, %Y at %-I:%M %p UTC")
-
-    # Generate the link to the logs
-    log_params = urllib.parse.urlencode({"dag_id": dag_id, "task_id": task_id, "execution_date": execution_date})
-    log_link = f"{base_url}/log?{log_params}"
-    log_link_markdown = f"[View Logs]({log_link})"
-
-    body = f"""| DAG | Task | Logs | Timestamp |
-        |-----|------|------|-----------|
-        |{dag_name}|{task_name}|{log_link_markdown}|{execution_date_pretty}|"""
-
-    payload = {"username": "Airflow", "text": body}
-
-    os.system(
-        f"""curl -i -X POST {mm_webhook_url} -H 'Content-Type: application/json' \
-        --data-binary @- <<'EOF'
-            {json.dumps(payload)}
-        EOF
-    """
-    )
-
-
-def split_date_parts(day: date, partition: str) -> List[dict]:
-
-    if partition == "month":
-        split_dict = {
-            "year": day.strftime("%Y"),
-            "month": day.strftime("%m"),
-            "part": day.strftime("%Y_%m"),
-        }
-
-    return split_dict
-
-
-def partitions(from_date: date, to_date: date, partition: str) -> List[dict]:
-    """
-    A list of partitions to build.
-    """
-
-    delta = to_date - from_date
-    all_parts = [split_date_parts((from_date + timedelta(days=i)), partition) for i in range(delta.days + 1)]
-
-    seen = set()
-    parts = []
-    # loops through every day and pulls out unique set of date parts
-    for p in all_parts:
-        if p["part"] not in seen:
-            seen.add(p["part"])
-            parts.append({k: v for k, v in p.items()})
-    return parts
-
 
 # Set the resources for the task pods
 pod_resources = Resources(request_memory="500Mi", request_cpu="500m")
