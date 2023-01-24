7798fd43026fcfceffc13ba7013cffa6935f58e2
Munish Sharma <munish7771@gmail.com>
2023-01-24 17:45:20 +0530

    Update freemium_opportunity.sql

--- transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_opportunity.sql
+++ transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_opportunity.sql
@@ -8,6 +8,7 @@
 WITH freemium_opportunity_to_sync AS (
     SELECT
         customers_with_cloud_paid_subs.*,
+        coalesce(account.dwh_external_id__c, customers_with_cloud_paid_subs.account_external_id) as account_lookup_id,
         SPLIT_PART(cloud_dns, '.', 0) ||
             ' Cloud Subscription' AS opportunity_name,
         'New Subscription' AS opportunity_type,
@@ -22,7 +23,8 @@ WITH freemium_opportunity_to_sync AS (
         ON customers_with_cloud_paid_subs.opportunity_external_id = opportunity.dwh_external_id__c
     LEFT JOIN {{ ref('account') }}
         ON customers_with_cloud_paid_subs.account_external_id = account.dwh_external_id__c
-    WHERE opportunity.id IS NULL
+        OR customers_with_cloud_paid_subs.domain = account.cbit__clearbitdomain__c
+    WHERE opportunity.id IS NULL AND account.dwh_external_id__c IS NOT NULL -- do not create opportunity if account does not exists
     AND customers_with_cloud_paid_subs.hightouch_sync_eligible
     AND customers_with_cloud_paid_subs.status in ('canceled', 'active') 
     AND customers_with_cloud_paid_subs.SKU = 'Cloud Professional'
