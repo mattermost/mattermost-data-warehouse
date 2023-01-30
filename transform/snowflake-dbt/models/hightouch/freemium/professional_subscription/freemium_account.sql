3b57c7a2b1005414d6aa5a02eb7695bf3ebef1c7
Munish Sharma <munish7771@gmail.com>
2023-01-30 13:54:40 +0530

    MM-49607: Professional opportunity sync notes (#1145)
    * Update freemium_opportunity.sql
    
    removed FY from opportunity name
    
    * Update freemium_account_update.sql
    
    updating account_type for all cloud professional accounts
    
    * Update freemium_account.sql
    
    * Update freemium_account_update.sql
    
    * Update freemium_opportunity.sql
    
    * Update freemium_opportunitylineitem.sql
    
    * Update freemium owner_id
    
    * Update freemium_account.sql
    
    * Update freemium_account_update.sql
    
    * Update freemium_contact.sql
    
    * Update freemium_contact_update.sql

--- transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_account.sql
+++ transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_account.sql
@@ -23,16 +23,14 @@ with existing_contacts as (
 ), customers_with_cloud_paid_subs as (
     SELECT
         customers_with_cloud_paid_subs.*,
-        '0053p0000064nt8AAA' AS ownerid
+        COALESCE(existing_leads.ownerid, existing_contacts.ownerid, '0053p0000064nt8AAA') AS ownerid
     FROM {{ ref('customers_with_cloud_paid_subs') }}
     LEFT JOIN {{ ref('account') }}
         ON customers_with_cloud_paid_subs.domain = account.cbit__clearbitdomain__c
             OR customers_with_cloud_paid_subs.account_external_id = account.dwh_external_id__c
-    LEFT JOIN {{ source('orgm', 'account_domain_mapping') }}
-        ON customers_with_cloud_paid_subs.domain = account_domain_mapping.domain
     LEFT JOIN existing_contacts ON customers_with_cloud_paid_subs.email = existing_contacts.email and existing_contacts.row_num = 1
     LEFT JOIN existing_leads ON customers_with_cloud_paid_subs.email = existing_leads.email and existing_leads.row_num = 1
-    WHERE account.id IS NULL AND account_domain_mapping.accountid IS NULL
+    WHERE account.id IS NULL AND existing_contacts.sfid IS NULL -- create account only when contact does not exist
     AND customers_with_cloud_paid_subs.hightouch_sync_eligible
     AND customers_with_cloud_paid_subs.status in ('canceled', 'active') 
     AND customers_with_cloud_paid_subs.SKU = 'Cloud Professional'
--- transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_account_update.sql
+++ transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_account_update.sql
@@ -8,14 +8,12 @@
 WITH customers_with_cloud_paid_subs as (
     SELECT
         customers_with_cloud_paid_subs.*,
-        '0053p0000064nt8AAA' AS ownerid
+        'Customer' as account_type
     FROM {{ ref('customers_with_cloud_paid_subs') }}
     LEFT JOIN {{ ref('account') }}
         ON customers_with_cloud_paid_subs.domain = account.cbit__clearbitdomain__c
             OR customers_with_cloud_paid_subs.account_external_id = account.dwh_external_id__c
-    LEFT JOIN {{ source('orgm', 'account_domain_mapping') }}
-        ON customers_with_cloud_paid_subs.domain = account_domain_mapping.domain
-    WHERE account.id IS NOT NULL AND account_domain_mapping.accountid IS NOT NULL
+    WHERE account.id IS NOT NULL
     AND customers_with_cloud_paid_subs.hightouch_sync_eligible
     AND customers_with_cloud_paid_subs.status in ('canceled', 'active') 
     AND customers_with_cloud_paid_subs.SKU = 'Cloud Professional'
