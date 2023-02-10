3ddc3f3077a9c47cc3b7f8c12b67cf6800e07294
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-02-10 14:43:01 +0200

    Hotfix: properly calculate number of days between start and end (#1158)

--- transform/snowflake-dbt/models/event_registry/event_fact.sql
+++ transform/snowflake-dbt/models/event_registry/event_fact.sql
@@ -14,7 +14,7 @@ SELECT
     , MIN(event_date) AS first_triggered_at
     , MAX(event_date) AS last_triggered_at
     , SUM(event_count) AS event_total_count
-    , SUM(event_count) / DATEDIFF(day, first_triggered_at, CURRENT_TIMESTAMP) AS event_daily_average
+    , SUM(event_count) / (DATEDIFF(day, first_triggered_at, CURRENT_TIMESTAMP) + 1) AS event_daily_average
 FROM
     {{ ref('daily_event_stats') }}
 GROUP BY
