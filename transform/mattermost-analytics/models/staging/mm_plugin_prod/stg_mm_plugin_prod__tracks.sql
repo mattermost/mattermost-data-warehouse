dfa2eacac3a7fc30fd1a94ab91a22566457fe2b8
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-02-14 22:42:05 +0200

    MM-50377: add plugin source (#1168)
    * MM-50377: add plugin source
    * MM-50377: rename type to event type

---
+++ transform/mattermost-analytics/models/staging/mm_plugin_prod/stg_mm_plugin_prod__tracks.sql
@@ -0,0 +1,21 @@
+{{
+    config({
+        "tags":"hourly",
+    })
+}}
+
+WITH tracks AS (
+    SELECT
+        {{ dbt_utils.star(ref('base_mm_plugin_prod__tracks')) }}
+    FROM
+        {{ ref ('base_mm_plugin_prod__tracks') }}
+)
+SELECT
+     id               AS event_id
+     , event          AS event_table
+     , event_text     AS event_name
+     , type           AS event_type
+     , user_id        AS server_id
+     , user_actual_id AS user_id
+     , received_at    AS received_at
+FROM tracks
