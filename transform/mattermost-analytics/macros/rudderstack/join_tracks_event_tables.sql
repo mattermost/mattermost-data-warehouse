36cd23c5d57ef1d26b949e341c5372ccb2ff24d2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-02-14 10:15:51 +0200

    MM-50377: Add staging models for mobile

---
+++ transform/mattermost-analytics/macros/rudderstack/join_tracks_event_tables.sql
@@ -0,0 +1,28 @@
+{% macro join_tracks_event_tables(schema, columns=[]) -%}
+    {%-
+        set rudderstack_tables = ['IDENTIFIES', 'USERS', 'TRACKS', 'PAGES', 'SCREENS', 'GROUPS', 'ALIASES', 'RUDDER_DISCARDS']
+    -%}
+    {%- set all_relations = dbt_utils.get_relations_by_pattern(schema, '%', database='RAW') | list -%}
+    {%- set relations = all_relations | rejectattr('identifier', 'in', rudderstack_tables) | list -%}
+    {%- set include = var('base_event_columns') -%}
+
+    {%- if columns -%}
+        {# If user defined columns to keep, filter them #}
+        {{
+            dbt_utils.union_relations(
+                relations=relations,
+                include=columns,
+                source_column_name=None
+            )
+        }}
+    {%- else -%}
+        {# Keep all columns #}
+        {{
+            dbt_utils.union_relations(
+                relations=relations,
+                source_column_name=None
+            )
+        }}
+    {%- endif -%}
+
+{%- endmacro%}
