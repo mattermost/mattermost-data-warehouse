941309412290f6170a8f828ad406c306b13470a1
Srikar Gudipudi <96433562+sri-byte@users.noreply.github.com>
2023-02-21 21:22:58 -0500

    Update transform/mattermost-analytics/models/staging/mm_telemetry_prod/stg_mm_telemetry_prod__performance_events.sql
    Co-authored-by: Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>

--- transform/mattermost-analytics/models/staging/mm_telemetry_prod/stg_mm_telemetry_prod__performance_events.sql
+++ transform/mattermost-analytics/models/staging/mm_telemetry_prod/stg_mm_telemetry_prod__performance_events.sql
@@ -4,12 +4,7 @@ config({
 })
 }}
 
-WITH MM_TELEMETRY_PROD AS 
-  {{ dbt_utils.union_relations(
-    relations=[
-      source('mm_telemetry_prod', 'event'),
-]
-, include=["context_page_referrer", "channel", "context_app_namespace", "user_actual_id", 
+{%- set include_columns = ["context_page_referrer", "channel", "context_app_namespace", "user_actual_id", 
     "context_library_name", "type", "context_app_version" , "user_actual_role", "context_page_url"
 , "context_os_name" , "context_page_title"  ,  "context_app_build", "context_page_search" 
 , "context_library_version",  "context_useragent", "context_app_name", "context_locale"
@@ -19,8 +14,18 @@ WITH MM_TELEMETRY_PROD AS
 , "gfyid", "field", "plugin_id" , "installed_version", "group_constrained", "value", "include_deleted" 
 , "role", "privacy", "scheme_id" , "warnmetricid"  , "metric" , "error"
 , "num_invitations_sent", "num_invitations" , "channel_sidebar" , "app" , "method" , "remaining" 
-, "screen" , "filter" , "version" , "uuid_ts", "id", "anonymous_id", "received_at", "sent_at", "original_timestamp", "timestamp", 
-        "context_ip", "event", "event_text"], where = " CATEGORY = 'performance'",
-  )}}
+, "screen" , "filter" , "version" , "uuid_ts", "id", "anonymous_id", "received_at", "sent_at", "original_timestamp", "timestamp", "context_ip", "event", "event_text"] %-}
+        
+WITH performance_event AS (
+    SELECT
+      {{ get_rudderstack_columns() }},
+        {% for column in include_columns %}
+        {{ column }} AS {{ column }}
+        {% if not loop.last %},{% endif %}
+    {% endfor %}
+    FROM
+      {{{ source('mm_telemetry_prod', 'event') }}
+    WHERE CATEGORY = 'performance'",
+)
 
-SELECT * FROM MM_TELEMETRY_PROD
+SELECT * FROM performance_events
