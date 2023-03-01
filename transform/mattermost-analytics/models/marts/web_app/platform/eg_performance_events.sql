ba39e25a1363a6e543aa6baa598adfada1a0e36b
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-03-01 09:14:10 -0500

    Fixed syntax issues and reference names

--- transform/mattermost-analytics/models/marts/web_app/platform/eg_performance_events.sql
+++ transform/mattermost-analytics/models/marts/web_app/platform/eg_performance_events.sql
@@ -1,28 +1,31 @@
 {{
     config({
+        "materialized": "incremental",
         "cluster_by": ['event_date']
     })
 }}
 
 WITH performance_rc AS (
     SELECT 
-        'int_mm_telemetry_rc__performance_events' AS _source_relation,
-        {{dbt_utils.star(ref('int_mm_telemetry_rc__performance_events'))}}
+        'int_mm_telemetry_rc_performance_events' AS _source_relation,
+        {{dbt_utils.star(ref('int_mm_telemetry_rc_performance_events'))}}
      FROM
-       {{ ref('int_mm_telemetry_rc__performance_events') }}
+       {{ ref('int_mm_telemetry_rc_performance_events') }}
 {% if is_incremental() %}
     WHERE received_at_date >= (SELECT MAX(received_at_date) FROM {{ this }}) 
-    WHERE _source_relation = 'int_mm_telemetry_rc__performance_events'
+    AND _source_relation = 'int_mm_telemetry_rc_performance_events'
 {% endif %}
-), performance_prod AS (
+), 
+performance_prod AS (
     SELECT
-        'int_mm_telemetry_prod__performance_events' AS _source_relation,
-        {{dbt_utils.star(ref('int_mm_telemetry_prod__performance_events'))}}
+        'int_mm_telemetry_prod_performance_events' AS _source_relation,
+        {{dbt_utils.star(ref('int_mm_telemetry_prod_performance_events'))}}
      FROM
-       {{ ref('int_mm_telemetry_prod__performance_events') }}
+       {{ ref('int_mm_telemetry_prod_performance_events') }}
 {% if is_incremental() %}
     WHERE received_at_date >= (SELECT MAX(received_at_date) FROM {{ this }}) 
-    WHERE _source_relation = 'int_mm_telemetry_prod__performance_events'
+    AND _source_relation = 'int_mm_telemetry_prod_performance_events'
+{% endif %}
 )
   SELECT * FROM performance_rc
   UNION_ALL
