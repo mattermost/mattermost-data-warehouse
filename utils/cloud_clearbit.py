9da53a6a6f10955371f987876c9fad99640055b2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-03-02 11:27:15 +0200

    Chore: lint clearbit scripts

--- utils/cloud_clearbit.py
+++ utils/cloud_clearbit.py
@@ -1,12 +1,9 @@
-import clearbit
-import psycopg2
 import os
-import snowflake.connector
-import sys
-from datetime import datetime
-from sqlalchemy import create_engine
+
+import clearbit
 import pandas as pd
-from extract.utils import snowflake_engine_factory, execute_query, execute_dataframe
+
+from extract.utils import execute_dataframe, snowflake_engine_factory
 
 
 def cloud_clearbit():
@@ -19,8 +16,8 @@ def cloud_clearbit():
 
         # RETRIEVE COLUMN NAMES IF TABLE ALREADY EXISTS
         try:
-            col_q = f'''
-            SELECT COLUMN_NAME 
+            col_q = '''
+            SELECT COLUMN_NAME
             FROM ANALYTICS.INFORMATION_SCHEMA.COLUMNS
             WHERE TABLE_NAME = 'CLOUD_CLEARBIT'
             AND TABLE_SCHEMA = 'MATTERMOST'
@@ -29,24 +26,24 @@ def cloud_clearbit():
             col_df = execute_dataframe(engine, query=col_q)
             clearbit_cols = []
             if len(col_df) > 0:
-                for index, row in col_df.iterrows():
+                for _index, row in col_df.iterrows():
                     clearbit_cols.append(row['COLUMN_NAME'].lower())
             else:
-               clearbit_cols = None
+                clearbit_cols = None
 
-        except:
+        except Exception:
             clearbit_cols = None
 
         # DETERMINE CLOUD_CLEARBIT TABLE IS BUILT AND HAS NOT BEEN DROPPED
         try:
-            query = f'''SELECT * FROM ANALYTICS.MATTERMOST.CLOUD_CLEARBIT limit 10'''
+            query = '''SELECT * FROM ANALYTICS.MATTERMOST.CLOUD_CLEARBIT LIMIT 10'''
             test = execute_dataframe(engine, query=query)
-        except:
-             test = None
+        except Exception:
+            test = None
 
         # RETRIEVE ALL WORKSPACES THAT HAVE NOT ALREADY BEEN ENRICHED BY CLEARBIT
         q = f'''
-        SELECT 
+        SELECT
             LSF.LICENSE_EMAIL
           , SPLIT_PART(LICENSE_EMAIL, '@', 2) AS EMAIL_DOMAIN
           , SF.SERVER_ID
@@ -68,16 +65,17 @@ def cloud_clearbit():
         '''
         df = execute_dataframe(engine, query=q)
 
-        # RETRIEVE CLEARBIT DATA FROM API USING CLEARBIT.ENRICHMENT.FIND AND THE USER'S EMAIL ADDRESS THAT CREATED THE CLOUD WORKSPACE
+        # RETRIEVE CLEARBIT DATA FROM API USING CLEARBIT.ENRICHMENT.FIND AND THE USER'S EMAIL ADDRESS THAT CREATED THE
+        # CLOUD WORKSPACE
         cloud_clearbit = []
         exceptions = 0
         cloud_exceptions = []
         response = None
-        for index, row in df.iterrows():
+        for _index, row in df.iterrows():
             response = None
             try:
                 response = clearbit.Enrichment.find(email=f'''{row['LICENSE_EMAIL']}''', stream=True)
-            except:
+            except Exception:
                 response = None
 
             if response is not None:
@@ -86,7 +84,6 @@ def cloud_clearbit():
             else:
                 exceptions += 1
                 cloud_exceptions.append([row['SERVER_ID']])
-
 
         # CHECK IF NEW DATA TO LOAD
         if len(cloud_clearbit) >= 1:
@@ -102,23 +99,26 @@ def cloud_clearbit():
                 cols = []
                 # CREATE DICTIONARY TO STORE RESULTS AS SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIRS
                 d = {}
-                # RETRIEVE ALL COLUMN NAMES REQUIRED TO GENERATE A DATAFRAME TO STORE EACH CLEARBIT PROPERTY AND STORE IN LIST
+                # RETRIEVE ALL COLUMN NAMES REQUIRED TO GENERATE A DATAFRAME TO STORE EACH CLEARBIT PROPERTY AND STORE
+                # IN LIST
                 for i in cloud_clearbit:
                     for key, value in i[1].items():
-                        if isinstance(value,dict):
+                        if isinstance(value, dict):
                             for k, v in value.items():
                                 if isinstance(v, dict):
                                     for k1, v1 in v.items():
                                         if isinstance(v1, dict):
-                                            for k2, v2 in v1.items():
-                                                cols.append(key.lower() + '_' + k.lower() + '_' + k1.lower() + '_' + k2.lower())
+                                            for k2, _v2 in v1.items():
+                                                cols.append(
+                                                    key.lower() + '_' + k.lower() + '_' + k1.lower() + '_' + k2.lower()
+                                                )
                                         else:
                                             cols.append(key.lower() + '_' + k.lower() + '_' + k1.lower())
                                 else:
                                     cols.append(key.lower() + '_' + k.lower())
                         else:
                             cols.append(key.lower())
-                # GENERATE SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIR RECORDS AND STORE IN DICTIONARY
+                    # GENERATE SERVER_ID:CLEARBIT RESPONSE KEY-VALUE PAIR RECORDS AND STORE IN DICTIONARY
                     d[i[0]] = i[1]
                 # ONLY RETAIN UNIQUE COLUMN VALUES AS SET THEN RECAST AS LIST FOR ITERATING PURPOSES.
                 cols = set(cols)
@@ -133,24 +133,49 @@ def cloud_clearbit():
             # ITERATE THROUGH CLOUD WORKSPACE CLEARBIT KEY-VALUE PAIRS
             # UPDATE EACH RESPECTIVE COLUMN PROPERTY USING INDEX AND COLUMN NAME
             for key, value in d.items():
-                for index, row in clearbit_df[clearbit_df['server_id'] == key].iterrows():
+                for index, _row in clearbit_df[clearbit_df['server_id'] == key].iterrows():
                     for k, v in value.items():
-                        if isinstance(v,dict):
-                            for k1, v1 in v.items() :
+                        if isinstance(v, dict):
+                            for k1, v1 in v.items():
                                 if isinstance(v1, dict):
                                     for k2, v2 in v1.items():
                                         if isinstance(v2, dict):
                                             for k3, v3 in v2.items():
-                                                clearbit_df.loc[index, k.lower() + '_' + k1.lower() + '_' + k2.lower() + '_' + k3.lower()] = v3
+                                                clearbit_df.loc[
+                                                    index,
+                                                    k.lower() + '_' + k1.lower() + '_' + k2.lower() + '_' + k3.lower(),
+                                                ] = v3
                                                 if isinstance(v3, dict):
                                                     for k4, v4 in v3.items():
-                                                        clearbit_df.loc[index, k.lower() + '_' + k1.lower() + '_' + k2.lower() + '_' + k3.lower() + '_' + k4.lower()] = v4
+                                                        clearbit_df.loc[
+                                                            index,
+                                                            k.lower()
+                                                            + '_'
+                                                            + k1.lower()
+                                                            + '_'
+                                                            + k2.lower()
+                                                            + '_'
+                                                            + k3.lower()
+                                                            + '_'
+                                                            + k4.lower(),
+                                                        ] = v4
                                                 else:
-                                                    clearbit_df.loc[index, k.lower() + '_' + k1.lower() + '_' + k2.lower() + '_' + k3.lower()] = v3
+                                                    clearbit_df.loc[
+                                                        index,
+                                                        k.lower()
+                                                        + '_'
+                                                        + k1.lower()
+                                                        + '_'
+                                                        + k2.lower()
+                                                        + '_'
+                                                        + k3.lower(),
+                                                    ] = v3
                                         else:
                                             clearbit_df.loc[index, k.lower() + '_' + k1.lower() + '_' + k2.lower()] = v2
                                 else:
-                                    clearbit_df.loc[index, k.lower() + '_' + k1.lower()] = str(v1) if type(v1) == list and len(v1) > 0 else v1
+                                    clearbit_df.loc[index, k.lower() + '_' + k1.lower()] = (
+                                        str(v1) if type(v1) == list and len(v1) > 0 else v1
+                                    )
                         else:
                             clearbit_df.loc[index, k.lower()] = v
 
@@ -158,8 +183,16 @@ def cloud_clearbit():
             clearbit_df2 = clearbit_df.convert_dtypes()
 
             # CAST REMAINING CLEARBIT OBJECT COLUMNS TO STRINGS
-            columns = ['company_site_phonenumbers','company_techcategories','company_domainaliases','company_tech','person_gravatar_urls','person_gravatar_avatars',\
-                    'company_tags','company_site_emailaddresses']
+            columns = [
+                'company_site_phonenumbers',
+                'company_techcategories',
+                'company_domainaliases',
+                'company_tech',
+                'person_gravatar_urls',
+                'person_gravatar_avatars',
+                'company_tags',
+                'company_site_emailaddresses',
+            ]
             # Ugly fix to handle exceptions consistently with how they are currently stored in the database
             clearbit_df2[columns] = clearbit_df2[columns].fillna('nan')
             clearbit_df2[columns] = clearbit_df2[columns].astype(str)
@@ -175,7 +208,14 @@ def cloud_clearbit():
             with engine.connect() as connection:
 
                 # ADD NEW WORKSPACE ROWS TO CLOUD_CLEARBIT TABLE
-                clearbit_df2.to_sql("cloud_clearbit", con=connection, index=False, schema="MATTERMOST", if_exists="append", chunksize=5000)
+                clearbit_df2.to_sql(
+                    "cloud_clearbit",
+                    con=connection,
+                    index=False,
+                    schema="MATTERMOST",
+                    if_exists="append",
+                    chunksize=5000,
+                )
                 print(f'''Success. Uploaded {len(clearbit_df2)} rows to ANALYTICS.MATTERMOST.CLOUD_CLEARBIT''')
                 print(f'''Exceptions: {exceptions}''')
                 print(f'''Exception Server ID's: {cloud_exceptions}''')
