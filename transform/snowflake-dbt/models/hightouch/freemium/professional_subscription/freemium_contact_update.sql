4b7ad282e888139b343ebb7a7800facefc593a92
Munish Sharma <munish7771@gmail.com>
2023-03-14 21:04:58 +0530

    Update freemium_contact_update.sql

--- transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_contact_update.sql
+++ transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_contact_update.sql
@@ -1,14 +1,14 @@
-{{config({
-    "schema": "hightouch",
+{{ config(
+    { "schema": "hightouch",
     "materialized": "view",
-    "tags":["hourly","blapi"]
-  })
-}}
+    "tags" :["hourly","blapi"] }
+) }}
 -- lead or contact already exists
 WITH existing_lead AS (
+
     SELECT
-        lead.id,
-        lead.email,
+        LEAD.id,
+        LEAD.email,
         CASE
             WHEN SUBSTR(
                 LEAD.ownerid,
@@ -17,28 +17,50 @@ WITH existing_lead AS (
             ) = '00G' THEN NULL
             ELSE LEAD.ownerid
         END AS ownerid
-    FROM {{ ref('lead') }}
-    WHERE converteddate IS NULL
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY lead.email ORDER BY lead.createddate DESC) = 1
-), freemium_contacts_to_sync as (
+    FROM
+        {{ ref('lead') }}
+    WHERE
+        converteddate IS NULL qualify ROW_NUMBER() over (
+            PARTITION BY LEAD.email
+            ORDER BY
+                LEAD.createddate DESC
+        ) = 1
+),
+freemium_contacts_to_sync AS (
     SELECT
         customers_with_cloud_paid_subs.email,
         customers_with_cloud_paid_subs.first_name,
-        COALESCE(customers_with_cloud_paid_subs.last_name, customers_with_cloud_paid_subs.email) AS contact_last_name,
+        COALESCE(
+            customers_with_cloud_paid_subs.last_name,
+            customers_with_cloud_paid_subs.email
+        ) AS contact_last_name,
         existing_lead.id AS duplicate_lead_id,
         -- existing_lead.email AS duplicate_lead_email,
         customers_with_cloud_paid_subs.account_external_id,
-        COALESCE(existing_lead.ownerid, '0053p0000064nt8AAA') AS ownerid,
+        COALESCE(
+            existing_lead.ownerid,
+            '0053p0000064nt8AAA'
+        ) AS ownerid,
         'Cloud Purchase' AS most_recent_action,
         'Cloud Professional' AS most_recent_action_detail,
         'Referral' AS lead_source,
         'Cloud Starter' AS lead_source_detail
-    FROM {{ ref('customers_with_cloud_paid_subs') }}
-    LEFT JOIN {{ ref('contact') }} ON customers_with_cloud_paid_subs.email = contact.email
-    LEFT JOIN existing_lead ON customers_with_cloud_paid_subs.email = existing_lead.email
-    WHERE contact.id IS NOT NULL
-    AND customers_with_cloud_paid_subs.hightouch_sync_eligible
-    AND customers_with_cloud_paid_subs.status in ('canceled', 'active') 
-    AND customers_with_cloud_paid_subs.SKU = 'Cloud Professional'
+    FROM
+        {{ ref('customers_with_cloud_paid_subs') }}
+        LEFT JOIN {{ ref('contact') }}
+        ON customers_with_cloud_paid_subs.email = contact.email
+        LEFT JOIN existing_lead
+        ON customers_with_cloud_paid_subs.email = existing_lead.email
+    WHERE
+        contact.id IS NOT NULL
+        AND customers_with_cloud_paid_subs.hightouch_sync_eligible
+        AND customers_with_cloud_paid_subs.status IN (
+            'canceled',
+            'active'
+        )
+        AND customers_with_cloud_paid_subs.sku = 'Cloud Professional'
 )
-SELECT * FROM freemium_contacts_to_sync
+SELECT
+    *
+FROM
+    freemium_contacts_to_sync
