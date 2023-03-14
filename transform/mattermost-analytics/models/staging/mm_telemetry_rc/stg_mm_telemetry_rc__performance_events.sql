9884bd0bbba41e17bd37fe28b0c9b950f860fa3f
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-03-14 08:46:05 -0400

    Removed userid field which is null

--- transform/mattermost-analytics/models/staging/mm_telemetry_rc/stg_mm_telemetry_rc__performance_events.sql
+++ transform/mattermost-analytics/models/staging/mm_telemetry_rc/stg_mm_telemetry_rc__performance_events.sql
@@ -3,7 +3,7 @@
 , "context_page_title", "context_app_build", "context_page_search" , "context_library_version"
 , "context_useragent", "context_app_name", "context_locale", "context_screen_density"  
 , "context_page_path", "context_os_version", "category" , "duration" , "num_of_request", "max_api_resource_size"
-, "longest_api_resource_duration" , "user_id", "userid", "root_id", "post_id", "sort" 
+, "longest_api_resource_duration" , "user_id", "root_id", "post_id", "sort" 
 , "team_id", "keyword", "count", "gfyid", "field"
 , "plugin_id", "installed_version", "group_constrained", "value", "include_deleted" 
 , "role", "privacy", "scheme_id", "metric", "error", "num_invitations_sent" 
