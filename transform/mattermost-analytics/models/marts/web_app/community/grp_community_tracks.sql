eadbbcd547c72e004fa2a7bfe6f47fcc20449e98
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-03-16 12:00:00 -0400

    Added a large warehouse to community_tracks

--- transform/mattermost-analytics/models/marts/web_app/community/grp_community_tracks.sql
+++ transform/mattermost-analytics/models/marts/web_app/community/grp_community_tracks.sql
@@ -2,7 +2,8 @@
     config({
         "materialized": "incremental",
         "cluster_by": ['event_date'],
-        "incremental_strategy": "append"
+        "incremental_strategy": "append",
+        "snowflake_warehouse": "transform_l"
     })
 }}
 
