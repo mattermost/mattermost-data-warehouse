ea82047f6c691fc477cf191dc34fbdefb2eb3100
Munish Sharma <munish7771@gmail.com>
2023-03-20 22:15:57 +0530

    Update hightouch.sql

--- transform/snowflake-dbt/macros/hightouch.sql
+++ transform/snowflake-dbt/macros/hightouch.sql
@@ -2,5 +2,5 @@
     CASE WHEN SUBSTR({{ ownerid }}, 0, 3) = '00G'
         THEN {{ var('salesforce_default_ownerid') }}
         ELSE COALESCE({{ ownerid }}, {{ var('salesforce_default_ownerid') }})
-    END AS ownerid
+    END
 {% endmacro %}
