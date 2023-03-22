f5bef65a17c027dcbf2b0a2fd91b07eea0e4723f
Srikar Gudipudi <96433562+sri-byte@users.noreply.github.com>
2023-03-22 10:42:44 -0400

    MM - 51427 Removing null columns from performance_events (#1214)
    * Removing null columns from performance_events
    
    * Fixed indentation
    
    * Removed tab from yml files
    
    * Fixed indentation with tests
    
    * Removes more context columns from performance_events and renamed table
    
    * Removed uuid_ts
    
    * Replacing * with dbt.utils

--- transform/mattermost-analytics/models/intermediate/web_app/platform/int_mm_telemetry_rc_performance_events.sql
+++ transform/mattermost-analytics/models/intermediate/web_app/platform/int_mm_telemetry_rc_performance_events.sql
@@ -5,7 +5,7 @@
     })
 }}
 
-SELECT * FROM 
+SELECT {{ dbt_utils.star(ref('stg_mm_telemetry_rc__performance_events')) }} FROM 
 {{ ref('stg_mm_telemetry_rc__performance_events') }} 
 
 {% if is_incremental() %}
