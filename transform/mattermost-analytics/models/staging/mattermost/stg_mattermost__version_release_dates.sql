29483cd0aa8d686b44c6a3ae97cf603b03f2584f
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-03-29 14:01:49 +0300

    MM-51305: fix regex

--- transform/mattermost-analytics/models/staging/mattermost/stg_mattermost__version_release_dates.sql
+++ transform/mattermost-analytics/models/staging/mattermost/stg_mattermost__version_release_dates.sql
@@ -6,7 +6,7 @@ WITH rd AS (
 )
 SELECT
     version AS version,
-    'v' || REGEXP_SUBSTR(version, '^\\d+\\.\\d') AS short_version,
+    'v' || REGEXP_SUBSTR(version, '^\\d+\\.\\d+') AS short_version,
     release_date::DATE AS planned_release_date,
     supported::BOOLEAN AS is_supported,
     release_number::INT AS release_number
