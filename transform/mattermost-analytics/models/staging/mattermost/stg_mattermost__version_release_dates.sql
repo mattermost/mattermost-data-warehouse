d5955c9fa1b5ae311d748f25e8e6f3a3e7e16fd8
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-03-29 13:58:47 +0300

    MM-51305: fix source name

--- transform/mattermost-analytics/models/staging/mattermost/stg_mattermost__version_release_dates.sql
+++ transform/mattermost-analytics/models/staging/mattermost/stg_mattermost__version_release_dates.sql
@@ -2,11 +2,11 @@ WITH rd AS (
     SELECT
         *
     FROM
-        {{ source('mattermost', 'version_release_date') }}
+        {{ source('mattermost', 'version_release_dates') }}
 )
 SELECT
     version AS version,
-    'v' || REGEXP(version, '^\\d+\\.\\d') AS short_version,
+    'v' || REGEXP_SUBSTR(version, '^\\d+\\.\\d') AS short_version,
     release_date::DATE AS planned_release_date,
     supported::BOOLEAN AS is_supported,
     release_number::INT AS release_number
