d302de9d018108e8fa197334a82af228f9100e25
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-10 15:49:16 +0300

    Hotfix: ignore _ column (#1234)

--- transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
+++ transform/snowflake-dbt/models/events/hourly/rudder_webapp_events.sql
@@ -2,12 +2,13 @@
     "materialized": "incremental",
     "schema": "events",
     "tags":"preunion",
-    "snowflake_warehouse": "transform_m"
+    "snowflake_warehouse": "transform_m",
+    "on_schema_change": "append_new_columns"
   })
 }}
 
 {% set rudder_relations = get_rudder_relations(schema=["mm_telemetry_prod"], database='RAW', 
                           table_inclusions="'event','inactive_server','inactive_server_emails_sent'") %}
-{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1]) }}
+{{ union_relations(relations = rudder_relations[0], tgt_relation = rudder_relations[1], exclude = ['_']) }}
 
 
