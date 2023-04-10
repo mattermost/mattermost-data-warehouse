77446e36670d4deeaec0b991d4caaa29029fa1d9
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-10 14:03:54 +0300

    MM-51945: temporarily ignore record failing to sync (#1230)

--- transform/snowflake-dbt/models/hightouch/product_campaigns/inserts/onprem_trial_request_inapp_campaignmember_to_insert_lead.sql
+++ transform/snowflake-dbt/models/hightouch/product_campaigns/inserts/onprem_trial_request_inapp_campaignmember_to_insert_lead.sql
@@ -6,6 +6,12 @@
 
 with campaignmembers_to_insert as (
     select * from {{ ref('onprem_trial_request_inapp_facts') }}
-    where campaignmember_sfid is null and lead_exists
+    where
+        -- Existing lead but no campaign member
+        campaignmember_sfid is null and lead_exists
+        -- Exceptions
+        and contact_external_id not in (
+            '01de907e-ca85-57ff-b7be-e0840f8bfd1f'  -- https://mattermost.atlassian.net/browse/MM-51945
+        )
 )
 select * from campaignmembers_to_insert
