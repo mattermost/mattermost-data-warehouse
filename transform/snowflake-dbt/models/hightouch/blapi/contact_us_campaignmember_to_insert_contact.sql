4197f2579d53a742fee80f2142d0629af0d283c7
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-19 15:58:32 +0300

    MM-51943: fix duplicate campaign member inserts on same batch

--- transform/snowflake-dbt/models/hightouch/blapi/contact_us_campaignmember_to_insert_contact.sql
+++ transform/snowflake-dbt/models/hightouch/blapi/contact_us_campaignmember_to_insert_contact.sql
@@ -7,5 +7,7 @@
 with campaignmembers_to_insert as (
     select * from {{ ref('contact_us_campaign_fact') }}
     where campaignmember_sfid is null and not lead_exists and contact_exists
+    -- For each insert batch, keep the most recent record according to "request to contact us"
+    qualify row_number() over (partition by email order by request_to_contact_us_date desc) = 1
 )
 select * from campaignmembers_to_insert
