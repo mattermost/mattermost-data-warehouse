5f968a96509cce217e5a73d850277f4b3eb768a0
Munish Sharma <munish7771@gmail.com>
2023-04-20 19:11:04 +0530

    Added base model for products

---
+++ transform/mattermost-analytics/models/staging/stripe/stg_stripe__products.sql
@@ -0,0 +1,39 @@
+with source as (
+
+    select * from {{ source('stripe_raw', 'products') }}
+
+),
+
+renamed as (
+
+    select
+        active,
+        attributes,
+        created,
+        description,
+        id,
+        images,
+        livemode,
+        metadata:"cws-product-family"::varchar as cws_product_family,
+        metadata:"cws-sku-name"::varchar as cws_sku_name,
+        metadata:"cws-allowable-addons"::varchar as cws_allowable_addons,
+        metadata,
+        name,
+        object,
+        shippable,
+        type,
+        updated,
+        _sdc_batched_at,
+        _sdc_extracted_at,
+        _sdc_received_at,
+        _sdc_sequence,
+        _sdc_table_version,
+        unit_label,
+        updated_by_event_type,
+        deactivate_on
+
+    from source
+
+)
+
+select * from renamed
