9fc9651a6c7677187e56d08a401f87cc990c64f8
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-24 15:47:08 +0300

    MM-51085: fix failing tests

--- transform/mattermost-analytics/models/marts/sales/hightouch/fct_trial_requests.sql
+++ transform/mattermost-analytics/models/marts/sales/hightouch/fct_trial_requests.sql
@@ -5,15 +5,19 @@ with trial_requests as (
            last_name,                                          -- Mapped to field last_name of lead
            company_size_bucket,                                -- Mapping TBD
            company_name,                                       -- Mapped to field company of lead
-           COALESCE(contact_email, email) as email,            -- Mapped to field email of lead
-           country_name,                                       -- Mapped to field country
+           coalesce(contact_email, email) as email,            -- Mapped to field email of lead
+           case
+               -- Fix inconsistency in country name so that it matches expected values
+               when country_name = 'United States of America' then 'United States'
+               else country_name
+            end as country_name,                               -- Mapped to field country
            start_at as trial_start_at,                         -- Mapped to request_a_trial_date__c and TBD
            num_users                                           -- Mapped to field numberofemployees of lead
     from {{ ref('stg_cws__trial_requests')}}
 )
 select
     tr.*,
-    l.sfid is not null as is_existing_lead
+    l.lead_id is not null as is_existing_lead
 from
     trial_requests tr
     left join {{ ref('stg_salesforce__lead') }} l on tr.email = l.email
