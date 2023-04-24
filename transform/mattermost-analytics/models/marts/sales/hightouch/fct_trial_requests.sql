a9a64d4d5bc19ead7c091787e81f30d33b12788f
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-24 13:45:44 +0300

    MM-51085: update sources, add indication on whether a lead exists

--- transform/mattermost-analytics/models/marts/sales/hightouch/fct_trial_requests.sql
+++ transform/mattermost-analytics/models/marts/sales/hightouch/fct_trial_requests.sql
@@ -1,12 +1,19 @@
+with trial_requests as (
+    select trial_request_id,                                   -- Primary key
+           first_name || ' ' || last_name as full_name,        -- Mapped to field name of lead
+           first_name,                                         -- Mapped to field first_name of lead
+           last_name,                                          -- Mapped to field last_name of lead
+           company_size_bucket,                                -- Mapping TBD
+           company_name,                                       -- Mapped to field company of lead
+           COALESCE(contact_email, email) AS email,            -- Mapped to field email of lead
+           country_name,                                       -- Mapped to field country
+           start_date                     AS trial_start_date, -- Mapped to request_a_trial_date__c and TBD
+           num_users                                           -- Mapped to field numberofemployees of lead
+    from {{ ref('stg_cws__trial_requests')}}
+)
 select
-    trial_request_id,
-    -- TODO: decide name
-    -- TODO: finalize how
-    company_size_bucket,
-    company_name,
-    COALESCE(contact_email, email) AS email,
-    country_name,
-    start_date AS trial_start_date,
-    num_users
+    tr.*,
+    l.sfid is not null as is_existing_lead
 from
-    {{ ref('stg_cws__trial_requests' )}}
+    trial_requests tr
+    left join {{ ref('stg_salesforce__lead') }} l on tr.email = l.email
--- transform/mattermost-analytics/models/staging/cws/stg_cws__trial_requests.sql
+++ transform/mattermost-analytics/models/staging/cws/stg_cws__trial_requests.sql
@@ -1,31 +1,41 @@
 
 with source as (
 
-    select * from {{ source('cws_test', 'trial_requests') }}
+    select * from {{ source('cws', 'trial_requests') }}
 
 ),
 
 renamed as (
 
     select
-        id AS trial_request_id,
-        companycountry AS country_name,
-        companyname AS company_name,
-        companysize AS company_size_bucket,
-        contactemail AS contact_email,
-        contactfirstname AS first_name,
-        contactlastname AS last_name,
-        email AS email,
-        name AS username,
-        receiveemailsaccepted is_receive_email_accepted,
-        serverid AS server_id,
-        sitename AS site_name,
-        siteurl AS site_url,
-        startdate AS start_date,
-        enddate AS end_date,
-        termsaccepted AS is_terms_accepted,
-        users AS num_users
+        id,
+
+        -- User info
+        name,
+        contactfirstname,
+        contactlastname,
+        email,
+        contactemail,
+
+        -- Company info
+        companycountry,
+        companyname,
+        companysize,
+
+        -- Installation info
+        serverid,
+        sitename,
+        siteurl,
+        users,
+
+        -- Trial info
+        startdate,
+        enddate,
+        receiveemailsaccepted,
+        termsaccepted
+
         -- Stitch columns omitted
+
     from source
 
 )
