f1e231bcecef69d6b87b0f6dbe21e69389a0d488
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-26 10:21:38 +0300

    MM-51085: fix email reference

--- transform/mattermost-analytics/models/marts/sales/hightouch/fct_in_product_trial_requests.sql
+++ transform/mattermost-analytics/models/marts/sales/hightouch/fct_in_product_trial_requests.sql
@@ -25,24 +25,34 @@ with trial_requests as (
         end as company_size,                                -- Mapping lower threshold
         company_name,                                       -- Mapped to field company of lead
         -- Salesforce lowercases email
-        lower(coalesce(contact_email, email)) as email,     -- Mapped to field email of lead
+        lower(coalesce(contact_email, email)) as normalized_email, -- Mapped to field email of lead
         case
            -- Fix inconsistency in country name so that it matches values expected by SF
            when country_name = 'United States of America' then 'United States'
            else country_name
-        end as country_name,                               -- Mapped to field country
+        end as country_name,                                -- Mapped to field country
         start_at as trial_start_at,                         -- Mapped to request_a_trial_date__c and TBD
         num_users                                           -- Mapped to field numberofemployees of lead
     from
         {{ ref('stg_cws__trial_requests')}}
+    -- Sync the most recent trial
+    qualify row_number() over (partition by normalized_email order by trial_start_at desc) = 1
 )
 select
+    -- Lead fields
     tr.*,
     l.lead_id is not null as is_existing_lead,
-    {{ validate_email('tr.email') }} as is_valid_email
+    -- Campaign member fields
+    l.lead_id,
+    'Responded' as status,
+    '{{ var('in_product_trial_campaign_id') }}' as campaign_id,
+    -- Extra validation
+    {{ validate_email('tr.normalized_email') }} as is_valid_email
 from
     trial_requests tr
-    left join {{ ref('stg_salesforce__lead') }} l on tr.email = l.email
+    left join {{ ref('stg_salesforce__lead') }} l on tr.normalized_email = l.email
 where
     -- Skip invalid emails
     is_valid_email
+-- Update oldest lead
+qualify row_number() over (partition by email order by created_at asc) = 1
