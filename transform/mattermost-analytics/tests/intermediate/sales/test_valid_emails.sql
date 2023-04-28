ac9ece9299d3460d2c2bfac7b5c8c3b36de3125d
Munish Sharma <munish7771@gmail.com>
2023-04-28 15:33:04 +0530

    Update test_valid_emails.sql

--- transform/mattermost-analytics/tests/intermediate/sales/test_valid_emails.sql
+++ transform/mattermost-analytics/tests/intermediate/sales/test_valid_emails.sql
@@ -7,7 +7,7 @@ select
     email,
     email is null
     or email = ''
-    or {{validate_email('email')}} as is_valid_email,
+    or {{validate_email('email')}} as is_valid_email
 from
     {{ ref('int_cloud_trial_requests') }}
 where
