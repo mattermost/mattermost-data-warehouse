951b572b25afe1889149a71cd8f2279be3dc1a2e
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-04-28 17:44:07 +0300

    MM-51752: do not update accounts without contacts

--- transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_account_update.sql
+++ transform/snowflake-dbt/models/hightouch/freemium/professional_subscription/freemium_account_update.sql
@@ -5,7 +5,20 @@
   })
 }}
 
-WITH customers_with_cloud_paid_subs as (
+WITH existing_contacts AS (
+    SELECT
+        contact.sfid,
+        contact.email,
+        contact.dwh_external_id__c,
+        contact.ownerid,
+        ROW_NUMBER() over (
+            PARTITION BY contact.email
+            ORDER BY
+                createddate DESC
+        ) AS row_num
+    FROM
+        {{ ref('contact') }}
+), customers_with_cloud_paid_subs as (
     SELECT
         customers_with_cloud_paid_subs.*,
         'Customer' as account_type
@@ -13,7 +26,11 @@ WITH customers_with_cloud_paid_subs as (
     LEFT JOIN {{ ref('account') }}
         ON customers_with_cloud_paid_subs.domain = account.cbit__clearbitdomain__c
             OR customers_with_cloud_paid_subs.account_external_id = account.dwh_external_id__c
+    LEFT JOIN existing_contacts
+        ON customers_with_cloud_paid_subs.email = existing_contacts.email
+        AND existing_contacts.row_num = 1
     WHERE account.id IS NOT NULL
+        AND existing_contacts.sfid IS NULL -- create account only when contact does not exist
     AND customers_with_cloud_paid_subs.hightouch_sync_eligible
     AND customers_with_cloud_paid_subs.status in ('canceled', 'active') 
     AND customers_with_cloud_paid_subs.SKU = 'Cloud Professional'
