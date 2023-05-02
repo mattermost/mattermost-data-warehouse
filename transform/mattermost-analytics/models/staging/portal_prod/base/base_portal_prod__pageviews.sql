3b05b3da2684d60364b90ef48b57135e8cbd547d
Munish Sharma <munish7771@gmail.com>
2023-05-02 16:38:55 +0530

    Revert "Update base_portal_prod__pageviews.sql"
    This reverts commit f0acfe5867efa2a67fa2ed9f50bd8096ada36d7e.

--- transform/mattermost-analytics/models/staging/portal_prod/base/base_portal_prod__pageviews.sql
+++ transform/mattermost-analytics/models/staging/portal_prod/base/base_portal_prod__pageviews.sql
@@ -1,7 +1,7 @@
 {{ dbt_utils.join(
     table_1={{ source('portal_prod', 'PAGEVIEW_CLOUD_LANDING_PAGE') }},
     table_2={{ source('portal_prod', 'PAGEVIEW_CREATE_SIGNUP_PASSWORD') }},
-    table_3={{ source('portal_prod', 'PAGEVIEW_VERIFY_EMAIL') }},
-    table_4={{ source('portal_prod', 'PAGEVIEW_CREATE_WORKSPACE') }},
+    table_2={{ source('portal_prod', 'PAGEVIEW_VERIFY_EMAIL') }},
+    table_2={{ source('portal_prod', 'PAGEVIEW_CREATE_WORKSPACE') }},
     keys=['user_id']
 )}}
