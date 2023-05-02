8e06fc194bd674a232ded044f5eb73116edd4c04
Munish Sharma <munish7771@gmail.com>
2023-05-02 19:32:21 +0530

    Update base_portal_prod__pageviews.sql

--- transform/mattermost-analytics/models/staging/portal_prod/base/base_portal_prod__pageviews.sql
+++ transform/mattermost-analytics/models/staging/portal_prod/base/base_portal_prod__pageviews.sql
@@ -1,6 +1,6 @@
 {% set rudder_relations = get_rudder_relations(schema=["PORTAL_PROD"], database="RAW", table_inclusions="'PAGEVIEW_CLOUD_LANDING_PAGE','PAGEVIEW_CREATE_SIGNUP_PASSWORD','PAGEVIEW_VERIFY_EMAIL','PAGEVIEW_CREATE_WORKSPACE'") %}
 
 {{ dbt_utils.union_relations(
-    relations=[{{ source('portal_prod', 'PAGEVIEW_CLOUD_LANDING_PAGE') }}, {{ source('portal_prod', 'PAGEVIEW_CREATE_SIGNUP_PASSWORD') }}, {{ source('portal_prod', 'PAGEVIEW_VERIFY_EMAIL') }}, {{ source('portal_prod', 'PAGEVIEW_CREATE_WORKSPACE') }}],
+    relations=rudder_relations,
     include=["user_id"]
 ) }}
