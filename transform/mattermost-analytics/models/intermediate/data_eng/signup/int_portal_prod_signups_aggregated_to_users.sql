cad413afe2b054f5f1f61b6e109a3766aa15845e
Munish Sharma <munish7771@gmail.com>
2023-05-03 19:23:56 +0530

    Update int_portal_prod_signups_aggregated_to_users.sql

--- transform/mattermost-analytics/models/intermediate/data_eng/signup/int_portal_prod_signups_aggregated_to_users.sql
+++ transform/mattermost-analytics/models/intermediate/data_eng/signup/int_portal_prod_signups_aggregated_to_users.sql
@@ -9,8 +9,7 @@ SELECT
     identifies.user_id,
     identifies.portal_customer_id,
     MAX(CASE WHEN pageviews.event_type = 'PAGEVIEW_VERIFY_EMAIL' THEN true ELSE false END) AS account_created,
-    MAX(CASE WHEN pageviews.event_type = 'PAGEVIEW_CREATE_WORKSPACE' THEN true ELSE false END) AS email_verified,
-    -- null AS workspace_created
+    MAX(CASE WHEN pageviews.event_type = 'PAGEVIEW_CREATE_WORKSPACE' THEN true ELSE false END) AS email_verified
 FROM
     {{ ref('stg_portal_prod__pageviews') }} pageviews
     JOIN {{ ref('stg_portal_prod__identifies') }} identifies
