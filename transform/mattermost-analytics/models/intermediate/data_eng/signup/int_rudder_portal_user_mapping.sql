0f171ebb02f60f52e6f16424b11db87831e9bc40
Munish Sharma <munish7771@gmail.com>
2023-05-04 17:16:42 +0530

    Updated intermediate tables

---
+++ transform/mattermost-analytics/models/intermediate/data_eng/signup/int_rudder_portal_user_mapping.sql
@@ -0,0 +1,13 @@
+SELECT
+    user_id,
+    portal_customer_id,
+    max(timestamp) as first_seen_at,
+    max(timestamp) as last_seen_at
+FROM
+    {{ ref('stg_portal_prod__identifies') }}
+GROUP BY
+    1,
+    2
+WHERE
+    portal_customer_id IS NOT NULL
+    OR user_id IS NOT NULL
