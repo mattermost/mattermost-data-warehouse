8d8052c25c56efe42636be3d15beed8c0e675cf5
Munish Sharma <munish7771@gmail.com>
2023-05-04 18:46:14 +0530

    Update int_rudder_portal_user_mapping.sql

--- transform/mattermost-analytics/models/intermediate/data_eng/signup/int_rudder_portal_user_mapping.sql
+++ transform/mattermost-analytics/models/intermediate/data_eng/signup/int_rudder_portal_user_mapping.sql
@@ -11,9 +11,9 @@ SELECT
     max(timestamp) as last_seen_at
 FROM
     {{ ref('stg_portal_prod__identifies') }}
-GROUP BY
-    1,
-    2
 WHERE
     portal_customer_id IS NOT NULL
     OR user_id IS NOT NULL
+GROUP BY
+    1,
+    2
