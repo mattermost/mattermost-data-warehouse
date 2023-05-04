5b2aa98a9bec7b3b63ce2de8d655f0528af50881
Munish Sharma <munish7771@gmail.com>
2023-05-05 00:08:01 +0530

    MM-51926: Capture cloud enterprise trial requests (#1261)
    * Added intermediate model
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Removing ;
    
    * Update int_cloud_trial_requests.sql
    
    * Update stg_stripe__subscriptions.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update dbt_project.yml
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Create fct_cloud_trial_requests.sql
    
    * Update dbt_project.yml
    
    * Update int_cloud_trial_requests.sql
    
    * Changes in fact model
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update _hightouch__models.yml
    
    * Update int_cloud_trial_requests.sql
    
    * Update _hightouch__models.yml
    
    * Update _hightouch__models.yml
    
    * Update _hightouch__models.yml
    
    * Update _hightouch__models.yml
    
    * Update int_cloud_trial_requests.sql
    
    * Update int_cloud_trial_requests.sql
    
    * Updating correct timestamp
    
    * Update fct_cloud_trial_requests.sql
    
    * Review changes
    
    * Update int_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Revert "Update fct_cloud_trial_requests.sql"
    
    This reverts commit 470d9f71cee386c406aafb3b6c763c6030ea1d27.
    
    * Update fct_cloud_trial_requests.sql
    
    * Update test_valid_emails.sql
    
    * Adding tests to intermediate model
    
    * Update test_valid_emails.sql
    
    * Changes in test
    
    * Update int_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Revert "Update int_cloud_trial_requests.sql"
    
    This reverts commit e75b1fe72712fdd59234f0f1b78c201ea67b18a1.
    
    * Test changes
    
    to be reverted once sucessfully tested.
    
    * Update int_cloud_trial_requests.sql
    
    * Update fct_cloud_trial_requests.sql
    
    * Revert "Test changes"
    
    This reverts commit 55bcfbb9e185baf6c84fdf1855742fc642b271ed.

---
+++ transform/mattermost-analytics/models/intermediate/sales/int_cloud_trial_requests.sql
@@ -0,0 +1,54 @@
+WITH customers as (
+    SELECT
+        customer_id,
+        email
+    FROM
+        {{ ref('stg_stripe__customers') }}
+    where
+        created_at >= '2023-04-27' -- only select customers after the release.
+),
+subscriptions as (
+    SELECT
+        subscription_id,
+        cws_installation,
+        cws_dns,
+        customer_id,
+        trial_start_at,
+        trial_end_at,
+        product_id
+    FROM
+        {{ ref('stg_stripe__subscriptions') }}
+),
+products as (
+    SELECT
+        product_id,
+        name,
+        sku
+    FROM
+        {{ ref('stg_stripe__products') }}
+),
+cloud_trial_requests as (
+    select
+        customers.customer_id,
+        customers.email,
+        subscriptions.subscription_id,
+        subscriptions.trial_start_at,
+        subscriptions.trial_end_at,
+        subscriptions.cws_installation,
+        products.product_id,
+        products.name as product_name
+    from
+        customers
+        -- Will lead to rows fanning out since a customer can have many subscriptions
+        left join subscriptions on subscriptions.customer_id = customers.customer_id
+        left join products on subscriptions.product_id = products.product_id
+        -- Trial data end is in the future
+        where CURRENT_DATE < subscriptions.trial_end_at
+        -- Only get cloud subscriptions
+        AND cws_installation is not null
+)
+
+select
+    *
+from
+    cloud_trial_requests
