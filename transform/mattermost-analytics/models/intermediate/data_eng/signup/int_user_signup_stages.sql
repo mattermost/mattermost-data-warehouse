fe8659295ef0484417ae0c320eadf3af8209c62d
Munish Sharma <munish7771@gmail.com>
2023-05-04 18:45:44 +0530

    Create int_user_signup_stages.sql

---
+++ transform/mattermost-analytics/models/intermediate/data_eng/signup/int_user_signup_stages.sql
@@ -0,0 +1,30 @@
+WITH rudder_portal_user_mappings as (
+    SELECT
+        user_id,
+        portal_customer_id
+    FROM
+        {{ ref('int_rudder_portal_user_mapping') }} 
+), pageview_email_verified as (
+    SELECT
+        user_id,
+        pageview_id,
+        timestamp
+    FROM
+        {{ ref('stg_portal_prod__pageview_email_verified') }}
+), user_signup_stages as ( 
+SELECT
+    portal_customer_id,
+    -- Account is created when portal_customer_id exists
+    true AS account_created, 
+    -- Email is verified and user is redirected to `pageview_create_workspace` screen.
+    if pageview_email_verified.pageview_id then true else false as email_verified
+FROM
+    rudder_portal_user_mappings
+LEFT JOIN 
+    pageview_email_verified
+ON pageview_email_verified.user_id = rudder_portal_user_mappings.user_id
+QUALIFY row_number() over (partition by portal_customer_id order by timestamp) = 1
+)
+
+select 
+    * from user_signup_stages
