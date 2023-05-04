8a871a0fd6b74d9d2a1f76d7d9bb25fbd44b8a5c
Munish Sharma <munish7771@gmail.com>
2023-05-04 21:29:19 +0530

    Update int_cloud_trial_requests.sql

--- transform/mattermost-analytics/models/intermediate/sales/int_cloud_trial_requests.sql
+++ transform/mattermost-analytics/models/intermediate/sales/int_cloud_trial_requests.sql
@@ -51,7 +51,6 @@ cloud_trial_requests as (
         where CURRENT_DATE < subscriptions.trial_end_at
         -- Only get cloud subscriptions
         AND cws_installation is not null
-        AND products.sku = 'cloud-enterprise'
 )
 
 select
