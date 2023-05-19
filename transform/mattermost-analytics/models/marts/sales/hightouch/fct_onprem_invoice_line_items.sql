90da4481d2555c1a7114bea8afd31ce9892fcf03
Srikar Gudipudi <96433562+sri-byte@users.noreply.github.com>
2023-05-19 10:06:38 -0400

    MM-47986 SFDC Sync: Create opportunity to reflect expansion (#1246)
    * Adding onprem_subscriptions table from Stripe
    
    * Adding source tables for hightouch models to new project (#1252)
    
    * Adding staging Stripe OnPrem subscriptions
    
    * Adding stg_salesforce__account
    
    * Adding to staging invoices and invoice_line_items
    
    * Fixing errors for customers_with_onprem_subs
    
    * Creating customers_with_onprem_subs in new project
    
    * Updated SFDC names to reflect staging tables
    
    * Removing blapi.products reference temporarily
    
    * Fixed table reference
    
    * Fixed column name reference
    
    * Update transform/mattermost-analytics/models/staging/stripe/stg_stripe__invoice_line_items.sql
    
    Co-authored-by: Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
    
    * Update transform/mattermost-analytics/models/staging/stripe/stg_stripe__invoices.sql
    
    Co-authored-by: Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
    
    * Adding salesforce account to models and removed customer shipping JSON
    
    * Created new intermediate table for hightouch onprem new purchases and expansions
    
    * Removed metadata and customer_shipping JSON objects
    
    * Updated target team name from target integration
    
    * Fixed field names and added tests
    
    * Updated more field names and references
    
    * Updated duplicate field name
    
    * Updated field name reference
    
    * Fixed subscription_id reference
    
    * Added primary keys to Stripe and Salesforce models
    
    * Updated subscription_id column reference
    
    * Fixed ambigious column name error
    
    * Updated opportunity type
    
    * Added table logic in description
    
    * Creating fact table for de-normalized invoice and subscription data
    
    * Removed intermediate table from dbt project
    
    * Updated parsing for JSON fields for invoices
    
    * Update transform/mattermost-analytics/models/staging/stripe/stg_stripe__invoices.sql
    
    Co-authored-by: Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
    
    * Adding relationships check to invoice_line_items
    
    * Updated parsing JSON string fields
    
    * Updated customer_id field name
    
    * Updated customer_id field name  in model file
    
    * Adding updated_at to fct_onprem_invoice_line_items
    
    * Adding street_address field to fct_onprem_invoice_line_items
    
    * Adding contact_email and account_id to fct_onprem_invoice_line_items
    
    * Fixed join condition
    
    ---------
    
    Co-authored-by: Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>

---
+++ transform/mattermost-analytics/models/marts/sales/hightouch/fct_onprem_invoice_line_items.sql
@@ -0,0 +1,71 @@
+{{config({
+    "materialized": 'table',
+    "unique_key":"invoice_id",
+  })
+}}
+
+
+WITH subscriptions AS (
+select s.subscription_id
+    , s.purchase_order_number
+    , s.customer_id 
+    , s.license_id
+    , s.edition
+    , s.product_id
+    , p.sku
+    FROM {{ ref('stg_stripe__subscriptions') }}  s 
+    JOIN {{ ref('stg_stripe__products') }} p ON s.product_id = p.product_id
+    where s.edition not ilike '%cloud%' 
+), invoices AS (
+    SELECT s.purchase_order_number
+        , s.customer_id 
+        , s.license_id
+        , s.edition
+        , s.product_id
+        , s.sku
+        , invoices.charge_id
+        , invoices.subscription_id
+        , 'ONL' || invoices.number AS invoice_number
+        , invoices.number as stripe_invoice_number
+        , invoices.invoice_id as invoice_id
+        , invoices.created_at as invoice_created_at
+        , invoices.customer_email AS email
+        , SPLIT_PART(invoices.customer_email, '@', 2) as domain
+        , invoices.customer_name
+        , invoices.line1
+        , invoices.line2
+        , coalesce(invoices.line1, '') || ' ' || coalesce(invoices.line2, '') as street_address
+        , invoices.postal_code
+        , invoices.city
+        , invoices.state
+        , invoices.country
+        , invoices.customer_full_name
+        , invoices.total
+        , invoices.subtotal
+        , invoices.updated_at
+        , ROW_NUMBER() OVER (PARTITION BY invoices.subscription_id ORDER BY invoices.created_at) as invoice_row_num
+    FROM {{ ref('stg_stripe__invoices') }} invoices
+    JOIN subscriptions s on s.subscription_id = invoices.subscription_id
+    WHERE invoices.status = 'paid'
+) 
+, invoice_line_items AS (
+    SELECT i.* 
+        , ili.quantity as quantity
+        , LAG(ili.quantity) OVER (PARTITION BY i.subscription_id ORDER BY i.invoice_created_at) previous_quantity
+        , ili.quantity - COALESCE(LAG(ili.quantity) OVER (PARTITION BY i.subscription_id ORDER BY i.invoice_created_at),0) seats_purchased
+    FROM {{ ref('stg_stripe__invoice_line_items') }} ili 
+    JOIN invoices i ON i.invoice_id = ili.invoice_id
+    WHERE amount > 0
+) select account.account_id 
+    , contact.email as contact_email
+    , CASE WHEN invoice_row_num = 1 THEN 'New Subscription' 
+      WHEN invoice_row_num > 1 THEN 'Expansion' 
+      END AS opportunity_type
+    , 'Online' as order_type
+    , '{{ var("salesforce_default_ownerid") }}' AS ownerid
+    , '6. Closed Won' as stagename
+    , domain || ' ' || sku || ' qty:' || seats_purchased || ' inv:' || invoice_number AS opportunity_name
+    , invoice_line_items.*    
+FROM invoice_line_items invoice_line_items
+LEFT JOIN {{ ref('stg_salesforce__contact') }} contact ON invoice_line_items.email = contact.email
+LEFT JOIN {{ ref('stg_salesforce__account') }} account ON contact.account_id = account.account_id
---
+++ transform/mattermost-analytics/models/staging/stripe/stg_stripe__invoice_line_items.sql
@@ -0,0 +1,37 @@
+with source as (
+
+    select * from {{ source('stripe_raw', 'invoice_line_items') }}
+
+),
+
+invoice_line_items as (
+
+    select
+          amount as amount
+        , currency as currency
+        , description as description
+        , discountable as discountable
+        , discounts as discounts
+        , discount_amounts as discount_amounts
+        , id as invoice_line_item_id
+        , invoice as invoice_id
+        , invoice_item as invoice_item
+        , livemode as livemode
+        , object as object
+        , period as period
+        , plan as plan
+        , price as price
+        , proration as proration
+        , proration_details as proration_details
+        , quantity as quantity
+        , subscription as subscription_id
+        , subscription_item as subscription_item
+        , tax_amounts as tax_amounts
+        , tax_rates as tax_rates
+        , type as type
+
+    from source
+
+)
+
+select * from invoice_line_items
