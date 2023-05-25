992ae73181c4ca858256cddca8578e946ef8b7b6
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-05-25 15:13:31 +0300

    Chore: add missing join condition

--- transform/mattermost-analytics/tests/staging/stripe/test_subscription__has_license_data.sql
+++ transform/mattermost-analytics/tests/staging/stripe/test_subscription__has_license_data.sql
@@ -7,7 +7,7 @@ select
     license_end_at is null as is_missing_license_end_at
 from
     {{ ref('stg_stripe__subscriptions')}} s
-    left join {{ ref('stg_stripe__subscription_items')}} si
+    left join {{ ref('stg_stripe__subscription_items')}} si on s.subscription_id = si.subscription_id
     left join {{ ref('stg_stripe__products')}} p on si.product_id = p.product_id
 where
     -- Onprem subscription/subscription items
