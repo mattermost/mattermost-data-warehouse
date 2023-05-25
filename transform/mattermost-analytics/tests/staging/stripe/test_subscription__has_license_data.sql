6153df23b0aaab2299c22a578a10d20f468fb3f3
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-05-25 23:51:44 +0300

    Update tests to consider backfilled licenses

--- transform/mattermost-analytics/tests/staging/stripe/test_subscription__has_license_data.sql
+++ transform/mattermost-analytics/tests/staging/stripe/test_subscription__has_license_data.sql
@@ -2,6 +2,16 @@
 select
     s.subscription_id,
     p.name as plan_name,
+    case
+        -- Handle backfills
+        when s.sfdc_migrated_license_id is not null then sfdc_migrated_started_at
+        else s.license_end_at
+    end as license_end_at,
+    case
+        -- Handle backfills
+        when s.sfdc_migrated_license_id is not null then s.current_period_end_at
+        else s.license_end_at
+    end as license_end_at,
     license_id is null as is_missing_license_id,
     license_end_at is null as is_missing_license_start_at,
     license_end_at is null as is_missing_license_end_at
@@ -14,7 +24,6 @@ where
     p.name not ilike '%cloud%'
     -- Skip support subscription items and focus on main plan
     and p.name <> 'Premier Support'
-    -- License id must be non null
     and (
         -- License id must be non null
         license_id is null
