879f76222c74cc0c62a73f56068906ea02e8108b
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-14 17:05:09 +0300

    MM-48421: fix SQL code

--- transform/mattermost-analytics/models/intermediate/product/int_user_active_days.sql
+++ transform/mattermost-analytics/models/intermediate/product/int_user_active_days.sql
@@ -12,8 +12,8 @@ with tmp as (
     select
         cast(received_at as date) as received_at_date,
         cast(timestamp as date) as activity_date,
-        user_id as server_id,
-        user_actual_id as user_id
+        server_id,
+        user_id
     from
         {{ ref('stg_mm_telemetry_prod__tracks') }}
     where
@@ -22,16 +22,17 @@ with tmp as (
        -- this filter will only be applied on an incremental run
       and received_at >= (select max(received_at_date) from {{ this }})
 {% endif %}
-    group by received_at_date, activity_date, user_id, user_actual_id
-    order by received_at_date, user_id
+    group by received_at_date, activity_date, server_id, user_id
+    order by received_at_date
 )
 select
     -- Surrogate key required as it's both a good practice, as well as allows merge incremental strategy.
-    {{ dbt_utils.generate_surrogate_key(['activity_date', 'source', 'user_id', 'server_id']) }} AS daily_user_id
-    , received_at_date
+    {{ dbt_utils.generate_surrogate_key(['activity_date', 'user_id', 'server_id']) }} AS daily_user_id
     , activity_date
     , server_id
     , user_id
     , 1 as is_active
+    -- Required for incremental loading
+    , received_at_date
 from
     tmp
