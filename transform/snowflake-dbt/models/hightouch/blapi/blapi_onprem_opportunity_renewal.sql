43b28d2b78963031a0a269c740b9610c351ebfce
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-14 13:17:09 +0300

    MM-53143: Fix string (#1291)

--- transform/snowflake-dbt/models/hightouch/blapi/blapi_onprem_opportunity_renewal.sql
+++ transform/snowflake-dbt/models/hightouch/blapi/blapi_onprem_opportunity_renewal.sql
@@ -33,7 +33,7 @@ WITH onprem_opportunities_to_sync AS (
     WHERE opportunity.id IS NULL
         AND customers_with_onprem_subs.is_renewed 
         AND customers_with_onprem_subs.account_sfid is not null 
-        AND customers_with_onprem_subs.account_type in ('Customer','Customer(Attrited)')
+        AND customers_with_onprem_subs.account_type in ('Customer','Customer (Attrited)')
         AND customers_with_onprem_subs.hightouch_sync_eligible
 )
 SELECT * FROM onprem_opportunities_to_sync
