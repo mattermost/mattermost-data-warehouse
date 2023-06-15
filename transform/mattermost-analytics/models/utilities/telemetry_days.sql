146128013bad81c4084f1e7ffd6993a78c6bd8b5
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-15 15:18:07 +0300

    MM-48421: fix current day

--- transform/mattermost-analytics/models/utilities/telemetry_days.sql
+++ transform/mattermost-analytics/models/utilities/telemetry_days.sql
@@ -1,6 +1,6 @@
--- Create a date spine starting on the first date that events were ingested
+-- Create a date spine starting on the first date that events were ingested up to (and including) today.
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="'2020-04-01'",
-    end_date="current_date"
+    end_date="dateadd(day, 1, current_date)"
 ) }}
