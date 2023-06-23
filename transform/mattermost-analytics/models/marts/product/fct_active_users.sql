eaf80a95bf79ad248f57a5ec9bdd0e9a0494e63e
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-23 12:12:58 +0300

    Improve docs

--- transform/mattermost-analytics/models/marts/product/fct_active_users.sql
+++ transform/mattermost-analytics/models/marts/product/fct_active_users.sql
@@ -1,8 +1,20 @@
+{%
+    set column_map = {
+        'is_active': 'active_users',
+        'is_desktop_or_server': 'desktop_active_users',
+        'is_mobile': 'mobile_active_users',
+        'is_old_server': 'legacy_active_users'
+    }
+%}
+
 select
-    activity_date,
-    server_id,
-    sum(is_active_today::integer) as daily_active_users,
-    sum(is_active_last_7_days::integer) as weekly_active_users,
-    sum(is_active_last_30_days::integer) as monthly_active_users
+    {{ dbt_utils.generate_surrogate_key(['activity_date', 'server_id']) }} AS id
+    , activity_date
+    , server_id
+{% for metric, column in column_map.items() %}
+    , sum({{metric}}_today::integer) as daily_{{column}}
+    , sum({{metric}}_last_7_days::integer) as weekly_{{column}}
+    , sum({{metric}}_last_30_days::integer) as monthly_{{column}}
+{% endfor %}
 from {{ ref('int_user_active_days_spined') }}
 group by activity_date, server_id
