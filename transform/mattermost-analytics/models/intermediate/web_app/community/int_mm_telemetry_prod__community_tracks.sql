a7058345446a6203930c176f1b0f6f6aa0623ccd
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-29 15:34:40 +0300

    Add source relation

--- transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_prod__community_tracks.sql
+++ transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_prod__community_tracks.sql
@@ -9,10 +9,11 @@
 
     SELECT
         {{dbt_utils.star(ref('stg_mm_telemetry_prod__tracks'))}},
-        _received_at::date as received_at_date
+        'stg_mm_telemetry_prod__tracks' AS _source_relation,
+        received_at::date as received_at_date
      FROM
        {{ ref('stg_mm_telemetry_prod__tracks') }}
     WHERE server_id = '{{ var("community_server_id") }}'
 {% if is_incremental() %}
-    AND received_at > (SELECT MAX(_received_at) FROM {{ this }})
+    AND received_at > (SELECT MAX(received_at) FROM {{ this }})
 {% endif %}
