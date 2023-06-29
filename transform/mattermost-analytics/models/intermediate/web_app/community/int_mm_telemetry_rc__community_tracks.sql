e78d9f6bedee57daae4736832de6165853a4314f
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-29 16:25:04 +0300

    Properly use rc

--- transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_rc__community_tracks.sql
+++ transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_rc__community_tracks.sql
@@ -12,7 +12,7 @@
         'stg_mm_telemetry_rc__tracks' AS _source_relation,
         received_at::date as received_at_date
      FROM
-       {{ ref('stg_mm_telemetry_rc_tracks') }}
+       {{ ref('stg_mm_telemetry_rc__tracks') }}
     WHERE server_id = '{{ var("community_server_id") }}'
 {% if is_incremental() %}
     AND received_at > (SELECT MAX(received_at) FROM {{ this }})
