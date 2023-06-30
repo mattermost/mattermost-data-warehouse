0e38b5da767a74ccb9c4e80ec250ed78ceeda3e2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-06-30 16:58:02 +0300

    MM-48421: adding daily server info snapshot (#1308)
    * MM-48421: adding daily server info snapshot
    
    * MM-48421: adding daily server info snapshot
    
    * MM-48421: add license model
    
    * MM-48421: refactor structure for latest daily, add license intermediate models
    
    * MM-48421: refactor structure for latest daily, add license intermediate models
    
    * MM-48421: fix references
    
    * MM-48421: make intermediate models private
    
    * MM-48421: make intermediate models private
    
    * MM-48421: make intermediate models private
    
    * MM-48421: fix field reference
    
    * MM-48421: fix field reference
    
    * MM-48421: rollback licenses
    
    * MM-48421: add excludable servers v1
    
    * MM-48421: add excludable servers v1
    
    * MM-48421: add excludable servers v1
    
    * More docs
    
    * Error fixes

--- transform/mattermost-analytics/models/intermediate/product/servers/_latest_daily/int_server_telemetry_legacy_latest_daily.sql
+++ transform/mattermost-analytics/models/intermediate/product/servers/_latest_daily/int_server_telemetry_legacy_latest_daily.sql
@@ -1,28 +1,20 @@
-{{
-    config({
-        "materialized": "table",
-    })
-}}
 select
     server_id,
     CAST(timestamp AS date) AS server_date,
-    {{ dbt_utils.generate_surrogate_key(['server_id', 'server_date']) }} AS id,
+    {{ dbt_utils.generate_surrogate_key(['server_id', 'server_date']) }} AS daily_server_id,
     count_system_admins,
     version_full,
     version_major,
     version_minor,
     version_patch,
-    installation_id,
-    installation_type,
-    anonymous_id,
-    server_ip,
     operating_system,
     database_type,
     database_version,
     edition,
     -- Can be used to identify potential upgrade/upgrade attempts or erroneous data
-    count(distinct version_full) over (partition by server_id, server_date) as count_versions_in_date
+    count(distinct version_full) over (partition by server_id, server_date) as count_reported_versions,
+    array_unique_agg(version_full) over (partition by server_id, server_date) as reported_versions
 from
-    {{ ref('stg_mm_telemetry_prod__server') }}
+    {{ ref('stg_mattermost2__server') }}
 -- Keep latest record per day
 qualify row_number() over (partition by server_id, server_date order by timestamp desc) = 1
---
+++ transform/mattermost-analytics/models/intermediate/product/servers/int_server_info_daily_snapshot.sql
@@ -0,0 +1,31 @@
+select
+    coalesce(s.daily_server_id, l.daily_server_id) as daily_server_id,
+    coalesce(s.server_id, l.server_id) as server_id,
+    coalesce(s.server_date, l.server_date) as snapshot_date,
+    coalesce(s.version_full, l.version_full) as version_full,
+    coalesce(s.version_major, l.version_major) as version_major,
+    coalesce(s.version_minor, l.version_minor) as version_minor,
+    coalesce(s.version_patch, l.version_patch) as version_patch,
+    coalesce(s.operating_system, l.operating_system) as operating_system,
+    coalesce(s.database_type, l.database_type) as database_type,
+    coalesce(s.database_version, l.database_version) as database_version,
+    coalesce(s.edition, l.edition) as is_enterprise_ready,
+    s.installation_id,
+    s.installation_id is not null as is_cloud,
+    s.server_ip,
+    s.installation_type,
+    array_distinct(
+        array_cat(
+                coalesce(s.reported_versions, array_construct()),
+                coalesce(l.reported_versions, array_construct())
+        )
+    ) as reported_versions,
+    array_size(array_distinct(
+        array_cat(
+                coalesce(s.reported_versions, array_construct()),
+                coalesce(l.reported_versions, array_construct())
+        )
+    )) as count_reported_versions
+from
+    {{ ref('int_server_telemetry_legacy_latest_daily') }} l
+    full outer join {{ ref('int_server_telemetry_latest_daily') }} s on s.daily_server_id = l.daily_server_id
