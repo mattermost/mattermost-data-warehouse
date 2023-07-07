b8df20c7029d8fdb991e7693cbe409b6e450bed4
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-07-07 16:03:09 +0300

    MM-48421: fix nulls

--- transform/mattermost-analytics/models/utilities/telemetry_days.sql
+++ transform/mattermost-analytics/models/utilities/telemetry_days.sql
@@ -1,7 +1,6 @@
 -- Create a date spine starting on the first date that events were ingested up to (and including) today.
-{% set start_date = var('telemetry_start_date') %}
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date=start_date,
+    start_date="'" + var('telemetry_start_date') + "'",
     end_date="dateadd(day, 1, current_date)"
 ) }}
