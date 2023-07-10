e22a75b03e74871245ee33bbd80fa6b28e4bdf2b
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-07-10 11:21:03 +0300

    Chore: fix alert links

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -54,7 +54,7 @@ def create_alert_body(context):
     """
     Creates post body to be sent to mattermost channel.
     """
-    base_url = "https://airflow.internal.mattermost.com"
+    base_url = "https://airflow.dataeng.internal.mattermost.com"
     execution_date = context["ts"]
     dag_context = context["dag"]
     dag_name = dag_context.dag_id
