feb13e1b2014b928404767998e6301628e58564b
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-08-21 13:13:43 -0400

    Added field names

--- transform/mattermost-analytics/models/intermediate/github_actions/int_github_actions.sql
+++ transform/mattermost-analytics/models/intermediate/github_actions/int_github_actions.sql
@@ -7,18 +7,18 @@
     with seed_file as (
     -- Github Actions data from seed file
     select
-        trim(Date)
-        , trim(product)
-        , trim(sku)
-        , trim(quantity)
-        , trim(unit_type)
-        , trim(price_per_unit)
-        , trim(multiplier)
-        , trim(owner)
-        , trim(repository_slug)
-        , trim(username)
-        , trim(actions_workflow)
-        , trim(notes)
+        trim(date) as date
+        , trim(product) as product
+        , trim(sku) as sku
+        , trim(quantity) as quantity
+        , trim(unit_type) as unit_type
+        , trim(price_per_unit) as price_per_unit
+        , trim(multiplier) as multiplier
+        , trim(owner) as owner
+        , trim(repository_slug) as repository_slug
+        , trim(username) as username
+        , trim(actions_workflow) as actions_workflow
+        , trim(notes) as notes
     from
         {{ ref('github_actions') }}
 )
