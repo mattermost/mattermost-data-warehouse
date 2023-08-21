15e944d847bb21b94e86e01abf27057cec3af7a1
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-08-21 12:59:45 -0400

    Creates int table for github actions from seed file

---
+++ transform/mattermost-analytics/models/intermediate/github_actions/int_github_actions.sql
@@ -0,0 +1,25 @@
+{{
+    config({
+        "materialized": "table",
+        "snowflake_warehouse": "transform_l"
+    })
+}}
+    with seed_file as (
+    -- Github Actions data from seed file
+    select
+        trim(Date)
+        , trim(product)
+        , trim(sku)
+        , trim(quantity)
+        , trim(unit_type)
+        , trim(price_per_unit)
+        , trim(multiplier)
+        , trim(owner)
+        , trim(repository_slug)
+        , trim(username)
+        , trim(actions_workflow)
+        , trim(notes)
+    from
+        {{ ref('github_actions') }}
+)
+select * from seed_file
