8548fc3efb453b3b857eb2c17f919ba2f40a4f0d
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-08-30 14:49:02 +0300

    Add stripe customer id

--- transform/mattermost-analytics/models/intermediate/product/customers/int_customer_spine.sql
+++ transform/mattermost-analytics/models/intermediate/product/customers/int_customer_spine.sql
@@ -9,17 +9,19 @@ with license_spine as (
     union
     select distinct license_id, customer_id from  {{ ref('stg_mattermost2__license') }}
     union
-    select distinct license_id from {{ ref('stg_cws__license') }}
+    select distinct license_id, customer_id from {{ ref('stg_cws__license') }}
 ), onprem_servers as (
     -- On prem licenses
     select distinct
         spine.customer_id,
         spine.license_id,
+        c.customer_id as stripe_customer_id,
         coalesce(rudder_license.server_id, segment_license.server_id) as server_id
     from
         license_spine as spine
-        left join {{ ref('stg_mm_telemetry_prod__license') }} as rudder_license on cws_license.license_id = rudder_license.license_id
-        left join {{ ref('stg_mattermost2__license') }} as segment_license on cws_license.license_id = segment_license.license_id
+        left join {{ ref('stg_mm_telemetry_prod__license') }} as rudder_license on spine.license_id = rudder_license.license_id
+        left join {{ ref('stg_mattermost2__license') }} as segment_license on spine.license_id = segment_license.license_id
+        left join {{ ref('stg_stripe__customers') }} c on spine.customer_id = c.portal_customer_id
 ), cloud_spine as (
     select installation_id from {{ ref('stg_mm_telemetry_prod__server') }}
     union
@@ -28,17 +30,19 @@ with license_spine as (
     -- Cloud installations
     select distinct
         c.portal_customer_id as customer_id,
+        c.customer_id as stripe_customer_id,
         s.cws_installation as installation_id,
         s.cws_dns as installation_hostname,
         srv.server_id
     from
         cloud_spine spine
         left join {{ ref('stg_mm_telemetry_prod__server') }} srv on srv.installation_id = spine.installation_id
-        left join {{ ref('stg_stripe__subscriptions') }} s on s.cws_installtion = spine.installation_id
+        left join {{ ref('stg_stripe__subscriptions') }} s on s.cws_installation = spine.installation_id
         left join {{ ref('stg_stripe__customers') }} c on s.customer_id = c.customer_id
 )
 select
     customer_id,
+    stripe_customer_id,
     server_id,
     license_id,
     null as installation_id,
@@ -51,6 +55,7 @@ union all
 
 select
     customer_id,
+    stripe_customer_id,
     server_id,
     null as license_id,
     installation_id,
