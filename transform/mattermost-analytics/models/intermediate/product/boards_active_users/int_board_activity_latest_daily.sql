aa3dbd0faf8e874e28318ed251fc63ecdf7b9ca5
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-09-27 15:40:58 +0300

    MM-54564: fix staging model name

---
+++ transform/mattermost-analytics/models/intermediate/product/boards_active_users/int_board_activity_latest_daily.sql
@@ -0,0 +1,15 @@
+select
+    server_id,
+    CAST(timestamp AS date) AS server_date,
+    {{ dbt_utils.generate_surrogate_key(['server_id', 'server_date']) }} AS daily_server_id,
+    daily_active_users,
+    monthly_active_users,
+    count_registered_users,
+    count_registered_deactivated_users
+from
+    {{ ref('stg_hacktoberboard_prod__activity') }}
+where
+    -- Ignore rows where server date is in the future.
+    server_date <= CURRENT_DATE()
+-- Keep latest record per day
+qualify row_number() over (partition by server_id, server_date order by timestamp desc) = 1
