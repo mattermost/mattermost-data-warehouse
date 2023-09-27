fcb5311720983cc00c571395accbc22e2ed1dcf7
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-09-27 14:36:13 +0300

    MM-54564: add customer and server info

--- transform/mattermost-analytics/models/marts/product/boards/dim_board_customers.sql
+++ transform/mattermost-analytics/models/marts/product/boards/dim_board_customers.sql
@@ -5,10 +5,8 @@ select
     c.contact_last_name,
     c.email,
     c.customer_id as stripe_customer_id,
-    s.cws_dns,
-    s.cws_installation as installation_id,
-    s.edition as license_name
-
+    s.cws_dns as server_hostname,
+    s.edition as plan_name
 from
     {{ ref('_int_server_installation_id_bridge')}} sib
     join {{ ref('stg_stripe__subscriptions')}} s on sib.installation_id = s.cws_installation
@@ -16,3 +14,4 @@ from
 where
     s.cws_installation is not null
     and sib.server_id in (select distinct server_id from {{ ref('int_boards_active_days_spined') }})
+qualify row_number() over (partition by server_id order by s.created desc) = 1
