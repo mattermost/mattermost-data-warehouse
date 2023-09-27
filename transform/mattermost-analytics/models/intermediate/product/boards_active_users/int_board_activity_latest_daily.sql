4b4b08294b6c5755acb33ce6b549977b3b4918ce
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-09-27 16:56:33 +0300

    MM-54564: resolve server id

--- transform/mattermost-analytics/models/intermediate/product/boards_active_users/int_board_activity_latest_daily.sql
+++ transform/mattermost-analytics/models/intermediate/product/boards_active_users/int_board_activity_latest_daily.sql
@@ -1,15 +1,23 @@
+
+with id_mapping as (
+    select
+        distinct server_id, telemetry_id
+    from
+        {{ ref('stg_hacktoberboard_prod__server') }}
+)
 select
-    server_id,
-    CAST(timestamp AS date) AS server_date,
-    {{ dbt_utils.generate_surrogate_key(['server_id', 'server_date']) }} AS daily_server_id,
-    daily_active_users,
-    weekly_active_users,
-    monthly_active_users,
-    count_registered_users
+    m.server_id,
+    CAST(a.timestamp AS date) AS server_date,
+    {{ dbt_utils.generate_surrogate_key(['m.server_id', 'a.server_date']) }} AS daily_server_id,
+    a.daily_active_users,
+    a.weekly_active_users,
+    a.monthly_active_users,
+    a.count_registered_users
 from
-    {{ ref('stg_hacktoberboard_prod__activity') }}
+    {{ ref('stg_hacktoberboard_prod__activity') }} a
+    left join id_mapping m
 where
     -- Ignore rows where server date is in the future.
-    server_date <= CURRENT_DATE()
+    a.server_date <= CURRENT_DATE()
 -- Keep latest record per day
 qualify row_number() over (partition by server_id, server_date order by timestamp desc) = 1
