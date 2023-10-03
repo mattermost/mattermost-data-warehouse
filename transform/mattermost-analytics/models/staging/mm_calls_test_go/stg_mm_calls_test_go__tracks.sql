f7dbe3437d66fc66694e7535a5d98accad2db2b9
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-02 22:12:18 -0400

    Cleaned up code and minor changes

--- transform/mattermost-analytics/models/staging/mm_calls_test_go/stg_mm_calls_test_go__tracks.sql
+++ transform/mattermost-analytics/models/staging/mm_calls_test_go/stg_mm_calls_test_go__tracks.sql
@@ -1,22 +1,13 @@
-WITH tracks AS (
-    SELECT
-        {{ dbt_utils.star(ref('base_mm_calls_test_go__tracks')) }}
-    FROM
-        {{ ref ('base_mm_calls_test_go__tracks') }}
-)
 SELECT
-        participants as participants,
         server_version as server_version,
-        call_id as call_id,
         plugin_build as plugin_build,
         event as event,
         user_id as server_id,
         received_at as received_at,
         plugin_version as plugin_version,
-        duration as duration,
         event_text as event_text,
         id as event_id,
         timestamp as timestamp,
-        screen_duration as screen_duration,
         actual_user_id as user_id
-FROM tracks
+    FROM
+        {{ ref ('base_mm_calls_test_go__tracks') }}
