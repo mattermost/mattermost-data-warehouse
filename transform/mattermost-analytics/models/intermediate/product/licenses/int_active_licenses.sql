e64ffbe7adf64de15f2ff5d2d50cbe5e240d0541
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-06 17:02:28 +0300

    Split active licenses with licenses per source

--- transform/mattermost-analytics/models/intermediate/product/licenses/int_active_licenses.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/int_active_licenses.sql
@@ -5,47 +5,14 @@
         "snowflake_warehouse": "transform_l"
     })
 }}
-with stripe_licenses as (
-    select
-        s.license_id,
-        -- Convert to epoch in order to remove timezone
-        to_timestamp_ntz(date_part(epoch_second, license_end_at)) as expire_at
-    from
-        {{ ref('stg_stripe__subscriptions')}} s
-        left join {{ ref('stg_stripe__subscription_items')}} si on s.subscription_id = si.subscription_id
-        left join {{ ref('stg_stripe__products')}} p on si.product_id = p.product_id
-    where
-        -- Onprem subscription/subscription items
-        p.name not ilike '%cloud%'
-        -- Skip support subscription items and focus on main plan
-        and p.name <> 'Premier Support'
-        -- Skip incomplete subscriptions
-        and s.status <> 'incomplete_expired'
-        -- Data before this date might not be in-line with specification
-        and s.created_at > '2021-04-01'
-        -- Ignore invalid records
-        and s.license_id is not null
-), all_licenses as (
-    select license_id, expire_at, 'CWS' as source from {{ ref('stg_cws__license') }}
-    union
-    select license_id, expire_at, 'Stripe' as source from stripe_licenses
-    union
-    select license_id, expire_at, 'Rudderstack' as source from {{ ref('stg_mm_telemetry_prod__license') }} where license_id is not null and license_name in ('E10', 'E20', 'enterprise', 'professional')
-    union
-    select license_id, expire_at, 'Segment' as source from  {{ ref('stg_mattermost2__license') }} where license_id is not null and license_name in ('E10', 'E20', 'enterprise', 'professional')
-    -- Legacy licenses
-    union
-    select license_id, expire_at, 'Legacy' as source from {{ ref('stg_licenses__licenses') }}
-), distinct_licenses as (
-    -- Licenses with more than one expiration date
-    select
+
+with distinct_licenses as (
+
+     select
         license_id,
-        count(distinct expire_at) as count_expiration_dates,
+        max(has_multiple_expiration_dates) as has_multiple_expiration_dates,
         array_agg(source) within group (order by source) as sources
-    from
-        all_licenses
-    where
-        license_id is not null
+    from {{ ref('int_licenses_per_source') }}
     group by 1
 )
 select
---
+++ transform/mattermost-analytics/models/intermediate/product/licenses/int_licenses_per_source.sql
@@ -0,0 +1,59 @@
+-- Temporarily materialize during development
+{{
+    config({
+        "materialized": "table",
+        "snowflake_warehouse": "transform_l"
+    })
+}}
+with stripe_licenses as (
+    select
+        s.license_id,
+        -- Convert to epoch in order to remove timezone
+        to_timestamp_ntz(date_part(epoch_second, license_end_at)) as expire_at
+    from
+        {{ ref('stg_stripe__subscriptions')}} s
+        left join {{ ref('stg_stripe__subscription_items')}} si on s.subscription_id = si.subscription_id
+        left join {{ ref('stg_stripe__products')}} p on si.product_id = p.product_id
+    where
+        -- Onprem subscription/subscription items
+        p.name not ilike '%cloud%'
+        -- Skip support subscription items and focus on main plan
+        and p.name <> 'Premier Support'
+        -- Skip incomplete subscriptions
+        and s.status <> 'incomplete_expired'
+        -- Data before this date might not be in-line with specification
+        and s.created_at > '2021-04-01'
+        -- Ignore invalid records
+        and s.license_id is not null
+), all_licenses as (
+    select license_id, expire_at, 'CWS' as source from {{ ref('stg_cws__license') }}
+    union
+    select license_id, expire_at, 'Stripe' as source from stripe_licenses
+    union
+    select license_id, expire_at, 'Rudderstack' as source from {{ ref('stg_mm_telemetry_prod__license') }} where license_id is not null and license_name in ('E10', 'E20', 'enterprise', 'professional')
+    union
+    select license_id, expire_at, 'Segment' as source from  {{ ref('stg_mattermost2__license') }} where license_id is not null and license_name in ('E10', 'E20', 'enterprise', 'professional')
+    -- Legacy licenses
+    union
+    select license_id, expire_at, 'Legacy' as source from {{ ref('stg_licenses__licenses') }}
+), license_expiration_date_counts as (
+    -- Licenses with more than one expiration date
+    select
+        license_id,
+        source,
+        count(distinct expire_at) as count_expiration_dates
+    from
+        all_licenses
+    where
+        license_id is not null
+    group by 1, 2
+    having count(distinct expire_at) > 1
+)
+select
+    all_licenses.license_id,
+    all_licenses.source,
+    -- Mark license IDs with > 1 expiration dates as outliers
+    outliers.count_expiration_dates is not null as has_multiple_expiration_dates
+from
+    all_licenses
+    left join license_expiration_date_counts outliers on all_licenses.license_id = outliers.license_id and all_licenses.source = outliers.source
