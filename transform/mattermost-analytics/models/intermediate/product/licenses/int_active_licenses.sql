f583be12c4467dc3023c612f30e06772e81f27ba
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-11 13:17:26 +0300

    Add rudder where missing CWS data

--- transform/mattermost-analytics/models/intermediate/product/licenses/int_active_licenses.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/int_active_licenses.sql
@@ -17,22 +17,34 @@ with active_licenses as (
     group by 1
     having
         max(expire_at) >= current_date
+), rudder_licenses as (
+    select
+        distinct
+            license_id,
+            issued_at,
+            start_at as starts_at,
+            expire_at,
+            license_name,
+            licensed_seats as number_of_users
+        from
+            {{ ref('stg_mm_telemetry_prod__license') }}
+        where license_id is not null
 )
 select
     active_licenses.license_id,
 
     -- License data
-    cws.issued_at,
-    cws.starts_at,
-    cws.expire_at,
+    coalesce(cws.issued_at, rudder.issued_at),
+    coalesce(cws.starts_at, rudder.starts_at),
+    coalesce(cws.expire_at, rudder.expire_at),
     cws.sku,
-    cws.sku_short_name,
+    coalesce(cws.sku_short_name, rudder.license_name),
     cws.is_gov_sku,
     cws.is_trial,
     cws.company_name,
     cws.customer_email,
     cws.customer_name,
-    cws.number_of_users,
+    coalesce(cws.number_of_users, number_of_users),
     cws.created_at,
     datediff(day, cws.starts_at::date, cws.expire_at::date) as duration_days,
     datediff(month, cws.starts_at::date, cws.expire_at::date) as duration_months,
@@ -46,3 +58,4 @@ select
 from
     active_licenses
     left join {{ ref('stg_cws__license') }} cws on active_licenses.license_id = cws.license_id
+    left join rudder_licenses rudder on active_licenses.license_id = rudder.license_id
