3856e76b86b58f739d6934d05470f305ac5556c5
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-11 15:09:24 +0300

    Update tests

--- transform/mattermost-analytics/tests/intermediate/product/licenses/test_stripe_licenses_in_cws.sql
+++ transform/mattermost-analytics/tests/intermediate/product/licenses/test_stripe_licenses_in_cws.sql
@@ -1,8 +1,29 @@
 -- Checks that all stripe licenses exist in CWS.
+
+with onprem_licenses as (
+    -- List of onprem licenses
+    select
+        s.license_id
+    from
+        {{ ref('stg_stripe__subscriptions')}} s
+        left join {{ ref('stg_stripe__subscription_items')}} si on s.subscription_id = si.subscription_id
+        left join {{ ref('stg_stripe__products')}} p on si.product_id = p.product_id
+    where
+        -- Onprem subscription/subscription items
+        p.name not ilike '%cloud%'
+        -- Skip support subscription items and focus on main plan
+        and p.name <> 'Premier Support'
+        -- Skip incomplete subscriptions
+        and s.status <> 'incomplete_expired'
+        -- Data before this date might not be in-line with specification
+        and s.created_at > '2021-04-01'
+        -- Ignore invalid records
+        and s.license_id is not null
+)
 select
-    subs.license_id
+    op.license_id
 from
-    {{ ref('stg_stripe__subscriptions')}} subs
-    left join {{ ref('stg_cws__license')}} cws on subs.license_id = cws.license_id
+    onprem_licenses op
+    left join {{ ref('stg_cws__license')}} cws on op.license_id = cws.license_id
 where
-    cws.license_id is null
+    and cws.license_id is null
