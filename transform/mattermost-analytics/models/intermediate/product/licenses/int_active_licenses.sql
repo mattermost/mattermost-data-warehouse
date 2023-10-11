f2fa901a84483e78aa02f1124a96669962026527
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-11 16:25:47 +0300

    Coalesce license name

--- transform/mattermost-analytics/models/intermediate/product/licenses/int_active_licenses.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/int_active_licenses.sql
@@ -25,7 +25,7 @@ select
     coalesce(cws.starts_at, rudder.starts_at),
     coalesce(cws.expire_at, rudder.expire_at),
     cws.sku,
-    coalesce(cws.sku_short_name, rudder.license_name),
+    coalesce(cws.sku_short_name, get(rudder.license_name, 0)),
     cws.is_gov_sku,
     cws.is_trial,
     cws.company_name,
