9c024fc8ff249208aa484f59a68765e718b28ce2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-11 14:51:01 +0300

    More elaborate dedup, fix tests

--- transform/mattermost-analytics/tests/intermediate/product/licenses/test_stripe_licenses_in_cws.sql
+++ transform/mattermost-analytics/tests/intermediate/product/licenses/test_stripe_licenses_in_cws.sql
@@ -1,6 +1,6 @@
 -- Checks that all stripe licenses exist in CWS.
 select
-    s.license_id
+    subs.license_id
 from
     {{ ref('stg_stripe__subscriptions')}} subs
     left join {{ ref('stg_cws__license')}} cws on subs.license_id = cws.license_id
