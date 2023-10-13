bf24a09140fc0aa07d4fa58ced08773e76130711
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-13 15:32:33 +0300

    Use correct surrogate key macro

--- transform/mattermost-analytics/models/marts/product/licenses/dim_license_type.sql
+++ transform/mattermost-analytics/models/marts/product/licenses/dim_license_type.sql
@@ -1,5 +1,5 @@
 select
-    {{ dbt_utils.surrogate_key(['product_id']) }} as license_type_id,
+    {{ dbt_utils.generate_surrogate_key(['product_id']) }} as license_type_id,
     sku,
     name,
     cws_product_family as product_family
