2cf17daf0c317309a80e690b1534f21b90fe0f91
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-13 21:54:51 -0400

    Splits feedback & score int models

---
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_nps_feedback.sql
@@ -0,0 +1,25 @@
+with mattermost_nps as (
+select 
+    distinct server_id as server_id
+    , user_id as user_id
+    , event_date as event_date
+    , server_version as server_version
+    , feedback as feedback
+    , user_role as user_role
+    , received_at as feedback_received_at
+    from {{ ref('stg_mattermost__nps_feedback') }} 
+), mm_plugin_prod as 
+(
+ select 
+    distinct server_id as server_id
+    , user_id as user_id
+    , event_date as event_date
+    , server_version as server_version
+    , feedback as feedback
+    , user_role as user_role
+    , received_at as feedback_received_at
+    from {{ ref('stg_mm_plugin_prod__nps_feedback') }}    
+) 
+select * from mattermost_nps
+union
+select * from mm_plugin_prod
---
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_nps_score.sql
@@ -0,0 +1,25 @@
+with mattermost_nps as (
+select 
+    distinct server_id as server_id
+    , user_id as user_id
+    , event_date as event_date
+    , server_version as server_version
+    , score as score
+    , user_role as user_role
+    , received_at as score_received_at
+    from {{ ref('stg_mattermost__nps_score') }} 
+), mm_plugin_prod as 
+(
+ select 
+    distinct server_id as server_id
+    , user_id as user_id
+    , event_date as event_date
+    , server_version as server_version
+    , score as score
+    , user_role as user_role
+    , received_at as score_received_at
+    from {{ ref('stg_mm_plugin_prod__nps_score') }}    
+) 
+select * from mattermost_nps
+union
+select * from mm_plugin_prod
