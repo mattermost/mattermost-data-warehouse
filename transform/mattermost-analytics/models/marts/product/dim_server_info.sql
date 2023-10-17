4a7ee9365d7d2e71a580a3c2ec50c2ce65207e99
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-17 15:06:43 +0300

    Add server first and last activity (#1358)
    * Add server first and last activity
    
    * Fix typo
    
    * Fix typo

--- transform/mattermost-analytics/models/marts/product/dim_server_info.sql
+++ transform/mattermost-analytics/models/marts/product/dim_server_info.sql
@@ -1,10 +1,12 @@
-with hosting_type_info as (
+with server_info as (
     -- Summarize server hosting type information
     select
         server_id,
         -- Aggregate is cloud in order to validate whether the server is cloud or not
         count_if(is_cloud = true) as count_is_cloud_days,
-        count_if(is_cloud = false) as count_not_is_cloud_days
+        count_if(is_cloud = false) as count_not_is_cloud_days,
+        min(activity_date) as first_activity_date,
+        max(activity_date) as last_activity_date
     from
          {{ ref('int_server_active_days_spined') }}
     group by
@@ -30,15 +32,17 @@ with hosting_type_info as (
     qualify row_number() over (partition by s.cws_dns order by s.created_at desc) = 1
 )
 select
-    hti.server_id,
+    si.server_id,
     case
         when count_is_cloud_days > 0 and count_not_is_cloud_days = 0 then 'Cloud'
         when count_is_cloud_days = 0 and count_not_is_cloud_days > 0 then 'Self-hosted'
         else 'Unknown'
     end as hosting_type,
     l.installation_id,
-    s.cloud_hostname
+    s.cloud_hostname,
+    si.first_activity_date,
+    si.last_activity_date
 from
-    hosting_type_info hti
-    join latest_values l on hti.server_id = l.server_id
+    server_info si
+    join latest_values l on si.server_id = l.server_id
     left join latest_cloud_subscription s on l.installation_id = s.installation_id
