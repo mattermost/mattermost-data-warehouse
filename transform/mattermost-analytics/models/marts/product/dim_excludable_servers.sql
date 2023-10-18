466e9266c5ccba2769d0cd3eda9adb254a0b1858
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-18 16:41:00 +0300

    Pivot exclusion reasons (#1359)
    * Pivot exclusion reasons
    
    * Improve column names, use boolean as values
    
    * Use proper aggregation function
    
    * Update documentation
    
    * Add QA servers

--- transform/mattermost-analytics/models/marts/product/dim_excludable_servers.sql
+++ transform/mattermost-analytics/models/marts/product/dim_excludable_servers.sql
@@ -1,8 +1,14 @@
 select
-    server_id,
-    array_agg(reason) within group (order by reason) as reasons
-from
-    {{ ref('int_excludable_servers') }}
-where
-    server_id is not null
+  server_id,
+  {{ dbt_utils.pivot(
+      'reason',
+      dbt_utils.get_column_values(ref('int_excludable_servers'), 'reason'),
+      agg='max',
+      then_value='true',
+      else_value='false',
+      quote_identifiers=False,
+      prefix='has_reason_'
+  ) }}
+from {{ ref('int_excludable_servers') }}
+where server_id is not null
 group by server_id
