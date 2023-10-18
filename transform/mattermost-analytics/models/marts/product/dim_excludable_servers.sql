0925cbde3e9604c096a2d9ae0e472fc9ad03add2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-18 15:38:17 +0300

    Pivot exclusion reasons

--- transform/mattermost-analytics/models/marts/product/dim_excludable_servers.sql
+++ transform/mattermost-analytics/models/marts/product/dim_excludable_servers.sql
@@ -1,8 +1,10 @@
 select
-    server_id,
-    array_agg(reason) within group (order by reason) as reasons
-from
-    {{ ref('int_excludable_servers') }}
-where
-    server_id is not null
+  server_id,
+  {{ dbt_utils.pivot(
+      'reason',
+      dbt_utils.get_column_values(ref('int_excludable_servers'), 'reason'),
+      prefix='reason_'
+  ) }}
+from {{ ref('int_excludable_servers') }}
+where server_id is not null
 group by server_id
