d42a595df0bf192f3b9f850126368d48edf39bc7
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-19 12:46:46 -0400

    Added custom test

---
+++ transform/mattermost-analytics/tests/marts/nps/test_nps_feedback_score_date.sql
@@ -0,0 +1,20 @@
+-- models/marts/nps/dim_nps_feedback.sql
+
+-- Custom test to check that score_date is equal to feedback_date when not null
+
+{{ config(
+    severity = 'warn'
+    error_if != 0
+) }}
+
+with test_data as (
+    select
+        score_date,
+        feedback_date
+    from {{ ref('dim_nps_feedback') }}
+) select
+    case
+        when score_date is not null  
+        and score_date != feedback_date then 'FAIL' END 
+        as status
+from test_data;
