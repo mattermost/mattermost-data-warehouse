f53678b42694ea636574ebe863293c6af1b72faa
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-19 12:47:23 -0400

    Added where clause to test

--- transform/mattermost-analytics/tests/marts/nps/test_nps_feedback_score_date.sql
+++ transform/mattermost-analytics/tests/marts/nps/test_nps_feedback_score_date.sql
@@ -12,6 +12,7 @@ with test_data as (
         score_date,
         feedback_date
     from {{ ref('dim_nps_feedback') }}
+    where score is not null
 ) select
     case
         when score_date is not null  
