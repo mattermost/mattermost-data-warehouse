1ba2f0859b0d2740fe4222032da74358a70bf6b7
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-23 15:43:09 -0400

    Added source for easy reference

--- transform/mattermost-analytics/models/intermediate/product/licenses/int_licenses_self_hosted.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/int_licenses_self_hosted.sql
@@ -11,12 +11,14 @@ with cws_licenses as (
         , customer_id as customer_id
         , customer_email as email
         , company_name as company_name
+        , 'CWS' as source
     from {{ ref('stg_cws__license') }}
 ), stripe_licenses as (
     select distinct license_id
         , c.portal_customer_id as customer_id
         , c.email as email
-        , c.name as company_name 
+        , c.name as company_name
+        , 'Stripe' as source
     from {{ ref('stg_stripe__subscriptions')}}  s 
     join {{ ref('stg_stripe__customers')}}  c on s.customer_id = c.customer_id
     where license_id is not null
@@ -25,6 +27,7 @@ with cws_licenses as (
         , customer_id 
         , contact_email as email
         , company_name 
+        , 'Legacy' as source
     from {{ ref('stg_licenses__licenses')}} 
 ), all_licenses as (
     select * from cws_licenses
@@ -37,6 +40,7 @@ with cws_licenses as (
     , al.customer_id
     , al.email
     , al.company_name 
+    , al.source
 from all_licenses al 
 left join {{ ref('stg_mm_telemetry_prod__license')}} l1 on al.license_id = l1.license_id
 left join {{ ref('stg_mattermost2__license')}} l2 on al.license_id = l2.license_id
