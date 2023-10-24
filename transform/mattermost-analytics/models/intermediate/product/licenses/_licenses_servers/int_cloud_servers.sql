d0d19bc8c3e5e4a97c1f1cd5ebde73fbf68a02d3
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-24 19:39:22 -0400

    Removed mattermost2 source

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
@@ -8,16 +8,6 @@ from {{ ref('stg_mm_telemetry_prod__license')}}
 where license_name in ('CLD')
 and installation_id is not null
 qualify row_number() over (partition by installation_date, server_id order by timestamp desc) = 1
-), mattermost2_license as (
-select timestamp::date as installation_date
-    , installation_id as installation_id
-    , server_id as server_id
-    , customer_id as customer_id
-    , license_name as edition
-from {{ ref('stg_mattermost2__license')}} 
-where license_name in ('CLD')
-and installation_id is not null
-qualify row_number() over (partition by installation_date, server_id order by timestamp desc) = 1
 ) select * from mm_telemetry_prod_license
-    union
-    select * from mattermost2_license
+  
+-- Installation ID exists only in rudderstack telemetry
