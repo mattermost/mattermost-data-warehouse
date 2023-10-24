403d300eb716e032b4606e4d1b5c521800ebf270
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-23 20:57:52 -0400

    Updated logic and added company_name

--- transform/mattermost-analytics/models/intermediate/product/licenses/int_self_hosted_customers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/int_self_hosted_customers.sql
@@ -10,8 +10,10 @@ select distinct a.license_id
     , a.server_id
     , a.customer_id
     , b.email as customer_email
+    , b.company_name
     , coalesce(a.edition, b.edition) as edition
     , b.source 
-from {{ ref('int_licenses_self_hosted')}} a 
-left join {{ ref('int_license_customers')}} b 
+from {{ ref('int_self_hosted_servers')}} a 
+left join {{ ref('int_self_hosted_licenses')}} b 
 on a.license_id = b.license_id or a.customer_id = b.customer_id
+where company_name is not null
