cd674ae178cad7c310f2afd18fa97770f9089866
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-24 15:17:24 -0400

    Adding intermediate model for cloud servers

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
@@ -1,21 +1,21 @@
 with mm_telemetry_prod_license as (
 select timestamp::date as license_date
-    , license_id as license_id
+    , installation_id as installation_id
     , server_id as server_id
     , customer_id as customer_id
     , license_name as edition
 from {{ ref('stg_mm_telemetry_prod__license')}} 
-where license_name in ('E10', 'E20', 'enterprise', 'professional')
+where license_name in ('CLD')
 and license_id is not null
 qualify row_number() over (partition by license_date, server_id order by timestamp desc)
 ), mattermost2_license as (
 select timestamp::date as license_date
-    , license_id as license_id
+    , installation_id as installation_id
     , server_id as server_id
     , customer_id as customer_id
     , license_name as edition
 from {{ ref('stg_mattermost2__license')}} 
-where license_name in ('E10', 'E20', 'enterprise', 'professional')
+where license_name in ('CLD')
 and license_id is not null
 qualify row_number() over (partition by license_date, server_id order by timestamp desc)
 ) select * from mm_telemetry_prod_license
