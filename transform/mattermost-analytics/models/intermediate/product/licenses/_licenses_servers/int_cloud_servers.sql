c928d08a93d3fa2fad58c4fc5b943000e4ecaa63
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-24 16:26:15 -0400

    Fixed window function

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
@@ -7,7 +7,7 @@ select timestamp::date as license_date
 from {{ ref('stg_mm_telemetry_prod__license')}} 
 where license_name in ('CLD')
 and license_id is not null
-qualify row_number() over (partition by license_date, server_id order by timestamp desc)
+qualify row_number() over (partition by license_date, server_id order by timestamp desc) = 1
 ), mattermost2_license as (
 select timestamp::date as license_date
     , installation_id as installation_id
@@ -17,7 +17,7 @@ select timestamp::date as license_date
 from {{ ref('stg_mattermost2__license')}} 
 where license_name in ('CLD')
 and license_id is not null
-qualify row_number() over (partition by license_date, server_id order by timestamp desc)
+qualify row_number() over (partition by license_date, server_id order by timestamp desc) = 1
 ) select * from mm_telemetry_prod_license
     union
     select * from mattermost2_license
