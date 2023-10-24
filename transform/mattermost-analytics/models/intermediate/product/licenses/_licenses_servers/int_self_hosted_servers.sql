9f6f492bc3fadf4aacf0940a80f6b033184c49b0
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-24 14:13:33 -0400

    Dropped int model and creates dim model

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_self_hosted_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_self_hosted_servers.sql
@@ -1,19 +1,23 @@
 with mm_telemetry_prod_license as (
-select distinct license_id
-, server_id
-, customer_id
-, license_name as edition
+select timestamp::date license_date
+    , license_id as license_id
+    , server_id as server_id
+    , customer_id as customer_id
+    , license_name as edition
 from {{ ref('stg_mm_telemetry_prod__license')}} 
 where license_name in ('E10', 'E20', 'enterprise', 'professional')
 and license_id is not null
+qualify row_number() over (partition by license_date, server_id order by timestamp desc)
 ), mattermost2_license as (
-select distinct license_id
-, server_id
-, customer_id
-, license_name as edition
+select timestamp::date as license_date
+    , license_id as license_id
+    , server_id as server_id
+    , customer_id as customer_id
+    , license_name as edition
 from {{ ref('stg_mattermost2__license')}} 
 where license_name in ('E10', 'E20', 'enterprise', 'professional')
 and license_id is not null
+qualify row_number() over (partition by license_date, server_id order by timestamp desc)
 ) select * from mm_telemetry_prod_license
     union
     select * from mattermost2_license
--- transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
+++ transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
@@ -1,18 +1,10 @@
--- Temporarily materialize during development
-{{
-    config({
-        "materialized": "table",
-        "snowflake_warehouse": "transform_l"
-    })
-}}
-
-select distinct a.license_id
-    , a.server_id
-    , a.customer_id
+select distinct a.license_id as license_id
+    , a.server_id as server_id
+    , a.customer_id as customer_id
     , b.email as customer_email
-    , b.company_name
+    , b.company_name as company_name
     , coalesce(a.edition, b.edition) as edition
-    , b.source 
+    , b.source as source
 from {{ ref('int_self_hosted_servers')}} a 
 left join {{ ref('int_self_hosted_licenses')}} b 
 on a.license_id = b.license_id or a.customer_id = b.customer_id
