8fc59cb84a527ae6904391f33c9886269396b156
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-10-24 20:09:41 -0400

    dropped customer_id column

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
@@ -2,7 +2,6 @@ with mm_telemetry_prod_license as (
 select timestamp::date as installation_date
     , installation_id as installation_id
     , server_id as server_id
-    , customer_id as customer_id
     , license_name as license_name
 from {{ ref('stg_mm_telemetry_prod__license')}} 
 where license_name in ('CLD')
