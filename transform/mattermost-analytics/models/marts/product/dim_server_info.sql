579d9c5e3009311b80e6a67b6b3c904329b35c98
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-27 16:24:34 +0300

    MM-54964: add server info for servers where only front end telemetry … (#1366)
    * MM-54964: add server info for servers where only front end telemetry exists
    
    * MM-54964: handle null in dates
    
    * MM-54964: null latest value for installation id

--- transform/mattermost-analytics/models/marts/product/dim_server_info.sql
+++ transform/mattermost-analytics/models/marts/product/dim_server_info.sql
@@ -1,5 +1,5 @@
-with server_info as (
-    -- Summarize server hosting type information
+with server_telemetry_summary as (
+    -- Summarize server hosting type information based on server-sent info
     select
         server_id,
         -- Aggregate is cloud in order to validate whether the server is cloud or not
@@ -11,6 +11,36 @@ with server_info as (
          {{ ref('int_server_active_days_spined') }}
     group by
         server_id
+), user_telemetry_summary as (
+    select
+        server_id,
+        -- No information about the type of the server from user telemetry
+        min(activity_date) as first_activity_date,
+        max(activity_date) as last_activity_date
+    from
+         {{ ref('int_user_active_days_spined') }}
+    where
+        is_active_today
+    group by
+        server_id
+), server_info as (
+   select
+       coalesce(st.server_id, ut.server_id) as server_id,
+       coalesce(st.count_is_cloud_days, 0) as count_is_cloud_days,
+       coalesce(st.count_not_is_cloud_days, 0) as count_not_is_cloud_days,
+       case
+           when st.first_activity_date is null then ut.first_activity_date
+           when ut.first_activity_date is null then st.first_activity_date
+           else least(st.first_activity_date, ut.first_activity_date)
+        end as first_activity_date,
+       case
+           when st.last_activity_date is null then ut.last_activity_date
+           when ut.last_activity_date is null then st.last_activity_date
+           else greatest(st.last_activity_date, ut.last_activity_date)
+        end as last_activity_date
+    from
+        server_telemetry_summary st
+        full outer join user_telemetry_summary ut on st.server_id = ut.server_id
 ), latest_values as (
     -- Get latest values for installation id (if exists) and full version string
     select
@@ -44,5 +74,5 @@ select
     si.last_activity_date
 from
     server_info si
-    join latest_values l on si.server_id = l.server_id
+    left join latest_values l on si.server_id = l.server_id
     left join latest_cloud_subscription s on l.installation_id = s.installation_id
