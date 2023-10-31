beae793eda3d5254caf36d5243153f6855430c8f
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-10-31 13:36:43 +0200

    MM-55026: fix column name

--- transform/mattermost-analytics/models/marts/release/dim_fix_versions.sql
+++ transform/mattermost-analytics/models/marts/release/dim_fix_versions.sql
@@ -3,12 +3,12 @@
 -- Implement filtering on this layer as it's only used here.
 select
     issue_id,
-    value:name::string as fix_version_name,
+    value:name::string as fix_version,
     -- Break down different variations of target version
-    regexp_substr(fix_version_name, 'v\\d+\.\\d+') as semver,
-    regexp_substr(fix_version_name, 'v(\\d+)', 1, 1, 'e', 1)::int as version_major,
-    regexp_substr(fix_version_name, '\\.(\\d+)', 1, 1, 'e', 1)::int as version_minor,
-    regexp_substr(fix_version_name, '\\.(\\d+)', 1, 2, 'e', 1)::int as version_patch,
+    regexp_substr(fix_version, 'v\\d+\.\\d+') as semver,
+    regexp_substr(fix_version, 'v(\\d+)', 1, 1, 'e', 1)::int as version_major,
+    regexp_substr(fix_version, '\\.(\\d+)', 1, 1, 'e', 1)::int as version_minor,
+    regexp_substr(fix_version, '\\.(\\d+)', 1, 2, 'e', 1)::int as version_patch,
     case
         when fix_version_name ilike '%mobile%' then 'Mobile'
         when fix_version_name ilike '%desktop%' then 'Desktop'
@@ -18,15 +18,15 @@ select
         when fix_version_name ilike '%apps%' then 'Apps'
     end as component,
     to_date(regexp_substr(fix_version_name, '\\d{2}/\\d{2}/\\d{2}'), 'mm/dd/yy') as cloud_release_date,
-    fix_version_name like any (
+    fix_version like any (
         {%- for month in months %}
             'v%({{month}}%)'{%- if not loop.last %},{% endif -%}
         {% endfor %}
     ) as is_on_prem_release,
-    fix_version_name like 'Cloud%v%' as is_cloud_release
+    fix_version like 'Cloud%v%' as is_cloud_release
 from
     {{ ref('stg_mattermost_jira__issues') }},
     lateral flatten(input => fix_versions)
 where
     -- Keep only relevant fix versions - ones that contain a version in the form `v[major].[minor]`
-    regexp_like(fix_version_name, '.*v\\d+\.\\d+.*', 'i')
+    regexp_like(fix_version, '.*v\\d+\.\\d+.*', 'i')
