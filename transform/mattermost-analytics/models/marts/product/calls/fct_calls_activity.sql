b04b31490096b520cf3c38727182a69079f2ccce
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-02 01:39:13 -0400

    Exclude calls servers not in product telemetry

--- transform/mattermost-analytics/models/marts/product/calls/fct_calls_activity.sql
+++ transform/mattermost-analytics/models/marts/product/calls/fct_calls_activity.sql
@@ -8,4 +8,5 @@
 select
     {{dbt_utils.star(ref('int_calls_active_days_spined'))}}
 from
-    {{ ref('int_calls_active_days_spined') }}
+    {{ ref('int_calls_active_days_spined') }} ca
+    where exists (select 1 from {{ ref('dim_server_info') }} si on ca.server_id = si.server_id)
