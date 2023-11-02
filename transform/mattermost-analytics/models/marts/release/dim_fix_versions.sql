d8f25eae6b43660a5d2e934cd57ca75ef358bffa
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-11-02 23:10:04 +0200

    MM-55026: fix ordering logic

--- transform/mattermost-analytics/models/marts/release/dim_fix_versions.sql
+++ transform/mattermost-analytics/models/marts/release/dim_fix_versions.sql
@@ -3,8 +3,8 @@
 with release_versions as (
     select
         short_version,
-        lag(short_version, 1) over(partition by release_number asc) as previous_release_version,
-        lag(short_version, -1) over(partition by release_number asc) as next_release_version
+        lag(short_version, 1) over(order by release_number asc) as previous_release_version,
+        lag(short_version, -1) over(order by release_number asc) as next_release_version
     from
          {{ ref('stg_mattermost__version_release_dates') }}
     where
