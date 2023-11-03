8735461f14dfb435c16ac704588cf6fe90155bd5
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-11-03 18:22:03 +0200

    MM-55026: move next release to release (#1376)
    * Progress commit
    
    * Progress commit
    
    * Fix relation alias in dbt_utils.star
    
    * Fix typo
    
    * Add references to release version timeframe and week after release
    
    * Add references to release version timeframe and week after release
    
    * Add references to release version timeframe and week after release
    
    * Add references to release version timeframe and week after release
    
    * Improve timeframe calculations
    
    * Improve timeframe calculations
    
    * Improve documentation
    
    * Add extra test

--- transform/mattermost-analytics/models/marts/release/fct_issues_daily_snapshot.sql
+++ transform/mattermost-analytics/models/marts/release/fct_issues_daily_snapshot.sql
@@ -1,14 +1,44 @@
+with release_versions as (
+    -- Keep releases
+    select
+        version,
+        short_version,
+        rc1_date,
+        planned_release_date,
+        actual_release_date,
+        -- Calculate 17th of month before the release's month. This is required as release timeframe is 17th to planned
+        -- release date.
+        dateadd(month, -1, planned_release_date) as _month_before_release,
+        dateadd(day, 17 - DAYOFMONTH(_month_before_release), _month_before_release) as release_timeframe_start,
+        dateadd(day, 7, actual_release_date) as week_after_release
+    from
+        {{ ref('stg_mattermost__version_release_dates') }}
+    where
+        rc1_date is not null
+)
 select
-    issue_id,
-    issue_key,
-    project_id,
-    created_at,
+    ji.issue_id,
+    ji.issue_key,
+    ji.project_id,
+    ji.created_at,
     case
-        when status_name in ('Closed', 'Done') then updated_at
+        when ji.status_name in ('Closed', 'Done') then ji.updated_at
     end as closed_at,
-    issue_type_name as issue_type,
-    status_name as status,
-    resolution_name as resolution,
-    {{ datediff("created_at", "closed_at", "day") }} as lead_time_in_days
+    ji.issue_type_name as issue_type,
+    ji.status_name as status,
+    ji.resolution_name as resolution,
+    {{ datediff("ji.created_at", "closed_at", "day") }} as lead_time_in_days,
+    -- Reference to release version if issue created during a release timeframe
+    rt.version as release_timeframe_version,
+    -- Flag whether the issue has been created after RC1 cut date
+    rt.version is not null and ji.created_at >= rt.rc1_date and ji.created_at <= rt.planned_release_date as is_created_after_rc1_cut,
+    -- Reference to release version if issue created during the week after the actual release date
+    ar.version as created_after_release_version
 from
-    {{ ref('stg_mattermost_jira__issues') }}
+    {{ ref('stg_mattermost_jira__issues') }} ji
+    -- Release timeframe
+    left join release_versions rt
+        on ji.created_at > rt.release_timeframe_start and ji.created_at <= rt.planned_release_date
+    -- Week after release
+    left join release_versions ar
+        on  ji.created_at >= ar.actual_release_date and ji.created_at <= ar.week_after_release
