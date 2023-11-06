cc3a26869a91272c2e50e139c7c723141abad2ae
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-06 10:58:36 -0500

    Moved user_telemetry logic to its own CTE

--- transform/mattermost-analytics/models/intermediate/product/servers/int_server_hosting_type.sql
+++ transform/mattermost-analytics/models/intermediate/product/servers/int_server_hosting_type.sql
@@ -9,14 +9,23 @@ with server_telemetry_summary as (
         {{ ref('int_server_active_days_spined') }}
     group by
         server_id
+), user_telemetry_summary as (
+    select
+        server_id
+    from
+        {{ ref('int_user_active_days_spined') }}
+    where
+        is_active_today
+    group by 
+        server_id
 ), server_info as (
    select
        coalesce(st.server_id, ut.server_id) as server_id,
        coalesce(st.count_is_cloud_days, 0) as count_is_cloud_days,
        coalesce(st.count_not_is_cloud_days, 0) as count_not_is_cloud_days
     from
-        server_telemetry_summary st full outer join
-        {{ ref('int_user_active_days_spined') }} ut on st.server_id = ut.server_id
+        server_telemetry_summary st 
+        full outer join user_telemetry_summary ut on st.server_id = ut.server_id
 ), latest_values as (
     -- Get latest values for installation id (if exists) and full version string
     select
