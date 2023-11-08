ef020b41a4f73b36e271c05dadc19f6476a3fbfb
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-08 14:17:57 -0500

    Updated filter for self-hosted servers

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_self_hosted_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_self_hosted_servers.sql
@@ -5,7 +5,7 @@ with mm_telemetry_prod_license as (
         , customer_id as customer_id
         , license_name as license_name
     from {{ ref('stg_mm_telemetry_prod__license')}} 
-    where installation_id is null
+    where license_id is not null
     qualify row_number() over (partition by server_id order by timestamp desc) = 1
 ), mattermost2_license as (
     select timestamp::date as license_date
@@ -14,7 +14,7 @@ with mm_telemetry_prod_license as (
         , customer_id as customer_id
         , license_name as license_name
     from {{ ref('stg_mattermost2__license')}} 
-    where installation_id is null
+    where license_id is not null
     qualify row_number() over (partition by server_id order by timestamp desc) = 1
 ) select * from mm_telemetry_prod_license
     union
