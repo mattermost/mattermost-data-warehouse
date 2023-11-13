95e1289ac8c7dd373975291a102afaaaf08ff3a3
Srikar Gudipudi <96433562+sri-byte@users.noreply.github.com>
2023-11-13 11:55:25 -0500

    Chore: Exclude calls servers not in product telemetry (#1371)
    * Exclude calls servers not in product telemetry
    
    * Updated the logic to use int model
    
    * Documents filter in the description of model yml
    
    * Fixed spacing

--- transform/mattermost-analytics/models/marts/product/calls/fct_calls_activity.sql
+++ transform/mattermost-analytics/models/marts/product/calls/fct_calls_activity.sql
@@ -8,4 +8,5 @@
 select
     {{dbt_utils.star(ref('int_calls_active_days_spined'))}}
 from
-    {{ ref('int_calls_active_days_spined') }}
+    {{ ref('int_calls_active_days_spined') }} ca
+    where exists (select 1 from {{ ref('int_server_hosting_type') }} si where ca.server_id = si.server_id)
