0e03570de0d5169899c24131097b4b1c1c4d7bd7
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-13 13:48:21 -0500

    Minor fixes and updated documentation

--- transform/mattermost-analytics/models/marts/product/nps/fct_nps_feedback.sql
+++ transform/mattermost-analytics/models/marts/product/nps/fct_nps_feedback.sql
@@ -7,7 +7,7 @@ select nf.server_id as server_id
        , nf.user_role as user_role
        , nf.event_date as feedback_date
        , ns.event_date as score_date
-       , nf.timestamp as timestamp
+       , nf.timestamp as feedback_timestamp
     from {{ ref('int_nps_feedback') }} nf 
     left join {{ ref('int_nps_score') }} ns 
         on nf.server_id = ns.server_id and nf.user_id = ns.user_id and nf.event_date = ns.event_date
