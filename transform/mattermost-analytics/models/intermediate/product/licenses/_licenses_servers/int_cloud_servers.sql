91028a289bdd122e3c5fd4845d24678b9eb8a089
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-15 01:14:07 -0500

    Added license_id to servers model

--- transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
+++ transform/mattermost-analytics/models/intermediate/product/licenses/_licenses_servers/int_cloud_servers.sql
@@ -2,6 +2,7 @@ select timestamp::date as installation_date
         , installation_id as installation_id
         , server_id as server_id
         , license_name as license_name
+        , license_id as license_id
     from {{ ref('stg_mm_telemetry_prod__license')}} 
     where installation_id is not null
     qualify row_number() over (partition by server_id order by timestamp desc) = 1
