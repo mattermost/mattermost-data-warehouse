cf184f8f4c2935b331f5ae00c25115db9f48bf81
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-19 21:20:27 -0500

    dropped source column

--- transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
+++ transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
@@ -6,14 +6,13 @@ with self_hosted_servers as (
         , license_name
     from {{ ref('int_self_hosted_servers_spined') }}
     qualify row_number() over (partition by server_id, license_id order by activity_date desc) = 1
-) select a.activity_date as license_date
+) select distinct a.activity_date as license_date
     , a.license_id as license_id
     , a.server_id as server_id
     , a.customer_id as customer_id
     , b.email as customer_email
     , b.company_name as company_name
     , coalesce(b.license_name, a.license_name) as license_name
-    , b.source as source
 from self_hosted_servers a 
 left join {{ ref('int_self_hosted_licenses')}} b 
 on a.license_id = b.license_id
