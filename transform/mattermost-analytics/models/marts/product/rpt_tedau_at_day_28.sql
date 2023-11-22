e6b37911c5ecc01b4e42c35a1a9a1159bb67f648
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-11-22 17:55:28 +0200

    MM-54964: removed unused join

--- transform/mattermost-analytics/models/marts/product/rpt_tedau_at_day_28.sql
+++ transform/mattermost-analytics/models/marts/product/rpt_tedau_at_day_28.sql
@@ -6,7 +6,6 @@
     from
         {{ ref('fct_active_users') }} fct
         join {{ ref('dim_daily_server_info') }} dsi on fct.daily_server_id = dsi.daily_server_id and dsi.age_in_days = 28
-        left join {{ ref('dim_server_info') }} si on fct.server_id = si.server_id
   )
   select
       si.server_id,
