ce201bca23802ab6f61eaf5449610fa0913e884c
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-26 21:58:32 -0500

    Removed license model join as now using spine

--- transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
+++ transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
@@ -1,19 +1,15 @@
-select a.server_id as server_id
-    , a.license_id as license_id
-    , b.customer_id as customer_id
-    , b.email as customer_email
-    , b.company_name as company_name
-    , coalesce(b.license_name, a.license_name) as license_name
-    , b.source as source
-    , max(a.activity_date) as max_activity_date
-from {{ ref('int_self_hosted_servers_spined')}} a 
-left join {{ ref('int_self_hosted_licenses')}} b 
-on a.license_id = b.license_id and a.customer_id = b.customer_id
+select server_id as server_id
+    , license_id as license_id
+    , customer_id as customer_id
+    , email as customer_email
+    , company_name as company_name
+    , license_name as license_name
+    , max(activity_date) as max_activity_date
+from {{ ref('int_self_hosted_servers_spined')}} 
 where company_name is not null or email is not null
-group by a.server_id 
-, a.license_id 
-, b.customer_id 
-, b.email
-, b.company_name
+group by server_id 
+, license_id 
+, customer_id 
+, email
+, company_name
 , license_name
-, b.source 
