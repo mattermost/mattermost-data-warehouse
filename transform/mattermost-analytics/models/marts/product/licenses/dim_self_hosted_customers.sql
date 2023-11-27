781b463893fe7700c6a741ba3ef764ac8793c14b
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-26 21:48:26 -0500

    Adds max_activity_date to dim model

--- transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
+++ transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
@@ -5,7 +5,15 @@ select a.server_id as server_id
     , b.company_name as company_name
     , coalesce(b.license_name, a.license_name) as license_name
     , b.source as source
-from {{ ref('int_self_hosted_servers')}} a 
+    , max(a.activity_date) as max_activity_date
+from {{ ref('int_self_hosted_servers_spined')}} a 
 left join {{ ref('int_self_hosted_licenses')}} b 
 on a.license_id = b.license_id and a.customer_id = b.customer_id
 where company_name is not null or email is not null
+group by a.server_id 
+, a.license_id 
+, b.customer_id 
+, b.email
+, b.company_name
+, license_name
+, b.source 
