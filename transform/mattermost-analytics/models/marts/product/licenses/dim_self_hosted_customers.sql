04481d641fc721f8d3cdee72be6a876dc7013119
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-11-26 22:48:09 -0500

    Fixed field name

--- transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
+++ transform/mattermost-analytics/models/marts/product/licenses/dim_self_hosted_customers.sql
@@ -1,7 +1,7 @@
 select server_id as server_id
     , license_id as license_id
     , customer_id as customer_id
-    , email as customer_email
+    , customer_email as customer_email
     , company_name as company_name
     , license_name as license_name
     , max(activity_date) as max_activity_date
