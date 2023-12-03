d2500ba43d47122f25556bd87cb4787b2a2ce2d3
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-12-03 18:08:48 +0200

    MM-55243: push proxy metrics

---
+++ transform/mattermost-analytics/models/staging/push_proxy/stg_push_proxy__test_logs.sql
@@ -0,0 +1,38 @@
+with source as (
+
+    select * from {{ source('push_proxy', 'test_logs') }}
+
+),
+
+renamed as (
+
+    select
+        type
+        , requesttime
+        , elb
+        , client_and_port
+        , target_and_port
+        , request_processing_time
+        , target_processing_time
+        , response_processing_time
+        , elb_status_code
+        , target_status_code
+        , received_bytes
+        , sent_bytes
+        , request
+        , user_agent
+        , ssl_cipher
+        , ssl_protocol
+        , target_group_arn
+        , trace_id
+        , domain_name
+        , chosen_cert_arn
+        , matched_rule_priority
+        , request_creation_time
+        , actions_executed
+
+    from source
+
+)
+
+select * from renamed
