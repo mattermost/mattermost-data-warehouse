202967e7603398058b1c49bbb3b89c4ce0e7c922
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-12-04 17:28:47 +0200

    Fix join expression

--- transform/mattermost-analytics/models/intermediate/product/notifications/int_notifications_hourly.sql
+++ transform/mattermost-analytics/models/intermediate/product/notifications/int_notifications_hourly.sql
@@ -10,13 +10,13 @@ config({
 select
     spine.date_hour,
     eu_logs.count_send_push as count_eu_send_push,
-    eu_logs.count_send_ack as count_eu_send_ack,
-    us_logs.count_send_push as count_us_send_push,
-    us_logs.count_send_ack as count_us_send_ack,
-    test_logs.count_send_push as count_test_send_push,
-    test_logs.count_send_ack as count_test__send_ack
+    coalesce(eu_logs.count_send_ack, 0) as count_eu_send_ack,
+    coalesce(us_logs.count_send_push, 0) as count_us_send_push,
+    coalesce(us_logs.count_send_ack, 0) as count_us_send_ack,
+    coalesce(test_logs.count_send_push, 0) as count_test_send_push,
+    coalesce(test_logs.count_send_ack, 0) as count_test__send_ack
 from
     {{ ref('notification_date_hour') }} spine
-    left join {{ ref('int_notifications_eu_hourly') }} eu_logs on spine.date_hour = eu_logs.date_hour
-    left join {{ ref('int_notifications_us_hourly') }} us_logs on spine.date_hour = us_logs.date_hour
-    left join {{ ref('int_notifications_test_hourly') }} test_logs on spine.date_hour = test_logs.date_hour
+    left join {{ ref('int_notifications_eu_hourly') }} eu_logs on spine.date_hour = eu_logs.request_date_hour
+    left join {{ ref('int_notifications_us_hourly') }} us_logs on spine.date_hour = us_logs.request_date_hour
+    left join {{ ref('int_notifications_test_hourly') }} test_logs on spine.date_hour = test_logs.request_date_hour
