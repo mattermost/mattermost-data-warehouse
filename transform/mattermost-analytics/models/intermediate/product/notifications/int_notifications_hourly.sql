fd5256792e77d23b0f980fc1b58ff2ac2e25c4fd
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-12-04 17:19:42 +0200

    Use incremental logic to improve performance

--- transform/mattermost-analytics/models/intermediate/product/notifications/int_notifications_hourly.sql
+++ transform/mattermost-analytics/models/intermediate/product/notifications/int_notifications_hourly.sql
@@ -6,39 +6,17 @@ config({
   })
 }}
 
-{% set log_tables = {"stg_push_proxy__de_logs": "Europe", "stg_push_proxy__test_logs": "Test", "stg_push_proxy__us_logs": "US"} %}
 
-
-with
-{% for table in log_tables.keys() %}
--- Aggregate each server's logs hourly, counting the number of push notification and acknowledgement requests
-{{table}}_hourly as (
-    select
-        date_trunc(hour, request_at) as request_date_hour,
-        count_if(url like '%/api/v1/send_push') as send_push,
-        count_if(url like '%/api/v1/ack') as send_ack
-    from
-        {{ ref(table) }}
-    where
-        http_method = 'POST'
-        and url like any ('%/api/v1/send_push', '%/api/v1/ack')
-        -- Need only successful requests
-        and elb_status_code = 200
-        -- Only last two years worth of data are needed
-        and request_at > '{{ var('notification_start_date') }}'
-    group by 1
-){% if not loop.last %},{% endif %}
-{% endfor %}
-
-{% for table, server in log_tables.items() %}
 select
-    request_date_hour,
-    send_push,
-    send_ack,
-    '{{server}}' as server
+    spine.date_hour,
+    eu_logs.count_send_push as count_eu_send_push,
+    eu_logs.count_send_ack as count_eu_send_ack,
+    us_logs.count_send_push as count_us_send_push,
+    us_logs.count_send_ack as count_us_send_ack,
+    test_logs.count_send_push as count_test_send_push,
+    test_logs.count_send_ack as count_test__send_ack
 from
-    {{table}}_hourly
-
-{% if not loop.last %}union all{% endif %}
-
-{% endfor %}
+    {{ ref('notification_date_hour') }} spine
+    left join {{ ref('int_notifications_eu_hourly') }} eu_logs on spine.date_hour = eu_logs.date_hour
+    left join {{ ref('int_notifications_us_hourly') }} us_logs on spine.date_hour = us_logs.date_hour
+    left join {{ ref('int_notifications_test_hourly') }} test_logs on spine.date_hour = test_logs.date_hour
