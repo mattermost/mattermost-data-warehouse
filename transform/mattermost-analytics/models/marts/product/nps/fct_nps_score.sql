412a42e950f5eeddc2bc067242433d701d114ce5
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-12-04 22:26:09 -0500

    fixed model reference and removed field not required

--- transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
+++ transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
@@ -4,14 +4,13 @@ with nps_server_daily_score as (
       , server_id as server_id
       , server_version as server_version
       , user_role as user_role
-      , min(server_install_date) as server_install_date
       , count(distinct case when score > 8 then user_id else null end) as promoters
       , count(distinct case when score <= 6 then user_id else null end) as detractors
       , count(distinct case when score > 6 and score <= 8 then user_id else null end) as passives
       , count(distinct user_id) as nps_users
       , avg(score) as avg_score
       , 100.0 * ((promoters / nps_users) - (detractors / nps_users)) as nps_score
-    from {{ ref('int_nps_feedback') }}
+    from {{ ref('int_nps_score') }}
     group by event_date
       , server_id 
       , server_version 
