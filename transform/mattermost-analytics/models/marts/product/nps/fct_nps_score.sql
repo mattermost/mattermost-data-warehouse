b7fcb5664bc9d328eae6794fa64ce82c771ddb82
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-12-08 12:11:10 -0500

    Cleaned up marts model logic and removed avg_score

--- transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
+++ transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
@@ -5,12 +5,12 @@ with nps_server_daily_score as (
       , server_version as server_version
       , user_role as user_role
       , count(distinct case when score > 8 then user_id else null end) as promoters
-      , count(distinct case when score <= 6 then user_id else null end) as detractors
-      , count(distinct case when score > 6 and score <= 8 then user_id else null end) as passives
+      , count(distinct case when score < 7 then user_id else null end) as detractors
+      , count(distinct case when score > 6 and score < 9 then user_id else null end) as passives
       , count(distinct user_id) as nps_users
-      , avg(score) as avg_score
       , 100.0 * ((promoters / nps_users) - (detractors / nps_users)) as nps_score
     from {{ ref('int_nps_score') }}
+    where event_date >= '{{ var('telemetry_start_date')}}'
     group by event_date
       , server_id 
       , server_version 
