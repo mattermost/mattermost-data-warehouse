31f2b444c2d0d5cf22637836371e6c95891b2d27
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-12-11 15:37:04 -0500

    Added remaining dimensions to surrogate key

--- transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
+++ transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
@@ -1,5 +1,5 @@
 with nps_server_daily_score as (
-    select {{ dbt_utils.generate_surrogate_key(['event_date', 'server_id']) }} as score_id
+    select {{ dbt_utils.generate_surrogate_key(['event_date', 'server_id', 'server_version', 'user_role']) }} as score_id
       , event_date as score_date
       , server_id as server_id
       , server_version as server_version
@@ -8,7 +8,7 @@ with nps_server_daily_score as (
       , count(distinct case when score < 7 then user_id else null end) as detractors
       , count(distinct case when score > 6 and score < 9 then user_id else null end) as passives
       , count(distinct user_id) as nps_users
-      , 100.0 * ((promoters / nps_users) - (detractors / nps_users)) as nps_score
+      , round(100.0 * ((promoters / nps_users) - (detractors / nps_users)),2) as nps_score
     from {{ ref('int_nps_score') }}
     where event_date >= '{{ var('telemetry_start_date')}}'
     group by event_date
