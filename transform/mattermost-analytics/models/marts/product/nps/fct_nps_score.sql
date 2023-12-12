8853c9508128387f367711cf43b3b0ed8596ca3a
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2023-12-12 11:40:20 -0500

    Drop NPS score metric

--- transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
+++ transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
@@ -8,7 +8,6 @@ with nps_server_daily_score as (
       , count(distinct case when score < 7 then user_id else null end) as detractors
       , count(distinct case when score > 6 and score < 9 then user_id else null end) as passives
       , count(distinct user_id) as nps_users
-      , round(100.0 * ((promoters / nps_users) - (detractors / nps_users)),2) as nps_score
     from {{ ref('int_nps_score') }}
     where event_date >= '{{ var('telemetry_start_date')}}'
     group by event_date
