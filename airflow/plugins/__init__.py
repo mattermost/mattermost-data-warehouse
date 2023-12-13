f0c5a96496c7d3e78ba6b01ed03f9d0c8881b9c1
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2023-12-13 14:40:29 +0200

    Test different import logic for plugin

--- airflow/plugins/__init__.py
+++
@@ -1,8 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-from airflow.plugins.mattermost.hooks import MattermostWebhookHook
-from airflow.plugins.mattermost.operators import MattermostOperator
-
-class MattermostPlugin(AirflowPlugin):
-    name = "mattermost"
-    hooks = [MattermostWebhookHook]
-    operators = [MattermostOperator]
