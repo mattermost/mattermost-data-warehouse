0feb1bf7001666d8465beaccadef42832477b412
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-01-23 21:04:41 -0500

    Updated logic for int model

--- transform/mattermost-analytics/models/intermediate/product/nps/int_daily_server_nps_score.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_daily_server_nps_score.sql
@@ -1,31 +1,35 @@
+{{
+    config({
+        "materialized": "table",
+    })
+}}
+
+
 with server_min_score_date as (
     select server_id as server_id
-        , server_version as server_version
-        , user_role as user_role
+        , user_id as user_id
         , min(event_date) as min_score_date
     from {{ ref('int_nps_score') }}
     where event_date >= '{{ var('telemetry_start_date')}}'
-    group by server_id 
-        , server_version 
-        , user_role
+    group by server_id
+        , user_id
 ), 
 spined as (
         select all_days.date_day
             , nps.server_id as server_id
-            , nps.server_version as server_version
-            , nps.user_role as user_role
+            , nps.user_id as user_id
         from server_min_score_date nps 
         left join {{ ref('telemetry_days') }} all_days on all_days.date_day >= nps.min_score_date
 ), server_daily_nps_score as (
     select
-        spined.date_day as activity_date
+        cast(spined.date_day as date) as activity_date
         , spined.server_id
-        , spined.server_version
-        , spined.user_role
-        , count(distinct case when nps_score.score > 8 then nps_score.user_id else null end) as promoters
-        , count(distinct case when nps_score.score < 7 then nps_score.user_id else null end) as detractors
-        , count(distinct case when nps_score.score > 6 and nps_score.score < 9 then nps_score.user_id else null end) as passives
-        , count(distinct nps_score.user_id) as nps_users
+        , nps_score.server_version
+        , nps_score.user_role
+        , count(distinct case when nps_score.score > 8 then spined.user_id else null end) as promoters
+        , count(distinct case when nps_score.score < 7 then spined.user_id else null end) as detractors
+        , count(distinct case when nps_score.score > 6 and nps_score.score < 9 then spined.user_id else null end) as passives
+        , count(distinct spined.user_id) as nps_users
     from
         spined
         left join {{ ref('int_nps_score') }} nps_score
