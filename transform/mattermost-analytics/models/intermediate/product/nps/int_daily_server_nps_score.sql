2a2e2c2b5767a2ca8e7bb57f3bbf459bffc63471
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-01-23 21:18:48 -0500

    Added dimensions to group by

--- transform/mattermost-analytics/models/intermediate/product/nps/int_daily_server_nps_score.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_daily_server_nps_score.sql
@@ -8,6 +8,8 @@
 with server_min_score_date as (
     select server_id as server_id
         , user_id as user_id
+        , server_version
+        , user_role
         , min(event_date) as min_score_date
     from {{ ref('int_nps_score') }}
     where event_date >= '{{ var('telemetry_start_date')}}'
@@ -24,8 +26,8 @@ spined as (
     select
         cast(spined.date_day as date) as activity_date
         , spined.server_id
-        , nps_score.server_version
-        , nps_score.user_role
+        , spined.server_version
+        , spined.user_role
         , count(distinct case when nps_score.score > 8 then spined.user_id else null end) as promoters
         , count(distinct case when nps_score.score < 7 then spined.user_id else null end) as detractors
         , count(distinct case when nps_score.score > 6 and nps_score.score < 9 then spined.user_id else null end) as passives
@@ -35,9 +37,11 @@ spined as (
         left join {{ ref('int_nps_score') }} nps_score
             on spined.date_day = nps_score.event_date
                 and spined.server_id = nps_score.server_id
+                and spined.server_version = nps_score.server_version
+                and spined.user_role = nps_score.user_role
         group by spined.date_day
         , spined.server_id
-        , nps_score.server_version
-        , nps_score.user_role
+        , spined.server_version
+        , spined.user_role
 ) select * 
         from server_daily_nps_score
