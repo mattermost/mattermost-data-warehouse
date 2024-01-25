f959c9f7a535ee5b5a3bef1f9258da4aaf2b2cca
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-01-25 14:33:40 +0200

    Chore: add calls events to event registry (#1436)

--- transform/mattermost-analytics/models/intermediate/data_eng/event_registry/int_events_aggregated_to_date.sql
+++ transform/mattermost-analytics/models/intermediate/data_eng/event_registry/int_events_aggregated_to_date.sql
@@ -11,6 +11,7 @@
             ref('int_mm_telemetry_rc_aggregated_to_date'),
             ref('int_mattermost2_aggregated_to_date'),
             ref('int_portal_prod_aggregated_to_date'),
+            ref('int_mm_calls_test_go_aggregated_to_date')
         ],
         source_column_name=None
     )
