c21ad28a93a2f7de5f91a216e8fb941350ed7823
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-02-01 08:53:02 -0500

    Updated code

--- transform/mattermost-analytics/models/intermediate/product/nps/int_daily_server_nps_score.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_daily_server_nps_score.sql
@@ -1,38 +1,62 @@
-with base_cte as (
-     select event_date
-          , server_id
-          , server_version
-          , user_role
-          , count(distinct case when nps_score.score > 8 then nps_score.user_id else null end) as promoters
-          , count(distinct case when nps_score.score < 7 then nps_score.user_id else null end) as detractors
-          , count(distinct case when nps_score.score > 6 and nps_score.score < 9 then nps_score.user_id else null end) as passives
-          , count(distinct user_id) as nps_users 
-     from {{ ref('int_nps_score') }}  nps_score
-     group by event_date
-          , server_id
-          , server_version
-          , user_role
-), spined as (
-     select td.date_day::date activity_date
-          , server_id
-          , user_role
-          , case when td.date_day::date = event_date then promoters else 0 end promoters
-          , case when td.date_day::date = event_date then detractors else 0 end detractors
-          , case when td.date_day::date = event_date then passives else 0 end passives
-          , case when td.date_day::date = event_date then nps_users else 0 end nps_users
-          , max(server_version) server_version
-     from {{ ref('telemetry_days') }} td 
-     left join base_cte b on td.date_day::date >= event_date 
-     group by td.date_day::date 
-          , server_id
-          , user_role
-          , case when td.date_day::date = event_date then promoters else 0 end 
-          , case when td.date_day::date = event_date then detractors else 0 end 
-          , case when td.date_day::date = event_date then passives else 0 end 
-          , case when td.date_day::date = event_date then nps_users else 0 end 
-     having td.date_day::date >= min(event_date)
-) select *
-     from spined
-
-
-
+WITH base_cte AS (
+    SELECT
+        event_date,
+        server_id,
+        server_version,
+        user_role,
+        COUNT(DISTINCT CASE WHEN nps_score.score > 8 THEN nps_score.user_id ELSE NULL END) AS promoters,
+        COUNT(DISTINCT CASE WHEN nps_score.score < 7 THEN nps_score.user_id ELSE NULL END) AS detractors,
+        COUNT(DISTINCT CASE WHEN nps_score.score > 6 AND nps_score.score < 9 THEN nps_score.user_id ELSE NULL END) AS passives,
+        COUNT(DISTINCT user_id) AS nps_users
+    FROM
+          {{ ref('int_nps_score') }} nps_score
+    GROUP BY
+        event_date,
+        server_id,
+        server_version,
+        user_role
+),
+tmp AS (
+    SELECT
+        td.date_day::DATE AS activity_date,
+        server_id,
+        user_role,
+        CASE WHEN td.date_day::DATE = event_date THEN promoters ELSE 0 END AS promoters,
+        CASE WHEN td.date_day::DATE = event_date THEN detractors ELSE 0 END AS detractors,
+        CASE WHEN td.date_day::DATE = event_date THEN passives ELSE 0 END AS passives,
+        CASE WHEN td.date_day::DATE = event_date THEN nps_users ELSE 0 END AS nps_users,
+        MAX(server_version) AS server_version
+    FROM
+        {{ ref('telemetry_days') }} td
+    LEFT JOIN
+        base_cte b ON td.date_day::DATE >= event_date
+    GROUP BY
+        td.date_day::DATE,
+        server_id,
+        user_role,
+        CASE WHEN td.date_day::DATE = event_date THEN promoters ELSE 0 END,
+        CASE WHEN td.date_day::DATE = event_date THEN detractors ELSE 0 END,
+        CASE WHEN td.date_day::DATE = event_date THEN passives ELSE 0 END,
+        CASE WHEN td.date_day::DATE = event_date THEN nps_users ELSE 0 END
+    HAVING
+        td.date_day::DATE >= MIN(event_date)
+)
+SELECT
+    *,
+    SUM(promoters) OVER (PARTITION BY server_id, user_role, server_version ORDER BY activity_date DESC ROWS BETWEEN CURRENT ROW AND 89 FOLLOWING) AS quarterly_promoters,
+    SUM(detractors) OVER (PARTITION BY server_id, user_role, server_version ORDER BY activity_date DESC ROWS BETWEEN CURRENT ROW AND 89 FOLLOWING) AS quarterly_detractors,
+    SUM(passives) OVER (PARTITION BY server_id, user_role, server_version ORDER BY activity_date DESC ROWS BETWEEN CURRENT ROW AND 89 FOLLOWING) AS quarterly_passives,
+    SUM(nps_users) OVER (PARTITION BY server_id, user_role, server_version ORDER BY activity_date DESC ROWS BETWEEN CURRENT ROW AND 89 FOLLOWING) AS quarterly_nps_users
+FROM
+    tmp
+GROUP BY
+    activity_date,
+    server_id,
+    user_role,
+    promoters,
+    detractors,
+    passives,
+    nps_users,
+    server_version
+ORDER BY
+    activity_date DESC
