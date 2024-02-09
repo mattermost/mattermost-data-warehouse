16865930ed71dd4ed5c7ea6606ced878bb15a8ba
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-02-09 00:02:16 -0500

    Added user_role

--- transform/mattermost-analytics/models/intermediate/product/nps/int_user_nps_score_latest.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_user_nps_score_latest.sql
@@ -3,7 +3,8 @@
         server_id as server_id,
         user_id as user_id,
         server_version as server_version,
-        score as score
+        score as score,
+        user_role as user_role
     FROM
         {{ ref('int_nps_score') }} nps_score
     qualify row_number() over (partition by server_id, user_id, server_version order by event_date desc) = 1
