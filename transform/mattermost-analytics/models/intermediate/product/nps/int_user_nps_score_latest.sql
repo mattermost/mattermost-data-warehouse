5b0cab74f9f83d4caa9f7b435414762e2694ff78
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-02-08 23:57:57 -0500

    Fixed column reference

--- transform/mattermost-analytics/models/intermediate/product/nps/int_user_nps_score_latest.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_user_nps_score_latest.sql
@@ -3,7 +3,7 @@
         server_id as server_id,
         user_id as user_id,
         server_version as server_version,
-        nps_score as score
+        score as score
     FROM
         {{ ref('int_nps_score') }} nps_score
     qualify row_number() over (partition by server_id, user_id, server_version order by event_date desc) = 1
