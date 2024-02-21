933175f70b50857c42ac3e80d4f001ab10d2c2f6
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-02-20 22:30:19 -0500

    Fixed server_version spine model

--- transform/mattermost-analytics/models/intermediate/product/nps/int_nps_server_version_spined.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_nps_server_version_spined.sql
@@ -1,26 +1,34 @@
-WITH first_server_version AS 
-(
+WITH first_server_version AS (
     SELECT 
-        server_id
-        , server_version
-        , min(event_date) min_event_date
+        server_id,
+        MIN(event_date) AS min_event_date
     FROM {{ ref('int_nps_score') }}
-    WHERE event_date >= '{{ var('telemetry_start_date')}}'
-        GROUP BY all
-)
-, spined AS (
+    GROUP BY all
+),
+spined AS (
     SELECT 
-        date_day::DATE AS activity_date
-        , server_id AS server_id
-        , FIRST_VALUE(server_version IGNORE NULLS) OVER (
-            PARTITION BY server_id 
-            ORDER BY activity_date DESC
-            ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING)
-        AS server_version
+        date_day::date AS activity_date,
+        server_id AS server_id
     FROM first_server_version fsd
     LEFT JOIN {{ ref('telemetry_days') }} td 
-        ON date_day::DATE >= min_event_date
-) SELECT * FROM spined 
-    ORDER BY activity_date DESC
-
-
+        ON date_day::date >= min_event_date
+),
+server_version_cte AS (
+    SELECT 
+        sp.activity_date,
+        sp.server_id,
+        MAX(nps_score.server_version) AS server_version
+    FROM spined sp 
+    LEFT JOIN {{ ref('int_nps_score') }} nps_score 
+        ON sp.server_id = nps_score.server_id AND sp.activity_date = nps_score.event_date
+    GROUP BY all
+)
+SELECT 
+    activity_date,
+    server_id,
+    FIRST_VALUE(nps_score.server_version IGNORE NULLS) OVER (
+        PARTITION BY sp.server_id 
+        ORDER BY activity_date DESC
+        ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
+    ) AS server_version
+FROM server_version_cte
