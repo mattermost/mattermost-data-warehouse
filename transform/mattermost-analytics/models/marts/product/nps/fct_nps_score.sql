4723bbf6ce8d2f2551128d4adb7f5c367aa1d311
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-02-23 09:18:50 -0500

    Updated column references after name changes

--- transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
+++ transform/mattermost-analytics/models/marts/product/nps/fct_nps_score.sql
@@ -81,14 +81,14 @@ with user_metrics as (
 SELECT a.*,
     {{ dbt_utils.generate_surrogate_key(['activity_date', 'server_id']) }} AS daily_server_id
     b.server_version AS server_version,
-    a.user_promoters + a.team_admin_promoters + a.system_admin_promoters AS count_promoters_daily,
-    a.user_detractors + a.team_admin_detractors + a.system_admin_detractors AS count_detractors_daily,
-    a.user_passives + a.team_admin_passives + a.system_admin_passives AS count_passives_daily,
-    a.user_nps_users + a.team_admin_nps_users + a.system_admin_nps_users AS count_nps_users_daily,
-    a.user_quarterly_promoters + a.team_admin_quarterly_promoters + a.system_admin_quarterly_promoters AS count_promoters_last90d,
-    a.user_quarterly_detractors + a.team_admin_quarterly_detractors + a.system_admin_quarterly_detractors AS count_detractors_last90d,
-    a.user_quarterly_passives + a.team_admin_quarterly_passives + a.system_admin_quarterly_passives AS count_passives_last90d,
-    a.user_quarterly_nps_users + a.team_admin_quarterly_nps_users + a.system_admin_quarterly_nps_users AS count_nps_users_last90d
+    a.count_user_promoters_daily + a.count_team_admin_promoters_daily + a.count_system_admin_promoters_daily AS count_promoters_daily,
+    a.count_user_detractors_daily + a.count_team_admin_detractors_daily + a.count_system_admin_detractors_daily AS count_detractors_daily,
+    a.count_user_passives_daily + a.count_team_admin_passives_daily + a.count_system_admin_passives_daily AS count_passives_daily,
+    a.count_user_nps_users_daily + a.count_team_admin_nps_users_daily + a.count_system_admin_nps_users_daily AS count_nps_users_daily,
+    a.count_user_promoters_last90d + a.count_team_admin_promoters_last90d + a.count_system_admin_promoters_last90d AS count_promoters_last90d,
+    a.count_user_detractors_last90d + a.count_team_admin_detractors_last90d + a.count_system_admin_detractors_last90d AS count_detractors_last90d,
+    a.count_user_passives_last90d + a.count_team_admin_passives_last90d + a.count_system_admin_passives_last90d AS count_passives_last90d,
+    a.count_user_nps_users_last90d + a.count_team_admin_nps_users_last90d + a.count_system_admin_nps_users_last90d AS count_nps_users_last90d
     from user_metrics a join
     {{ ref('int_nps_server_version_spined') }} b 
     on a.server_id = b.server_id and a.activity_date = b.activity_date
