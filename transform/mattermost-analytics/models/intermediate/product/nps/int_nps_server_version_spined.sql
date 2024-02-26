36e940353b9013332729c11d792635721b9ac448
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-02-26 09:28:56 -0500

    Reverted server_version spine logic

--- transform/mattermost-analytics/models/intermediate/product/nps/int_nps_server_version_spined.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_nps_server_version_spined.sql
@@ -1,21 +1,28 @@
 WITH first_server_version AS (
     SELECT 
         server_id,
-        server_version,
         MIN(event_date) AS min_event_date
     FROM {{ ref('int_nps_score') }}
     WHERE event_date >= '{{ var('nps_start_date')}}'
-    GROUP BY all
-),
-spined AS (
+    group by all
+), spined AS (
     SELECT 
         date_day::date AS activity_date,
-        server_id AS server_id,
-        case when date_day::date = min_event_date then server_version end as server_version
+        server_id AS server_id
     FROM first_server_version fsd
     LEFT JOIN {{ ref('nps_days') }} td 
         ON date_day::date >= min_event_date
-) SELECT 
+),
+server_version_cte AS (
+    SELECT 
+        sp.activity_date,
+        sp.server_id,
+        nps_score.server_version AS server_version
+    FROM spined sp 
+    LEFT JOIN {{ ref('int_nps_score') }} nps_score 
+        ON sp.server_id = nps_score.server_id AND sp.activity_date = nps_score.event_date
+)
+SELECT 
     activity_date,
     server_id,
     FIRST_VALUE(server_version IGNORE NULLS) OVER (
@@ -23,5 +30,6 @@ spined AS (
         ORDER BY activity_date DESC
         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
     ) AS server_version
-FROM spined
-QUALIFY ROW_NUMBER() OVER (PARTITION BY activity_date, server_id ORDER BY activity_date DESC) = 1
+FROM server_version_cte
+QUALIFY ROW_NUMBER() OVER (PARTITION BY activity_date, server_id ORDER BY activity_date DESC, server_version DESC) = 1
+order by 1 desc
