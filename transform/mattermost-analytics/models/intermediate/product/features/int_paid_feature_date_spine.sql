bddbcbe0609866eb522d2d96132c5ca0fb655e12
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-03-01 17:15:41 +0200

    Temporarily materialize table

--- transform/mattermost-analytics/models/intermediate/product/features/int_paid_feature_date_spine.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_paid_feature_date_spine.sql
@@ -13,4 +13,7 @@ with paid_feature_daily_usage as (
         {{ ref('int_feature_daily_usage_per_user') }} u
         join {{ ref('paid_features') }} f on u.event_name = f.event_name and u.category = f.category and f.event_type = u.event_type
 )
-select * from paid_feature_daily_usage
+select
+    *
+from
+    paid_feature_daily_usage
