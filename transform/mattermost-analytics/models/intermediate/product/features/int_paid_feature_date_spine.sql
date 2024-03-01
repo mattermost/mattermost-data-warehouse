0b6588eb82bcc0af07cad3d51c06b317d4e83292
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-03-01 17:40:27 +0200

    Pivot paid features

--- transform/mattermost-analytics/models/intermediate/product/features/int_paid_feature_date_spine.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_paid_feature_date_spine.sql
@@ -5,15 +5,37 @@
 }}
 
 
-with paid_feature_daily_usage as (
+with feature_aliases as (
     select
-    {{ dbt_utils.star(from=ref('int_feature_daily_usage_per_user'), relation_alias='u') }},
-    f.feature_name
+        f.event_name
+        , f.category
+        , f.event_type
+        , a.alias as feature_name
+    from
+        {{ ref('paid_features') }} f
+        join {{ ref('paid_feature_aliases') }} a on f.feature_name = a.feature_name
+
+), paid_feature_daily_usage as (
+    select
+        u.activity_date
+        , u.server_id
+        , u.user_id
+        , f.feature_name,
+        , u.event_count
     from
         {{ ref('int_feature_daily_usage_per_user') }} u
-        join {{ ref('paid_features') }} f on u.event_name = f.event_name and u.category = f.category and f.event_type = u.event_type
+        join feature_aliases f on u.event_name = f.event_name and u.category = f.category and f.event_type = u.event_type
 )
 select
-    *
+    activity_date
+    , server_id
+    , user_id
+    , {{
+        dbt_utils.pivot(
+            'feature_name',
+            dbt_utils.get_column_values(ref('paid_feature_aliases'), 'alias'),
+            prefix='feature_'
+        )
+    }}
 from
     paid_feature_daily_usage
