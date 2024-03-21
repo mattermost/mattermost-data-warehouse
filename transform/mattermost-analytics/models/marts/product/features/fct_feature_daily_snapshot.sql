0d2a0abf74ea714b15fb4aec41a590be26c380b2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-03-21 18:37:11 +0200

    Restructure merging

--- transform/mattermost-analytics/models/marts/product/features/fct_feature_daily_snapshot.sql
+++ transform/mattermost-analytics/models/marts/product/features/fct_feature_daily_snapshot.sql
@@ -20,28 +20,34 @@ with server_date_range as (
         server_date_range sd
         left join {{ ref('telemetry_days') }} all_days
             on all_days.date_day >= sd.first_active_day and all_days.date_day <= sd.last_active_day
+), metrics_per_server_day as (
+    -- Collapse metrics from date/server/user to date/server
+    select
+        server_spine.daily_server_id
+        , server_spine.server_id
+        , server_spine.activity_date
+        -- Feature activity
+    {% for metric_column in metric_cols -%}
+        , coalesce(sum({{metric_column}}), 0) as {{metric_column}}
+    {%- endfor %}
+    from
+        server_spine
+        left join {{ ref('int_feature_daily_spine') }} feature_spine
+            on server_spine.server_id = feature_spine.server_id and server_spine.activity_date = feature_spine.activity_date
+    group by
+        server_spine.daily_server_id
+        , server_spine.server_id
+        , server_spine.activity_date
 )
+-- Merge with other sources
 select
-    server_spine.daily_server_id
-    , server_spine.server_id
-    , server_spine.activity_date
-    -- Feature activity
-{% for metric_column in metric_cols -%}
-    , coalesce(sum({{metric_column}}), 0) as {{metric_column}}
-{%- endfor %}
+    m.*,
     -- Server-reported activity
     , coalesce(sas.daily_active_users, 0) as server_daily_active_users
     , coalesce(sas.monthly_active_users, 0) as server_monthly_active_users
     , coalesce(sas.count_registered_users, 0) as count_registered_users
     , coalesce(sas.count_registered_deactivated_users, 0) as count_registered_deactivated_users
-
 from
-    server_spine
-    left join {{ ref('int_feature_daily_spine') }} feature_spine
-        on server_spine.server_id = feature_spine.server_id and server_spine.activity_date = feature_spine.activity_date
+    metrics_per_server_day m
     left join {{ ref('int_server_active_days_spined')}} sas
-        on sas.server_id = server_active_days.server_id and sas.activity_date = user_active_days.activity_date
-group by
-    server_spine.daily_server_id
-    , server_spine.server_id
-    , server_spine.activity_date
+            on m.server_id = sas.server_id and m.activity_date = sas.activity_date
