149e1d0643374bed324d76b198ac6f04923f08d6
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-03-25 12:34:51 +0200

    Add unknown features and is_active flag

--- transform/mattermost-analytics/models/intermediate/product/features/int_feature_daily_spine.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_feature_daily_spine.sql
@@ -6,7 +6,7 @@
 }}
 
 {# List of known features #}
-{%- set features = dbt_utils.get_column_values(ref('feature_aliases'), 'alias') -%}
+{%- set features = dbt_utils.get_column_values(ref('feature_aliases'), 'alias') + ['unknown'] -%}
 
 with server_feature_date_range as (
     select
@@ -54,6 +54,7 @@ select
         ) > 0, 1, 0
     ) as {{ dbt_utils.slugify('count_' ~ feature_column ~ '_users_monthly') }}
 {% endfor %}
+    , iff(features.server_id is not null, 1, 0) as is_active
 from
     spine
     left join {{ ref('int_feature_daily_usage_pivoted') }} features
