f18c74f7c95ff6d003819fa319e0d19f8045a5c0
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-04-01 05:48:01 -0400

    Fixed typo

--- transform/mattermost-analytics/models/marts/product/notifications/fct_notification_stats.sql
+++ transform/mattermost-analytics/models/marts/product/notifications/fct_notification_stats.sql
@@ -1,11 +1,11 @@
 select
     spine.date_hour,
-    {{ dbt_utils.safe_add('eu_logs.count_send_push', 'logs_eu.count_send_push') }} as count_eu_send_push,
-    {{ dbt_utils.safe_add('eu_logs.count_send_ack', 'logs_eu.count_send_ack') }} as count_eu_send_ack,
-    {{ dbt_utils.safe_add('us_logs.count_send_push', 'logs_us.count_send_push') }} as count_us_send_push,
-    {{ dbt_utils.safe_add('us_logs.count_send_ack', 'logs_us.count_send_ack') }} as count_us_send_ack,
-    {{ dbt_utils.safe_add('test_logs.count_send_push', 'logs_test.count_send_push') }} as count_test_send_push,
-    {{ dbt_utils.safe_add('test_logs.count_send_ack', 'logs_test.count_send_ack') }} as count_test_send_ack
+    {{ dbt_utils.safe_add(['eu_logs.count_send_push', 'logs_eu.count_send_push']) }} as count_eu_send_push,
+    {{ dbt_utils.safe_add(['eu_logs.count_send_ack', 'logs_eu.count_send_ack']) }} as count_eu_send_ack,
+    {{ dbt_utils.safe_add(['us_logs.count_send_push', 'logs_us.count_send_push']) }} as count_us_send_push,
+    {{ dbt_utils.safe_add(['us_logs.count_send_ack', 'logs_us.count_send_ack']) }} as count_us_send_ack,
+    {{ dbt_utils.safe_add(['test_logs.count_send_push', 'logs_test.count_send_push']) }} as count_test_send_push,
+    {{ dbt_utils.safe_add(['test_logs.count_send_ack', 'logs_test.count_send_ack']) }} as count_test_send_ack
 from
     {{ ref('notification_date_hour') }} spine
     left join {{ ref('int_notifications_eu_hourly') }} eu_logs on spine.date_hour = eu_logs.request_date_hour
