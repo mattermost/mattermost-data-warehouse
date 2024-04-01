d587a772be1efcc78151caaa5eb919f571da6f99
Srikar Gudipudi <96433562+sri-byte@users.noreply.github.com>
2024-04-01 07:02:32 -0400

    MM-57112 Add new sources to push proxy server logs (#1470)
    * Adds staging and intermediate models for new push proxy sources
    
    * Fixes typo
    
    * Fixed field reference
    
    * Update yml file
    
    * Add intermediate sources to mart model
    
    * Fix typo
    
    * Handles addition of old & new source fields
    
    * Fixed typo
    
    * Added descriptions to new fields from new sources
    
    * Fixed indent
    
    * Added missing descriptions in staging model.yml file

--- transform/mattermost-analytics/models/marts/product/notifications/fct_notification_stats.sql
+++ transform/mattermost-analytics/models/marts/product/notifications/fct_notification_stats.sql
@@ -1,13 +1,17 @@
 select
     spine.date_hour,
-    coalesce(eu_logs.count_send_push, 0) as count_eu_send_push,
-    coalesce(eu_logs.count_send_ack, 0) as count_eu_send_ack,
-    coalesce(us_logs.count_send_push, 0) as count_us_send_push,
-    coalesce(us_logs.count_send_ack, 0) as count_us_send_ack,
-    coalesce(test_logs.count_send_push, 0) as count_test_send_push,
-    coalesce(test_logs.count_send_ack, 0) as count_test_send_ack
+    {{ dbt_utils.safe_add(['eu_logs.count_send_push', 'logs_eu.count_send_push']) }} as count_eu_send_push,
+    {{ dbt_utils.safe_add(['eu_logs.count_send_ack', 'logs_eu.count_send_ack']) }} as count_eu_send_ack,
+    {{ dbt_utils.safe_add(['us_logs.count_send_push', 'logs_us.count_send_push']) }} as count_us_send_push,
+    {{ dbt_utils.safe_add(['us_logs.count_send_ack', 'logs_us.count_send_ack']) }} as count_us_send_ack,
+    {{ dbt_utils.safe_add(['test_logs.count_send_push', 'logs_test.count_send_push']) }} as count_test_send_push,
+    {{ dbt_utils.safe_add(['test_logs.count_send_ack', 'logs_test.count_send_ack']) }} as count_test_send_ack
 from
     {{ ref('notification_date_hour') }} spine
     left join {{ ref('int_notifications_eu_hourly') }} eu_logs on spine.date_hour = eu_logs.request_date_hour
     left join {{ ref('int_notifications_us_hourly') }} us_logs on spine.date_hour = us_logs.request_date_hour
     left join {{ ref('int_notifications_test_hourly') }} test_logs on spine.date_hour = test_logs.request_date_hour
+    left join {{ ref('int_notifications_logs_eu_hourly') }} logs_eu on spine.date_hour = logs_eu.request_date_hour
+    left join {{ ref('int_notifications_logs_us_hourly') }} logs_us on spine.date_hour = logs_us.request_date_hour
+    left join {{ ref('int_notifications_logs_test_hourly') }} logs_test on spine.date_hour = logs_test.request_date_hour
+
---
+++ transform/mattermost-analytics/models/staging/push_proxy/stg_push_proxy__logs_test_new.sql
@@ -0,0 +1,49 @@
+with source as (
+
+    select * from {{ source('push_proxy', 'logs_test_new') }}
+
+),
+
+renamed as (
+
+    select
+        type
+        , time as request_at
+        , elb
+        , client_and_port
+        , target_and_port
+        , request_processing_time
+        , target_processing_time
+        , response_processing_time
+        , elb_status_code
+        , target_status_code
+        , received_bytes
+        , sent_bytes
+        , split_part(request, ' ', 1) as http_method
+        , split_part(request, ' ', 2) as url
+        , split_part(request, ' ', 3) as http_version
+        , user_agent
+        , ssl_cipher
+        , ssl_protocol
+        , target_group_arn
+        , trace_id
+        , domain_name
+        , chosen_cert_arn
+        , matched_rule_priority
+        , request_creation_time
+        , actions_executed
+        , redirect_url 
+        , error_reason
+        , target_port_list
+        , target_status_code_list
+        , classification
+        , classification_reason
+    from source
+
+    where
+        -- remove invalid data
+        array_size(split(request, ' ')) = 3
+
+)
+
+select * from renamed
