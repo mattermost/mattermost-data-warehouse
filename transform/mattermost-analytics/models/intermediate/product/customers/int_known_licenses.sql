9b50e584d65a0d8748d91d2bcd58b82b2df30bef
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-04-12 13:16:50 +0300

    Fix typo

--- transform/mattermost-analytics/models/intermediate/product/customers/int_known_licenses.sql
+++ transform/mattermost-analytics/models/intermediate/product/customers/int_known_licenses.sql
@@ -2,7 +2,7 @@
 -- Performs deduplication in case a license exists both in CWS and legacy licenses.
 select
     coalesce(cws.license_id, legacy.license_id) as license_id
-    , coalescw(cws.company_name, legacy.company_name) as company_name
+    , coalesce(cws.company_name, legacy.company_name) as company_name
     , coalesce(cws.customer_email, legacy.contact_email) as contact_email
     , coalesce(cws.sku_short_name, 'Unkonown') as sku_short_name
     , coalesce(cws.expire_at, legacy.expire_at) as expire_at
