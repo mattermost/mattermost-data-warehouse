d77933e9bede2227ea89d980e85b1416706dc540
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-04-15 17:54:45 +0300

    Add seed files for playbook events, test event expectations

---
+++ transform/mattermost-analytics/tests/staging/incident_response_prod/test_tracks__all_actions_are_known.sql
@@ -0,0 +1,18 @@
+-- Check if there are actions sent via telemetry that are not in the list of known events/actions.
+with telemetry_actions as (
+    select
+        event_name, event_action, count(*)
+    from
+        {{ ref('stg_incident_response_prod__tracks') }}
+    where
+        user_id is not null
+    group by event_name, event_action
+)
+select
+    t.event_name, t.event_action
+from
+    telemetry_actions t
+    left join {{ ref('playbook_events') }} pe on t.event_name = pe.event_name and t.event_action = pe.event_action
+where
+    pe.event_name is null
+    or pe.event_action is null
---
+++ transform/mattermost-analytics/tests/staging/incident_response_prod/test_tracks__documented_actions_have_telemetry.sql
@@ -0,0 +1,20 @@
+-- Check if there are documented actions that do not have matching events
+-- via telemetry.
+
+with telemetry_actions as (
+    select
+        event_name, event_action, max(timestamp)
+    from
+        {{ ref('stg_incident_response_prod__tracks') }}
+    where
+        user_id is not null
+    group by event_name, event_action
+)
+select
+    pe.event_name, pe.event_action
+from
+    {{ ref('playbook_events') }} pe
+    left join {telemetry_actions t on t.event_name = pe.event_name and t.event_action = pe.event_action
+where
+    t.event_name is null
+    or t.event_action is null
