d4a3f5a9ee4ba19d6e068cdce0edd7724c606765
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-05-01 01:36:07 -0400

    Creates new dimension model for categories

---
+++ transform/mattermost-analytics/models/marts/product/nps/dim_nps_feedback_category.sql
@@ -0,0 +1,21 @@
+{{
+config({
+    "materialized": 'incremental',
+    "incremental_strategy": "append",
+    "unique_key": ['feedback_id'],
+  })
+}}
+
+select nf.user_id
+       , nf.feedback as feedback
+       , nf.event_date as feedback_date
+       , nf.timestamp as feedback_timestamp
+       , md5(nf.feedback) as feedback_id
+       , '' as feedback_category
+       , '' as feedback_subcategory
+    from {{ ref('int_nps_feedback') }} nf 
+    where nf.feedback is not null 
+    {% if is_incremental() %}
+       -- this filter will only be applied on an incremental run
+      and nf.timestamp >= (select max(feedback_timestamp) from {{ this }})
+    {% endif %}
