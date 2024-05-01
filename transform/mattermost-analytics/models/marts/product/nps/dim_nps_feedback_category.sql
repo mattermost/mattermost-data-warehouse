6bcd3242b925986ce991d3e012ce863308cd7298
Srikar Gudipudi <srikar.gudipudi@mattermost.com>
2024-05-01 05:22:10 -0400

    defines the data type for the category columns

--- transform/mattermost-analytics/models/marts/product/nps/dim_nps_feedback_category.sql
+++ transform/mattermost-analytics/models/marts/product/nps/dim_nps_feedback_category.sql
@@ -11,8 +11,8 @@ select nf.user_id
        , nf.event_date as feedback_date
        , nf.timestamp as feedback_timestamp
        , md5(nf.feedback) as feedback_id
-       , '' as feedback_category
-       , '' as feedback_subcategory
+       , cast(null as varchar) as feedback_category
+       , cast(null as varchar) as feedback_subcategory
     from {{ ref('int_nps_feedback') }} nf 
     where nf.feedback is not null 
     {% if is_incremental() %}
