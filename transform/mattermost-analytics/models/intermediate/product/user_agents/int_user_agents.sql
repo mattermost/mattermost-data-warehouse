0903d3ba93ff6956a272a7d53fb4efc986ffaa48
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-05-13 14:07:24 +0300

    Hotfix: fix reference to udf (#1505)

--- transform/mattermost-analytics/models/intermediate/product/user_agents/int_user_agents.sql
+++ transform/mattermost-analytics/models/intermediate/product/user_agents/int_user_agents.sql
@@ -22,7 +22,7 @@ parsed as (
         case 
             when previously_parsed.user_agent_id is not null 
             then previously_parsed.user_agent
-            else coalesce(parse_user_agent(tmp.context_user_agent), null)
+            else coalesce(mattermost_analytics.parse_user_agent(tmp.context_user_agent), null)
         end as user_agent
     from tmp tmp 
         left join {{ this }} previously_parsed on tmp.user_agent_id = previously_parsed.user_agent_id
