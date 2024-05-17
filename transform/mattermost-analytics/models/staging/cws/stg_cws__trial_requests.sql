294a745ddd19d914b518e05f63b3ba49cd7e0a47
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-05-17 15:37:26 +0300

    Hotfix: test pull request - please ignore

--- transform/mattermost-analytics/models/staging/cws/stg_cws__trial_requests.sql
+++ transform/mattermost-analytics/models/staging/cws/stg_cws__trial_requests.sql
@@ -94,7 +94,5 @@ where
         '1drwa3piy3f6xx4b3mtqz4x9ty',
         'fhp9ab3ndif4udr1hxcwyhmfhr',
         'jkat884rkfdydy1eeo8xoonxfh',
-        '8m5ewgixcbdf7xfdc88ptnxhqh',
-        '9ym578hnc7fwbdm66y19d5aa1o',
         'qbi41takhtndpy67y15cigkxge'
     )
