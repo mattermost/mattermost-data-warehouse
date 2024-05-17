2b0db47cd186d3363488c7e7dba92861fa4d0427
Nick Misasi <nick.misasi@mattermost.com>
2024-05-17 13:37:08 -0400

    Add dim for customers, adjust the subscription history fact to have an is_latest column for later usage in Looker

---
+++ transform/mattermost-analytics/models/marts/sales/dim_customers.sql
@@ -0,0 +1,9 @@
+select
+    customer_id
+    , contact_first_name
+    , contact_last_name
+    , name
+    , email
+from 
+    dbt_staging.stg_stripe__customers
+;
--- transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
+++ transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
@@ -1,11 +1,13 @@
-with subscription_history as (
-    select 
-        subscription_history_event_id
-        ,  subscription_id
-        ,  licensed_seats
-        , created_at
-
-    from {{ ref('stg_cws__subscription_history') }}
-)
-
-select * from subscription_history
+select 
+    sh.subscription_history_event_id
+    , sh.subscription_id
+    , s.customer_id
+    , COALESCE(sh.licensed_seats, s.QUANTITY) as licensed_seats
+    , sh.created_at
+    , s.CWS_DNS,
+    , s.CWS_INSTALLATION
+    , row_number() over(partition by sh.subscription_id order by sh.created_at desc) = 1 as is_latest
+from
+    dbt_staging.stg_cws__subscription_history sh
+    left join dbt_staging.stg_stripe__subscriptions s on sh.subscription_id = s.subscription_id
+;
