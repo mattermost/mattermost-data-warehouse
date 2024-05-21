73ba89d2040722b3c46331cfafc97a0223c272d6
Nick Misasi <nick.misasi@mattermost.com>
2024-05-21 14:20:21 -0400

    Updates for PR Review

--- transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
+++ transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
@@ -2,10 +2,10 @@ select
     sh.subscription_history_event_id
     , sh.subscription_id
     , s.customer_id
-    , COALESCE(sh.licensed_seats, s.QUANTITY) as licensed_seats
+    , COALESCE(sh.licensed_seats, s.quantity) as licensed_seats
     , sh.created_at
-    , s.CWS_DNS,
-    , s.CWS_INSTALLATION
+    , s.cws_dns
+    , s.cws_installation
     , row_number() over(partition by sh.subscription_id order by sh.created_at desc) = 1 as is_latest
 from
     dbt_staging.stg_cws__subscription_history sh
