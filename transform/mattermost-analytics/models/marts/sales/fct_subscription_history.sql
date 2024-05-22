933fcf59a10b51d41e77e75ab0c506cbacb980bc
Nick Misasi <nick.misasi@mattermost.com>
2024-05-22 10:20:03 -0400

    Update transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
    Co-authored-by: Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>

--- transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
+++ transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
@@ -8,5 +8,5 @@ select
     , s.cws_installation
     , row_number() over(partition by sh.subscription_id order by sh.created_at desc) = 1 as is_latest
 from
-    dbt_staging.stg_cws__subscription_history sh
-    left join dbt_staging.stg_stripe__subscriptions s on sh.subscription_id = s.subscription_id
+    {{ ref('stg_cws__subscription_history') }} sh
+    left join {{ ref('stg_stripe__subscriptions') }} s on sh.subscription_id = s.subscription_id
