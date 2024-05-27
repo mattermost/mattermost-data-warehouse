ce52e6c6af4699b8efc05c7a9efc0a64d1ff3b90
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-05-27 17:51:28 +0300

    Report for cloud trials

--- transform/mattermost-analytics/models/intermediate/sales/trials/int_trial_request_activity.sql
+++
@@ -1,7 +0,0 @@
--- Temporarily materialize
-{{
-    config({"materialized": 'table'})
-}}
-select * from {{ ref('int_cloud_trial_request_activity') }}
-union
-select * from {{ ref('int_onprem_trial_request_activity') }}
