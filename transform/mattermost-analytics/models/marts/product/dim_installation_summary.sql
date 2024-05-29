066baa02946ee95353a5ea5b62d3c0f79adbf601
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-05-29 19:22:53 +0300

    Add documentation

--- transform/mattermost-analytics/models/marts/product/dim_installation_summary.sql
+++ transform/mattermost-analytics/models/marts/product/dim_installation_summary.sql
@@ -9,7 +9,7 @@ select
     last_value(daily_active_users) over (partition by installation_id order by activity_date asc) as last_daily_active_users,
     last_value(monthly_active_users) over (partition by installation_id order by activity_date asc) as last_monthly_active_users,
     last_value(server_id) over (partition by installation_id order by activity_date asc) as last_server_id,
-    count(distinct server_id) over (partition by installation_id) as count_server_ids,
+    count(distinct server_id) over (partition by installation_id) as count_server_ids
 from
     {{ ref('int_server_active_days_spined') }}
 where
