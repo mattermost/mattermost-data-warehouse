4ee9ab2dd3c2f9dd146d811447203cffd8458dbd
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-05-31 13:14:14 +0300

    Change query result name

--- transform/mattermost-analytics/macros/features/load_feature_mappings.sql
+++ transform/mattermost-analytics/macros/features/load_feature_mappings.sql
@@ -1,21 +1,23 @@
 {% macro load_feature_mappings() %}
 
-    {%- call statement('rules', fetch_result=True) -%}
+    {%- call statement('get_query_results', fetch_result=True) -%}
         select * from {{ ref('tracking_plan') }} order by feature_name
     {%- endcall -%}
 
-    {%- set rules_result = load_result('rules') -%}
-
     {%- set rules = {} -%}
 
-    {%- for key, group in rules_result.table.rows | groupby('FEATURE_NAME') -%}
-        {% do rules.update({key: list(group)}) %}
-    {%- endfor -%}
+    {%- if execute -%}
+        {{ log('Loading feature mappings') }}
 
+        {%- set rules_result = load_result('get_query_results').table.rows -%}
+
+        {%- for key, group in rules_result | groupby('FEATURE_NAME') -%}
+            {% do rules.update({key: list(group)}) %}
+        {%- endfor -%}
+    {%- endif -%}
 
 
     {{ log(rules) }}
-
     {{ return(rules) }}
 
 {% endmacro %}
