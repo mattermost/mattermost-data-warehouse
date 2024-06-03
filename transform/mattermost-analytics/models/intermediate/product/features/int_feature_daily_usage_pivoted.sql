00129f014eee889264cf2ff87aadffe3318df34b
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-03 13:25:06 +0300

    Replace daily usage

--- transform/mattermost-analytics/models/intermediate/product/features/int_feature_daily_usage_pivoted.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_feature_daily_usage_pivoted.sql
@@ -1,65 +1,26 @@
---
--- Creates a table where each row holds which known features were used by each server/user.
---
-with servers_with_known_features as (
-    -- Keep only servers with at least one known feature. This reduces the execution time of the current query.
-    -- This filter can be removed in the future if there's a need to expand the breakdown even for servers without any
-    -- known features.
-    select
-        distinct server_id
-    from
-        {{ ref('int_mattermost_daily_usage_per_user') }}
-    where
-        feature_alias <> 'unknown'
-
-    union
-
-    select
-        distinct server_id
-    from
-        {{ ref('int_playbooks_daily_usage_per_user') }}
-    where
-        feature_alias <> 'unknown'
-), feature_daily_usage as (
-    -- Create a matrix of daily feature usage per user.
-    select
-        u.activity_date
-        , u.server_id
-        , u.user_id
-        , u.feature_alias
-        , u.event_count
-    from
-        {{ ref('int_mattermost_daily_usage_per_user') }} u
-    where
-        server_id in (select server_id from servers_with_known_features)
+{{
+    config({
+        "materialized": "table"
+    })
+}}
 
 
-    union all
+{%
+    set column_names = dbt_utils.get_filtered_columns_in_relation(
+        from=ref('int_feature_daily_usage_per_user'),
+        except=["daily_user_id", "activity_date", "server_id", "user_id", "received_at_date", "count_unknown"]
+    )
+%}
 
-    select
-        u.activity_date
-        , u.server_id
-        , u.user_id
-        , u.feature_alias
-        , u.event_count
-    from
-        {{ ref('int_playbooks_daily_usage_per_user') }} u
-    where
-        server_id in (select server_id from servers_with_known_features)
-)
--- Row per date, server, user. Contains one column per known feature.
+
 select
-    activity_date
-    , server_id
-    , user_id
-    , {{ dbt_utils.pivot(
-            'feature_alias',
-            dbt_utils.get_column_values(ref('feature_aliases'), 'alias') + ['unknown'],
-            agg='sum',
-            then_value='event_count',
-            quote_identifiers=False
-        ) }}
+    {{ dbt_utils.star(from=ref('int_feature_daily_usage_per_user'), except=["received_at_date"]) }}
 from
-    feature_daily_usage
-group by
-    activity_date, server_id, user_id
+    {{ ref('int_feature_daily_usage_per_user') }}
+where
+-- Keep only servers with at least one known value
+    false
+{% for column_name in column_names %}
+    or {{ column_name }} > 0
+{% endfor %}
+
