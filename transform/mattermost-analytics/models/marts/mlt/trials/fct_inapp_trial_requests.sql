d251fb4c41673e9a2bfce52732e8a442ec7b8ec0
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-04 17:17:16 +0300

    Add tests, company type information

--- transform/mattermost-analytics/models/marts/mlt/trials/fct_inapp_trial_requests.sql
+++ transform/mattermost-analytics/models/marts/mlt/trials/fct_inapp_trial_requests.sql
@@ -7,12 +7,16 @@ with deduped_trial_requests as (
     qualify row_number() over (partition by trial_request_id order by start_at asc) = 1
 ), aggregates as (
     select
-        coalesce(contact_email, email) as trial_email
-        , count(distinct trial_request_id) as total_trial_requests
-        , min(start_at) as first_trial_start_at
-        , max(start_at) as last_trial_start_at
+        coalesce(tr.contact_email, tr.email) as trial_email
+        , count(distinct tr.trial_request_id) as total_trial_requests
+        , min(tr.start_at) as first_trial_start_at
+        , max(tr.start_at) as last_trial_start_at
+        , array_unique_agg(lead.company_type__c)[0]::varchar as company_type
     from
         deduped_trial_requests
+        left join dbt_staging.stg_salesforce__lead lead on tr.contact_email = lead.email or tr.email = lead.email
+    where
+        lead.is_deleted = false
     group by all
 )
 select
@@ -41,6 +45,7 @@ select
     , agg.total_trial_requests
     , agg.first_trial_start_at
     , agg.last_trial_start_at
+    , agg.company_type
 from
     deduped_trial_requests tr
     left join aggregates agg on coalesce(tr.contact_email, tr.email) = agg.trial_email
