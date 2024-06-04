7d099a56d03a10d4ce9b5d83cbec765490faf0aa
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-04 17:48:00 +0300

    Remove company type

--- transform/mattermost-analytics/models/marts/mlt/trials/fct_inapp_trial_requests.sql
+++ transform/mattermost-analytics/models/marts/mlt/trials/fct_inapp_trial_requests.sql
@@ -11,11 +11,8 @@ with deduped_trial_requests as (
         , count(distinct tr.trial_request_id) as total_trial_requests
         , min(tr.start_at) as first_trial_start_at
         , max(tr.start_at) as last_trial_start_at
-        , array_unique_agg(lead.company_type__c)[0]::varchar as company_type
-        , count(distinct lead.company_type__c) as number_of_company_types
     from
         deduped_trial_requests tr
-        left join dbt_staging.stg_salesforce__lead lead on tr.contact_email = lead.email or tr.email = lead.email
     where
         lead.is_deleted = false
     group by all
@@ -30,7 +27,10 @@ select
     , {{ validate_email('trial_email') }} as is_valid_trial_email
     , tr._name as name
     , coalesce(company_name, 'Unknown') as company_name
-    , tr.company_size_bucket
+    , case
+        when tr.company_size_bucket = '' then 'Unknown'
+        else tr.company_size_bucket
+    end as company_size_bucket
     , tr.site_name
     , tr.site_url
     , case
@@ -46,8 +46,6 @@ select
     , agg.total_trial_requests
     , agg.first_trial_start_at
     , agg.last_trial_start_at
-    , agg.company_type
-    , agg.number_of_company_types
 from
     deduped_trial_requests tr
     left join aggregates agg on coalesce(tr.contact_email, tr.email) = agg.trial_email
