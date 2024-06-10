0a49c589b5be09534fb862008aa733ef54dfdf9b
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-10 17:18:52 +0300

    Fix upstream reference

--- transform/mattermost-analytics/models/intermediate/product/features/int_feature_daily_spine.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_feature_daily_spine.sql
@@ -9,7 +9,7 @@
 
 {%
     set count_feature_columns = dbt_utils.get_filtered_columns_in_relation(
-        from=ref('int_feature_daily_usage_pivoted'),
+        from=ref('int_feature_daily_usage_per_user'),
         except=[
             "daily_user_id", "activity_date", "server_id", "user_id", "received_at_date",
             "count_known_feature", "count_unknown_feature", "count_total_events"
@@ -20,7 +20,7 @@
 
 {%
     set count_columns = dbt_utils.get_filtered_columns_in_relation(
-        from=ref('int_feature_daily_usage_pivoted'),
+        from=ref('int_feature_daily_usage_per_user'),
         except=[
             "daily_user_id", "activity_date", "server_id", "user_id", "received_at_date"
         ]
