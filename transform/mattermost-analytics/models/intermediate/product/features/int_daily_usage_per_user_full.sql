0173beca15f1fa904ed9c3a880bc22cd2b31beab
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-11 21:42:31 +0300

    Don't coalesce received at date

--- transform/mattermost-analytics/models/intermediate/product/features/int_daily_usage_per_user_full.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_daily_usage_per_user_full.sql
@@ -5,21 +5,22 @@
     })
 }}
 
-{%- set common_columns = ['daily_user_id', 'activity_date', 'server_id', 'user_id'] -%}
+{%- set join_columns = ['daily_user_id', 'activity_date', 'server_id', 'user_id'] -%}
 {%- set agg_columns = ['count_known_feature', 'count_unknown_feature', 'count_total_events'] -%}
+{%- set skip_columns = join_columns + agg_columns + ['received_at_date'] -%}
 
 -- Contains all daily feature usage data per user/server/date and from all sources
 select
-{% for column in common_columns %}
+{% for column in join_columns %}
     {%- if not loop.first %} , {% endif -%} coalesce(m.{{column}}, p.{{column}}) as {{column}}
 {% endfor %}
     -- Mattermost features
-{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_feature_daily_usage_per_user'), except=common_columns + agg_columns) %}
+{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_feature_daily_usage_per_user'), except=skip_columns) %}
 {%- for col in cols %}
     , coalesce(m.{{ adapter.quote(col)|trim }}, 0) as {{ adapter.quote(col)|trim }}
 {%- endfor -%}
     -- Playbook features
-{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_playbooks_daily_usage_per_user'), except=common_columns + agg_columns) %}
+{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_playbooks_daily_usage_per_user'), except=skip_columns) %}
 {%- for col in cols %}
     , coalesce(p.{{ adapter.quote(col)|trim }}, 0) as {{ adapter.quote(col)|trim }}
 {%- endfor -%}
@@ -30,6 +31,6 @@ select
 from
     {{ ref('int_feature_daily_usage_per_user') }} m
     full outer join {{ ref('int_playbooks_daily_usage_per_user') }} p
-        on  {% for column in common_columns %}
+        on  {% for column in join_columns %}
             {%- if not loop.first %} and {% endif -%} m.{{column}} = p.{{column}}
         {% endfor %}
