6d9194f7245118b0b4f0f120b91b1aa9597c7d5f
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-11 21:29:38 +0300

    Remove received at date

--- transform/mattermost-analytics/models/intermediate/product/features/int_daily_usage_per_user_full.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_daily_usage_per_user_full.sql
@@ -5,7 +5,7 @@
     })
 }}
 
-{%- set common_columns = ['daily_user_id', 'activity_date', 'server_id', 'user_id', 'received_at_date'] -%}
+{%- set common_columns = ['daily_user_id', 'activity_date', 'server_id', 'user_id'] -%}
 {%- set agg_columns = ['count_known_feature', 'count_unknown_feature', 'count_total_events'] -%}
 
 -- Contains all daily feature usage data per user/server/date and from all sources
