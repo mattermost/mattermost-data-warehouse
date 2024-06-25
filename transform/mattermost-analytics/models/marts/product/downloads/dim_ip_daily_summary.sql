a6f667f22fb2dc95480f3c8252e3417106668cc2
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-06-25 21:47:04 +0300

    Convert dimension to incremental model

--- transform/mattermost-analytics/models/marts/product/downloads/dim_ip_daily_summary.sql
+++ transform/mattermost-analytics/models/marts/product/downloads/dim_ip_daily_summary.sql
@@ -1,3 +1,12 @@
+{{
+config({
+    "materialized": 'incremental',
+    "incremental_strategy": "delete+insert",
+    "unique_key": ['daily_ip_id'],
+    "cluster_by": ['log_date'],
+    "snowflake_warehouse": "transform_l"
+  })
+}}
 select
     {{ dbt_utils.generate_surrogate_key(['log_date', 'client_ip']) }} as daily_ip_id
     , log_date
@@ -12,6 +21,10 @@ where
     and status like '2%'
     -- Keep only requests to download specific version
     and version is not null
+    {% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    and log_date >= (select max(log_date) from {{ this }})
+    {% endif %}
 group by
     log_date
     , client_ip
