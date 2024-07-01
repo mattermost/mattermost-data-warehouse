1121a0ef3c44e6d3632ebab31ca855a085752bc0
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-07-01 18:16:24 +0300

    Add fact table

--- transform/mattermost-analytics/models/intermediate/product/trial_requests/int_onprem_trial_requests_history.sql
+++ transform/mattermost-analytics/models/intermediate/product/trial_requests/int_onprem_trial_requests_history.sql
@@ -1,6 +1,3 @@
-
-{% set company_types = ['SMB', 'Enterprise', 'Midmarket', 'Federal', 'Academic', 'MME', 'Non-Profit'] %}
-
 with deduped_trial_requests as (
     -- Deduplicate CWS licenses due to stitch duplications
     select
@@ -8,23 +5,6 @@ with deduped_trial_requests as (
     from
         {{ ref('stg_cws__trial_requests')}}
     qualify row_number() over (partition by trial_request_id order by start_at asc) = 1
-), aggregates as (
-    -- Calculate aggregates for each email appearing in trial requests.
-    select
-        coalesce(tr.contact_email, tr.email) as trial_email
-        , count(distinct tr.trial_request_id) as total_trial_requests
-        , min(tr.start_at) as first_trial_start_at
-        , max(tr.start_at) as last_trial_start_at
-        , count(distinct l.company_type__c) as num_company_types
-{% for company_type in company_types %}
-        , count_if(l.company_type__c = '{{ company_type }}') > 0 as marked_as_{{ company_type.lower().replace('-', '_') }}
-{% endfor %}
-    from
-        deduped_trial_requests tr
-        left join {{ ref('stg_salesforce__lead')}} l on l.email = tr.contact_email or l.email = tr.email
-    where
-        not l.is_deleted
-    group by all
 )
 select
     -- Normalize columns appearing both in on-prem and cloud trials.
@@ -44,14 +24,5 @@ select
         else 'In-Product'
     end as request_source
     , 'in-product' as request_type
-    -- Aggregates
---     , agg.total_trial_requests
---     , agg.first_trial_start_at
---     , agg.last_trial_start_at
---     , agg.num_company_types
--- {% for company_type in company_types %}
---     , agg.marked_as_{{ company_type.lower().replace('-', '_') }}
--- {% endfor %}
 from
     deduped_trial_requests tr
-    left join aggregates agg on coalesce(tr.contact_email, tr.email) = agg.trial_email
