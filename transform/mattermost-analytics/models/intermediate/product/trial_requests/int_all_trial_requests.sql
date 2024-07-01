2992ef3a97d04f096afec98e17940ef494bfa604
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-07-01 15:53:31 +0300

    Rename models

--- transform/mattermost-analytics/models/intermediate/product/trial_requests/int_all_trial_requests.sql
+++ transform/mattermost-analytics/models/intermediate/product/trial_requests/int_all_trial_requests.sql
@@ -7,7 +7,7 @@
 }}
 with all_trial_requests as (
     {{ dbt_utils.union_relations(
-        relations=[ref('int_cloud_trial_requests'), ref('int_onprem_trial_requests')],
+        relations=[ref('int_cloud_trial_requests_history'), ref('int_onprem_trial_requests_history')],
     ) }}
 )
 
