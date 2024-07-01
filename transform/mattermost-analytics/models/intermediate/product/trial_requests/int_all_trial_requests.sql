3bce94ccf8101aba324961fa0a5682efbedd9e65
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-07-01 16:00:18 +0300

    Replace union_relationships with plain old union

--- transform/mattermost-analytics/models/intermediate/product/trial_requests/int_all_trial_requests.sql
+++ transform/mattermost-analytics/models/intermediate/product/trial_requests/int_all_trial_requests.sql
@@ -1,14 +1,10 @@
 -- Temporarily materialize
-{{
-    config({
-        "materialized": "table",
-        "snowflake_warehouse": "transform_l"
-    })
-}}
 with all_trial_requests as (
-    {{ dbt_utils.union_relations(
-        relations=[ref('int_cloud_trial_requests_history'), ref('int_onprem_trial_requests_history')],
-    ) }}
+
+    select * from {{ ref('int_cloud_trial_requests_history') }}
+    union
+    select * from {{ ref('int_onprem_trial_requests_history') }}
+
 )
 
 select * from all_trial_requests
