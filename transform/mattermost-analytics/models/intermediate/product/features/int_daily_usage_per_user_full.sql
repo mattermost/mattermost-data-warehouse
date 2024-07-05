33e0ab676b8d4ff2554bd141b4d9d45b125d1045
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-07-05 15:28:03 +0300

    Make joins and aggs dynamic

--- transform/mattermost-analytics/models/intermediate/product/features/int_daily_usage_per_user_full.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/int_daily_usage_per_user_full.sql
@@ -9,19 +9,20 @@
 {%- set agg_columns = ['count_known_features', 'count_unknown_features', 'count_total'] -%}
 {%- set skip_columns = join_columns + agg_columns + ['received_at_date'] -%}
 
--- List of models that include partial daily feature usage.
+-- List of models and their aliases. that include partial daily feature usage.
+-- Add new models here.
 {%-
-set partial_daily_models = [
-    ref('int_feature_daily_usage_per_user'),
-    ref('int_playbooks_daily_usage_per_user'),
-    ref('int_calls_daily_usage_per_user')
-]
+set partial_daily_models = {
+    ref('int_feature_daily_usage_per_user'): "mattermost",
+    ref('int_playbooks_daily_usage_per_user'): "playbooks",
+    ref('int_calls_daily_usage_per_user'): "calls"
+}
 -%}
 
 -- Create a list of dates for each user/server where the user shows some "presence".
 -- Results in a spine of the output dates, helping avoid full outer joins.
 with user_daily_presence as (
-{% for model in partial_daily_models %}
+{% for model in partial_daily_models.keys() %}
 
     select
     {% for column in join_columns %}
@@ -39,36 +40,28 @@ select
 {% for column in join_columns %}
     {%- if not loop.first %} , {% endif -%} spine.{{column}}
 {% endfor %}
-    -- Mattermost features
-{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_feature_daily_usage_per_user'), except=skip_columns) %}
-{% for col in cols %}
-    , coalesce(m.{{ adapter.quote(col)|trim }}, 0) as {{ adapter.quote(col)|trim }}
+    -- Features from individual models
+{% for model, alias in partial_daily_models.items() %}
+    -- {{ alias }} features
+    {% set cols = dbt_utils.get_filtered_columns_in_relation(from=model, except=skip_columns) %}
+    {% for col in cols %}
+        , coalesce(m.{{ adapter.quote(col)|trim }}, 0) as {{ adapter.quote(col)|trim }}
+    {% endfor %}
 {% endfor %}
-    -- Playbook features
-{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_playbooks_daily_usage_per_user'), except=skip_columns) %}
-{% for col in cols %}
-    , coalesce(p.{{ adapter.quote(col)|trim }}, 0) as {{ adapter.quote(col)|trim }}
-{% endfor %}
-    -- Calls features
-{% set cols = dbt_utils.get_filtered_columns_in_relation(from=ref('int_calls_daily_usage_per_user'), except=skip_columns) %}
-{% for col in cols %}
-    , coalesce(c.{{ adapter.quote(col)|trim }}, 0) as {{ adapter.quote(col)|trim }}
-{% endfor %}
+
     -- Aggregate columns are joined by summing up the values from all sources
 {% for column in agg_columns %}
-    , coalesce(m.{{column}}, 0) + coalesce(p.{{column}}, 0) + coalesce(c.{{column}}, 0) as {{column}}
+    , {% for alias in partial_daily_models.values() %}
+    {%- if not loop.first -%} + {% endif -%} spine.{{column}} coalesce({{alias}}.{{column}}, 0)
+    {% endfor %}
+       as {{column}}
 {% endfor %}
 from
     user_daily_presence spine
-    left join {{ ref('int_feature_daily_usage_per_user') }} m
+{% for model, alias in partial_daily_models.items() %}
+    left join {{ ref('int_feature_daily_usage_per_user') }} {{ alias }}
         on  {% for column in join_columns %}
-            {%- if not loop.first %} and {% endif -%} spine.{{column}} = m.{{column}}
+            {%- if not loop.first %} and {% endif -%} spine.{{column}} = {{alias}}.{{column}}
         {% endfor %}
-    left join {{ ref('int_playbooks_daily_usage_per_user') }} p
-        on  {% for column in join_columns %}
-            {%- if not loop.first %} and {% endif -%} spine.{{column}} = p.{{column}}
-        {% endfor %}
-    left join {{ ref('int_calls_daily_usage_per_user') }} c
-        on  {% for column in join_columns %}
-            {%- if not loop.first %} and {% endif -%} spine.{{column}} = c.{{column}}
-        {% endfor %}
+{% endfor %}
+
