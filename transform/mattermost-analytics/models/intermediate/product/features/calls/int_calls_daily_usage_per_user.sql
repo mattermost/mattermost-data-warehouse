a349fc94e4d52ed4f0a0a2b99bfb346cd2813314
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-07-05 11:45:00 +0300

    Fix column name

--- transform/mattermost-analytics/models/intermediate/product/features/calls/int_calls_daily_usage_per_user.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/calls/int_calls_daily_usage_per_user.sql
@@ -10,9 +10,9 @@ select
     , server_id
     , user_id
     , {{ dbt_utils.generate_surrogate_key(['activity_date', 'server_id', 'user_id']) }} as daily_user_id
-    , count_if(feature_name = 'Calls' and array_size(feature_sku) > 0) as count_calls
+    , count_if(feature_name = 'Calls' and array_size(feature_skus) > 0) as count_calls
     , count_calls as count_known_features
-    , count_if(array_size(feature_sku) = 0) as count_unknown_features
+    , count_if(array_size(feature_skus) = 0) as count_unknown_features
     , count(event_id) as count_total
 from
     {{ ref('stg_mm_calls_test_go__tracks') }} e
