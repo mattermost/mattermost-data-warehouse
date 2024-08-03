e60e651103c9e7e6567ea860885bfaddf7a2c9a9
Catalin Tomai <catalin.tomai@mattermost.com>
2024-08-04 00:49:39 +0100

    surface security update data

--- transform/mattermost-analytics/models/marts/product/fct_active_servers.sql
+++ transform/mattermost-analytics/models/marts/product/fct_active_servers.sql
@@ -33,6 +33,9 @@ select
     s.has_diagnostics_data,
     s.is_missing_activity_data,
     l.daily_server_id is null as is_missing_license_data
+
+    -- Server-reported activity
+    , coalesce(sas.daily_active_users, 0) as server_daily_active_users
 from
     {{ ref('int_server_active_days_spined') }} s
     left join {{ ref('int_server_license_daily') }} l on s.daily_server_id = l.daily_server_id
