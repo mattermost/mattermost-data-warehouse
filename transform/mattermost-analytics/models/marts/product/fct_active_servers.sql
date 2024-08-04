9e59613c9deabd49d526fdb2b6a3427d3a4d59a9
Catalin Tomai <catalin.tomai@mattermost.com>
2024-08-04 12:10:30 +0100

    fix

--- transform/mattermost-analytics/models/marts/product/fct_active_servers.sql
+++ transform/mattermost-analytics/models/marts/product/fct_active_servers.sql
@@ -33,8 +33,7 @@ select
     s.has_legacy_telemetry_data,
     s.has_diagnostics_data,
     s.is_missing_activity_data,
-    l.daily_server_id is null as is_missing_license_data,
-
+    l.daily_server_id is null as is_missing_license_data
 from
     {{ ref('int_server_active_days_spined') }} s
     left join {{ ref('int_server_license_daily') }} l on s.daily_server_id = l.daily_server_id
