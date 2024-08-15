2d764f03dfdab82e4eb76cf6edc01c96d4647524
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-08-15 12:29:52 +0300

    Hotfix: version parts as integers

--- transform/mattermost-analytics/models/staging/mattermost2/stg_mattermost2__server.sql
+++ transform/mattermost-analytics/models/staging/mattermost2/stg_mattermost2__server.sql
@@ -13,9 +13,9 @@ with source as (
         -- Server info
         , system_admins  as count_system_admins
         , version        as version_full
-        , split_part(version, '.', 1) as version_major
-        , split_part(version, '.', 2) as version_minor
-        , split_part(version, '.', 3) as version_patch
+        , try_to_decimal(split_part(version, '.', 1)) as version_major
+        , try_to_decimal(split_part(version, '.', 2)) as version_minor
+        , try_to_decimal(split_part(version, '.', 3)) as version_patch
 
         -- Deployment info
         , operating_system
