3f5b999a1e58820926194442c7e166b96231bce4
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-08-15 12:47:48 +0300

    Hotfix: version parts as integers

--- transform/mattermost-analytics/models/staging/mm_telemetry_prod/stg_mm_telemetry_prod__server.sql
+++ transform/mattermost-analytics/models/staging/mm_telemetry_prod/stg_mm_telemetry_prod__server.sql
@@ -14,9 +14,9 @@ with source as (
         -- Server info
         , system_admins  as count_system_admins
         , version        as version_full
-        , split_part(version, '.', 1) as version_major
-        , split_part(version, '.', 2) as version_minor
-        , split_part(version, '.', 3) as version_patch
+        , try_to_decimal(split_part(version, '.', 1)) as version_major
+        , try_to_decimal(split_part(version, '.', 2)) as version_minor
+        , try_to_decimal(split_part(version, '.', 3)) as version_patch
         , coalesce(context_traits_installationid,  context_traits_installation_id) as installation_id
         , installation_type
         , anonymous_id
