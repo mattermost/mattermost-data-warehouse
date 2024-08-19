da06184c94a9894763d847a3d6d35a28158481fa
catalintomai <56169943+catalintomai@users.noreply.github.com>
2024-08-19 13:23:15 +0100

    MM 60178 - - Surface installation_id field in order to build a unified cloud + self-hosted trial requests view (#1579)

--- transform/mattermost-analytics/models/intermediate/product/trial_requests/int_cloud_trial_requests_history.sql
+++ transform/mattermost-analytics/models/intermediate/product/trial_requests/int_cloud_trial_requests_history.sql
@@ -14,7 +14,7 @@ with customers as (
 subscriptions as (
     select
         subscription_id
-        , cws_installation
+        , cws_installation as installation_id
         , cws_dns
         , customer_id
         , trial_start_at
@@ -29,6 +29,7 @@ subscriptions as (
 select
     'stripe:' || subscriptions.subscription_id as trial_request_id
     , null as server_id
+    , installation_id
     , customers.email as trial_email
     , customers.email as contact_email
     , null as user_email
@@ -54,4 +55,4 @@ where
     -- Only get trial subscriptions
     subscriptions.trial_start_at is not null
     -- Only get cloud subscriptions
-    AND cws_installation is not null
+    AND installation_id is not null
