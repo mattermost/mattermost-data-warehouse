fb9c56f6da843a0f86d7a4fe85c939facb4001ff
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-08-20 17:18:52 +0300

    MM-58167: add country to server information

--- transform/mattermost-analytics/models/marts/product/dim_server_info.sql
+++ transform/mattermost-analytics/models/marts/product/dim_server_info.sql
@@ -8,7 +8,8 @@ with server_telemetry_summary as (
         first_value(count_registered_active_users) over (partition by server_id order by activity_date asc) as first_count_registered_active_users,
         last_value(count_registered_active_users) over (partition by server_id order by activity_date asc) as last_count_registered_active_users,
         last_value(daily_active_users) over (partition by server_id order by activity_date asc) as last_daily_active_users,
-        last_value(monthly_active_users) over (partition by server_id order by activity_date asc) as last_monthly_active_users
+        last_value(monthly_active_users) over (partition by server_id order by activity_date asc) as last_monthly_active_users,
+        last_value(server_ip) over (partition by server_id order by activity_date asc) as last_server_ip
     from
         {{ ref('int_server_active_days_spined') }}
     -- Keep only one row per server as the current query creates duplicates
@@ -42,7 +43,14 @@ with server_telemetry_summary as (
        st.first_count_registered_active_users,
        st.last_count_registered_active_users,
        st.last_daily_active_users,
-       st.last_monthly_active_users
+       st.last_monthly_active_users,
+       st.last_server_ip,
+       parse_ip(st.last_server_ip, 'INET', 1) as parsed_server_ip,
+        case
+            when parsed_server_ip:error is null
+                then parse_ip(si.server_ip || '/7', 'INET'):ipv4_range_start
+            else null
+        end as ip_bucket
     from
         server_telemetry_summary st
         full outer join user_telemetry_summary ut on st.server_id = ut.server_id
@@ -59,7 +67,15 @@ select
     si.first_count_registered_active_users,
     si.last_count_registered_active_users,
     si.last_daily_active_users,
-    si.last_monthly_active_users
+    si.last_monthly_active_users,
+    si.last_server_ip,
+    case
+        when si.parsed_server_ip:error is not null then 'Unknown'
+        else coalesce(l.country_name, 'Unknown')
+    end as last_known_ip_country
 from
     server_info si
     left join {{ ref('int_server_hosting_type') }} ht on si.server_id = ht.server_id
+    left join {{ ref('int_ip_country_lookup') }} l
+                on si.ip_bucket = l.join_bucket
+                    and si.parsed_server_ip:ipv4 between l.ipv4_range_start and l.ipv4_range_end
