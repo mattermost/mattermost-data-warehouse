90738739ae8b29e47b4bcce8b7e6340c9e8ca938
catalintomai <56169943+catalintomai@users.noreply.github.com>
2024-08-20 15:12:47 +0100

    Chore: Add more Stripe fields to fct_subscription_history model (#1581)
    * add fields

--- transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
+++ transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
@@ -4,12 +4,15 @@ select
     , s.customer_id
     , COALESCE(sh.licensed_seats, s.quantity) as licensed_seats
     , sh.created_at
+    , s.converted_to_paid_at
     , s.cws_dns
     , s.cws_installation
     , s.license_start_at
     , s.license_end_at
     , s.billing_type
     , s.status
+    , s.product_id
+    , s.current_product_id
     , CASE 
         WHEN sh.subscription_id IS NULL THEN true -- If there isn't a matching subscription history value this is treated as the latest one
         ELSE ROW_NUMBER() OVER (PARTITION BY sh.subscription_id ORDER BY sh.created_at DESC) = 1 
