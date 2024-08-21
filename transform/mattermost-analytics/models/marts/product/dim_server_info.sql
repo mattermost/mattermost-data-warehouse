03411a64433eb8111a2eda9df1884b702146315e
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-08-21 15:42:03 +0300

    Refactor to run join once

--- transform/mattermost-analytics/models/marts/product/dim_server_info.sql
+++ transform/mattermost-analytics/models/marts/product/dim_server_info.sql
@@ -1,20 +1,4 @@
-with server_telemetry_summary as (
-    select
-        server_id,
-        min(activity_date) over (partition by server_id) as first_activity_date,
-        max(activity_date) over (partition by server_id ) as last_activity_date,
-        first_value(binary_edition) over (partition by server_id order by activity_date asc) as first_binary_edition,
-        last_value(binary_edition) over (partition by server_id order by activity_date asc) as last_binary_edition,
-        first_value(count_registered_active_users) over (partition by server_id order by activity_date asc) as first_count_registered_active_users,
-        last_value(count_registered_active_users) over (partition by server_id order by activity_date asc) as last_count_registered_active_users,
-        last_value(daily_active_users) over (partition by server_id order by activity_date asc) as last_daily_active_users,
-        last_value(monthly_active_users) over (partition by server_id order by activity_date asc) as last_monthly_active_users,
-        last_value(server_ip) over (partition by server_id order by activity_date asc) as last_server_ip
-    from
-        {{ ref('int_server_active_days_spined') }}
-    -- Keep only one row per server as the current query creates duplicates
-    qualify row_number() over (partition by server_id order by server_id) = 1
-), user_telemetry_summary as (
+with user_telemetry_summary as (
     select
         server_id,
         min(activity_date) as first_activity_date,
@@ -25,31 +9,6 @@ with server_telemetry_summary as (
         is_active_today
     group by
         server_id
-), server_ips as (
-    -- Store distinct IPs to avoid parsing the same IP multiple times
-    select
-        distinct
-            st.last_server_ip,
-            parse_ip(st.last_server_ip, 'INET', 1) as parsed_server_ip,
-        case
-            when parsed_server_ip:error is null
-                then parse_ip(st.last_server_ip || '/7', 'INET'):ipv4_range_start
-            else null
-        end as ip_bucket
-    from
-        server_telemetry_summary st
-), ip2country as (
-    select
-        si.last_server_ip,
-        case
-            when si.parsed_server_ip:error is not null then 'Unknown'
-            else coalesce(l.country_name, 'Unknown')
-        end as last_known_ip_country
-    from
-        server_ips si
-        left join {{ ref('int_ip_country_lookup') }} l
-            on si.ip_bucket = l.join_bucket
-                and si.parsed_server_ip:ipv4 between l.ipv4_range_start and l.ipv4_range_end
 ), server_info as (
    select
        coalesce(st.server_id, ut.server_id) as server_id,
@@ -71,7 +30,7 @@ with server_telemetry_summary as (
        st.last_monthly_active_users,
        st.last_server_ip
     from
-        server_telemetry_summary st
+        {{ ref('int_server_telemetry_summary') }} st
         full outer join user_telemetry_summary ut on st.server_id = ut.server_id
 )
 select
@@ -92,4 +51,4 @@ select
 from
     server_info si
     left join {{ ref('int_server_hosting_type') }} ht on si.server_id = ht.server_id
-    left join ip2country ip on si.last_server_ip = ip.last_server_ip
+    left join {{ ref('int_server_ip_to_country') }} ip on si.server_id = ip.server_id
