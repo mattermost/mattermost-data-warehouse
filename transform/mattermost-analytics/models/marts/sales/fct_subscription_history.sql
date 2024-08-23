fc81f212d470e8c6c9d54a77eda5858dce9e83bf
catalintomai <56169943+catalintomai@users.noreply.github.com>
2024-08-23 11:07:37 +0100

    Chore: add product name to the fct_subscription_history model (#1583)

--- transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
+++ transform/mattermost-analytics/models/marts/sales/fct_subscription_history.sql
@@ -1,9 +1,10 @@
-select 
+select
     sh.subscription_history_event_id
     , s.subscription_id
     , s.customer_id
     , COALESCE(sh.licensed_seats, s.quantity) as licensed_seats
     , sh.created_at
+    , s.created_at as subscription_created_at
     , s.converted_to_paid_at
     , s.cws_dns
     , s.cws_installation
@@ -17,7 +18,9 @@ select
         WHEN sh.subscription_id IS NULL THEN true -- If there isn't a matching subscription history value this is treated as the latest one
         ELSE ROW_NUMBER() OVER (PARTITION BY sh.subscription_id ORDER BY sh.created_at DESC) = 1 
     END AS is_latest
+    , p.name as product_name
 from
     {{ ref('stg_cws__subscription_history') }} sh
     right join {{ ref('stg_stripe__subscriptions') }} s on sh.subscription_id = s.subscription_id
+    left join {{ ref('stg_stripe__products')}} p on coalesce(s.product_id, s.current_product_id) = p.product_id
     where s.cws_installation IS NOT NULL
