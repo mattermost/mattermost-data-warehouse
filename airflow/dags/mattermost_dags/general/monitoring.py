8ef9bb559a38154796ae265e8c053a07476f5642
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-05 12:52:35 +0300

    Chore: change job schedule

--- airflow/dags/mattermost_dags/general/monitoring.py
+++ airflow/dags/mattermost_dags/general/monitoring.py
@@ -18,7 +18,7 @@ with DAG(
     'monitoring',
     default_args=default_args,
     start_date=datetime(2017, 3, 20),
-    schedule='@daily',
+    schedule="0 10 * * *",
     catchup=False,
 ) as dag:
     check_stitch_extractions = SimpleHttpOperator(
