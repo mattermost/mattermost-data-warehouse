c07032d0c8f03212ffa06f5e94cc55c698efd27f
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-06 10:56:06 +0300

    Chore: permissions for new rudderstack user (#1597)
    * Chore: permissions for new rudderstack user
    
    * fix: Switch to org secrets
    
    * Fix linter issues
    
    ---------
    
    Co-authored-by: Antonis Stamatiou <stamatiou.antonis@gmail.com>

--- airflow/tests/general/test_monitoring.py
+++ airflow/tests/general/test_monitoring.py
@@ -62,12 +62,9 @@ def test_hightouch_check_syncs_fail(load_data):
     response = load_data('monitoring/syncs_fail.json')
     failed_syncs = hightouch_check_syncs(response)
     # Expected to return dict containing failed loads
-    assert failed_syncs == [{
-        "id": 99999,
-        "slug": "test_sync_1",
-        "status": "warning",
-        "updatedAt": "2022-12-07T12:59:20Z"
-    }]
+    assert failed_syncs == [
+        {"id": 99999, "slug": "test_sync_1", "status": "warning", "updatedAt": "2022-12-07T12:59:20Z"}
+    ]
 
 
 def test_hightouch_check_syncs_error(load_data):
