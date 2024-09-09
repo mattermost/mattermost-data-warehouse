f182b29a4094df0622114da7b120446b3b62841c
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-09 15:17:12 +0300

    Remove nulls

--- transform/mattermost-analytics/tests/staging/mm_telemetry_prod/test_installation_id_server_id_relationship.sql
+++ transform/mattermost-analytics/tests/staging/mm_telemetry_prod/test_installation_id_server_id_relationship.sql
@@ -3,5 +3,7 @@ select
     installation_id, count(distinct server_id) as server_count
 from
     {{ ref('stg_mm_telemetry_prod__server') }}
+where
+    installation_id is not null
 group by installation_id
 having count(distinct server_id) > 1
