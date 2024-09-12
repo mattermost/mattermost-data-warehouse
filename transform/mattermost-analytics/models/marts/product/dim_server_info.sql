b5f0570768d12fc39df7552bccd97bad16db662e
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-12 11:55:02 +0300

    MM-60430: add latest customer info to server info (#1601)
    * MM-60430: add latest customer info to server info
    
    * Fix cloud case

--- transform/mattermost-analytics/models/marts/product/dim_server_info.sql
+++ transform/mattermost-analytics/models/marts/product/dim_server_info.sql
@@ -32,6 +32,23 @@ with user_telemetry_summary as (
     from
         {{ ref('int_server_telemetry_summary') }} st
         full outer join user_telemetry_summary ut on st.server_id = ut.server_id
+), latest_license as (
+    select
+        server_id,
+        license_id
+    from
+        {{ ref('int_server_license_daily') }}
+    where
+        license_id is not null
+    qualify row_number() over(partition by server_id order by license_telemetry_date desc) = 1
+), latest_stripe_customer as (
+    select
+        sub.cws_installation,
+        cus.name
+    from
+        {{ ref('stg_stripe__subscriptions') }} sub
+        left join {{ ref('stg_stripe__customers') }} cus on sub.customer_id = cus.customer_id
+    qualify row_number() over(partition by cws_installation order by sub.created_at desc) = 1
 )
 select
     si.server_id,
@@ -47,8 +64,16 @@ select
     si.last_daily_active_users,
     si.last_monthly_active_users,
     si.last_server_ip,
-    coalesce(ip.last_known_ip_country, 'Unknown') as last_known_ip_country
+    coalesce(ip.last_known_ip_country, 'Unknown') as last_known_ip_country,
+    case
+        when ht.hosting_type = 'Self-hosted' then coalesce(k.company_name, 'Unknown')
+        when ht.hosting_type = 'Cloud' then coalesce(cus.name, 'Unknown')
+        when ht.hosting_type = 'Unknown' then  coalesce(k.company_name, cus.name, 'Unknown')
+    end as company_name
 from
     server_info si
     left join {{ ref('int_server_hosting_type') }} ht on si.server_id = ht.server_id
     left join {{ ref('int_server_ip_to_country') }} ip on si.server_id = ip.server_id
+    left join latest_license ll on si.server_id = ll.server_id
+    left join {{ ref('int_known_licenses') }} k on ll.license_id = k.license_id
+    left join latest_stripe_customer cus on ht.installation_id = cus.cws_installation
