ddbac3c0cf04194242c528c5198049c88f3da33c
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-16 13:13:14 +0300

    MM-60551: ignore non-standard whitespaces

--- transform/mattermost-analytics/macros/validation/validate_email.sql
+++ transform/mattermost-analytics/macros/validation/validate_email.sql
@@ -1,6 +1,6 @@
 {% macro validate_email(column) -%}
     {%-
-        set invalid_chars = ['\\\\', '/', '(', ')', '&', '$', '^', '&', '!', ' ', '<', '>', '“']
+        set invalid_chars = ['\\\\', '/', '(', ')', '&', '$', '^', '&', '!', ' ', '<', '>', '“', '\u2006', '\ua0']
     -%}
     {{ column }} LIKE '%_@__%.__%' and {{ column }} not like '%@%@%'
     {% for char in invalid_chars %}
