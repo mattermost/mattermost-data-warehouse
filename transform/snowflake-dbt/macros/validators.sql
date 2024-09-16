b01555276fb2695b4f523853c373aec60e36ba71
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-16 13:22:32 +0300

    Escape unicode chars

--- transform/snowflake-dbt/macros/validators.sql
+++ transform/snowflake-dbt/macros/validators.sql
@@ -1,6 +1,6 @@
 {% macro validate_email(column) -%}
     {%-
-        set invalid_chars = ['\\\\', '/', '(', ')', '&', '$', '^', '&', '!', ' ', '<', '>', '“', '\\u2006', '\\ua0']
+        set invalid_chars = ['\\\\', '/', '(', ')', '&', '$', '^', '&', '!', ' ', '<', '>', '“', '\\u2006', '\\u00a0']
     -%}
     {{ column }} LIKE '%_@__%.__%' and {{ column }} not like '%@%@%'
     {% for char in invalid_chars %}
