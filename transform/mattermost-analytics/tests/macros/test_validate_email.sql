2d8033cdc764d0f680e24f5d70505854f357ffc9
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-18 17:15:41 +0300

    Escape unicode chars

--- transform/mattermost-analytics/tests/macros/test_validate_email.sql
+++ transform/mattermost-analytics/tests/macros/test_validate_email.sql
@@ -13,8 +13,8 @@ with test_values as (
             ('a!b@test.com'),
             ('<user@test.com>'),
             ('a“b@test.com'),
-            ('ab@test.co\u2006m'),
-            ('ab@test.co\uu00a0'),
+            ('ab@test.co\\u2006m'),
+            ('ab@test.co\\uu00a0'),
             ('ab@test.com`')
     ) as t(test_email)
 )
