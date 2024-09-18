a472755e9a871da6cca9a217b88f5a7f753aded8
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-09-18 17:18:45 +0300

    MM-60551: add extra character for invalid email (#1609)
    * MM-60551: add extra character for invalid email
    
    * Escape unicode chars

--- transform/mattermost-analytics/tests/macros/test_validate_email.sql
+++ transform/mattermost-analytics/tests/macros/test_validate_email.sql
@@ -12,7 +12,10 @@ with test_values as (
             ('a/b@test.com'),
             ('a!b@test.com'),
             ('<user@test.com>'),
-            ('a“b@test.com')
+            ('a“b@test.com'),
+            ('ab@test.co\\u2006m'),
+            ('ab@test.co\\uu00a0'),
+            ('ab@test.com`')
     ) as t(test_email)
 )
 select
