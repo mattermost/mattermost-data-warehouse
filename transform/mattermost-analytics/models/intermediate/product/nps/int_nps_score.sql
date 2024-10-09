0c60a052b745c29ec79668ee741a139a8285a23f
Catalin Tomai <catalin.tomai@mattermost.com>
2024-10-09 17:32:53 +0100

    more work

--- transform/mattermost-analytics/models/intermediate/product/nps/int_nps_score.sql
+++ transform/mattermost-analytics/models/intermediate/product/nps/int_nps_score.sql
@@ -5,8 +5,8 @@ select
     , license_id as license_id
     , event_date as event_date
     , timestamp as timestamp
-    -- server_version only contains major and minor
     , server_version_full
+    -- server_version only contains major and minor
     , server_version_major || '.' || server_version_minor as server_version
     , score as score
     , user_role as user_role
