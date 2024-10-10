e5c9f862b28de30b5b7058e0ad2c02dd2fe76c36
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-10-10 15:57:05 +0300

    MM-60600: defered merge of events table

---
+++ transform/mattermost-analytics/models/staging/mm_telemetry_prod/base/base_events.sql
@@ -0,0 +1,16 @@
+{{config({
+    "materialized": 'incremental',
+    "snowflake_warehouse": "transform_l"
+  })
+}}
+select
+    {{dbt_utils.star(source('mm_telemetry_prod', 'event') )}}
+from
+    {{ source('mm_telemetry_prod', 'event') }}
+where
+{% if is_incremental() %}
+    -- Hack to disable incremental for now
+    false
+{% else %}
+    received_at < '2024-10-01'
+{% endif %}
