5a55b65a6b22bd60b34b85fea0652cb0d9feb6be
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-10-11 20:29:29 +0300

    Remove extra qualify from delta

--- transform/mattermost-analytics/models/staging/mm_telemetry_prod/base/base_events_delta.sql
+++ transform/mattermost-analytics/models/staging/mm_telemetry_prod/base/base_events_delta.sql
@@ -4,18 +4,15 @@
     "snowflake_warehouse": "transform_l",
     "incremental_strategy": "delete+insert",
     "unique_key": ['id'],
-    "cluster_by": ['TO_DATE(timestamp)'],
   })
 }}
 select
-    {{dbt_utils.star(source('mm_telemetry_prod', 'event') )}},
-    current_timestamp() as watermark
+    {{dbt_utils.star(source('mm_telemetry_prod', 'event') )}}
 from
     {{ source('mm_telemetry_prod', 'event') }}
 where
 {% if is_incremental() %}
     received_at >= (select max(received_at) FROM {{ this }})
 {% else %}
-    received_at >= '2024-01-01'
+    received_at >= '2024-09-01'
 {% endif %}
-qualify 1 = row_number() over (partition by id order by received_at desc)
