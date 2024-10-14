f7c63a79ec8c618e3f6094d96f1ce21197a653ee
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-10-14 17:33:31 +0300

    Add deduplication

--- transform/mattermost-analytics/models/staging/mm_telemetry_prod/base/base_events_delta.sql
+++ transform/mattermost-analytics/models/staging/mm_telemetry_prod/base/base_events_delta.sql
@@ -1,8 +1,7 @@
 {{config({
     'materialized': 'table',
     'transient': true,
-    'snowflake_warehouse': 'transform_l',
-    'cluster_by': ['to_date(timestamp)'],
+    'snowflake_warehouse': 'transform_l'
   })
 }}
 select
@@ -11,3 +10,4 @@ from
     {{ source('mm_telemetry_prod', 'event') }}
 where
     received_at >= (select max(received_at) FROM {{ ref('base_events') }})
+qualify 1 = row_number() over(partition by id order by received_at desc)
