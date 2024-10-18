6eff14543408e34b47b9b554f6881fa16a4e8cf3
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-10-18 16:05:40 +0300

    Add delta table and base table DBT configuration

---
+++ transform/mattermost-analytics/models/staging/mm_telemetry_prod/base/base_events_delta.sql
@@ -0,0 +1,20 @@
+{{
+    config({
+        "materialized": "incremental",
+        "incremental_strategy": "delete+insert",
+        "unique_key": ['id'],
+        "cluster_by": ['received_at_date'],
+        on_schema_change='append_new_columns'
+    })
+}}
+
+select
+    {{ dbt_utils.star(from=source('mm_telemetry_prod', 'event')) }}
+from
+    {{ source('mm_telemetry_prod', 'event') }}
+where
+{% if is_incremental() %}
+    received_at >= (select max(received_at) from {{ this }})
+{% else %}
+    received_at >= (select max(received_at) from {{ source('rudder_support', 'base_events') }})
+{% endif %}
