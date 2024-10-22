a8a3c2a692c95a2e5cf6c72a5746ea540693d268
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-10-22 19:55:10 +0300

    Typo fixes

--- transform/mattermost-analytics/models/reports/docs/rpt_docs_feedback.sql
+++ transform/mattermost-analytics/models/reports/docs/rpt_docs_feedback.sql
@@ -9,7 +9,7 @@
 }}
 with feedback as (
     select
-        id as event_id
+        event_id
         , timestamp
         , rating
         , feedback
@@ -18,7 +18,7 @@ with feedback as (
         , context_ip as client_ip
         , parse_ip(context_ip, 'INET', 1) as parsed_ip
         , case
-            when parsed_server_ip:error is null
+            when parsed_ip:error is null
                 then parse_ip(context_ip || '/7', 'INET'):ipv4_range_start
             else null
         end as ip_bucket
@@ -44,7 +44,7 @@ select
     , f.label
     , f.received_at
     , f.client_ip
-    , l.ip_country_name
+    , l.country_name as geolocated_country_name
     , f.page_path
     , f.browser_locale
     , f.page_search
@@ -55,4 +55,4 @@ from
     feedback f
     left join {{ ref('int_ip_country_lookup') }} l
                 on f.ip_bucket = l.join_bucket
-                    and f..parsed_ip:ipv4 between l.ipv4_range_start and l.ipv4_range_end
+                    and f.parsed_ip:ipv4 between l.ipv4_range_start and l.ipv4_range_end
