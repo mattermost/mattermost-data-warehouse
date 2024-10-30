6ca296fb031921d26c7bbcb3fe283fcb36b9a3b4
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-10-30 13:07:16 +0200

    MM-61359: handle empty dataframe

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -5,7 +5,7 @@ from textwrap import dedent
 import pandas as pd
 from responses import Response
 
-from utils.helpers import daterange, post_to_mattermost
+from utils.helpers import daterange, post_df_to_mattermost
 
 
 def test_include_start_exclude_end():
@@ -45,7 +45,7 @@ def test_post_to_mattermost(responses):
     df = pd.DataFrame({'id': [1, 2], 'title': ['The Great Gatsby', 'The Lord of the Rings']})
 
     # WHEN: request is made to Mattermost
-    post_to_mattermost(url, channel, df, headers=['ISBN', 'Book Title'])
+    post_df_to_mattermost(url, channel, df, ['ISBN', 'Book Title'], 'No books found')
 
     # THEN: A request is made to the Mattermost server
     assert len(responses.calls) == 1
@@ -58,5 +58,32 @@ def test_post_to_mattermost(responses):
             |      2 | The Lord of the Rings |
         '''
         ).strip(),
+        'channel': 'test-channel',
+    }
+
+
+def test_post_to_mattermost_with_fallback(responses):
+    # GIVEN: A Mattermost URL, and channel that returns HTTP OK
+    url = 'https://mattermost.a-test-server.com/path/to/hookid'
+    channel = 'test-channel'
+
+    response = Response(
+        method="POST",
+        url='https://mattermost.a-test-server.com/path/to/hookid',
+        status=200,
+        content_type='application/json',
+    )
+    responses.add(response)
+
+    # GIVEN: An empty dataframe
+    df = pd.DataFrame()
+
+    # WHEN: request is made to Mattermost
+    post_df_to_mattermost(url, channel, df, ['ISBN', 'Book Title'], 'No books found')
+
+    # THEN: A request is made to the Mattermost server with fallback message
+    assert len(responses.calls) == 1
+    assert json.loads(responses.calls[0].request.body) == {
+        'text': 'No books found',
         'channel': 'test-channel',
     }
--- utils/helpers.py
+++ utils/helpers.py
@@ -11,12 +11,18 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 
-def post_to_mattermost(url: str, channel: str, df: pd.DataFrame, headers: List[str]):
-    msg = tabulate(df, headers=headers, tablefmt='github', showindex='never')
-    response = requests.post(
-        url,
-        json={"text": msg, "channel": channel},
-    )
+def post_df_to_mattermost(url: str, channel: str, df: pd.DataFrame, headers: List[str], empty_data_message: str):
+    if df is None or df.empty:
+        response = requests.post(
+            url,
+            json={"text": empty_data_message, "channel": channel},
+        )
+    else:
+        msg = tabulate(df, headers=headers, tablefmt='github', showindex='never')
+        response = requests.post(
+            url,
+            json={"text": msg, "channel": channel},
+        )
 
     if response.status_code != 200:
         raise ValueError(f'Request to Mattermost returned {response.status_code}, the response is:\n{response.text}')
