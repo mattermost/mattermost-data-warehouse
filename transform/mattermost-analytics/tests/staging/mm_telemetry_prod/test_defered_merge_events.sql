e3712a82f06f42a00d777483af0ca02e28071662
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-11-04 11:18:34 +0200

    MM-60600: rollback delta logic

--- transform/mattermost-analytics/tests/staging/mm_telemetry_prod/test_defered_merge_events.sql
+++ transform/mattermost-analytics/tests/staging/mm_telemetry_prod/test_defered_merge_events.sql
@@ -4,7 +4,7 @@
     })
 }}
 
--- Assert that there's no overlap between base and delta table for more than the last 2 days.
+-- Assert that there's no overlap between base and delta table.
 -- Overlap should be addressed by the post-hook of the delta table.
 with base_table as (
     select max(received_at) as last_received_at from {{ source('rudder_support', 'base_events') }}
