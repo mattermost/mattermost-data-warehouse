ff58c76fbef11ad7f16b405474be73bf977e32cf
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-11-05 16:32:52 +0200

    Chore: consolidate NPS jobs

--- airflow/dags/mattermost_dags/general/nps_feedback.py
+++ airflow/dags/mattermost_dags/general/nps_feedback.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 from mattermost_dags.airflow_utils import MATTERMOST_DATAWAREHOUSE_IMAGE, pod_defaults, send_alert
 from mattermost_dags.kube_secrets import (
+    NPS_WEBHOOK_URL,
     SNOWFLAKE_ACCOUNT,
     SNOWFLAKE_PASSWORD,
     SNOWFLAKE_TRANSFORM_DATABASE,
@@ -27,11 +28,23 @@ default_args = {
 
 # Create the DAG
 dag = DAG(
-    "snowflake_job",
+    "nps_daily_feedback",
     default_args=default_args,
-    schedule="0 0 * * *",
+    schedule="0 12 * * *",
     catchup=False,
     max_active_runs=1,  # Don't allow multiple concurrent dag executions
+    doc_md="""
+### Daily NPS Feedback
+
+#### Purpose
+
+This DAG updates NPS feedback and sends it to the appropriate channel once per day. It runs the following tasks:
+- Calculate categories of feedback.
+- Post NPS feedback to Mattermost channel.
+
+#### Notes
+- DAG was previously split in two parts, that resulted in data not being available.
+""",
 )
 
 
@@ -67,4 +80,24 @@ nps_subcategory_updates = get_container_operator(
     "nps-subcategory-updates", "data_action_nps_feedback_subcategory_update", "mattermost"
 )
 
-automated_nps_feedback_category_update >> nps_subcategory_updates >> nps_category_updates
+
+post_nps_feedback = KubernetesPodOperator(
+    **pod_defaults,
+    image=MATTERMOST_DATAWAREHOUSE_IMAGE,  # Uses latest build from master
+    task_id="post-nps-feedback",
+    name="post-nps-feedback",
+    secrets=[
+        SNOWFLAKE_ACCOUNT,
+        SNOWFLAKE_USER,
+        SNOWFLAKE_PASSWORD,
+        SNOWFLAKE_TRANSFORM_ROLE,
+        SNOWFLAKE_TRANSFORM_WAREHOUSE,
+        SNOWFLAKE_TRANSFORM_SCHEMA,
+        SNOWFLAKE_TRANSFORM_DATABASE,
+        NPS_WEBHOOK_URL,
+    ],
+    arguments=["python -m utils.post_nps_data"],
+    dag=dag,
+)
+
+automated_nps_feedback_category_update >> nps_subcategory_updates >> nps_category_updates >> post_nps_feedback
