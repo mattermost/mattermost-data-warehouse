544a4242f1749a3c42658b8fdc2bff433ca80496
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-11-18 19:42:39 +0200

    Aggregate server events

---
+++ transform/mattermost-analytics/models/intermediate/product/features/mattermost/int_feature_server_daily_usage_per_user.sql
@@ -0,0 +1,28 @@
+{{
+    config({
+        "materialized": "table",
+        "snowflake_warehouse": "transform_l"
+    })
+}}
+
+select
+    -- Surrogate key required as it's both a good practice, as well as allows merge incremental strategy.
+    {{ dbt_utils.generate_surrogate_key(['activity_date', 'server_id', 'user_id']) }} as daily_user_id
+    , activity_date
+    , server_id
+    , user_id
+    ,  {{ dbt_utils.pivot(
+          'color',
+          dbt_utils.get_column_values(ref('int_server_feature_attribution'), 'feature_name'),
+          agg='sum',
+          prefix='feature_'
+      ) }}
+    , count_if(feature_name is null) as count_unknown_features
+    , count(event_id) as count_total
+from
+    {{ ref('int_server_feature_attribution') }}
+group by
+    daily_user_id
+    , activity_date
+    , server_id
+    , user_id
