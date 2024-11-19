ab2d53b6488385c9f3a71ca67a0aaebf08d05458
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-11-19 13:16:42 +0200

    Handle missing column

--- transform/mattermost-analytics/models/intermediate/product/features/mattermost/int_feature_server_daily_usage_per_user.sql
+++ transform/mattermost-analytics/models/intermediate/product/features/mattermost/int_feature_server_daily_usage_per_user.sql
@@ -5,6 +5,14 @@
     })
 }}
 
+{%-
+    set values = dbt_utils.get_column_values(ref('int_server_feature_attribution'), 'feature_name')
+-%}
+
+{%- if 'unknown_features' not in values -%}
+    {%- values.append('unknown_features') {% endif -%}
+{%- endif -%}
+
 select
     -- Surrogate key required as it's both a good practice, as well as allows merge incremental strategy.
     {{ dbt_utils.generate_surrogate_key(['activity_date', 'server_id', 'user_id']) }} as daily_user_id
@@ -13,7 +21,7 @@ select
     , user_id
     , {{ dbt_utils.pivot(
           'feature_name',
-          dbt_utils.get_column_values(ref('int_server_feature_attribution'), 'feature_name'),
+          values,
           agg='sum',
           prefix='count_'
       ) }}
