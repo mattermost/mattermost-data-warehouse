dcc109687cb67f414a321592e0fcb347b73c7d71
Ioannis Foukarakis <ioannis.foukarakis@mattermost.com>
2024-11-27 12:44:17 +0200

    Run full refresh

--- transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_prod__community_tracks.sql
+++ transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_prod__community_tracks.sql
@@ -5,19 +5,20 @@
         "incremental_strategy": "delete+insert",
         "unique_key": ['event_id'],
         "snowflake_warehouse": "transform_l",
-
+        full_refresh: True
     })
 }}
 
-    SELECT
-        {{ dbt_utils.star(ref('stg_mm_telemetry_prod__tracks'), except=["context_user_agent"]) }},
-        'stg_mm_telemetry_prod__tracks' AS _source_relation,
-        received_at::date AS received_at_date
-     FROM
-       {{ ref('stg_mm_telemetry_prod__tracks') }}
-    WHERE server_id = '{{ var("community_server_id") }}'
+SELECT
+    {{ dbt_utils.star(ref('stg_mm_telemetry_prod__tracks'), except=["context_user_agent"]) }},
+    'stg_mm_telemetry_prod__tracks' AS _source_relation,
+    received_at::date AS received_at_date
+ FROM
+   {{ ref('stg_mm_telemetry_prod__tracks') }}
+WHERE
+    server_id = '{{ var("community_server_id") }}'
 {% if is_incremental() %}
     AND received_at >= (SELECT MAX(received_at) FROM {{ this }})
-    -- Dedup events in the same batch
-    qualify row_number() over (partition by event_id order by timestamp desc) = 1
 {% endif %}
+-- Dedup events in the same batch
+qualify row_number() over (partition by event_id order by timestamp desc) = 1
--- transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_rc__community_tracks.sql
+++ transform/mattermost-analytics/models/intermediate/web_app/community/int_mm_telemetry_rc__community_tracks.sql
@@ -8,13 +8,13 @@
     })
 }}
 
-    SELECT
-        {{ dbt_utils.star(ref('stg_mm_telemetry_rc__tracks')) }},
-        'stg_mm_telemetry_rc__tracks' AS _source_relation,
-        received_at::date as received_at_date
-     FROM
-       {{ ref('stg_mm_telemetry_rc__tracks') }}
-    WHERE server_id = '{{ var("community_server_id") }}'
+SELECT
+    {{ dbt_utils.star(ref('stg_mm_telemetry_rc__tracks')) }},
+    'stg_mm_telemetry_rc__tracks' AS _source_relation,
+    received_at::date as received_at_date
+FROM
+   {{ ref('stg_mm_telemetry_rc__tracks') }}
+WHERE server_id = '{{ var("community_server_id") }}'
 {% if is_incremental() %}
     AND received_at >= (SELECT MAX(received_at) FROM {{ this }})
 {% endif %}
