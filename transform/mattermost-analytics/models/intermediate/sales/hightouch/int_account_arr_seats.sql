73ca9590f62b29b0b56d4e4769ffa09ebe6cdc5e
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-13 16:22:11 +0200

    Fix column refereces

--- transform/mattermost-analytics/models/intermediate/sales/hightouch/int_account_arr_seats.sql
+++ transform/mattermost-analytics/models/intermediate/sales/hightouch/int_account_arr_seats.sql
@@ -19,23 +19,23 @@ WITH account_w_arr AS (
 ), seats_licensed AS (
     SELECT
         a.account_id AS account_id,
-        SUM(CASE WHEN product2.family = 'License' THEN opportunitylineitem.quantity ELSE 0 END) AS seats
+        SUM(CASE WHEN product2.family = 'License' THEN oli.quantity ELSE 0 END) AS seats
     FROM {{ ref('stg_salesforce__account') }} a
     LEFT JOIN {{ ref('stg_salesforce__opportunity') }} o ON a.account_id = o.account_id AND o.status_wlo__c = 'Won'
     LEFT JOIN {{ ref('stg_salesforce__opportunity_line_item') }} oli ON o.opportunity_id = oli.opportunity_id
-    LEFT JOIN {{ ref('stg_salesforce__product2') }} product2 ON oli.product2id = product2.product2_id
+    LEFT JOIN {{ ref('stg_salesforce__product2') }} product2 ON oli.product2_id = product2.product2_id
     WHERE
         current_date >= oli.start_date__c AND current_date <= oli.end_date__c
     GROUP BY 1
-    HAVING SUM(CASE WHEN product2.family = 'License' THEN opportunitylineitem.quantity ELSE 0 END) > 0
+    HAVING SUM(CASE WHEN product2.family = 'License' THEN oli.quantity ELSE 0 END) > 0
 ), account_arr_and_seats AS (
     SELECT
         a.account_id AS account_id,
         GREATEST(COALESCE(total_arr, 0),0) AS total_arr,
         GREATEST(COALESCE(seats, 0),0) AS seats
     FROM {{ ref('stg_salesforce__account') }} a
-    LEFT JOIN account_w_arr ON a.account_id = account_w_arr.account_sfid
-    LEFT JOIN seats_licensed ON a.account_id = seats_licensed.account_sfid
+    LEFT JOIN account_w_arr ON a.account_id = account_w_arr.account_id
+    LEFT JOIN seats_licensed ON a.account_id = seats_licensed.account_id
 )
 
 SELECT * FROM account_arr_and_seats
