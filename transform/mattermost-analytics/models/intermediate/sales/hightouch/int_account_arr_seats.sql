9e343ebaf9a6819cd2d1217d75126fe3ab8bc902
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-13 15:36:39 +0200

    Fix table reference

--- transform/mattermost-analytics/models/intermediate/sales/hightouch/int_account_arr_seats.sql
+++ transform/mattermost-analytics/models/intermediate/sales/hightouch/int_account_arr_seats.sql
@@ -7,9 +7,9 @@ WITH account_w_arr AS (
     SELECT
         a.account_id AS account_id,
         SUM(365*(oli.total_price)/(datediff(day, oli.end_date__c::date , oli.start_date__c::date ))) AS total_arr
-    FROM {{ ref('stg_salesforce_opportunity_line_item') }} oli
-    LEFT JOIN {{ ref('stg_salesforce_opportunity') }} o ON o.opportunity_id = oli.opportunity_id
-    LEFT JOIN {{ ref('stg_salesforce_account') }} a ON a.account_id = o.account_id
+    FROM {{ ref('stg_salesforce__opportunity_line_item') }} oli
+    LEFT JOIN {{ ref('stg_salesforce__opportunity') }} o ON o.opportunity_id = oli.opportunity_id
+    LEFT JOIN {{ ref('stg_salesforce__account') }} a ON a.account_id = o.account_id
     WHERE
         opportunity.iswon
         AND oli.end_date__c::date - oli.start_date__c::date <> 0
@@ -19,10 +19,10 @@ WITH account_w_arr AS (
 ), seats_licensed AS (
     SELECT account.sfid AS account_sfid,
         SUM(CASE WHEN product2.family = 'License' THEN opportunitylineitem.quantity ELSE 0 END) AS seats
-    FROM {{ ref('stg_salesforce_account') }} a
-    LEFT JOIN {{ ref('stg_salesforce_opportunity') }} o ON a.account_id = o.account_id AND o.status_wlo__c = 'Won'
-    LEFT JOIN {{ ref('stg_salesforce_opportunity_line_item') }} oli ON o.opportunity_id = oli.opportunity_id
-    LEFT JOIN {{ ref('stg_salesforce_product2') }} ON oli.product2id = product2.product2_id
+    FROM {{ ref('stg_salesforce__account') }} a
+    LEFT JOIN {{ ref('stg_salesforce__opportunity') }} o ON a.account_id = o.account_id AND o.status_wlo__c = 'Won'
+    LEFT JOIN {{ ref('stg_salesforce__opportunity_line_item') }} oli ON o.opportunity_id = oli.opportunity_id
+    LEFT JOIN {{ ref('stg_salesforce__product2') }} ON oli.product2id = product2.product2_id
     WHERE current_date >= oli.start_date__c AND current_date <= oli.end_date__c
     GROUP BY 1
     HAVING SUM(CASE WHEN product2.family = 'License' THEN opportunitylineitem.quantity ELSE 0 END) > 0
@@ -31,7 +31,7 @@ WITH account_w_arr AS (
         account.account_id AS account_id,
         GREATEST(COALESCE(total_arr, 0),0) AS total_arr,
         GREATEST(COALESCE(seats, 0),0) AS seats
-    FROM {{ ref('stg_salesforce_account') }}
+    FROM {{ ref('stg_salesforce__account') }}
     LEFT JOIN account_w_arr ON account.sfid = account_w_arr.account_sfid
     LEFT JOIN seats_licensed ON account.sfid = seats_licensed.account_sfid
 )
