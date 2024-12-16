04e1744c49ca3e839e569920b80542bc530121a3
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-16 14:41:19 +0200

    Add missing alias

--- transform/mattermost-analytics/models/intermediate/sales/hightouch/int_opportunity_line_item_daily_arr.sql
+++ transform/mattermost-analytics/models/intermediate/sales/hightouch/int_opportunity_line_item_daily_arr.sql
@@ -6,7 +6,7 @@
 
 WITH leap_years AS (
     SELECT dates.date_day as date
-    FROM {{ ref('arr_days') }}
+    FROM {{ ref('arr_days') }} dates
     WHERE dates.date_day LIKE '%-02-29'
     GROUP BY 1
 ), opportunitylineitems_impacted AS (
@@ -25,10 +25,10 @@ SELECT
     SUM(CASE WHEN opportunity.is_closed AND NOT opportunity.iswon THEN 365*(opportunitylineitem.totalprice)/(opportunitylineitem.end_date__c::date - opportunitylineitem.start_date__c::date + 1 - crosses_leap_day)ELSE 0 END )::int AS lost_arr,
     SUM(CASE WHEN NOT opportunity.is_closed THEN 365*(opportunitylineitem.totalprice)/(opportunitylineitem.end_date__c::date - opportunitylineitem.start_date__c::date + 1 - crosses_leap_day) ELSE 0 END )::int AS open_arr
 FROM
-    {{ ref( 'stg_salesforce__opportunity_line_item') }}  AS opportunitylineitem
+    {{ ref( 'stg_salesforce__opportunity_line_item') }} AS opportunitylineitem
     LEFT JOIN opportunitylineitems_impacted ON opportunitylineitems_impacted.opportunitylineitem_sfid = opportunitylineitem.sfid
-    LEFT JOIN {{ ref( 'stg_salesforce__opportunity') }}  AS opportunity ON opportunity.opportunity_id = opportunitylineitem.opportunity_id
-    LEFT JOIN {{ ref('arr_days') }}  AS util_dates ON util_dates.date_day::date >= opportunitylineitem.start_date__c::date AND util_dates.date::date <= opportunitylineitem.end_date__c::date
+    LEFT JOIN {{ ref( 'stg_salesforce__opportunity') }} AS opportunity ON opportunity.opportunity_id = opportunitylineitem.opportunity_id
+    LEFT JOIN {{ ref('arr_days') }} AS util_dates ON util_dates.date_day::date >= opportunitylineitem.start_date__c::date AND util_dates.date::date <= opportunitylineitem.end_date__c::date
 WHERE
     opportunitylineitem.end_date__c::date-opportunitylineitem.start_date__c::date <> 0
     AND opportunitylineitem.end_date__c::date - opportunitylineitem.start_date__c::date + 1 - crosses_leap_day <> 0
