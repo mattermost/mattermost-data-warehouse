233d027ff7c4686b9e188f4e40b0ec26e8c493ea
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-16 12:10:54 +0200

    Fix typo

--- transform/mattermost-analytics/models/intermediate/sales/hightouch/int_account_arr_seats.sql
+++ transform/mattermost-analytics/models/intermediate/sales/hightouch/int_account_arr_seats.sql
@@ -32,7 +32,7 @@ WITH dates as (
 ), account_w_arr AS (
     SELECT
         a.account_id AS account_id,
-        SUM(365*(oli.total_price)/(datediff(day, oli.start_date__c::date, oli.end_date__c::date) +  + 1 - crosses_leap_day))) AS total_arr
+        SUM(365*(oli.total_price)/(datediff(day, oli.start_date__c::date, oli.end_date__c::date) +  + 1 - crosses_leap_day)) AS total_arr
     FROM
         {{ ref('stg_salesforce__opportunity_line_item') }} oli
         LEFT JOIN opportunitylineitems_impacted ON opportunitylineitems_impacted.opportunity_line_item_id = oli.opportunity_line_item_id
