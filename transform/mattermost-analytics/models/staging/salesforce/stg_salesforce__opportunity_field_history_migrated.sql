1526d06cdd4ea64535d0e9ba7383fefabb8ef520
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-16 17:02:40 +0200

    Fix source reference

--- transform/mattermost-analytics/models/staging/salesforce/stg_salesforce__opportunity_field_history_migrated.sql
+++ transform/mattermost-analytics/models/staging/salesforce/stg_salesforce__opportunity_field_history_migrated.sql
@@ -6,12 +6,12 @@ SELECT
              o.opportunity_id as opportunity_id,
              coalesce(NEWVALUE__BO::VARCHAR,NEWVALUE__DE::VARCHAR,NEWVALUE__FL::VARCHAR,NEWVALUE__ST::VARCHAR) as newvalue,
              coalesce(OLDVALUE__BO::VARCHAR,OLDVALUE__DE::VARCHAR,OLDVALUE__FL::VARCHAR,OLDVALUE__ST::VARCHAR) as oldvalue
-FROM {{ source('salesforce','opportunityfieldhistory') }} o
+FROM {{ source('salesforce', 'opportunityfieldhistory') }} o
 INNER JOIN (
     SELECT id,
             MAX(_sdc_sequence) AS seq,
             MAX(_sdc_batched_at) AS batch
-    FROM {{ source('orgm_raw','opportunityfieldhistory') }}
+    FROM {{ source('salesforce', 'opportunityfieldhistory') }}
     GROUP BY id) oo
 ON o.id = oo.id
     AND o._sdc_sequence = oo.seq
